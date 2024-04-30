2b4219febe236359064f66528e476cb4b240c77a
anttiey <hanuuny@gmail.com>
2024-04-30 20:57:33 +0900

    test: Title 스토리북 테스트 추가
    Co-authored-by: pakxe <pigkill40@naver.com>

---
+++ components/src/lib/components/Title/Title.stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import Title from './Title';
+
+const meta = {
+  title: 'components/Title',
+  component: Title,
+  parameters: {
+    controls: { exclude: 'children' },
+  },
+} satisfies Meta<typeof Title>;
+
+export default meta;
+
+type Story = StoryObj<typeof Title>;
+
+export const Default: Story = {
+  args: {
+    children: <p>제목 테스트</p>,
+  },
+};
