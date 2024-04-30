a78586ad67bd452ec02a8406415d916db47b23bb
Largopie <106071687+Largopie@users.noreply.github.com>
2024-04-30 17:38:29 +0900

    feat: isCloseButton 상태에 따라 closeButton 렌더링 유무 구현
    Co-authored-by: soosoo22 <lllllh00@naver.com>

--- components/src/lib/Modal.tsx
+++ components/src/lib/Modal.tsx
@@ -1,5 +1,6 @@
 import styles from './Modal.module.css';
 import { PropsWithChildren } from 'react';
+import closeButton from './assets/closeButton.svg';
 
 type positionProps = 'center' | 'bottom';
 
@@ -7,15 +8,21 @@ interface ModalProps {
   position: positionProps;
   title: string;
   onClose: () => void;
+  isCloseButton: boolean;
 }
 
-const Modal = ({ position, title, children, onClose }: PropsWithChildren<ModalProps>) => {
+const Modal = ({ position, title, children, onClose, isCloseButton }: PropsWithChildren<ModalProps>) => {
   return (
     <div className={`${styles.container} ${styles[position]}`}>
       <div className={styles.backDrop} onClick={onClose}></div>
-      <div className={styles.modal}>
+      <div className={styles.modalContent}>
         <div className={styles.titleContainer}>
           <h2 className={styles.title}>{title}</h2>
+          {isCloseButton && (
+            <button className={styles.closeButton} onClick={onClose}>
+              <img src={closeButton} />
+            </button>
+          )}
         </div>
         <div>{children}</div>
       </div>
