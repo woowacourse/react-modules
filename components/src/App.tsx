92644baafb57bb4c35109e9effc0228d9c6d9065
jaeml06 <jaeml0630@gmail.com>
2024-04-30 22:07:39 +0900

    test(Modal): 모달 모듈 스토리북 테스트
    Co-authored-by: 해시 <dle234@users.noreply.github.com>

--- components/src/App.tsx
+++ components/src/App.tsx
@@ -1,12 +1,34 @@
 import React from "react";
-import "./App.css";
+import Modal from "./lib/Modal";
+import styled from "styled-components";
+import Shin from './lib/asset/shinhan.svg'
 
 function App() {
+  const handleClose=(e : React.MouseEvent) => {
+    console.log(e)
+    console.log('취소');
+  }
+  const handleConfirm = (e : React.MouseEvent) => {
+    console.log(e)
+    console.log('확인');
+  }
   return (
-    <>
-      <h1>Component Modules</h1>
+    <> 
+      <Modal buttonLayout='column' position="bottom" title="제목입니다." isXButton={true}   closeButtonContent = '닫기'
+  confirmButtonContent='확인' handleConfirm={handleConfirm} handleClose={handleClose}>{<Temp><input></input>dasdfasdfasdfs</Temp>}</Modal>
     </>
   );
 }
+
+export const Temp = styled.div`
+height: 50vh;
+background-color: red;
+`
+
+export const Wide = styled.div`
+width:500vw;
+  height:500vh;
+  background-color: red;
+`
 
 export default App;
