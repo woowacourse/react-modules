262672cc61307c66784ffd3f70e85476760b314b
rbgksqkr <rbgks1937@naver.com>
2024-04-30 17:50:03 +0900

    feat: 모달 토글 기능 구현
    Co-authored-by: jinhokim98 <rlawlsgh1227@gmail.com>

--- components/src/App.tsx
+++ components/src/App.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
-import './App.css';
+import React, { useReducer } from 'react';
+
+import Modal from './lib/Modal';
+import SelectBank from './components/SelectBank';
 
 function App() {
+  const [isOpen, toggleIsOpen] = useReducer(prev => !prev, false);
+
   return (
     <>
-      <h1>Component Modules</h1>
+      <button onClick={toggleIsOpen}>모달열기</button>
+      <Modal isOpen={isOpen} onClose={() => toggleIsOpen()} title="카드사 선택" position="center">
+        <SelectBank />
+      </Modal>
     </>
   );
 }
