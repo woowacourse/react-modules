7186c1563ef2ece2b5ab7e672eb8f9173e46845f
Hyelim Choi <HyelimChoi01@gmail.com>
2024-04-30 20:14:02 +0900

    feat: 버튼 컴포넌트
    Co-Authored-By: Hyerin <125418818+ooherin@users.noreply.github.com>

---
+++ components/src/lib/Button.tsx
@@ -0,0 +1,34 @@
+import styled from 'styled-components';
+
+type ButtonColorType = 'black' | 'white';
+
+interface ButtonProps {
+  label: string;
+  onClick: () => void;
+  colorType?: ButtonColorType;
+}
+
+const Button = ({ label, onClick, colorType = 'white' }: ButtonProps) => {
+  return (
+    <ButtonWrapper onClick={onClick} $colorType={colorType}>
+      {label}
+    </ButtonWrapper>
+  );
+};
+
+export default Button;
+
+const ButtonWrapper = styled.button<{ $colorType: ButtonColorType }>`
+  width: 100%;
+  height: 44px;
+  font-size: 16px;
+  font-weight: 700;
+  border-radius: 5px;
+  background-color: ${({ $colorType }) =>
+    $colorType === 'black' ? '#333333' : 'white'};
+  color: ${({ $colorType }) => ($colorType === 'black' ? 'white' : '#333333')};
+  border: 1px solid
+    ${({ $colorType }) => ($colorType === 'white' ? '#bababa' : '#333333')};
+  margin-bottom: 10px;
+  cursor: pointer;
+`;
