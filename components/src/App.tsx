79c52672606efd7c5e78e37af2119d38a1e80795
jaeml06 <jaeml0630@gmail.com>
2024-04-30 22:16:44 +0900

    chore : 빌드 초기 설정
    Co-Authored-By: 해시 <108859281+dle234@users.noreply.github.com>

--- components/src/App.tsx
+++ components/src/App.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Modal from "./lib/Modal";
 import styled from "styled-components";
-import Shin from './lib/asset/shinhan.svg'
 
 function App() {
   const handleClose=(e : React.MouseEvent) => {
