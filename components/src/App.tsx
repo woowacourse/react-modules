defac70c21ee46dcaacf1cf5ef8568c3dd2b8cb2
rbgksqkr <rbgks1937@naver.com>
2024-04-30 21:45:09 +0900

    design: 은행 이미지 반응형 적용
    Co-authored-by: jinhokim98 <rlawlsgh1227@gmail.com>

--- components/src/App.tsx
+++ components/src/App.tsx
@@ -15,6 +15,9 @@ function App() {
         onClose={() => toggleIsOpen()}
         title="카드사 선택"
         position="center"
+        closeButton={<button style={{ width: '100%' }}>닫기 버튼</button>}
+        confirmButton={<button style={{ width: '100%' }}>확인 버튼</button>}
+        buttonPosition="column"
       />
     </>
   );
