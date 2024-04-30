078af225a77f0461a755226bfbfefdf6c168d818
hain-tain <hain7ce@gmail.com>
2024-04-30 20:32:38 +0900

    feat: Button component 추가
    Co-authored-by: Todari <rhymint@gmail.com>

---
+++ components/src/lib/Button/Button.styled.ts
@@ -0,0 +1,106 @@
+import { ButtonSize, ButtonStyle, ButtonWidth } from '../../types/type';
+
+import styled from 'styled-components';
+
+const BUTTON_SIZE_TABLE = {
+  small: { width: '100px', height: '32px', fontSize: '12px' },
+  medium: { width: '160px', height: '44px', fontSize: '15px' },
+  large: { width: '240px', height: '56px', fontSize: '18px' },
+};
+
+const textColor = (primaryColor: string) => {
+  const bigint = parseInt(primaryColor.slice(1, 7), 16);
+  const r = (bigint >> 16) & 255;
+  const g = (bigint >> 8) & 255;
+  const b = bigint & 255;
+
+  if (r + g + b <= 384) return '#FFFFFF';
+  return '#000000';
+};
+
+const hoverColor = (backgroundColor: string) => {
+  const bigint = parseInt(backgroundColor.slice(1, 7), 16);
+  const r = (bigint >> 16) & 255;
+  const g = (bigint >> 8) & 255;
+  const b = bigint & 255;
+
+  const total = r + g + b;
+
+  if (total <= 384) {
+    const R = Math.floor(r + 32).toString(16);
+    const G = Math.floor(g + 32).toString(16);
+    const B = Math.floor(b + 32).toString(16);
+    return '#' + R + G + B;
+  }
+  const R = Math.floor(r - 32).toString(16);
+  const G = Math.floor(g - 32).toString(16);
+  const B = Math.floor(b - 32).toString(16);
+  return '#' + R + G + B;
+};
+
+const BUTTON_STYLE_TABLE = (primaryColor: string = '#333333') => {
+  return {
+    primary: {
+      backgroundColor: primaryColor,
+      color: textColor(primaryColor),
+      border: 'none',
+    },
+    border: {
+      backgroundColor: '#ffffff',
+      color: primaryColor,
+      border: `2px solid ${primaryColor}`,
+    },
+    text: {
+      backgroundColor: 'transparent',
+      color: primaryColor,
+      border: 'none',
+    },
+  };
+};
+
+export const Button = styled.button<{
+  size: ButtonSize;
+  width: ButtonWidth;
+  buttonStyle: ButtonStyle;
+  primaryColor: string;
+}>`
+  width: ${(props) => {
+    switch (props.width) {
+      case 'full':
+        return '100%';
+      case 'fit':
+        return 'fit-content';
+      default:
+        return BUTTON_SIZE_TABLE[props.size].width;
+    }
+  }};
+  padding: 0 16px;
+  height: ${(props) => BUTTON_SIZE_TABLE[props.size].height};
+  background-color: ${(props) =>
+    BUTTON_STYLE_TABLE(props.primaryColor)[props.buttonStyle].backgroundColor};
+  border: ${(props) =>
+    BUTTON_STYLE_TABLE(props.primaryColor)[props.buttonStyle].border};
+  border-radius: 8px;
+
+  &:hover {
+    background-color: ${(props) => {
+      switch (props.buttonStyle) {
+        case 'primary':
+          return hoverColor(props.primaryColor);
+        default:
+          return hoverColor('#FFFFFF');
+      }
+    }};
+  }
+`;
+
+export const ButtonText = styled.p<{
+  size: ButtonSize;
+  buttonStyle: ButtonStyle;
+  primaryColor?: string;
+}>`
+  font-size: ${(props) => BUTTON_SIZE_TABLE[props.size].fontSize};
+  color: ${(props) =>
+    BUTTON_STYLE_TABLE(props.primaryColor)[props.buttonStyle].color};
+  font-weight: 700;
+`;
---
+++ components/src/lib/Button/Button.tsx
@@ -0,0 +1,32 @@
+import * as Styled from './Button.styled';
+
+import { ButtonProps } from '../../types/type';
+
+const Button = ({
+  text,
+  onClick,
+  size = 'medium',
+  width = 'fixed',
+  buttonStyle = 'primary',
+  primaryColor = '#333333',
+}: ButtonProps) => {
+  return (
+    <Styled.Button
+      onClick={onClick}
+      size={size}
+      width={width}
+      buttonStyle={buttonStyle}
+      primaryColor={primaryColor}
+    >
+      <Styled.ButtonText
+        size={size}
+        buttonStyle={buttonStyle}
+        primaryColor={primaryColor}
+      >
+        {text}
+      </Styled.ButtonText>
+    </Styled.Button>
+  );
+};
+
+export default Button;
