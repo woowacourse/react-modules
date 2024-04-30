77011cb14fa247b1147584212cb844dd61d6577b
Seongjin Hong <hongseongjin.to@gmail.com>
2024-04-30 21:44:57 +0900

    test: 로컬 테스트용 App 설정 파일 추가

--- components/src/App.tsx
+++ components/src/App.tsx
@@ -1,12 +1,74 @@
-import React from "react";
-import "./App.css";
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import './App.css';
+
+import Modal from './lib/Modal/Modal';
+
+const buttons: ModalButtonProps[] = [
+  {
+    text: '동의하고 저장하기',
+    style: 'primary',
+    onClick: () => alert('동의하기 눌렀어요!'),
+  },
+  {
+    text: '닫기',
+    style: 'secondary',
+    onClick: () => alert('닫기 눌렀어요!'),
+  },
+];
+interface ModalButtonProps {
+  text: string;
+  style: 'primary' | 'secondary';
+  onClick: () => void;
+}
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <>
       <h1>Component Modules</h1>
+      <button onClick={() => setIsOpen(true)}>Click me!</button>
+
+      <Modal
+        isOpen={isOpen}
+        title="모달 테스트"
+        position="bottom"
+        hasCloseButton={true}
+        footerButtons={buttons}
+        onClose={() => setIsOpen(false)}
+      >
+        <Form>
+          <InputCheckBox>
+            <input
+              name="checkbox-1"
+              type="checkbox"
+              value=""
+            />
+            <label htmlFor="checkbox-1">[필수] 개인정보 수집이용 동의</label>
+          </InputCheckBox>
+          <InputCheckBox>
+            <input
+              name="checkbox-2"
+              type="checkbox"
+              value=""
+            />
+            <label htmlFor="checkbox-2">[필수] 고객정보 제 3자 제공 동의</label>
+          </InputCheckBox>
+        </Form>
+      </Modal>
     </>
   );
 }
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+`;
+
+const InputCheckBox = styled.div`
+  margin-bottom: 8px;
+`;
 
 export default App;
