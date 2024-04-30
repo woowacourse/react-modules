bf6cb60953ff0c452fd6ac2ab4131b156f519a2e
Hyelim Choi <HyelimChoi01@gmail.com>
2024-04-30 20:14:10 +0900

    feat: 모달 컴포넌트 사용
    Co-Authored-By: Hyerin <125418818+ooherin@users.noreply.github.com>

--- components/src/App.tsx
+++ components/src/App.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import './App.css';
-import Modal from './lib/Modal';
+import Modal from './lib/Modal/Modal';
 import { useState } from 'react';
+import GlobalStyles from './style/global';
+import DeleteIcon from './assets/deleteIcon.svg?react';
 
 function App() {
   const [isModalOpen, setIsModalOpen] = useState(true);
@@ -19,21 +21,17 @@ function App() {
 
   return (
     <>
-      <h1>Component Modules</h1>
-      <h1>Component Modules</h1>
-      <h1>Component Modules</h1>
-      <h1>Component Modules</h1>
-      <h1>Component Modules</h1>
+      <GlobalStyles />
       <h1>Component Modules</h1>
       <button onClick={handleOpen}>버튼</button>
-      <Modal
-        position="center"
-        title="알림"
-        isOpen={isModalOpen}
-        onConfirm={handleConfirm}
-        onClose={handleClose}
-      >
-        <div>모달 내용입니다.</div>
+      <Modal isOpen={isModalOpen} position="center" onClose={handleClose}>
+        <Modal.Title> 약관에 동의해 주세요</Modal.Title>
+        <Modal.CloseIcon onClick={handleClose}>
+          <DeleteIcon />
+        </Modal.CloseIcon>
+        <Modal.Content>[필수] 개인정보 수집약관 동의</Modal.Content>
+        <Modal.ConfirmButton label="동의" onConfirm={handleConfirm} />
+        <Modal.CloseButton label="닫기" onClose={handleClose} />
       </Modal>
     </>
   );
