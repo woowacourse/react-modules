5a46e6be9b77b34d3f41e7477d1db79d0e91092c
rbgksqkr <rbgks1937@naver.com>
2024-04-30 21:03:50 +0900

    feat: Modal 조건부 렌더링 및 유연한 style 인터페이스 제공
    Co-authored-by: jinhokim98 <rlawlsgh1227@gmail.com>

--- components/src/lib/Modal.tsx
+++ components/src/lib/Modal.tsx
@@ -1,21 +1,44 @@
 import { useEffect, useRef } from 'react';
 import { createPortal } from 'react-dom';
 import styles from './Modal.module.css';
-import CloseButton from './assets/closeButton.svg';
+import CloseIcon from './assets/closeButton.svg';
+
+type ModalPosition = 'center' | 'bottom';
+type ButtonPosition = 'row' | 'row-reverse' | 'column' | 'column-reverse';
+
+interface ModalStyle {
+  dimmed?: React.CSSProperties;
+  modal?: React.CSSProperties;
+  modalHeader?: React.CSSProperties;
+  modalTitle?: React.CSSProperties;
+}
 
 interface ModalProps {
   isOpen: boolean;
   onClose: () => void;
   title: string;
-  position: 'center' | 'bottom';
+  position: ModalPosition;
   content?: React.ReactNode;
-  style?: React.CSSProperties;
+  style?: ModalStyle;
   closeButton?: React.ReactNode;
   confirmButton?: React.ReactNode;
-  buttonPosition?: 'row' | 'row-reverse' | 'column' | 'column-reverse';
-  onConfirm?: () => void;
+  buttonPosition?: ButtonPosition;
   closeOnOutsideClick?: boolean;
+  customCloseIcon?: string;
+  hideCloseIcon?: boolean;
 }
+
+const MODAL_TYPE: Record<ModalPosition, string> = {
+  center: styles.modal,
+  bottom: styles.modalBottom,
+};
+
+const BUTTON_POSITION_TYPE: Record<ButtonPosition, string> = {
+  row: styles.buttonRow,
+  'row-reverse': styles.buttonRowReverse,
+  column: styles.buttonColumn,
+  'column-reverse': styles.buttonColumnReverse,
+};
 
 const Modal = ({
   isOpen,
@@ -27,10 +50,14 @@ const Modal = ({
   closeButton,
   confirmButton,
   buttonPosition,
-  onConfirm,
   closeOnOutsideClick = true,
 }: ModalProps) => {
   const modalRef = useRef<HTMLDivElement>(null);
+
+  const isButton = closeButton || confirmButton;
+  const buttonLayoutStyle = buttonPosition
+    ? BUTTON_POSITION_TYPE[buttonPosition]
+    : styles.buttonRow;
 
   useEffect(() => {
     const clickOutSide = (event: MouseEvent) => {
@@ -52,18 +79,21 @@ const Modal = ({
     <>
       {isOpen &&
         createPortal(
-          <div className={styles.dimmed}>
-            <section className={styles.modal} ref={modalRef}>
-              <header className={styles.modalHeader}>
-                <div className={styles.title}>{title}</div>
-                <img
-                  src={CloseButton}
-                  alt="close"
-                  className={styles.closeButton}
-                  onClick={onClose}
-                />
+          <div className={styles.dimmed} style={style?.dimmed}>
+            <section className={MODAL_TYPE[position]} ref={modalRef} style={style?.modal}>
+              <header className={styles.modalHeader} style={style?.modalHeader}>
+                <span className={styles.title} style={style?.modalTitle}>
+                  {title}
+                </span>
+                <img src={CloseIcon} alt="close" className={styles.closeButton} onClick={onClose} />
               </header>
-              <section>{content}</section>
+              {content && <section>{content}</section>}
+              {isButton && (
+                <footer className={buttonLayoutStyle}>
+                  {closeButton}
+                  {confirmButton}
+                </footer>
+              )}
             </section>
           </div>,
           document.body,
