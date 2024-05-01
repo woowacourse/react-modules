4f4bb74c09692c6794931cf368936d5732456088
Seongjin Hong <hongseongjin.to@gmail.com>
2024-05-01 18:01:01 +0900

    feat: useCardHolder 훅 구현 및 테스트 작성
    Co-authored-by: cys4585 <cys4585@users.noreply.github.com>

---
+++ hooks/src/lib/useCardHolder/useCardHolder.ts
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import ValidationResult from "../types/ValidationResult";
+import Validation from "../../utils/Validation";
+
+interface CardHolderValidationResult {
+  cardHolder: string;
+  validationResult: ValidationResult;
+  handleUpdateCardHolder: (value: string) => void;
+}
+
+export default function useCardHolder(
+  initialValue: string,
+  alwaysUpdateCardHolder: boolean = false
+): CardHolderValidationResult {
+  const [cardHolder, setCardHolder] = useState(initialValue);
+  const [validationResult, setValidationResult] = useState<ValidationResult>({
+    isValid: true,
+  });
+
+  const handleUpdateCardHolder = (value: string) => {
+    if (!validateCardHolder(value)) {
+      setValidationResult({
+        isValid: false,
+        errorMessage: "영문자만 입력할 수 있습니다.",
+      });
+      if (alwaysUpdateCardHolder) setCardHolder(value);
+      return;
+    }
+
+    setValidationResult({ isValid: true });
+    setCardHolder(value);
+  };
+
+  return {
+    cardHolder,
+    validationResult,
+    handleUpdateCardHolder,
+  };
+}
+const validateCardHolder = (value: string) => {
+  return Validation.isEnglishWithSpace(value);
+};
