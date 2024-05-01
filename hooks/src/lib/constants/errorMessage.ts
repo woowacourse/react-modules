565713707bbab3fc602898c4a5f7767115b22060
jinsil <jinsil378900@gmail.com>
2024-05-01 19:56:52 +0900

    refactor: 유효성 검사, 에러메세지 분리

---
+++ hooks/src/lib/constants/errorMessage.ts
@@ -0,0 +1,10 @@
+const ERROR_MESSAGE = Object.freeze({
+  INVALID_CARD_NUMBER_LENGTH: "숫자 4자리를 입력해주세요.",
+  ONLY_NUMBER: "숫자만 입력해주세요.",
+  EMPTY_VALUE: "값을 입력해주세요.",
+  INVALID_MONTH_LENGTH: "월은 숫자 2자리입니다.",
+  INVALID_YEAR_LENGTH: "연도는 숫자 2자리입니다.",
+  OUT_OF_RANGE_MONTH: "월은 1부터 12사이의 숫자입니다.",
+});
+
+export default ERROR_MESSAGE;
---
+++ hooks/src/lib/utils/validate.ts
@@ -0,0 +1,23 @@
+const validator = {
+  isValidEmptyValue: (value: string) => {
+    return value !== "";
+  },
+
+  isValidDigit: (value: string) => {
+    return /^\d+$/.test(value);
+  },
+
+  isEnglish: (value: string) => {
+    return /^[a-zA-Z\s]+$/.test(value);
+  },
+
+  isValidLength: ({ value, matchedLength }: { value: string; matchedLength: number }) => {
+    return value.length === matchedLength;
+  },
+
+  isNumberInRange: ({ min, max, compareNumber }: { min: number; max: number; compareNumber: number }) => {
+    return compareNumber >= min && compareNumber <= max;
+  },
+};
+
+export default validator;
