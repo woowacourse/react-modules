15dfb34830fddbaa623f6bdc6ea73f822ee95994
jaeml06 <jaeml0630@gmail.com>
2024-05-01 20:55:37 +0900

    feat: cardNumberValidation 커스텀 훅 기능 구현
    Co-Authored-By: 해시 <108859281+dle234@users.noreply.github.com>

---
+++ hooks/src/lib/useCardNumberValidation.ts
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+import { Validation } from './validate';
+
+const useCardNumberValidation = () => {
+  const [cardNumberValidation, setCardNumberValidation] = useState({
+    errorMessage: {
+      cardNumber1: '',
+      cardNumber2: '',
+      cardNumber3: '',
+      cardNumber4: '',
+    },
+    isError: {
+      cardNumber1: false,
+      cardNumber2: false,
+      cardNumber3: false,
+      cardNumber4: false,
+    }
+  });
+
+  const cardNumberValidateHandler = (value: string, name: string) => {
+    try {
+      Validation['cardNumber'](value);
+      setCardNumberValidation((prev) => ({
+        ...prev,
+        errorMessage: { ...prev.errorMessage, [name]: '' },
+        isError: { ...prev.isError, [name]: false },
+      }));
+    } catch (error) {
+      if (error instanceof Error) {
+        setCardNumberValidation((prev) => ({
+          errorMessage: { ...prev.errorMessage, [name]: error.message },
+          isError: { ...prev.isError, [name]: true },
+        }));
+      }
+    }
+  };
+
+  return {cardNumberValidation, cardNumberValidateHandler}
+};
+export default useCardNumberValidation;
