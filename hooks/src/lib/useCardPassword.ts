b43a1f271e875396fa1c78fd8e9c92c9ac5020e3
hain-tain <hain7ce@gmail.com>
2024-05-01 21:23:13 +0900

    feat: useCardPassword hook 추가
    Co-authored-by: Todari <rhymint@gmail.com>

---
+++ hooks/src/lib/useCardPassword.ts
@@ -0,0 +1,50 @@
+import useCardValidate from './useCardValidate';
+
+const useCardPassword = (initValue: string, maxLength: number = 2) => {
+  const validateOnChange = (newValue: string) => {
+    if (newValue.length > maxLength) {
+      return {
+        isValid: false,
+        errorMessage: `비밀번호는 ${maxLength}글자 까지만 입력이 가능해요.`,
+      };
+    }
+    if (!/^\d*$/.test(newValue)) {
+      return {
+        isValid: false,
+        errorMessage: '비밀번호는 숫자만 입력이 가능해요.',
+      };
+    }
+    return { isValid: true, errorMessage: '' };
+  };
+
+  const validateOnBlur = () => {
+    if (value.length !== maxLength) {
+      return {
+        isValid: true,
+        errorMessage: `비밀번호는 ${maxLength}글자로 입력해 주세요.`,
+      };
+    }
+    return { isValid: true, errorMessage: '' };
+  };
+
+  const {
+    value,
+    errorMessage,
+    onChangeHandler,
+    onFocusHandler,
+    onBlurHandler,
+  } = useCardValidate({
+    initValue,
+    validateOnChange,
+    validateOnBlur,
+  });
+
+  return {
+    value,
+    errorMessage,
+    onChangeHandler,
+    onBlurHandler,
+    onFocusHandler,
+  };
+};
+export default useCardPassword;
