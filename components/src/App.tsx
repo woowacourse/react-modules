a3e699358045253a57b9a597c6ad23a5b67bac99
anttiey <hanuuny@gmail.com>
2024-05-01 22:10:02 +0900

    fix: GlobalStyles 제거
    Co-authored-by: pakxe <pigkill40@naver.com>

--- components/src/App.tsx
+++ components/src/App.tsx
@@ -1,23 +1,25 @@
 import React, { useState } from 'react';
-import { Modal } from '../src/lib/Modal';
+import { Modal } from './lib';
 import './App.css';
-import GlobalStyles from './Global.style';
 
 function App() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
     <div>
-      <GlobalStyles />
       <button onClick={() => setIsOpen(true)}>모달열기</button>
-      <Modal isOpen={isOpen} position="center" size="lg" close={() => setIsOpen(false)}>
+      <Modal isOpen={isOpen} position="center" backdropType="blur" size="lg" close={() => setIsOpen(false)}>
         <Modal.Header>
           <Modal.Title>제목</Modal.Title>
           <Modal.CloseButton close={() => setIsOpen(false)} />
         </Modal.Header>
-        <Modal.Body>여긴 본문</Modal.Body>
-        <Modal.Button text="확인" mode="primary"></Modal.Button>
-        <Modal.Button text="닫기" mode="secondary" onClick={() => setIsOpen(false)}></Modal.Button>
+        <Modal.Body>
+          <p>모달입니다.</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Modal.Button text="확인" mode="primary" size="lg"></Modal.Button>
+          <Modal.Button text="닫기" mode="secondary" size="lg" onClick={() => setIsOpen(false)}></Modal.Button>
+        </Modal.Footer>
       </Modal>
     </div>
   );
