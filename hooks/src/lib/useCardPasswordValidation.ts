961b285b8f9b9f8f2c34f7cbedff518ef705d6ae
jaeml06 <jaeml0630@gmail.com>
2024-05-01 20:58:58 +0900

    feat: 카드 비밀번호 입력 validation 로직 구현
    Co-Authored-By: 해시 <108859281+dle234@users.noreply.github.com>

---
+++ hooks/src/lib/useCardPasswordValidation.ts
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+import { Validation } from './validate';
+
+const usePasswordValidation = () => {
+  const [passwordValidation, setPasswordValidation] = useState({
+    errorMessage: {
+      password : ''
+    },
+    isError: {
+      password: false
+    }
+  });
+
+  const passwordValidateHandler = (value: string, name: string) => {
+    try {
+      Validation['password'](value);
+      setPasswordValidation((prev) => ({
+        ...prev,
+        errorMessage: { ...prev.errorMessage, [name]: '' },
+        isError: { ...prev.isError, [name]: false },
+      }));
+    } catch (error) {
+      if (error instanceof Error) {
+        setPasswordValidation((prev) => ({
+          errorMessage: { ...prev.errorMessage, [name]: error.message },
+          isError: { ...prev.isError, [name]: true },
+        }));
+      }
+    }
+  };
+
+  return {passwordValidation, passwordValidateHandler}
+};
+export default usePasswordValidation;
