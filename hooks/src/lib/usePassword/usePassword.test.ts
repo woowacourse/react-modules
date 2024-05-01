8eb696f26710e025f9a9853959b38f5f9a649145
Lee sang Yeop <mma7710@naver.com>
2024-05-01 14:23:51 +0900

    feat(usePassword): blur, enter 이벤트 발생 시 유효성 검사 추가
    Co-authored-by: Soyeon Choe <soi-ha@users.noreply.github.com>

--- hooks/src/lib/usePassword/usePassword.test.ts
+++ hooks/src/lib/usePassword/usePassword.test.ts
@@ -1,5 +1,6 @@
 import { renderHook, act } from '@testing-library/react';
 import usePassword from './usePassword';
+import { ERROR_MESSAGE } from '../../constants';
 
 describe('신용카드 비밀번호 입력 테스트', () => {
 	const initialValue = {
@@ -16,6 +17,10 @@ describe('신용카드 비밀번호 입력 테스트', () => {
 		const userInput = '99';
 		const { result } = renderHook(() => usePassword(initialValue));
 		const target = { value: userInput, name: 'password' };
+		const expectedValidationResult = {
+			isValid: true,
+			errorMessage: '',
+		};
 
 		act(() => {
 			result.current.handlePasswordChange({
@@ -25,12 +30,17 @@ describe('신용카드 비밀번호 입력 테스트', () => {
 		});
 
 		expect(result.current.inputValue.password).toEqual(userInput);
+		expect(result.current.validationResult).toEqual(expectedValidationResult);
 	});
 
 	it('입력값이 숫자가 아닐 때 업데이트가 안된다', () => {
 		const userInput = 'notUpdate';
 		const { result } = renderHook(() => usePassword(initialValue));
 		const target = { value: userInput, name: 'password' };
+		const expectedValidationResult = {
+			isValid: false,
+			errorMessage: ERROR_MESSAGE.onlyNumber,
+		};
 
 		act(() => {
 			result.current.handlePasswordChange({
@@ -40,12 +50,17 @@ describe('신용카드 비밀번호 입력 테스트', () => {
 		});
 
 		expect(result.current.inputValue.password).not.toEqual(userInput);
+		expect(result.current.validationResult).toEqual(expectedValidationResult);
 	});
 
 	it('입력값이 2자리 초과시 업데이트가 안된다', () => {
 		const userInput = '123';
 		const { result } = renderHook(() => usePassword(initialValue));
 		const target = { value: userInput, name: 'password' };
+		const expectedValidationResult = {
+			isValid: false,
+			errorMessage: ERROR_MESSAGE.onlyNumber,
+		};
 
 		act(() => {
 			result.current.handlePasswordChange({
@@ -55,5 +70,46 @@ describe('신용카드 비밀번호 입력 테스트', () => {
 		});
 
 		expect(result.current.inputValue.password).not.toEqual(userInput);
+		expect(result.current.validationResult).toEqual(expectedValidationResult);
+	});
+
+	it('입력 값이 모두 채워지고 블러/엔터 이벤트 발생 시 에러가 발생하지 않는다', () => {
+		const userInput = '12';
+		const { result } = renderHook(() => usePassword(initialValue));
+		const target = { value: userInput, name: 'password' };
+		const expectedValidationResult = {
+			isValid: true,
+			errorMessage: '',
+		};
+
+		act(() => {
+			result.current.handlePasswordBlur({
+				target,
+				currentTarget: target,
+			} as React.FocusEvent<HTMLInputElement>);
+		});
+
+		expect(result.current.inputValue.password).toEqual(userInput);
+		expect(result.current.validationResult).toEqual(expectedValidationResult);
+	});
+
+	it('입력 값이 모두 채워지지 않고 블러/엔터 이벤트 발생 시 에러가 발생한다', () => {
+		const userInput = '1';
+		const { result } = renderHook(() => usePassword(initialValue));
+		const target = { value: userInput, name: 'password' };
+		const expectedValidationResult = {
+			isValid: false,
+			errorMessage: ERROR_MESSAGE.passwordOutOfRange,
+		};
+
+		act(() => {
+			result.current.handlePasswordBlur({
+				target,
+				currentTarget: target,
+			} as React.FocusEvent<HTMLInputElement>);
+		});
+
+		expect(result.current.inputValue.password).not.toEqual(userInput);
+		expect(result.current.validationResult).toEqual(expectedValidationResult);
 	});
 });
--- hooks/src/lib/usePassword/usePassword.ts
+++ hooks/src/lib/usePassword/usePassword.ts
@@ -1,23 +1,82 @@
-import { ChangeEvent } from 'react';
+import { ChangeEvent, useState, KeyboardEvent, FocusEvent } from 'react';
 import { useInput } from '../common';
 import Validator from '../../utils/validator';
-import { OPTION } from '../../constants';
+import { ERROR_MESSAGE, OPTION } from '../../constants';
+import { EventProcessor, NameValuePair } from '../common/useInput';
 
 const usePassword = <T extends object>(initialValue: T) => {
-	const { inputValue, handleInputChange } = useInput(initialValue);
+	const { inputValue, handleInputChange, handleEventProcessor } = useInput(initialValue);
+	const [validationResult, setValidationResult] = useState<ValidationResult>({
+		isValid: true,
+		errorMessage: '',
+	});
 
 	const handlePasswordChange = (e: ChangeEvent<HTMLInputElement>) => {
 		if (e.target !== e.currentTarget) return;
 
 		const { value } = e.target;
-		if (!Validator.checkNumber(value, OPTION.passwordMaxLength)) return;
+		if (!Validator.checkNumberAndOver(value, OPTION.passwordMaxLength)) {
+			return setValidationResult({
+				isValid: false,
+				errorMessage: ERROR_MESSAGE.onlyNumber,
+			});
+		}
 
 		handleInputChange(e);
+		setValidationResult({
+			isValid: true,
+			errorMessage: '',
+		});
 	};
 
-	// TODO: Blur&Enter시 2자리가 채워졌는 유효성검사
+	const handlePasswordBlur = (e: FocusEvent<HTMLInputElement>) => {
+		if (e.target !== e.currentTarget) return;
 
-	return { inputValue, handlePasswordChange } as const;
+		const { value } = e.target;
+		if (!Validator.checkFillNumber(value, OPTION.passwordMaxLength))
+			return setValidationResult({
+				isValid: false,
+				errorMessage: ERROR_MESSAGE.passwordOutOfRange,
+			});
+
+		handleEventProcessor(processor, e);
+		setValidationResult({
+			isValid: true,
+			errorMessage: '',
+		});
+	};
+
+	const handleCardNumberEnter = (e: KeyboardEvent<HTMLInputElement>) => {
+		if (e.target !== e.currentTarget) return;
+		if (e.key !== 'Enter') return;
+
+		const { value } = e.target as HTMLInputElement;
+		if (!value || !Validator.checkFillNumber(value, OPTION.passwordMaxLength))
+			return setValidationResult({
+				isValid: false,
+				errorMessage: ERROR_MESSAGE.passwordOutOfRange,
+			});
+
+		handleEventProcessor(processor, e);
+		setValidationResult({
+			isValid: true,
+			errorMessage: '',
+		});
+	};
+
+	const processor = (e: EventProcessor) => {
+		const { value, name } = e.target as NameValuePair;
+
+		return { name, value } as const;
+	};
+
+	return {
+		inputValue,
+		validationResult,
+		handlePasswordChange,
+		handlePasswordBlur,
+		handleCardNumberEnter,
+	} as const;
 };
 
 export default usePassword;
