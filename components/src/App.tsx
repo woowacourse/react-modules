e491455775cf2d1cd250d87baf435d51315393f9
badahertz52 <badahertz52@gmail.com>
2024-05-01 11:10:23 +0900

    refactor: App.tsx 수정
    Co-Authored-By: KIM DAEUN <midekuna@gmail.com>

--- components/src/App.tsx
+++ components/src/App.tsx
@@ -1,10 +1,35 @@
-import React from "react";
-import "./App.css";
+import { useState } from 'react';
+import './App.css';
+import BasicCenterModal from './lib/Modal/BasicCenterModal/index';
+import BasicBottomModal from './lib/Modal/BasicBottomModal/index';
+import { Modal } from './lib';
 
 function App() {
+  const [openCenterModal, setOpenCenterModal] = useState(false);
+  const [openBottomModal, setOpenBottomModal] = useState(false);
   return (
     <>
-      <h1>Component Modules</h1>
+      <button onClick={() => setOpenCenterModal(true)}>basic center open</button>
+      <button onClick={() => setOpenBottomModal(true)}>basic bottom open</button>
+      <BasicCenterModal
+        closeButtonType="icon"
+        isOpen={openCenterModal}
+        title="basic center"
+        closeModal={() => setOpenCenterModal(false)}
+      >
+        <div>basic center modal</div>
+      </BasicCenterModal>
+      <BasicBottomModal
+        closeButtonType="box"
+        isOpen={openBottomModal}
+        title="basic bottom"
+        closeModal={() => setOpenBottomModal(false)}
+      >
+        <Modal.ActionAndCloseButton extraAction={() => alert('extraAction!!')}>
+          <div>Action And Close Button</div>
+        </Modal.ActionAndCloseButton>
+        <div> basic bottom modal</div>
+      </BasicBottomModal>
     </>
   );
 }
