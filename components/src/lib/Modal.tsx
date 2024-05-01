04598e699a1d83ad50422d04530dbc791cc4dea4
bmj0053 <bmj05378@gmail.com>
2024-05-01 11:39:05 +0900

    feat: Modal Backdrop 클릭 시 닫는 기능 추가 및 Container 스타일 props 수정
    Co-authored-by: Yoonkyoung Kim <dev.yoonkyoung@gmail.com>

--- components/src/lib/Modal.tsx
+++ components/src/lib/Modal.tsx
@@ -1,25 +1,63 @@
-import React from 'react';
-import styled from 'styled-components';
+import React, { MouseEvent } from 'react';
 import { CLOSE_BUTTON } from '../assets/images/index';
+import styled from 'styled-components';
 
 interface ContainerProps {
-  position: string;
-  transformValue: string;
+  $position: string;
+  $transform: string;
 }
 
+const BackDrop = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  backdrop-filter: blur(5px);
+  background-color: rgba(0, 0, 0, 0.35);
+`;
+
 const Container = styled.div<ContainerProps>`
-  background-color: #ffffff;
   position: fixed;
-  top: ${(props) => props.position};
+  top: ${(props) => props.$position};
   left: 50%;
-  transform: ${(props) => props.transformValue};
+  transform: ${(props) => props.$transform};
+  display: flex;
+  flex-direction: column;
+  gap: 2.4rem;
+  padding: 2.4rem 3.2rem;
+  box-sizing: border-box;
+  border-radius: 0.8rem;
+  background-color: #ffffff;
 `;
 
 const Header = styled.div`
   display: flex;
   justify-content: space-between;
-  height: 100px;
+  align-items: center;
   color: #000000;
+`;
+
+const Title = styled.div`
+  font-weight: bold;
+  font-size: 1.8rem;
+`;
+
+const CloseIcon = styled.img`
+  width: 2.4rem;
+  height: 2.4rem;
+`;
+
+const CloseButton = styled.button`
+  height: 2.75rem;
+  background-color: transparent;
+  color: #8b95a1;
+  font-weight: bold;
+  font-size: 1.5rem;
+  border: 1px solid #cccccc;
+  border-radius: 0.5rem;
+  padding: 0.8rem auto;
+  box-sizing: border-box;
 `;
 
 /**
@@ -46,7 +84,15 @@ function Modal({
     toggleModal();
   };
 
-  const handlePosition = () => {
+  const handleBackdropClick = (
+    event: MouseEvent<HTMLDivElement, globalThis.MouseEvent>,
+  ) => {
+    if (event.target === event.currentTarget) {
+      handleCloseButton();
+    }
+  };
+
+  const calculateModalPosition = () => {
     if (position === 'center') {
       return '50%';
     } else if (position === 'bottom') {
@@ -56,7 +102,7 @@ function Modal({
     }
   };
 
-  const handleTransform = () => {
+  const calculateModalTransform = () => {
     if (position === 'center') {
       return 'translate(-50%, -50%)';
     } else if (position === 'bottom') {
@@ -67,18 +113,23 @@ function Modal({
   };
 
   return (
-    <Container position={handlePosition()} transformValue={handleTransform()}>
-      <Header>
-        <div>{title}</div>
-        {closeOption === 'icon' && (
-          <img onClick={handleCloseButton} src={CLOSE_BUTTON} />
+    <BackDrop onClick={(e) => handleBackdropClick(e)}>
+      <Container
+        $position={calculateModalPosition()}
+        $transform={calculateModalTransform()}
+      >
+        <Header>
+          <Title>{title}</Title>
+          {closeOption === 'icon' && (
+            <CloseIcon onClick={handleCloseButton} src={CLOSE_BUTTON} />
+          )}
+        </Header>
+        <div>{children}</div>
+        {closeOption === 'button' && (
+          <CloseButton onClick={handleCloseButton}>닫기</CloseButton>
         )}
-      </Header>
-      <div>{children}</div>
-      {closeOption === 'button' && (
-        <button onClick={handleCloseButton}>닫기</button>
-      )}
-    </Container>
+      </Container>
+    </BackDrop>
   );
 }
 
