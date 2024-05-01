e43b308550305109e0ebdcc9a7c7113d095a5eac
jaeml06 <jaeml0630@gmail.com>
2024-05-01 20:56:36 +0900

    feat: useCardHolderValidation 커스텀 훅 작성
    Co-Authored-By: 해시 <108859281+dle234@users.noreply.github.com>

---
+++ hooks/src/lib/useCardHolderValidation.ts
@@ -0,0 +1,36 @@
+import {  useState } from 'react';
+import { Validation } from './validate';
+
+
+
+const useCardHolderValidation = () => {
+  const [cardHolderValidation, setCardHolderValidation] = useState({
+    errorMessage: {
+      cardHolder : '',
+    },
+    isError: {cardHolder:false}
+  });
+
+  const cardHolderValidateHandler = (value: string, name: string) => {
+    try {
+      Validation['userName'](value);
+      setCardHolderValidation((prev) => ({
+        ...prev,
+        errorMessage: { ...prev.errorMessage, [name]: '' },
+        isError: { ...prev.isError, [name]: false },
+      }));
+    } catch (error) {
+      if (error instanceof Error) {
+        setCardHolderValidation((prev) => ({
+          errorMessage: { ...prev.errorMessage, [name]: error.message },
+          isError: { ...prev.isError, [name]: true },
+        }));
+      }
+    }
+  };
+
+
+
+  return {cardHolderValidation, cardHolderValidateHandler}
+};
+export default useCardHolderValidation;
