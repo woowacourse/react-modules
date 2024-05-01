cf5d6bbe1b37997a5cd3eed9dd7a833c6bd9a78f
rbgksqkr <rbgks1937@naver.com>
2024-05-01 12:02:43 +0900

    feat: useInput 커스텀 훅 구현
    Co-authored-by: jinhokim98 <rlawlsgh1227@gmail.com>

---
+++ hooks/src/lib/useInput.ts
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+
+interface ValidationResult {
+  isValid: boolean;
+  errorMessage?: string;
+}
+
+interface ValidatorProps {
+  onChange: (value: string) => ValidationResult;
+  onBlur: (value: string) => ValidationResult;
+}
+
+const useInput = (initialValue: string, validator: ValidatorProps) => {
+  const [value, setValue] = useState(initialValue);
+  const [errorInfo, setErrorInfo] = useState<ValidationResult>({
+    isValid: true,
+    errorMessage: '',
+  });
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const validationResult = validator.onChange(event.target.value);
+    setErrorInfo(validationResult);
+    if (!validationResult.isValid) return;
+    setValue(event.target.value);
+  };
+
+  const handleBlur = (event: React.FocusEvent<HTMLInputElement, Element>) => {
+    setErrorInfo(validator.onBlur(event.target.value));
+  };
+
+  return {
+    value,
+    setValue,
+    handleChange,
+    handleBlur,
+    errorInfo,
+    setErrorInfo,
+  };
+};
+
+export default useInput;
