3c4eba191aa291b6a7022fe356479eb83a252e30
Seongjin Hong <hongseongjin.to@gmail.com>
2024-05-01 18:01:17 +0900

    feat: useCardCVC 훅 구현 및 테스트 작성
    Co-authored-by: cys4585 <cys4585@users.noreply.github.com>

---
+++ hooks/src/lib/useCardCVC/useCardCVC.ts
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import ValidationResult from "../types/ValidationResult";
+import Validation from "../../utils/Validation";
+
+interface CVCValidationResult {
+  CVC: string;
+  validationResult: ValidationResult;
+  handleUpdateCVC: (value: string) => void;
+}
+
+export default function useCardCVC(
+  initialValue: string,
+  alwaysUpdateCVC: boolean = false
+): CVCValidationResult {
+  const [CVC, setCVC] = useState(initialValue);
+  const [validationResult, setValidationResult] = useState<ValidationResult>({
+    isValid: true,
+  });
+
+  const handleUpdateCVC = (value: string) => {
+    if (!validateCVC(value)) {
+      setValidationResult({
+        isValid: false,
+        errorMessage: "CVC 번호는 3자리 숫자로 입력하셔야 합니다.",
+      });
+      if (alwaysUpdateCVC) setCVC(value);
+      return;
+    }
+    setValidationResult({ isValid: true });
+    setCVC(value);
+  };
+
+  return { CVC, validationResult, handleUpdateCVC };
+}
+
+function validateCVC(value: string) {
+  return Validation.isNumeric(value) && Validation.hasLength(value, 3);
+}
