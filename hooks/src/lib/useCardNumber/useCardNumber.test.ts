2a9efc484f3debc5ddcaf8903a874ab034d6eab3
Lee sang Yeop <mma7710@naver.com>
2024-05-01 14:23:30 +0900

    feat(useCardNumber): blur, enter 이벤트 발생 시 유효성 검사 추가
    Co-authored-by: Soyeon Choe <soi-ha@users.noreply.github.com>

--- hooks/src/lib/useCardNumber/useCardNumber.test.ts
+++ hooks/src/lib/useCardNumber/useCardNumber.test.ts
@@ -1,5 +1,6 @@
 import { renderHook, act } from '@testing-library/react';
 import useCardNumber from './useCardNumber';
+import { ERROR_MESSAGE } from '../../constants';
 
 describe('신용카드 번호 입력 테스트', () => {
 	const initialValue = {
@@ -18,6 +19,10 @@ describe('신용카드 번호 입력 테스트', () => {
 		const userInput = '1234';
 		const { result } = renderHook(() => useCardNumber(initialValue));
 		const target = { value: userInput, name: 'first' };
+		const expectedValidationResult = {
+			isValid: true,
+			errorMessage: '',
+		};
 
 		act(() => {
 			result.current.handleCardNumberChange({
@@ -27,12 +32,17 @@ describe('신용카드 번호 입력 테스트', () => {
 		});
 
 		expect(result.current.inputValue.first).toEqual(userInput);
+		expect(result.current.validationResult).toEqual(expectedValidationResult);
 	});
 
 	it('입력값이 숫자가 아닐 때 업데이트가 안된다', () => {
 		const userInput = 'notUpdate';
 		const { result } = renderHook(() => useCardNumber(initialValue));
 		const target = { value: userInput, name: 'first' };
+		const expectedValidationResult = {
+			isValid: false,
+			errorMessage: ERROR_MESSAGE.onlyNumber,
+		};
 
 		act(() => {
 			result.current.handleCardNumberChange({
@@ -42,12 +52,17 @@ describe('신용카드 번호 입력 테스트', () => {
 		});
 
 		expect(result.current.inputValue.first).not.toEqual(userInput);
+		expect(result.current.validationResult).toEqual(expectedValidationResult);
 	});
 
 	it('입력값이 4자리 이상일 때 업데이트가 안된다', () => {
 		const userInput = '12345';
 		const { result } = renderHook(() => useCardNumber(initialValue));
 		const target = { value: userInput, name: 'first' };
+		const expectedValidationResult = {
+			isValid: false,
+			errorMessage: ERROR_MESSAGE.onlyNumber,
+		};
 
 		act(() => {
 			result.current.handleCardNumberChange({
@@ -57,5 +72,46 @@ describe('신용카드 번호 입력 테스트', () => {
 		});
 
 		expect(result.current.inputValue.first).not.toEqual(userInput);
+		expect(result.current.validationResult).toEqual(expectedValidationResult);
+	});
+
+	it('입력 값이 모두 채워지고 블러/엔터 이벤트 발생 시 에러가 발생하지 않는다', () => {
+		const userInput = '1234';
+		const { result } = renderHook(() => useCardNumber(initialValue));
+		const target = { value: userInput, name: 'first' };
+		const expectedValidationResult = {
+			isValid: true,
+			errorMessage: '',
+		};
+
+		act(() => {
+			result.current.handleCardNumberBlur({
+				target,
+				currentTarget: target,
+			} as React.FocusEvent<HTMLInputElement>);
+		});
+
+		expect(result.current.inputValue.first).toEqual(userInput);
+		expect(result.current.validationResult).toEqual(expectedValidationResult);
+	});
+
+	it('입력 값이 모두 채워지지 않고 블러/엔터 이벤트 발생 시 에러가 발생한다', () => {
+		const userInput = '125';
+		const { result } = renderHook(() => useCardNumber(initialValue));
+		const target = { value: userInput, name: 'first' };
+		const expectedValidationResult = {
+			isValid: false,
+			errorMessage: ERROR_MESSAGE.cardNumberOutOfRange,
+		};
+
+		act(() => {
+			result.current.handleCardNumberBlur({
+				target,
+				currentTarget: target,
+			} as React.FocusEvent<HTMLInputElement>);
+		});
+
+		expect(result.current.inputValue.first).not.toEqual(userInput);
+		expect(result.current.validationResult).toEqual(expectedValidationResult);
 	});
 });
--- hooks/src/lib/useCardNumber/useCardNumber.ts
+++ hooks/src/lib/useCardNumber/useCardNumber.ts
@@ -1,23 +1,82 @@
-import { ChangeEvent } from 'react';
+import { ChangeEvent, KeyboardEvent, FocusEvent, useState } from 'react';
 import { useInput } from '../common';
 import Validator from '../../utils/validator';
-import { OPTION } from '../../constants';
+import { ERROR_MESSAGE, OPTION } from '../../constants';
+import { EventProcessor, NameValuePair } from '../common/useInput';
 
 const useCardNumber = <T extends object>(initialValue: T) => {
-	const { inputValue, handleInputChange } = useInput(initialValue);
+	const { inputValue, handleInputChange, handleEventProcessor } = useInput(initialValue);
+	const [validationResult, setValidationResult] = useState<ValidationResult>({
+		isValid: true,
+		errorMessage: '',
+	});
 
 	const handleCardNumberChange = (e: ChangeEvent<HTMLInputElement>) => {
 		if (e.target !== e.currentTarget) return;
 
 		const { value } = e.target;
-		if (!Validator.checkNumber(value, OPTION.cardNumberMaxLength)) return;
+		if (!Validator.checkNumberAndOver(value, OPTION.cardNumberMaxLength)) {
+			return setValidationResult({
+				isValid: false,
+				errorMessage: ERROR_MESSAGE.onlyNumber,
+			});
+		}
 
 		handleInputChange(e);
+		setValidationResult({
+			isValid: true,
+			errorMessage: '',
+		});
 	};
 
-	// TODO: Blur&Enter시 4자리가 채워졌는 유효성검사
+	const handleCardNumberBlur = (e: FocusEvent<HTMLInputElement>) => {
+		if (e.target !== e.currentTarget) return;
 
-	return { inputValue, handleCardNumberChange } as const;
+		const { value } = e.target;
+		if (!Validator.checkFillNumber(value, OPTION.cardNumberMaxLength))
+			return setValidationResult({
+				isValid: false,
+				errorMessage: ERROR_MESSAGE.cardNumberOutOfRange,
+			});
+
+		handleEventProcessor(processor, e);
+		setValidationResult({
+			isValid: true,
+			errorMessage: '',
+		});
+	};
+
+	const handleCardNumberEnter = (e: KeyboardEvent<HTMLInputElement>) => {
+		if (e.target !== e.currentTarget) return;
+		if (e.key !== 'Enter') return;
+
+		const { value } = e.target as HTMLInputElement;
+		if (!value || !Validator.checkFillNumber(value, OPTION.cardNumberMaxLength))
+			return setValidationResult({
+				isValid: false,
+				errorMessage: ERROR_MESSAGE.cardNumberOutOfRange,
+			});
+
+		handleEventProcessor(processor, e);
+		setValidationResult({
+			isValid: true,
+			errorMessage: '',
+		});
+	};
+
+	const processor = (e: EventProcessor) => {
+		const { value, name } = e.target as NameValuePair;
+
+		return { name, value } as const;
+	};
+
+	return {
+		inputValue,
+		validationResult,
+		handleCardNumberChange,
+		handleCardNumberBlur,
+		handleCardNumberEnter,
+	} as const;
 };
 
 export default useCardNumber;
