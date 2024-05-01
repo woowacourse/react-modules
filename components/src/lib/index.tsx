3dc49010ed3ae1718bbb0ae2c157e6afd53b2325
badahertz52 <badahertz52@gmail.com>
2024-05-01 11:13:42 +0900

    refactor: 모달 모듈 export
    Co-Authored-By: KIM DAEUN <midekuna@gmail.com>

---
+++ components/src/lib/index.tsx
@@ -0,0 +1,4 @@
+export { default as Modal } from './Modal';
+export { default as BasicCenterModal } from './Modal/BasicCenterModal/index';
+export { default as BasicBottomModal } from './Modal/BasicBottomModal/index';
+export { default as useModal } from './hooks/useModal';
