fa0ed3d17bdd18dff5363a7bf08ab9f34b5e6fd5
Hyelim Choi <HyelimChoi01@gmail.com>
2024-05-01 20:55:43 +0900

    chore: constants 값 선언
    Co-Authored-By: Hyerin <125418818+ooherin@users.noreply.github.com>

--- hooks/src/constants/system.ts
+++ hooks/src/constants/system.ts
@@ -1,3 +1,13 @@
 export const UPPERCASE_AND_SPACE_ONLY = /^[A-Z\s]+$/;
 
 export const DOUBLE_BLANK = / {2,}/;
+
+export const MONTH_RANGE = {
+  MIN: 1,
+  MAX: 12,
+};
+
+export const YEAR_RANGE = {
+  MIN: 0,
+  MAX: 99,
+};
