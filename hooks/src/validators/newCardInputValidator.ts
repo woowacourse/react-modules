3a61261a85797b361827b8b86d78973af32c5b6e
ImxYJL <allensain14@gmail.com>
2024-05-01 22:11:40 +0900

    feat: 커스텀 훅 유효성 검증 로직 구현
    Co-authored-by: Yuseon Kim <useon@users.noreply.github.com>

---
+++ hooks/src/validators/newCardInputValidator.ts
@@ -0,0 +1,87 @@
+export const validateCardNumber = (
+  value: string,
+  cardNumbersLength: number
+): boolean => {
+  if (value !== "" && Number.isNaN(Number(value))) {
+    return true;
+  }
+
+  if (value !== "" && value.length < cardNumbersLength) {
+    return true;
+  }
+
+  return false;
+};
+
+export const validateCardCompany = (
+  value: string,
+  defaultValue: string
+): boolean => {
+  if (value === defaultValue) return true;
+
+  return false;
+};
+
+export const validateCardExpiration = (
+  value: string,
+  type: string
+): boolean => {
+  if (value !== "" && Number.isNaN(Number(value))) {
+    return true;
+  }
+
+  if (value !== "" && value.length != 2) {
+    return true;
+  }
+
+  if (
+    value !== "" &&
+    type === "MM" &&
+    !(Number(value) >= 1 && Number(value) <= 12)
+  ) {
+    return true;
+  }
+
+  return false;
+};
+
+export const validateUserName = (
+  value: string,
+  cardUserNameLength: number
+): boolean => {
+  if (
+    value !== "" &&
+    !new RegExp(`^[a-zA-Z\\s]{0,${cardUserNameLength}}$`).test(value)
+  ) {
+    return true;
+  }
+
+  return false;
+};
+
+export const validateCVC = (value: string, cardCVCLength: number): boolean => {
+  if (value !== "" && Number.isNaN(Number(value))) {
+    return true;
+  }
+
+  if (value !== "" && value.length < cardCVCLength) {
+    return true;
+  }
+
+  return false;
+};
+
+export const validatePassword = (
+  value: string,
+  passwordLength: number
+): boolean => {
+  if (value !== "" && Number.isNaN(Number(value))) {
+    return true;
+  }
+
+  if (value !== "" && value.length !== passwordLength) {
+    return true;
+  }
+
+  return false;
+};
