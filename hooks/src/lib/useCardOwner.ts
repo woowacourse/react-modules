2b0bcc98c7a7ad81e41e2775a016a95431a617cc
Largopie <106071687+Largopie@users.noreply.github.com>
2024-05-01 16:00:16 +0900

    chore(useCardOwner): 로직 순서 변경
    Co-authored-by: soosoo22 <lllllh00@naver.com>

--- hooks/src/lib/useCardOwner.ts
+++ hooks/src/lib/useCardOwner.ts
@@ -5,17 +5,17 @@ const useCardOwner = (initialValue = '') => {
     return /^[a-zA-Z ]*$/.test(value);
   };
 
-  const preventInputValidations: ValidationType[] = [
-    {
-      validate: isEnglish,
-      message: '영어만 입력 가능합니다.',
-    },
-  ];
-
   const inputValidations: ValidationType[] = [
     {
       validate: (value: string) => value !== '',
       message: '소유자 이름을 영어로 입력해주세요.',
+    },
+  ];
+
+  const preventInputValidations: ValidationType[] = [
+    {
+      validate: isEnglish,
+      message: '영어만 입력 가능합니다.',
     },
   ];
 
