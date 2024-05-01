dd4535aa862bab6f47b78ec4de5a66e11314439d
hain-tain <hain7ce@gmail.com>
2024-05-01 21:30:07 +0900

    feat: useCardHolderName hook 추가
    Co-authored-by: Todari <rhymint@gmail.com>

---
+++ hooks/src/lib/useCardHolderName.ts
@@ -0,0 +1,44 @@
+import useCardValidate from './useCardValidate';
+
+const useCardHolderName = (initValue: string, maxLength: number = 50) => {
+  const validateOnChange = (newValue: string) => {
+    if (newValue.length > maxLength) {
+      return {
+        isValid: false,
+        errorMessage: `이름은 ${maxLength}글자 까지만 입력이 가능해요.`,
+      };
+    }
+    if (!/^\[a-zA-Z]*$/.test(newValue)) {
+      return {
+        isValid: false,
+        errorMessage: '이름은 영어만 입력이 가능해요.',
+      };
+    }
+    return { isValid: true, errorMessage: '' };
+  };
+
+  const validateOnBlur = () => {
+    return { isValid: true, errorMessage: '' };
+  };
+
+  const {
+    value,
+    errorMessage,
+    onChangeHandler,
+    onFocusHandler,
+    onBlurHandler,
+  } = useCardValidate({
+    initValue,
+    validateOnChange,
+    validateOnBlur,
+  });
+
+  return {
+    value,
+    errorMessage,
+    onChangeHandler,
+    onBlurHandler,
+    onFocusHandler,
+  };
+};
+export default useCardHolderName;
