beb08b37871951d6af4af405ff6001327ab260bd
hain-tain <hain7ce@gmail.com>
2024-05-01 21:22:37 +0900

    feat: useCardValidate hook 추가
    Co-authored-by: Todari <rhymint@gmail.com>

---
+++ hooks/src/lib/useCardValidate.ts
@@ -0,0 +1,49 @@
+import { useState } from 'react';
+
+interface UseCardValidateProps {
+  initValue: string;
+  validateOnChange: (value: string) => ValidateResult;
+  validateOnBlur: () => ValidateResult;
+}
+
+interface ValidateResult {
+  isValid: boolean;
+  errorMessage: string;
+}
+
+const useCardValidate = ({
+  initValue,
+  validateOnChange,
+  validateOnBlur,
+}: UseCardValidateProps) => {
+  const [value, setValue] = useState(initValue);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue = e.target.value;
+    const { isValid, errorMessage } = validateOnChange(newValue);
+    if (!isValid) {
+      setErrorMessage(errorMessage);
+      return;
+    }
+    setErrorMessage('');
+    setValue(newValue);
+  };
+
+  const onFocusHandler = () => {
+    setErrorMessage('');
+  };
+
+  const onBlurHandler = () => {
+    validateOnBlur();
+  };
+
+  return {
+    value,
+    errorMessage,
+    onChangeHandler,
+    onBlurHandler,
+    onFocusHandler,
+  };
+};
+export default useCardValidate;
