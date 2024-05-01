10d8b1a02a8031491b81e5450046b8d38feb3c42
Seongjin Hong <hongseongjin.to@gmail.com>
2024-05-01 18:01:30 +0900

    feat: useCardPassword 훅 구현 및 테스트 작성
    Co-authored-by: cys4585 <cys4585@users.noreply.github.com>

---
+++ hooks/src/lib/useCardPassword/useCardPassword.ts
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import ValidationResult from "../types/ValidationResult";
+import Validation from "../../utils/Validation";
+
+interface PasswordValidationResult {
+  password: string;
+  validationResult: ValidationResult;
+  handleUpdatePassword: (value: string) => void;
+}
+
+export default function useCardPassword(
+  initialValue: string,
+  alwaysUpdatePassword: boolean = false
+): PasswordValidationResult {
+  const [password, setPassword] = useState(initialValue);
+  const [validationResult, setValidationResult] = useState<ValidationResult>({
+    isValid: true,
+  });
+
+  const handleUpdatePassword = (value: string) => {
+    if (!validatePassword(value)) {
+      setValidationResult({
+        isValid: false,
+        errorMessage: "두 자리의 숫자여야 합니다. 다시 입력해주세요.",
+      });
+      if (alwaysUpdatePassword) setPassword(value);
+      return;
+    }
+
+    setValidationResult({ isValid: true });
+    setPassword(value);
+  };
+
+  return {
+    password,
+    validationResult,
+    handleUpdatePassword,
+  };
+}
+
+const validatePassword = (value: string) => {
+  return Validation.isNumeric(value) && Validation.hasLength(value, 2);
+};
