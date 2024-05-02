f699623e570a4b99b0036e9aca78730ba4b3c979
Lee sang Yeop <mma7710@naver.com>
2024-05-02 15:01:05 +0900

    chore(storybook): 스토리북 설정 - docs 추가 - 전역 preview style 추가
    Co-authored-by: Soyeon Choe <soi-ha@users.noreply.github.com>

---
+++ components/.storybook/preview.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import type { Preview } from '@storybook/react';
+
+const preview: Preview = {
+	parameters: {
+		controls: {
+			matchers: {
+				color: /(background|color)$/i,
+				date: /Date$/i,
+			},
+		},
+	},
+};
+
+export const decorators = [
+	(Story) => (
+		<div style={{ width: '100px', height: '300px' }}>
+			<Story />
+		</div>
+	),
+];
+
+export default preview;
