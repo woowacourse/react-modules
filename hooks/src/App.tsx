59eb228d84051b172964a8e6e8a4cbb4b65e75a1
soosoo22 <lllllh00@naver.com>
2024-05-02 16:32:28 +0900

    chore: 빌드한 hooks로 교체

--- hooks/src/App.tsx
+++ hooks/src/App.tsx
@@ -1,35 +1,24 @@
-import React from 'react';
-import useCardExpirationDate from './lib/useCardExpirationDate';
+import React from "react";
+import {
+  useCardCompany,
+  useCardCVC,
+  useCardExpirationDate,
+  useCardNumbers,
+  useCardOwner,
+  useCardPassword,
+} from "soosoo-react-payments-hooks";
 
 function App() {
+  const { cardCompany, isCardCompanyValid } = useCardCompany();
+  const { cardCVC, isCardCVCValid } = useCardCVC();
   const { month, year, isCardExpirationDateValid } = useCardExpirationDate();
-
-  const error = [month.error, year.error].find(({ state }) => state);
+  const { cardNumbers, cardBrand, isCardNumbersValid } = useCardNumbers();
+  const { cardOwner, isCardOwnerValid } = useCardOwner();
+  const { cardPassword, isCardPasswordValid } = useCardPassword();
 
   return (
     <>
-      <h1>Hooks Modules</h1>
-
-      <input
-        autoFocus
-        ref={month.ref}
-        style={{ border: `2px solid ${month.error.state ? 'red' : 'black'}`, padding: '4px' }}
-        maxLength={2}
-        type='text'
-        value={month.value}
-        onChange={(e) => month.onChange(e, 2)}
-      />
-
-      <input
-        ref={year.ref}
-        style={{ border: `2px solid ${year.error.state ? 'red' : 'black'}`, padding: '4px' }}
-        maxLength={2}
-        type='text'
-        value={year.value}
-        onChange={(e) => year.onChange(e, 2)}
-      />
-      {error && <p style={{ color: 'red' }}>{error.message}</p>}
-      {isCardExpirationDateValid && <p style={{ color: 'red' }}>검증 완료</p>}
+      <h1>Soosoo Hooks Modules</h1>
     </>
   );
 }
