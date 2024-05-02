6209ea135853fb0fa40421192dded6930cb9c473
Largopie <106071687+Largopie@users.noreply.github.com>
2024-05-02 16:23:09 +0900

    chore: npm 배포 및 설치 확인
    Co-authored-by: soosoo22 <lllllh00@naver.com>

--- hooks/src/App.tsx
+++ hooks/src/App.tsx
@@ -1,35 +1,17 @@
+import { useCardNumbers, useCardExpirationDate, useCardCompany, useCardOwner, useCardCVC, useCardPassword } from 'nakta-react-payments-hooks';
 import React from 'react';
-import useCardExpirationDate from './lib/useCardExpirationDate';
 
 function App() {
+  const { cardNumbers, cardBrand, isCardNumbersValid } = useCardNumbers();
+  const { cardCompany, isCardCompanyValid } = useCardCompany();
   const { month, year, isCardExpirationDateValid } = useCardExpirationDate();
-
-  const error = [month.error, year.error].find(({ state }) => state);
+  const { cardOwner, isCardOwnerValid } = useCardOwner();
+  const { cardCVC, isCardCVCValid } = useCardCVC();
+  const { cardPassword, isCardPasswordValid } = useCardPassword();
 
   return (
     <>
       <h1>Hooks Modules</h1>
-
-      <input
-        autoFocus
-        ref={month.ref}
-        style={{ border: `2px solid ${month.error.state ? 'red' : 'black'}`, padding: '4px' }}
-        maxLength={2}
-        type='text'
-        value={month.value}
-        onChange={(e) => month.onChange(e, 2)}
-      />
-
-      <input
-        ref={year.ref}
-        style={{ border: `2px solid ${year.error.state ? 'red' : 'black'}`, padding: '4px' }}
-        maxLength={2}
-        type='text'
-        value={year.value}
-        onChange={(e) => year.onChange(e, 2)}
-      />
-      {error && <p style={{ color: 'red' }}>{error.message}</p>}
-      {isCardExpirationDateValid && <p style={{ color: 'red' }}>검증 완료</p>}
     </>
   );
 }
