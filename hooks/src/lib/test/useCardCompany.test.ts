257125a2fa8804cb286b4907403efce090971dcf
Hyelim Choi <HyelimChoi01@gmail.com>
2024-05-02 10:59:58 +0900

    test: useCompany 테스트 추가
    Co-Authored-By: Hyerin <125418818+ooherin@users.noreply.github.com>

---
+++ hooks/src/lib/test/useCardCompany.test.ts
@@ -0,0 +1,54 @@
+import { renderHook } from '@testing-library/react';
+import React from 'react';
+import useCardCompany from '../useCardCompany';
+import { cardCompanyErrorMessage } from '../../constants/error';
+
+export const ExampleCardBrands = [
+  'BC카드',
+  '신한카드',
+  '카카오뱅크',
+  '현대카드',
+  '우리카드',
+  '롯데카드',
+  '하나카드',
+  '국민카드',
+];
+
+describe('useCardHolder 훅 테스트', () => {
+  it('초기값이 정확히 설정되어야 한다.', () => {
+    const initialValue = '';
+    const { result } = renderHook(() =>
+      useCardCompany({ initialValue, optionArray: ExampleCardBrands })
+    );
+
+    expect(result.current.value).toBe(initialValue);
+  });
+
+  it('입력값이 정확히 업데이트 되어야 한다.', () => {
+    const initialValue = '';
+    const { result } = renderHook(() =>
+      useCardCompany({ initialValue, optionArray: ExampleCardBrands })
+    );
+
+    const validValue = ExampleCardBrands[0];
+    React.act(() => {
+      result.current.onSelect(validValue);
+    });
+
+    expect(result.current.value).toBe(validValue);
+  });
+
+  it('유효하지 않은 옵션 선택시 에러를 낸다.', () => {
+    const initialValue = '';
+    const { result } = renderHook(() =>
+      useCardCompany({ initialValue, optionArray: ExampleCardBrands })
+    );
+
+    const invalidValue = '헤일리 은행';
+    React.act(() => {
+      result.current.onSelect(invalidValue);
+    });
+
+    expect(result.current.errorMessages).toBe(cardCompanyErrorMessage);
+  });
+});
