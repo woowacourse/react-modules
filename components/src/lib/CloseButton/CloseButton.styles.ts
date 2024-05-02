709159a830f64d73de69332ef60e98ff0b3c2428
lurgi <wahoo9040@naver.com>
2024-05-02 09:10:37 +0900

    feat: CloseButton 스타일 추가
    Co-authored-by: novice0840 <novice0840@users.noreply.github.com>

--- components/src/lib/CloseButton/CloseButton.styles.ts
+++ components/src/lib/CloseButton/CloseButton.styles.ts
@@ -2,7 +2,11 @@ import { css } from "@emotion/react";
 import COLOR_PALETTE from "../colorPalette";
 
 export const closeButton = css({
+  all: "unset",
   padding: "0",
   backgroundColor: COLOR_PALETTE.background,
   color: COLOR_PALETTE.colorHighlight,
+  "&:hover": {
+    cursor: "pointer",
+  },
 });
