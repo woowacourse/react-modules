b1ad78dd3ea6fe6c538b5f008d81e4901aa37757
Seongjin Hong <hongseongjin.to@gmail.com>
2024-05-02 11:38:08 +0900

    chore: 카드 번호 훅의 로컬 테스트를 위한 App.tsx 코드 업로드
    Co-authored-by: cys4585 <cys4585@users.noreply.github.com>

--- hooks/src/App.tsx
+++ hooks/src/App.tsx
@@ -1,10 +1,133 @@
 import React from "react";
+
+import {
+  useCardCVC,
+  useCardBrand,
+  useCardExpiryDate,
+  useCardNumbers,
+} from "@seongjinme/card-validation";
+
 import "./App.css";
 
+const cardBrands = ["신한카드", "현대카드", "카카오뱅크"];
+
 function App() {
+  const { CVC, validationResult, handleUpdateCVC } = useCardCVC("");
+  const {
+    expiryDate,
+    validationResult: expiryDateValidationResult,
+    handleUpdateExpiryDate,
+  } = useCardExpiryDate({
+    month: "",
+    year: "",
+  });
+  const {
+    brand,
+    validationResult: brandValidationResult,
+    handleUpdateBrand,
+  } = useCardBrand("", cardBrands);
+  const {
+    cardNumbers,
+    validationResult: cardNumbersValidationResult,
+    validStates,
+    handleUpdateCardNumbers,
+  } = useCardNumbers(["", "", "", ""]);
+
+  const handleChangeCardBrand = (
+    event: React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    handleUpdateBrand(event.target.value);
+  };
+
   return (
     <>
       <h1>Hooks Modules</h1>
+      <section>
+        <h2>useCVC</h2>
+        <input
+          type="text"
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+            handleUpdateCVC(event.target.value)
+          }
+        />
+        <p>CVC: {CVC}</p>
+        <p>isValid: {validationResult.isValid.toString()}</p>
+        <p>errorMessage: {validationResult.errorMessage}</p>
+      </section>
+
+      <section>
+        <h2>useExpiryDate</h2>
+        <input
+          type="text"
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+            handleUpdateExpiryDate({
+              month: event.target.value,
+              year: expiryDate.year,
+            })
+          }
+        />
+        <input
+          type="text"
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+            handleUpdateExpiryDate({
+              month: expiryDate.month,
+              year: event.target.value,
+            })
+          }
+        />
+        <p>
+          ExpiryDate: {expiryDate.month} / {expiryDate.year}
+        </p>
+        <p>isValid: {expiryDateValidationResult.isValid.toString()}</p>
+        <p>errorMessage: {expiryDateValidationResult.errorMessage}</p>
+      </section>
+
+      <section>
+        <h2>useCardBrand</h2>
+        <select onChange={handleChangeCardBrand}>
+          {cardBrands.map((brand) => (
+            <option key={brand} value={brand}>
+              {brand}
+            </option>
+          ))}
+        </select>
+        <p>brand: {brand}</p>
+        <p>isValid: {brandValidationResult.isValid.toString()}</p>
+        <p>errorMessage: {brandValidationResult.errorMessage}</p>
+      </section>
+
+      <section>
+        <h2>useCardNumbers</h2>
+        <input
+          type="text"
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+            handleUpdateCardNumbers(0, event.target.value)
+          }
+        />
+        <input
+          type="text"
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+            handleUpdateCardNumbers(1, event.target.value)
+          }
+        />
+        <input
+          type="text"
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+            handleUpdateCardNumbers(2, event.target.value)
+          }
+        />
+        <input
+          type="text"
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+            handleUpdateCardNumbers(3, event.target.value)
+          }
+        />
+
+        <p>cardNumbers: {cardNumbers.join(", ")}</p>
+        <p>validStates: {validStates.join(", ")}</p>
+        <p>isValid: {cardNumbersValidationResult.isValid.toString()}</p>
+        <p>errorMessage: {cardNumbersValidationResult.errorMessage}</p>
+      </section>
     </>
   );
 }
