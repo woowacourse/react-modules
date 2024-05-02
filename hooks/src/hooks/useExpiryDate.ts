806fc1459b512a0e9ea4680934e20516d29f99ac
skiende74 <skiende74@gmail.com>
2024-05-02 16:29:34 +0900

    feat(useExpiryDate): useExpiryDate 기능 구현 및 테스트 추가

---
+++ hooks/src/hooks/useExpiryDate.ts
@@ -0,0 +1,33 @@
+import useInput, { InputState } from "./domains/useInput";
+import { Validator } from "./domains/validation";
+import validations from "./domains/validations";
+import { makeOnBlur, makeOnChange } from "./domains/makeCallback";
+
+const validators: Validator[] = [
+  {
+    validate: (value) => /^\d*$/.test(value),
+    errorMessage: "숫자만 입력 가능합니다.",
+  },
+  {
+    validate: (value) => Number(value) >= 1 && Number(value) <= 12,
+    errorMessage: "월의 범위는 1~12여야 합니다.",
+    index: [0],
+  },
+  {
+    validate: (value) => value.length === 2,
+    errorMessage: "필드의 길이는 2여야합니다.",
+    type: "blur",
+  },
+];
+const useExpiryDate = () => {
+  const expiryDates: InputState[] = [useInput(""), useInput("")];
+
+  validations(expiryDates, validators);
+
+  const onChanges = expiryDates.map(makeOnChange);
+  const onBlurs = expiryDates.map(makeOnBlur);
+
+  return { expiryDates, onChanges, onBlurs };
+};
+
+export default useExpiryDate;
