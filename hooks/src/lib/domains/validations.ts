e31de3dbfc4f66c96f8892463ea7ce93eb9d0fff
skiende74 <skiende74@gmail.com>
2024-05-02 17:07:16 +0900

    chore: 폴더명을 lib로 변경

--- hooks/src/lib/domains/validations.ts
+++ hooks/src/lib/domains/validations.ts
@@ -6,12 +6,6 @@ const validations = (inputStates: InputState[], validators: Validator[]) => {
     const matchedValidators = validators.filter(({ index }) => (index !== undefined ? index.includes(i) : true));
     validation(inputStates[i], matchedValidators);
   }
-  // const globalValidators = validators.filter(({ type }) => type === "global");
-  // if (inputStates.every(({ status }) => status === "blur")) {
-  //   for (let i = 0; i < inputStates.length; i++) {
-  //     validation(inputStates[i], globalValidators);
-  //   }
-  // }
 };
 
 export default validations;
