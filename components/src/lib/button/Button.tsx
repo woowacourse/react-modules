46d491215fcc51705fab2e86a42c9653eb028f45
ImxYJL <allensain14@gmail.com>
2024-05-02 16:00:16 +0900

    build: 모달 라이브러리 빌드를 위한 파일 세팅
    Co-authored-by: Yuseon Kim <useon@users.noreply.github.com>

--- components/src/lib/button/Button.tsx
+++
@@ -1,7 +0,0 @@
-interface ButtonProps extends React.ComponentPropsWithRef<"button"> {}
-
-const Button: React.FC<ButtonProps> = ({ children, ...restProps }) => {
-  return <button {...restProps}>{children}</button>;
-};
-
-export default Button;
