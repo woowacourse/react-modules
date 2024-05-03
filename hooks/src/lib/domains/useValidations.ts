7d2a43ef20f0006d3d9aeb297f9eb3fb01f75094
skiende74 <skiende74@gmail.com>
2024-05-03 14:38:58 +0900

    feat(useValidations): 의존성 변경시에만 검증하는 useValidation 훅 구현

---
+++ hooks/src/lib/domains/useValidations.ts
@@ -0,0 +1,28 @@
+import { useEffect } from "react";
+import { Validator } from "./validation";
+import { InputState } from "./useInput";
+import validations from "./validations";
+import { makeOnBlur, makeOnChange } from "./makeCallback";
+
+const flatten = <K extends keyof InputState>(states: InputState[], key: K) =>
+  states.map((inputState) => inputState[key]);
+
+const useDeepCompareEffect = <T>(callback: () => void, dependencies: T[]) => {
+  const dependency = dependencies.map((dependency) => JSON.stringify(dependency)).join("+");
+  useEffect(callback, [dependency]);
+};
+
+const useValidations = (inputStates: InputState[], validators: Validator[]) => {
+  const values = flatten(inputStates, "value");
+  const isErrors = flatten(inputStates, "isError");
+
+  useDeepCompareEffect(() => {
+    validations(inputStates, validators);
+  }, [values, isErrors]);
+
+  const onChanges = inputStates.map(makeOnChange);
+  const onBlurs = inputStates.map(makeOnBlur);
+  return { inputStates, onChanges, onBlurs };
+};
+
+export default useValidations;
