37b13e6ee38946fb98fdc969a3814c5d96925188
Seongjin Hong <hongseongjin.to@gmail.com>
2024-05-03 14:31:58 +0900

    refactor: 모달 dimmer의 위치 지정 CSS 속성을 inset으로 대체

--- components/src/lib/Modal/Modal.style.ts
+++ components/src/lib/Modal/Modal.style.ts
@@ -11,8 +11,7 @@ const colors = {
 
 export const ModalOverlay = styled.div`
   position: fixed;
-  top: 0;
-  left: 0;
+  inset: 0;
   width: 100%;
   height: 100%;
   background-color: rgba(0, 0, 0, 0.5);
