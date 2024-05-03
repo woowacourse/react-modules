8cccddc99a24b45ba70d77de0bf2934bdd3bf88f
Hyelim Choi <HyelimChoi01@gmail.com>
2024-05-04 04:36:39 +0900

    chore: 사용 안하는 인터페이스 삭제

--- hooks/src/types/cardHolder.ts
+++ hooks/src/types/cardHolder.ts
@@ -1,9 +1,5 @@
 import { ErrorStatus } from './errorStatus';
 
-export interface CardHolder {
-  cardHolder: string;
-}
-
 export type CardHolderError =
   | ErrorStatus.ONLY_UPPERCASE
   | ErrorStatus.IS_DOUBLE_BLANK;
--- hooks/src/types/cvc.ts
+++ hooks/src/types/cvc.ts
@@ -1,7 +1,3 @@
 import { ErrorStatus } from './errorStatus';
 
-export interface CVC {
-  CVC: string;
-}
-
-export type cvcError = ErrorStatus.IS_NOT_NUMBER | ErrorStatus.INVALID_LENGTH;
+export type CVCError = ErrorStatus.IS_NOT_NUMBER | ErrorStatus.INVALID_LENGTH;
