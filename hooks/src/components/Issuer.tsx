6502e46ca49743cc3e64f188513e8444336f1ef8
badahertz52 <badahertz52@gmail.com>
2024-05-03 20:40:02 +0900

    feat: useCardIssuer에 validations추가

--- hooks/src/components/Issuer.tsx
+++ hooks/src/components/Issuer.tsx
@@ -1,12 +1,16 @@
 import { MouseEvent } from 'react';
 import { useCardIssuer } from '../lib/index';
 
+const cardIssuerProps = {
+  validationErrorMessages: {
+    empty: '카드 발급사 선택해주세요.',
+    issuer: '올바른 카드 발급사를 선택해주세요.',
+  },
+  validations: { issuers: ['배민', '카카오', '현대'] },
+};
+
 export default function Issuer() {
-  const { cardIssuer, updateValue, errorMessage } = useCardIssuer({
-    validationErrors: {
-      empty: '카드 발급사 선택해주세요.',
-    },
-  });
+  const { cardIssuer, updateValue, errorMessage } = useCardIssuer(cardIssuerProps);
   const handleClick = (e: MouseEvent<HTMLFieldSetElement>) => {
     if (!(e.target instanceof HTMLButtonElement)) return;
     updateValue(e.target.name);
--- hooks/src/lib/useCardIssuer.ts
+++ hooks/src/lib/useCardIssuer.ts
@@ -3,15 +3,30 @@ import { validateFilledValue } from './utils/validators';
 import { ErrorMessage, UseCardModuleProps } from './types';
 import useCardValidation from './useCardValidation';
 
-interface CardIssuerValidationErrors {
+interface CardIssuerValidationErrorMessages {
   empty: string;
+  issuer: string;
 }
 
-export default function useCardIssuer(props: UseCardModuleProps<CardIssuerValidationErrors>) {
+/**
+ * @property {string[]} issuers : 카드 발급사 이름 배열
+ */
+interface CardIssuerValidations {
+  issuers: string[];
+}
+
+type UseCardIssuerProps = UseCardModuleProps<CardIssuerValidationErrorMessages, CardIssuerValidations>;
+
+export default function useCardIssuer({ validationErrorMessages, validations: { issuers } }: UseCardIssuerProps) {
   const [cardIssuer, setCardIssuer] = useState('');
   const [errorMessage, setErrorMessage] = useState<ErrorMessage>(null);
 
-  const totalValidators = [{ test: validateFilledValue, errorMessage: props.validationErrors.empty }];
+  const validateCorrectIssuer = (value: string) => !!issuers.find((i) => i === value);
+
+  const totalValidators = [
+    { test: validateFilledValue, errorMessage: validationErrorMessages.empty },
+    { test: validateCorrectIssuer, errorMessage: validationErrorMessages.issuer },
+  ];
 
   const { handleUpdateValue } = useCardValidation<string>({
     blurEventValidators: undefined,
