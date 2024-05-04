6f6933da66ea3f6ce6d7a20b61548abf30d8ddda
jaeml06 <jaeml0630@gmail.com>
2024-05-04 23:49:26 +0900

    refactor: validate 파일 명 변경

--- hooks/src/lib/validate.ts
+++ hooks/src/lib/validate.ts
@@ -7,39 +7,39 @@ import {
   PASSWORD,
   UPPERCASE_AND_SPACE_ONLY,
   YEAR_RANGE,
-} from './constants/system';
+} from './constants/cardDataValidation';
 
-function checkMaxLength(value: string, maxLength: number) {
+function throwIfNoneMaxLength(value: string, maxLength: number) {
   if (value.length !== maxLength) {
     throw new Error(`${maxLength}자로 입력해주세요.`);
   }
 }
-function checkTrimBlank(value: string) {
+function throwIfTrimBlank(value: string) {
   if (value.trim() === '' && value !== '') {
     throw new Error(ERROR_MESSAGES.INVALID_TRIM_BLANK);
   }
 }
 
-function checkDoubleBlank(value: string) {
+function throwIfDoubleBlank(value: string) {
   if (DOUBLE_BLANK.test(value)) {
     throw new Error(ERROR_MESSAGES.INVALID_DOUBLE_BLANK);
   }
 }
 
-function validateNumber(value: string) {
+function throwIfNoneNumber(value: string) {
   if (!Number.isInteger(Number(value))) {
     throw new Error(ERROR_MESSAGES.INVALID_ONLY_NUMBER);
   }
 }
 
-function checkEmpty(value: string) {
+function throwIfEmpty(value: string) {
   if (value.length === 0) {
     return true;
   }
 }
 
 function validateMonth(value: string) {
-  if (checkEmpty(value) || value === '0') return;
+  if (throwIfEmpty(value) || value === '0') return;
   const month = Number(value);
   if (!(MONTH_RANGE.MIN <= month && month <= MONTH_RANGE.MAX)) {
     throw new Error(ERROR_MESSAGES.INVALID_MONTH);
@@ -47,14 +47,14 @@ function validateMonth(value: string) {
 }
 
 function validateYear(value: string) {
-  if (checkEmpty(value)) return;
+  if (throwIfEmpty(value)) return;
   const year = Number(value);
   if (!(YEAR_RANGE.MIN <= year && year <= YEAR_RANGE.MAX)) {
     throw new Error(ERROR_MESSAGES.INVALID_YEAR);
   }
 }
 
-function validateUpperCase(value: string) {
+function throwIfNoneUpperCase(value: string) {
   if (!UPPERCASE_AND_SPACE_ONLY.test(value) && value.length !== 0) {
     throw new Error(ERROR_MESSAGES.INVALID_ONLY_UPPERCASE);
   }
@@ -66,34 +66,34 @@ interface ValidationMap {
 
 export const Validation: ValidationMap = {
   cardNumber: (value: string) => {
-    checkTrimBlank(value);
-    validateNumber(value);
-    checkMaxLength(value, CARD_NUMBER.FIELD_LENGTH);
+    throwIfTrimBlank(value);
+    throwIfNoneNumber(value);
+    throwIfNoneMaxLength(value, CARD_NUMBER.FIELD_LENGTH);
   },
   month: (value: string) => {
-    checkTrimBlank(value);
-    validateNumber(value);
+    throwIfTrimBlank(value);
+    throwIfNoneNumber(value);
     validateMonth(value);
   },
   year: (value: string) => {
-    checkTrimBlank(value);
-    validateNumber(value);
+    throwIfTrimBlank(value);
+    throwIfNoneNumber(value);
     validateYear(value);
   },
   userName: (value: string) => {
-    checkTrimBlank(value);
-    checkDoubleBlank(value);
-    validateUpperCase(value);
+    throwIfTrimBlank(value);
+    throwIfDoubleBlank(value);
+    throwIfNoneUpperCase(value);
   },
   CVC: (value: string) => {
-    checkTrimBlank(value);
-    validateNumber(value);
-    checkMaxLength(value, CVC_LIMIT.FIELD_LENGTH);
+    throwIfTrimBlank(value);
+    throwIfNoneNumber(value);
+    throwIfNoneMaxLength(value, CVC_LIMIT.FIELD_LENGTH);
   },
 
   password: (value: string) => {
-    checkTrimBlank(value);
-    validateNumber(value);
-    checkMaxLength(value, PASSWORD.FIELD_LENGTH);
+    throwIfTrimBlank(value);
+    throwIfNoneNumber(value);
+    throwIfNoneMaxLength(value, PASSWORD.FIELD_LENGTH);
   },
 };
