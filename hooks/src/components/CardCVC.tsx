0b4ed8ba1fba242ad5c5cf528194be4780fa87e6
badahertz52 <badahertz52@gmail.com>
2024-05-04 23:50:14 +0900

    refactor: useCardCVC 수정 및 CardCVC 수정
    - useCardCVC : props, 반환값 수정에 따른 프로덕션 코드 수정
    - CardCVC' useCardCVC수정에 따른 수정

--- hooks/src/components/CardCVC.tsx
+++ hooks/src/components/CardCVC.tsx
@@ -1,28 +1,53 @@
+import { ChangeEvent, FocusEvent, useState } from 'react';
 import { useCVC } from '../lib';
+import { UseCardCVCProps } from '../lib/useCVC';
 
-const useCVCProps = {
-  validationErrorMessages: {
+const LENGTH = 3;
+
+const useCVCProps = (cardCVC: string): UseCardCVCProps => ({
+  errorMessages: {
     empty: '값을 입력해주세요.',
     number: '숫자만 입력 가능해요.',
     length: '세자리 숫자여야 합니다.',
   },
-};
+  cardCVC,
+  validation: {
+    length: LENGTH,
+  },
+  isNeedValidValue: true,
+});
 
 export default function CardCVC() {
-  const cvcResult = useCVC(useCVCProps);
+  const [cardCVC, setCardCVC] = useState('');
+  const [showError, setShowError] = useState(false);
+
+  const { validationFirstErrorMessage, formattedValue, validationResult } = useCVC(useCVCProps(cardCVC));
+  const { isFilledValue, isValidNumber, isValidLength } = validationResult;
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setCardCVC(e.target.value);
+    setShowError(true);
+  };
+
+  const handleBlur = (e: FocusEvent<HTMLInputElement>) => {
+    setCardCVC(e.target.value);
+    setShowError(true);
+  };
+
+  const className = showError && !(isFilledValue && isValidNumber && isValidLength) ? 'error' : undefined;
 
   return (
     <div>
       <h3>card cvc</h3>
       <input
-        value={cvcResult.cvc}
+        className={className}
+        value={formattedValue}
         type="text"
-        data-testid="cvc-input"
-        maxLength={3}
-        onBlur={cvcResult.handleBlur}
-        onChange={cvcResult.handleChange}
+        data-testid="card-cvc-input"
+        onBlur={handleBlur}
+        onChange={handleChange}
       />
-      <div data-testid="cvc-error">오류 :{cvcResult.errorMessage}</div>
+      <div data-testid="card-cvc-error">{showError && validationFirstErrorMessage}</div>
     </div>
   );
 }
--- hooks/src/lib/useCVC.ts
+++ hooks/src/lib/useCVC.ts
@@ -1,50 +1,76 @@
-import { ChangeEvent, useState, FocusEvent } from 'react';
-import { validateNumber, validateFilledValue, validateLength } from './utils/validators';
-import { ErrorMessage, UseCardModuleProps } from './types';
-import useCardValidation from './useCardValidation';
-interface ValidationErrorMessages {
+import { useState, useEffect } from 'react';
+import { validateNumber, validateFilledValue, validateLength, isValid } from './utils/validators';
+import { UseCardModuleReturn } from './types';
+
+import { INVALID_INPUT_VALUE } from './constants';
+import { sliceText } from './utils/textFormatter';
+
+export interface CardCVCValidationErrorMessages {
   empty: string;
   number: string;
   length: string;
 }
+export interface UseCardCVCProps {
+  cardCVC: string;
+  errorMessages: CardCVCValidationErrorMessages;
+  validation: {
+    length: number;
+  };
+  isNeedValidValue: boolean;
+}
+export interface UseCardCVCResult {
+  isFilledValue: boolean;
+  isValidNumber: boolean;
+  isValidLength: boolean;
+}
 
-export default function useCVC(props: UseCardModuleProps<ValidationErrorMessages>) {
-  const { empty, number, length } = props.validationErrorMessages;
-  const [cvc, setCVC] = useState('');
-  const [errorMessage, setErrorMessage] = useState<ErrorMessage>(null);
+export type UseCardCVCReturn = UseCardModuleReturn<UseCardCVCResult, string>;
 
-  const validateCVCLength = (value: string) => validateLength(value, 3);
+export default function useCVC(props: UseCardCVCProps): UseCardCVCReturn {
+  const { cardCVC, errorMessages, validation, isNeedValidValue } = props;
 
-  const changeEventValidators = [{ test: validateNumber, errorMessage: number }];
-  const blurEventValidators = [
-    { test: validateFilledValue, errorMessage: empty },
-    { test: validateCVCLength, errorMessage: length },
-  ];
-  const totalValidators = [blurEventValidators[0], ...changeEventValidators, blurEventValidators[1]];
+  type ErrorMessageKey = keyof typeof errorMessages;
+  type Error = ErrorMessageKey[] | null;
+  const [error, setError] = useState<Error>(null);
 
-  const { handleValidationChange, handleValidationBlur, handleUpdateValue } = useCardValidation<string>({
-    blurEventValidators,
-    changeEventValidators,
-    totalValidators,
-    setValue: setCVC,
-    setErrorMessage,
-  });
+  const validateCVC = () => {
+    const newError: ErrorMessageKey[] = [];
 
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    handleValidationChange(e.target.value);
+    if (!validateFilledValue(cardCVC)) newError.push('empty');
+    if (!validateNumber(cardCVC)) newError.push('number');
+    if (!validateLength(cardCVC, validation.length)) newError.push('length');
+
+    setError(newError[0] ? newError : null);
   };
 
-  const handleBlur = (e: FocusEvent<HTMLInputElement>) => {
-    handleValidationBlur(e.target.value);
+  /**
+   *  유요한 값만 input에 나타나기를 원하는 경우(isNeedValidValue===true), length에 대한 검사를 제외한 오류가 있을 경우 input의 value를 빈문자열로 변경한다.
+   * @param value input value
+   * @param error 유효성 검사 결과
+   * @returns 변경된 글자
+   */
+  const getFormattedValue = (value: string) => {
+    const slicedText = sliceText(value, validation.length);
+    const isOnlyLengthError = error?.[0] === 'length';
+
+    if (isNeedValidValue) {
+      return error && !isOnlyLengthError ? INVALID_INPUT_VALUE : slicedText;
+    }
+
+    return slicedText;
   };
+
+  useEffect(() => {
+    validateCVC();
+  }, [cardCVC]);
 
   return {
-    cvc,
-    setCVC,
-    isValid: !!errorMessage,
-    errorMessage,
-    handleChange,
-    handleBlur,
-    updateValue: handleUpdateValue,
+    validationFirstErrorMessage: error ? errorMessages[error[0]] : null,
+    validationResult: {
+      isFilledValue: isValid<ErrorMessageKey>('empty', error),
+      isValidNumber: isValid<ErrorMessageKey>('number', error),
+      isValidLength: isValid<ErrorMessageKey>('length', error),
+    },
+    formattedValue: getFormattedValue(cardCVC),
   };
 }
