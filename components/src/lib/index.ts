92eb84df5ce0fdebf6c711482b4e5ac6cb870105
lurgi <wahoo9040@naver.com>
2024-05-04 21:26:22 +0900

    feat: export파일 수정

--- components/src/lib/index.ts
+++ components/src/lib/index.ts
@@ -1,1 +1,5 @@
 export { default as Modal } from "./Modal/Modal";
+export { default as CloseButton } from "./CloseButton/CloseButton";
+export { default as LongButton } from "./LongButton/LongButton";
+export { default as ModalHeader } from "./ModalHeader/ModalHeader";
+export { default as Title } from "./Title/Title";
