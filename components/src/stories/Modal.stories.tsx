f2148eaef1d3693405a02db856685f667a2fba8c
hanyoung <phanyoung@naver.com>
2024-05-04 22:36:06 +0900

    test(Modal.stories): 합성 컴포넌트 형태로 리팩토링된 Modal에 맞게 스토리 수정

--- components/src/stories/Modal.stories.tsx
+++ components/src/stories/Modal.stories.tsx
@@ -1,20 +1,85 @@
-import { Meta, StoryObj } from "@storybook/react";
+import React from "react";
+import { StoryObj } from "@storybook/react";
 import Modal from "../lib/Modal";
+import { useArgs } from "@storybook/client-api";
+import { ModalPosition, ModalTheme } from "../lib/types/modalProps";
 
 const meta = {
   title: "Modal",
   component: Modal,
-} satisfies Meta<typeof Modal>;
+
+  argTypes: {
+    isOpen: {
+      control: {
+        type: "boolean",
+      },
+    },
+    position: {
+      options: ["center", "bottom"],
+      control: {
+        type: "radio",
+      },
+    },
+    buttonTheme: {
+      options: ["dark", "light"],
+      control: {
+        type: "radio",
+      },
+    },
+  },
+};
 
 export default meta;
 
-type Story = StoryObj<typeof Modal>;
+interface ModalStoryProps {
+  isOpen: boolean;
+  position: ModalPosition;
+  title: string;
+  buttonText: string;
+  contentText: string;
+  buttonTheme: ModalTheme;
+}
+type Story = StoryObj<ModalStoryProps>;
 
 export const Default: Story = {
   args: {
-    onClick: () => {},
-    title: "예시 제목",
-    buttonText: "확인 버튼",
+    isOpen: false,
     position: "center",
+    title: "Modal Title",
+    contentText: "This is the content of the modal.",
+    buttonText: "OK",
+    buttonTheme: "dark",
+  },
+
+  render: ({ position, title, contentText, buttonText, buttonTheme }: ModalStoryProps) => {
+    const [args, updateArgs] = useArgs();
+
+    const onOpen = () => {
+      updateArgs({ isOpen: true });
+    };
+    const onClose = () => {
+      updateArgs({ isOpen: false });
+    };
+
+    return (
+      <>
+        <button onClick={onOpen}>Open Modal</button>
+        <Modal isOpen={args.isOpen} onClose={onClose}>
+          <Modal.Dimmer />
+          <Modal.Content position={position}>
+            <header>
+              <h2 style={{ margin: 0 }}>{title}</h2>
+              <Modal.CloseButton />
+            </header>
+            <main style={{ margin: "10px 0" }}>{contentText}</main>
+            <footer>
+              <Modal.Button theme={buttonTheme} onClick={() => alert("modal button clicked!")}>
+                {buttonText}
+              </Modal.Button>
+            </footer>
+          </Modal.Content>
+        </Modal>
+      </>
+    );
   },
 };
