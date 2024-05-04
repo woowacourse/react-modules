1309565cf37fd3162d4fd53765c5299f1dab8e72
ss0526100 <ss0526100@gmail.com>
2024-05-04 19:14:04 +0900

    style: self-closing eslint 적용

--- components/src/lib/Modal.tsx
+++ components/src/lib/Modal.tsx
@@ -1,8 +1,8 @@
-import styled from "@emotion/styled";
-import CloseIcon from "./assets/close-icon.png";
-import { useEffect } from "react";
+import CloseIcon from './assets/close-icon.png';
+import styled from '@emotion/styled';
+import { useEffect } from 'react';
 
-type ModalPosition = "center" | "bottom";
+type ModalPosition = 'center' | 'bottom';
 interface ModalProps extends React.HTMLAttributes<HTMLDivElement> {
   onClose: () => void;
   onConfirm?: () => void;
@@ -19,21 +19,21 @@ export default function Modal({
   buttonText,
   children,
   hasCloseButton = true,
-  position = "center",
+  position = 'center',
 }: ModalProps) {
   useEffect(() => {
     const handleModalKeyDown = (e: KeyboardEvent) => {
-      if (e.key === "Escape") {
+      if (e.key === 'Escape') {
         onClose();
       }
     };
 
-    window.addEventListener("keydown", handleModalKeyDown);
+    window.addEventListener('keydown', handleModalKeyDown);
   }, []);
 
   return (
     <div>
-      <ModalDimmer onClick={onClose}></ModalDimmer>
+      <ModalDimmer onClick={onClose} />
       <ModalContent position={position}>
         <ModalHeader>
           {title && <ModalTitle>{title}</ModalTitle>}
@@ -41,7 +41,9 @@ export default function Modal({
         </ModalHeader>
         {children}
         {buttonText && (
-          <ModalConfirmButton onClick={onConfirm ?? onClose}>{buttonText}</ModalConfirmButton>
+          <ModalConfirmButton onClick={onConfirm ?? onClose}>
+            {buttonText}
+          </ModalConfirmButton>
         )}
       </ModalContent>
     </div>
@@ -49,53 +51,53 @@ export default function Modal({
 }
 
 const ModalDimmer = styled.div({
-  position: "fixed",
+  position: 'fixed',
   top: 0,
   left: 0,
-  width: "100vw",
-  height: "100vh",
-  backgroundColor: "#00000059",
+  width: '100vw',
+  height: '100vh',
+  backgroundColor: '#00000059',
 });
 
 const ModalHeader = styled.div({
-  height: "20px",
-  display: "flex",
-  position: "relative",
-  flexDirection: "row",
-  justifyContent: "space-between",
-  alignItems: "center",
-  width: "100%",
+  height: '20px',
+  display: 'flex',
+  position: 'relative',
+  flexDirection: 'row',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  width: '100%',
 
-  marginBottom: "8px",
+  marginBottom: '8px',
 });
 
 const ModalCloseButton = styled.button({
-  border: "none",
-  backgroundColor: "transparent",
-  cursor: "pointer",
+  border: 'none',
+  backgroundColor: 'transparent',
+  cursor: 'pointer',
   backgroundImage: `url(${CloseIcon})`,
-  backgroundSize: "contain",
-  width: "14px",
-  height: "14px",
-  padding: "5px",
-  marginLeft: "10px",
+  backgroundSize: 'contain',
+  width: '14px',
+  height: '14px',
+  padding: '5px',
+  marginLeft: '10px',
 
-  "&:only-child": {
-    position: "absolute",
-    margin: "0 auto",
-    left: "100%",
+  '&:only-child': {
+    position: 'absolute',
+    margin: '0 auto',
+    left: '100%',
   },
 });
 
 const ModalTitle = styled.p({
-  fontSize: "18px",
+  fontSize: '18px',
   margin: 0,
   fontWeight: 700,
-  "&:only-child": {
-    position: "absolute",
-    margin: "0 auto",
-    left: "50%",
-    transform: "translateX(-50%)",
+  '&:only-child': {
+    position: 'absolute',
+    margin: '0 auto',
+    left: '50%',
+    transform: 'translateX(-50%)',
   },
 });
 
@@ -103,42 +105,44 @@ const MODAL_CONTENT_STYLE: {
   [key in ModalPosition]: React.CSSProperties;
 } = {
   center: {
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
   },
   bottom: {
-    boxSizing: "border-box",
+    boxSizing: 'border-box',
     bottom: 0,
     left: 0,
-    width: "100%",
-    borderBottomLeftRadius: "0",
-    borderBottomRightRadius: "0",
+    width: '100%',
+    borderBottomLeftRadius: '0',
+    borderBottomRightRadius: '0',
   },
 };
 
-const ModalContent = styled.aside<{ position: ModalPosition }>(({ position }) => {
-  return {
-    borderRadius: "8px",
-    position: "fixed",
-    backgroundColor: "white",
-    padding: "24px 32px",
-    ...MODAL_CONTENT_STYLE[position],
-  };
-});
+const ModalContent = styled.aside<{ position: ModalPosition }>(
+  ({ position }) => {
+    return {
+      borderRadius: '8px',
+      position: 'fixed',
+      backgroundColor: 'white',
+      padding: '24px 32px',
+      ...MODAL_CONTENT_STYLE[position],
+    };
+  }
+);
 
 const ModalConfirmButton = styled.button({
-  width: "100%",
-  height: "44px",
-  backgroundColor: "#333333",
+  width: '100%',
+  height: '44px',
+  backgroundColor: '#333333',
   border: 0,
-  borderRadius: "5px",
+  borderRadius: '5px',
 
   fontWeight: 700,
-  fontSize: "15px",
-  lineHeight: "21.72px",
-  alignItems: "center",
-  color: "white",
-  marginTop: "10px",
-  cursor: "pointer",
+  fontSize: '15px',
+  lineHeight: '21.72px',
+  alignItems: 'center',
+  color: 'white',
+  marginTop: '10px',
+  cursor: 'pointer',
 });
