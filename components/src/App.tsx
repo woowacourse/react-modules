6721a45e11143f4902c5d8f1cca9d03c2c93e633
jaeml06 <jaeml0630@gmail.com>
2024-05-05 02:04:38 +0900

    refactor: open 상태 변수명 변경

--- components/src/App.tsx
+++ components/src/App.tsx
@@ -1,37 +1,49 @@
-import React, { useState } from "react";
-import styled from "styled-components";
-import Modal from "./lib/Modal"
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import Modal from './lib/Modal';
 
 function App() {
-  const [open, setOpen] = useState(false);
-  const handleClose=(e : React.MouseEvent) => {
-    console.log(e);
-    setOpen(false);
-    console.log('취소');
-  }
-  const handleConfirm = (e : React.MouseEvent) => {
-    console.log(e)
-    setOpen(false)
-    console.log('확인');
-  }
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const handleClose = (e: React.MouseEvent) => {
+    setIsModalOpen(false);
+  };
+  const handleConfirm = (e: React.MouseEvent) => {
+    setIsModalOpen(false);
+  };
   return (
-    <> 
-    <button onClick={()=>setOpen(true)}></button>
-    { open&& <Modal buttonLayout='column' position="bottom" title="제목입니다." isXButton={true}   closeButtonContent = '닫기'
-  confirmButtonContent='확인' handleConfirm={handleConfirm} handleClose={handleClose}>{<Temp><input></input>dasdfasdfasdfs</Temp>}</Modal>}
+    <>
+      <button onClick={() => setIsModalOpen(true)}></button>
+      {isModalOpen && (
+        <Modal
+          buttonLayout='column'
+          position='bottom'
+          title='제목입니다.'
+          isXButton={true}
+          closeButtonContent='닫기'
+          confirmButtonContent='확인'
+          handleConfirm={handleConfirm}
+          handleClose={handleClose}
+        >
+          {
+            <Temp>
+              <input></input>
+            </Temp>
+          }
+        </Modal>
+      )}
     </>
   );
 }
 
 export const Temp = styled.div`
-height: 50vh;
-background-color: red;
-`
+  height: 50vh;
+  background-color: red;
+`;
 
 export const Wide = styled.div`
-width:500vw;
-  height:500vh;
+  width: 500vw;
+  height: 500vh;
   background-color: red;
-`
+`;
 
 export default App;
