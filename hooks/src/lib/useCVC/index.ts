1e273b0c5aacf73450f4b7d1d6b6d76914e99fba
ss0526100 <ss0526100@gmail.com>
2024-05-05 15:04:55 +0900

    feat(useCVC):Validator배열을 통한 유효성 검증

--- hooks/src/lib/useCVC/index.ts
+++ hooks/src/lib/useCVC/index.ts
@@ -1,28 +1,40 @@
 import REGEXPS from '../constants/regExps';
+import { Validator } from '../type';
 import { useState } from 'react';
-import useValidation from '../useValidation';
 
-const cvcValidators = [
-  (value: string) => {
-    if (value.length !== 3) {
-      return 'CVC는 3자리여야 합니다.';
-    }
+const CVC_LENGTH = 3;
+
+export const CVC_ERROR_MESSAGE = {
+  invalidLength: `CVC는 ${CVC_LENGTH}자리여야 합니다.`,
+  notDigit: 'CVC 값은 숫자만 포함해야 합니다.',
+} as const;
+
+type ErrorMessage = (typeof CVC_ERROR_MESSAGE)[keyof typeof CVC_ERROR_MESSAGE];
+
+const cvcValidators: Validator<string, ErrorMessage>[] = [
+  {
+    checkIsValid: (cvc: string) => cvc.length === CVC_LENGTH,
+    message: CVC_ERROR_MESSAGE.notDigit,
   },
-  (value: string) => {
-    if (!REGEXPS.onlyDigitNumber.test(value)) {
-      return 'CVC 값은 숫자만 포함해야 합니다.';
-    }
+  {
+    checkIsValid: (cvc: string) => REGEXPS.onlyDigitNumber.test(cvc),
+    message: CVC_ERROR_MESSAGE.notDigit,
   },
 ];
 
 export default function useCVC() {
-  const [value, setValue] = useState('');
-  const { errorStatus, validate } = useValidation(cvcValidators);
+  const [cvc, setCVC] = useState('');
 
-  const setCVC = (string: string) => {
-    setValue(string);
-    validate(string);
-  };
+  const errorMessage = cvcValidators.reduce(
+    (message: ErrorMessage | null, validator) => {
+      if (message !== null) return message;
+      if (validator.checkIsValid(cvc)) return message;
+      return validator.message;
+    },
+    null
+  );
 
-  return { cvc: value, setCVC, errorStatus };
+  const isValid = errorMessage === null;
+
+  return { cvc, setCVC, errorMessage, isValid };
 }
