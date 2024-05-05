7b96de58b87ee14ed861b7148b75624b011f4997
ss0526100 <ss0526100@gmail.com>
2024-05-05 15:04:13 +0900

    feat(useCardIssuer):Validator배열을 통한 유효성 검증

--- hooks/src/lib/useCardIssuer/index.ts
+++ hooks/src/lib/useCardIssuer/index.ts
@@ -1,37 +1,48 @@
-import { useState } from "react";
-import useValidation from "../useValidation";
+import { Validator } from '../type';
+import { useState } from 'react';
+
+export default function useCardIssuer() {
+  const [cardIssuer, setCardIssuer] = useState('');
+
+  const errorMessage = cardIssuerValidators.reduce(
+    (message: ErrorMessage | null, validator) => {
+      if (message !== null) return message;
+      if (validator.checkIsValid(cardIssuer)) return message;
+      return validator.message;
+    },
+    null
+  );
+
+  const isValid = errorMessage === null;
+
+  return { cardIssuer, setCardIssuer, errorMessage, isValid };
+}
 
 export const CARD_ISSUERS = [
-  "BC카드",
-  "신한카드",
-  "카카오뱅크",
-  "현대카드",
-  "우리카드",
-  "롯데카드",
-  "하나카드",
-  "국민카드",
+  'BC카드',
+  '신한카드',
+  '카카오뱅크',
+  '현대카드',
+  '우리카드',
+  '롯데카드',
+  '하나카드',
+  '국민카드',
 ] as const;
 
 export type CardIssuer = typeof CARD_ISSUERS;
 
+export const CARD_ISSUER_ERROR_MESSAGE = {
+  notIssuer: '지정된 카드 발행사가 아닙니다.',
+} as const;
+
+type ErrorMessage =
+  (typeof CARD_ISSUER_ERROR_MESSAGE)[keyof typeof CARD_ISSUER_ERROR_MESSAGE];
+
 const cardIssuerSet = new Set<string>(CARD_ISSUERS);
 
-const cardIssuerValidators = [
-  (value: string) => {
-    if (!cardIssuerSet.has(value)) {
-      return "지정된 카드 발행사가 아닙니다.";
-    }
+const cardIssuerValidators: Validator<string, ErrorMessage>[] = [
+  {
+    checkIsValid: (issuer: string) => cardIssuerSet.has(issuer),
+    message: CARD_ISSUER_ERROR_MESSAGE.notIssuer,
   },
 ];
-
-export default function useCardIssuer() {
-  const [value, setValue] = useState("");
-  const { errorStatus, validate } = useValidation(cardIssuerValidators);
-
-  const setCardIssuer = (string: string) => {
-    setValue(string);
-    validate(string);
-  };
-
-  return { cardIssuer: value, setCardIssuer, errorStatus };
-}
--- hooks/src/lib/useCardIssuer/useCardIssuer.test.ts
+++ hooks/src/lib/useCardIssuer/useCardIssuer.test.ts
@@ -1,30 +1,37 @@
-import React from "react";
-import { renderHook } from "@testing-library/react";
-import useCardIssuer, { CARD_ISSUERS } from ".";
+import useCardIssuer, { CARD_ISSUERS } from '.';
 
-describe("useCardIssuer에 대한 테스트 케이스", () => {
-  describe("유효성 검증에 실패하는 경우", () => {
-    test.each(["소파카드", "라이언카드", "BCCard"])(
-      "카드사 옵션이 아닌 값(%s)을 입력한 경우 유효하지 않은 값으로 판단한다.",
-      (value) => {
-        const { result } = renderHook(() => useCardIssuer());
+import React from 'react';
+import { renderHook } from '@testing-library/react';
 
-        React.act(() => result.current.setCardIssuer(value));
+describe('useCardIssuer에 대한 테스트 케이스', () => {
+  const testWrongCase = (issuer: string) => {
+    const { result } = renderHook(() => useCardIssuer());
 
-        expect(result.current.errorStatus.isValid).toBe(false);
-        expect(result.current.errorStatus.errorMessage).not.toBeNull();
-      }
+    React.act(() => result.current.setCardIssuer(issuer));
+
+    expect(result.current.isValid).toBe(false);
+    expect(result.current.errorMessage).not.toBeNull();
+  };
+
+  const testValidCase = (issuer: string) => {
+    const { result } = renderHook(() => useCardIssuer());
+
+    React.act(() => result.current.setCardIssuer(issuer));
+
+    expect(result.current.isValid).toBe(true);
+    expect(result.current.errorMessage).toBeNull();
+  };
+  describe('유효성 검증에 실패하는 경우', () => {
+    test.each(['소파카드', '라이언카드', 'BCCard'])(
+      '카드사 옵션이 아닌 값(%s)을 입력한 경우 유효하지 않은 값으로 판단한다.',
+      testWrongCase
     );
   });
 
-  describe("유효성 검증에 성공하는 경우", () => {
-    test.each(CARD_ISSUERS)("카드사 옵션(%s)을 입력한 경우 유효한 값으로 판단한다.", (value) => {
-      const { result } = renderHook(() => useCardIssuer());
-
-      React.act(() => result.current.setCardIssuer(value));
-
-      expect(result.current.errorStatus.isValid).toBe(true);
-      expect(result.current.errorStatus.errorMessage).toBeNull();
-    });
+  describe('유효성 검증에 성공하는 경우', () => {
+    test.each(CARD_ISSUERS)(
+      '카드사 옵션(%s)을 입력한 경우 유효한 값으로 판단한다.',
+      testValidCase
+    );
   });
 });
