96319f32bc9fbdfb437e5a57f2f448de2faceed1
ss0526100 <ss0526100@gmail.com>
2024-05-05 15:32:35 +0900

    test(useExpiryDate): 검사 내용 변경

--- hooks/src/lib/useExpiryDate/useExpiryDate.test.ts
+++ hooks/src/lib/useExpiryDate/useExpiryDate.test.ts
@@ -17,16 +17,16 @@ describe('useExpiryDate에 대한 테스트 케이스', () => {
     );
   });
 
-  describe('expiryYear 유효성 검증에 실패하는 경우', () => {
+  describe('expiryMonth 유효성 검증에 성공하는 경우', () => {
     test.each(['01', '12'])(
       '월(月)의 범위 내의 값(%s)을 입력한 경우 유효한 값으로 판단한다.',
-      year => {
+      month => {
         const { result } = renderHook(() => useExpiryDate());
 
-        React.act(() => result.current.setExpiryYear(year));
+        React.act(() => result.current.setExpiryMonth(month));
 
-        expect(result.current.isValidExpiryYear).toBe(false);
-        expect(result.current.expiryYearErrorMessage).not.toBeNull();
+        expect(result.current.isValidExpiryMonth).toBe(false);
+        expect(result.current.expiryMonthErrorMessage).not.toBeNull();
       }
     );
   });
