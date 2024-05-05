563667b172ad64c0de67089cfa858187ae5efe57
Largopie <106071687+Largopie@users.noreply.github.com>
2024-05-06 00:08:44 +0900

    test(useCardCVC): 로직에 맞추어 테스트 수정
    Co-authored-by: soosoo22 <lllllh00@naver.com>

--- hooks/src/lib/useCardCVC.test.ts
+++ hooks/src/lib/useCardCVC.test.ts
@@ -3,29 +3,34 @@ import { ChangeEvent, act } from 'react';
 import useCardCVC from './useCardCVC';
 
 describe('useCardCVC 테스트', () => {
-  it('입력값이 3자리가 아닌 경우 에러 상태가 true가 된다.', () => {
-    const userInput = '12';
-    const { result } = renderHook(() => useCardCVC());
+  it('초기값이 정확하게 설정되어야 한다.', () => {
+    const initialValue = '123';
+    const { result } = renderHook(() => useCardCVC(initialValue));
 
-    act(() => {
-      result.current.cardCVC.onChange({
-        target: { value: userInput },
-      } as ChangeEvent<HTMLInputElement>);
-    });
-
-    expect(result.current.cardCVC.error.state).toBeTruthy();
+    expect(result.current.value).toBe(initialValue);
   });
 
-  it('입력값이 숫자가 아닌 경우 입력을 제한한다.', () => {
-    const userInput = 'nakta';
+  it.each([['12'], ['1234']])('입력값이 3자리가 아닌 경우 에러 상태가 true가 된다.', (userInput: string) => {
     const { result } = renderHook(() => useCardCVC());
 
     act(() => {
-      result.current.cardCVC.onChange({
+      result.current.onChange({
         target: { value: userInput },
       } as ChangeEvent<HTMLInputElement>);
     });
 
-    expect(result.current.cardCVC.value).toBe('');
+    expect(result.current.error.state).toBeTruthy();
+  });
+
+  it.each([['nakta'], ['@/[]'], [' '], ['한글입력']])('입력값이 숫자가 아닌 경우 입력을 제한한다.', (userInput: string) => {
+    const { result } = renderHook(() => useCardCVC());
+
+    act(() => {
+      result.current.onChange({
+        target: { value: userInput },
+      } as ChangeEvent<HTMLInputElement>);
+    });
+
+    expect(result.current.value).toBe('');
   });
 });
