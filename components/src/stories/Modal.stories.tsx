af4bd4abea0f6c2e3200c83536949bae008d4943
김진호 <rlawlsgh1227@gmail.com>
2024-05-05 15:45:00 +0900

    fix: 컴포넌트 변경으로 인한 스토리북 변경

--- components/src/stories/Modal.stories.tsx
+++ components/src/stories/Modal.stories.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable storybook/prefer-pascal-case */
 import type { Meta, StoryObj } from '@storybook/react';
-import Modal from '../lib/Modal';
+import { Modal } from '../lib/index';
 import React from 'react';
 
 const meta = {
@@ -14,139 +14,162 @@ type Story = StoryObj<typeof meta>;
 
 export const 모달이_열림_center: Story = {
   args: {
-    isOpen: true,
-    title: '마루',
-    position: 'center',
+    open: true,
+    children: <Modal.Header title="마루" onClose={() => console.log('마루')} />,
+    type: 'dialog',
     onClose: () => console.log('마루'),
   },
 };
 
 export const 모달이_열림_bottom: Story = {
   args: {
-    isOpen: true,
-    title: '마루',
-    position: 'bottom',
+    open: true,
+    children: <Modal.Header title="마루" onClose={() => console.log('마루')} />,
+    type: 'drawer',
     onClose: () => console.log('마루'),
   },
 };
 
 export const 모달이_닫힘: Story = {
   args: {
-    isOpen: false,
-    title: '마루',
-    position: 'center',
+    open: false,
+    children: <Modal.Header title="마루" onClose={() => console.log('마루')} />,
+    type: 'dialog',
     onClose: () => console.log('마루'),
   },
 };
 
 export const 모달_버튼_1개: Story = {
   args: {
-    isOpen: true,
-    title: '마루',
-    position: 'center',
+    open: true,
+    children: (
+      <>
+        <Modal.Header title="마루" onClose={() => console.log('마루')} />
+        <Modal.Footer closeButton={{ hide: true }} />
+      </>
+    ),
+    type: 'dialog',
     onClose: () => console.log('마루'),
-    closeButton: <button style={{ width: '100%' }}>쿠키</button>,
   },
 };
 
 export const 모달_버튼_2개_ROW: Story = {
   args: {
-    isOpen: true,
-    title: '마루',
-    position: 'center',
+    open: true,
+    children: (
+      <>
+        <Modal.Header title="마루" onClose={() => console.log('마루')} />
+        <Modal.Footer buttonPosition="row" />
+      </>
+    ),
+    type: 'dialog',
     onClose: () => console.log('마루'),
-    buttonPosition: 'row',
-    closeButton: <button style={{ width: '100%' }}>쿠키</button>,
-    confirmButton: <button style={{ width: '100%' }}>마루</button>,
   },
 };
 
 export const 모달_버튼_2개_ROW_REVERSE: Story = {
   args: {
-    isOpen: true,
-    title: '마루',
-    position: 'center',
+    open: true,
+    children: (
+      <>
+        <Modal.Header title="마루" onClose={() => console.log('마루')} />
+        <Modal.Footer buttonPosition="row-reverse" />
+      </>
+    ),
+    type: 'dialog',
     onClose: () => console.log('마루'),
-    buttonPosition: 'row-reverse',
-    closeButton: <button style={{ width: '100%' }}>쿠키</button>,
-    confirmButton: <button style={{ width: '100%' }}>마루</button>,
   },
 };
 
 export const 모달_버튼_2개_COLUMN: Story = {
   args: {
-    isOpen: true,
-    title: '마루',
-    position: 'center',
+    open: true,
+    children: (
+      <>
+        <Modal.Header title="마루" onClose={() => console.log('마루')} />
+        <Modal.Footer buttonPosition="column" />
+      </>
+    ),
+    type: 'dialog',
     onClose: () => console.log('마루'),
-    buttonPosition: 'column',
-    closeButton: <button style={{ width: '100%' }}>쿠키</button>,
-    confirmButton: <button style={{ width: '100%' }}>마루</button>,
   },
 };
 
 export const 모달_버튼_2개_COLUMN_REVERSE: Story = {
   args: {
-    isOpen: true,
-    title: '마루',
-    position: 'center',
+    open: true,
+    children: (
+      <>
+        <Modal.Header title="마루" onClose={() => console.log('마루')} />
+        <Modal.Footer buttonPosition="column-reverse" />
+      </>
+    ),
+    type: 'dialog',
     onClose: () => console.log('마루'),
-    buttonPosition: 'column-reverse',
-    closeButton: <button style={{ width: '100%' }}>쿠키</button>,
-    confirmButton: <button style={{ width: '100%' }}>마루</button>,
   },
 };
 
 export const 모달_닫기_아이콘_숨김: Story = {
   args: {
-    isOpen: true,
-    title: '마루',
-    position: 'center',
+    open: true,
+    children: (
+      <>
+        <Modal.Header title="마루" onClose={() => console.log('마루')} hideCloseIcon />
+        <Modal.Footer buttonPosition="column-reverse" />
+      </>
+    ),
+    type: 'dialog',
     onClose: () => console.log('마루'),
-    hideCloseIcon: true,
-    buttonPosition: 'row-reverse',
-    closeButton: <button style={{ width: '100%' }}>쿠키</button>,
-    confirmButton: <button style={{ width: '100%' }}>마루</button>,
   },
 };
 
 export const 모달_닫기_아이콘_커스텀: Story = {
   args: {
-    isOpen: true,
-    title: '마루',
-    position: 'center',
+    open: true,
+    children: (
+      <>
+        <Modal.Header
+          title="마루"
+          onClose={() => console.log('마루')}
+          customCloseIcon={
+            'https://github.com/jinhokim98/react-payments/blob/step2/src/assets/image/failure.png?raw=true'
+          }
+        />
+        <Modal.Footer buttonPosition="column-reverse" />
+      </>
+    ),
+    type: 'dialog',
     onClose: () => console.log('마루'),
-    customCloseIcon:
-      'https://github.com/jinhokim98/react-payments/blob/step2/src/assets/image/failure.png?raw=true',
-    buttonPosition: 'row-reverse',
-    closeButton: <button style={{ width: '100%' }}>쿠키</button>,
-    confirmButton: <button style={{ width: '100%' }}>마루</button>,
   },
 };
 
 export const 모달_닫기_아이콘_커스텀_오류: Story = {
   args: {
-    isOpen: true,
-    title: '마루',
-    position: 'center',
+    open: true,
+    children: (
+      <>
+        <Modal.Header title="마루" onClose={() => console.log('마루')} customCloseIcon={'이미지'} />
+        <Modal.Footer buttonPosition="column-reverse" />
+      </>
+    ),
+    type: 'dialog',
     onClose: () => console.log('마루'),
-    customCloseIcon: '이미지',
-    buttonPosition: 'row-reverse',
-    closeButton: <button style={{ width: '100%' }}>쿠키</button>,
-    confirmButton: <button style={{ width: '100%' }}>마루</button>,
   },
 };
 
 export const 모달_컨텐츠: Story = {
   args: {
-    isOpen: true,
-    title: '마루',
-    position: 'center',
+    open: true,
+    children: (
+      <>
+        <Modal.Header title="마루" onClose={() => console.log('마루')} />
+        <Modal.Content>
+          <div>안녕 마루와 쿠키의 나이스 모달</div>
+        </Modal.Content>
+        <Modal.Footer buttonPosition="column-reverse" />
+      </>
+    ),
+    type: 'dialog',
     onClose: () => console.log('마루'),
-    content: <div>안녕 나는 마루랑 쿠키야</div>,
-    customCloseIcon: '이미지',
-    buttonPosition: 'row-reverse',
-    closeButton: <button style={{ width: '100%' }}>쿠키</button>,
-    confirmButton: <button style={{ width: '100%' }}>마루</button>,
   },
 };
