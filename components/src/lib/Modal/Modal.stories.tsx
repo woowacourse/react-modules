287c27fc8e2e823a5abc5c17a96143470673c732
손진영 <sm099@naver.com>
2024-05-06 01:23:22 +0900

    test(Modal): 모달 Story 추가
    1. overflow 상태 제거
    
    2. modal button만 있는 case 추가
    
    3. modal close button만 있는 case 추가
    
    4. modal close button 및 modal button에 대한 case 추가
    
    5. modal footer의 flex direction이 column인 case 추가

--- components/src/lib/Modal/Modal.stories.tsx
+++ components/src/lib/Modal/Modal.stories.tsx
@@ -82,12 +82,12 @@ export const Bottom: Story = {
   },
 };
 
-export const OverflowContentModal: Story = {
+export const AcceptTermsModalWithoutCloseButton: Story = {
   args: Default.args,
   parameters: {
     docs: {
       description: {
-        story: 'modal content가 modal viewport를 초과하는 case',
+        story: 'footer 내 button만 존재하는 case',
       },
     },
   },
@@ -95,25 +95,30 @@ export const OverflowContentModal: Story = {
     return (
       <div className="app">
         <Modal {...args}>
-          <Modal.ModalHeader title="카드사 선택"></Modal.ModalHeader>
-          <Modal.ModalContent>
-            <div style={{ margin: '12px 0px', backgroundColor: 'slategrey', width: '1000px', height: '300px' }}>
-              Box
-            </div>
+          <Modal.ModalHeader title="약관에 동의해 주세요" />
+          <Modal.ModalContent style={{ margin: '12px 0px 0px 0px' }}>
+            <div style={{ height: '300px', width: '100%', backgroundColor: 'black' }} />
           </Modal.ModalContent>
-          <Modal.ModalFooter></Modal.ModalFooter>
+          <Modal.ModalFooter direction="column">
+            <Modal.ModalButton onClick={args.onToggle} color="primary">
+              동의하고 저장하기
+            </Modal.ModalButton>
+            <Modal.ModalButton onClick={args.onToggle} color="secondary">
+              닫기
+            </Modal.ModalButton>
+          </Modal.ModalFooter>
         </Modal>
       </div>
     );
   },
 };
 
-export const OverflowFooterModal: Story = {
+export const AcceptTermsModalWithCloseButton: Story = {
   args: Default.args,
   parameters: {
     docs: {
       description: {
-        story: 'modal footer가 modal viewport를 초과하는 case',
+        story: 'header 내 close button과 footer 내 button이 동시에 있는 case',
       },
     },
   },
@@ -121,12 +126,72 @@ export const OverflowFooterModal: Story = {
     return (
       <div className="app">
         <Modal {...args}>
-          <Modal.ModalHeader title="카드사 선택"></Modal.ModalHeader>
-          <Modal.ModalContent>
-            <div style={{ backgroundColor: 'red', width: '100%', height: '30px' }}>Content</div>
+          <Modal.ModalHeader title="약관에 동의해 주세요">
+            <Modal.ModalCloseButton onClick={args.onToggle} />
+          </Modal.ModalHeader>
+          <Modal.ModalContent style={{ margin: '12px 0px' }}>
+            <div style={{ height: '300px', width: '100%', backgroundColor: 'black' }} />
           </Modal.ModalContent>
-          <Modal.ModalFooter>
-            <div style={{ backgroundColor: 'slategrey', width: '1000px', height: '300px' }}>Footer</div>
+          <Modal.ModalFooter direction="row">
+            <Modal.ModalButton onClick={args.onToggle} color="primary">
+              동의하고 저장하기
+            </Modal.ModalButton>
+          </Modal.ModalFooter>
+        </Modal>
+      </div>
+    );
+  },
+};
+
+export const CardSelectModal: Story = {
+  args: Default.args,
+  parameters: {
+    docs: {
+      description: {
+        story: 'header 내 close button만 존재하는 case',
+      },
+    },
+  },
+  render: ({ ...args }) => {
+    return (
+      <div className="app">
+        <Modal {...args}>
+          <Modal.ModalHeader title="약관에 동의해 주세요">
+            <Modal.ModalCloseButton onClick={args.onToggle} />
+          </Modal.ModalHeader>
+          <Modal.ModalContent style={{ height: '150px', margin: '12px 0px' }}>
+            <div style={{ width: '100%', height: '100%', backgroundColor: 'black' }} />
+          </Modal.ModalContent>
+        </Modal>
+      </div>
+    );
+  },
+};
+
+export const ModalWithDirectionRow: Story = {
+  args: Default.args,
+  parameters: {
+    docs: {
+      description: {
+        story: 'modal footer의 flex direction이 row인 경우',
+      },
+    },
+  },
+  render: ({ ...args }) => {
+    return (
+      <div className="app">
+        <Modal {...args}>
+          <Modal.ModalHeader title="약관에 동의해 주세요" />
+          <Modal.ModalContent style={{ margin: '12px 0px' }}>
+            <div style={{ height: '300px', width: '100%', backgroundColor: 'black' }} />
+          </Modal.ModalContent>
+          <Modal.ModalFooter direction="row">
+            <Modal.ModalButton onClick={args.onToggle} color="primary">
+              동의하고 저장하기
+            </Modal.ModalButton>
+            <Modal.ModalButton onClick={args.onToggle} color="secondary">
+              닫기
+            </Modal.ModalButton>
           </Modal.ModalFooter>
         </Modal>
       </div>
