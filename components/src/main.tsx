d1c9927b97d4852789fd3665e0c8bbb8c503509b
simhorim <hori5020@gmail.com>
2024-05-06 05:23:53 +0900

    design(theme): themeProvider 테마 추가

--- components/src/main.tsx
+++ components/src/main.tsx
@@ -3,10 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import GlobalStyle from "./GlobalStyle.ts";
+import { ThemeProvider } from "styled-components";
+import theme from "./theme.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <GlobalStyle />
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
