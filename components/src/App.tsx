d3f621404f5e284d2d74ef9caea5d26cc6a2d8a5
bmj0053 <bmj05378@gmail.com>
2024-05-05 22:35:46 +0900

    design: App 스타일 설정 및 모달 오픈 버튼 추가

--- components/src/App.tsx
+++ components/src/App.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import './reset.css';
-import { Modal, useModal } from 'woowacourse-react-modal-component';
 
+import Modal from './lib/Modal';
+import useModal from './lib/hooks/useModal';
 import CardCompanySelector from './components/CardCompanySelector';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
   return (
     <>
       <h1>Component Modules</h1>
+      <button onClick={() => toggleModal()}>모달 버튼</button>
       {isOpen && (
         <Modal
           toggleModal={toggleModal}
