33b5e6a0dedba7d33ee2bdf9ee4ee437198d94c8
ss0526100 <ss0526100@gmail.com>
2024-05-05 15:06:27 +0900

    feat(usePasswordPrefix):Validator배열을 통한 유효성 검증

--- hooks/src/lib/usePasswordPrefix/index.ts
+++ hooks/src/lib/usePasswordPrefix/index.ts
@@ -1,29 +1,39 @@
 import REGEXPS from '../constants/regExps';
+import { Validator } from '../type';
 import { useState } from 'react';
-import useValidation from '../useValidation';
 
 const PASSWORD_PREFIX_LENGTH = 2;
-const passwordPrefixValidators = [
-  (value: string) => {
-    if (value.length !== PASSWORD_PREFIX_LENGTH) {
-      return `비밀번호 앞자리는 ${PASSWORD_PREFIX_LENGTH}자리여야 합니다.`;
-    }
+
+export const PASSWORD_PREFIX_ERROR_MESSAGE = {
+  invalidLength: `비밀번호 앞자리는 ${PASSWORD_PREFIX_LENGTH}자리여야 합니다.`,
+  notDigit: '비밀번호는 숫자만 포함해야 합니다.',
+} as const;
+
+type ErrorMessage =
+  (typeof PASSWORD_PREFIX_ERROR_MESSAGE)[keyof typeof PASSWORD_PREFIX_ERROR_MESSAGE];
+
+const passwordPrefixValidators: Validator<string, ErrorMessage>[] = [
+  {
+    checkIsValid: (prefix: string) => prefix.length === PASSWORD_PREFIX_LENGTH,
+    message: PASSWORD_PREFIX_ERROR_MESSAGE.invalidLength,
   },
-  (value: string) => {
-    if (!REGEXPS.onlyDigitNumber.test(value)) {
-      return '비밀번호는 숫자만 포함해야 합니다.';
-    }
+  {
+    checkIsValid: (prefix: string) => REGEXPS.onlyDigitNumber.test(prefix),
+    message: PASSWORD_PREFIX_ERROR_MESSAGE.notDigit,
   },
 ];
 
 export default function usePasswordPrefix() {
-  const [value, setValue] = useState('');
-  const { errorStatus, validate } = useValidation(passwordPrefixValidators);
+  const [passwordPrefix, setPasswordPrefix] = useState('');
+  const errorMessage = passwordPrefixValidators.reduce(
+    (message: ErrorMessage | null, validator) => {
+      if (message !== null) return message;
+      if (validator.checkIsValid(passwordPrefix)) return message;
+      return validator.message;
+    },
+    null
+  );
 
-  const setPasswordPrefix = (string: string) => {
-    setValue(string);
-    validate(string);
-  };
-
-  return { passwordPrefix: value, setPasswordPrefix, errorStatus };
+  const isValid = errorMessage === null;
+  return { passwordPrefix, setPasswordPrefix, errorMessage, isValid };
 }
