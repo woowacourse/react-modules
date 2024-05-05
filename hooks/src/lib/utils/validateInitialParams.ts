db413ecd40e52a4030dd66a650db6b8162a64089
Seongjin Hong <hongseongjin.to@gmail.com>
2024-05-06 02:29:32 +0900

    feat: 커스텀 훅의 파라미터 설정값에 대한 에러 체크 함수 추가

---
+++ hooks/src/lib/utils/validateInitialParams.ts
@@ -0,0 +1,51 @@
+import Validation from './Validation';
+
+export function validateAllowedLength({
+  allowedLength,
+  minLength,
+  maxLength,
+  errorMessage,
+}: {
+  allowedLength: number;
+  minLength: number;
+  maxLength: number;
+  errorMessage: string;
+}) {
+  if (
+    !Validation.isNumberInRange({
+      min: minLength,
+      max: maxLength,
+      value: allowedLength,
+    })
+  ) {
+    throw new Error(errorMessage);
+  }
+}
+
+export function validateAllowedYearLength({
+  allowedYearLength,
+  allowedRange,
+  errorMessage,
+}: {
+  allowedYearLength: number;
+  allowedRange: number[];
+  errorMessage: string;
+}) {
+  if (!allowedRange.includes(allowedYearLength)) {
+    throw new Error(errorMessage);
+  }
+}
+
+export function validateTotalCardNumbersLength({
+  format,
+  totalLength,
+  errorMessage,
+}: {
+  format: number[];
+  totalLength: number;
+  errorMessage: string;
+}) {
+  if (format.reduce((totalLength, length) => totalLength + length, 0) > totalLength) {
+    throw new Error(errorMessage);
+  }
+}
