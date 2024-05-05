c4588d3b3fa9812765e353021c501a3d6a98e0c0
Largopie <106071687+Largopie@users.noreply.github.com>
2024-05-05 23:47:20 +0900

    test(useCardCompany): 로직에 맞추어 테스트 수정
    Co-authored-by: soosoo22 <lllllh00@naver.com>

--- hooks/src/lib/useCardCompany.test.ts
+++ hooks/src/lib/useCardCompany.test.ts
@@ -2,32 +2,24 @@ import { renderHook } from '@testing-library/react';
 import { ChangeEvent, act } from 'react';
 import useCardCompany from './useCardCompany';
 describe('useCardCompany 테스트', () => {
-  const CARD_COMPANY_ERROR_MESSAGE = '카드사를 선택해주세요.';
+  it('초기값이 정확하게 설정되어야 한다.', () => {
+    const initialValue = 'BC카드';
+    const { result } = renderHook(() => useCardCompany(initialValue));
 
-  it(`카드사를 선택하지 않은 경우 에러 상태가 true가 된다.`, () => {
-    const userInput = '';
-    const { result } = renderHook(() => useCardCompany());
-
-    act(() => {
-      result.current.cardCompany.onChange({
-        target: { value: userInput },
-      } as ChangeEvent<HTMLInputElement>);
-    });
-
-    expect(result.current.cardCompany.error.state).toBeTruthy();
+    expect(result.current.value).toBe(initialValue);
   });
 
-  it(`카드사를 선택하지 않은 경우 ${CARD_COMPANY_ERROR_MESSAGE} 에러 메시지를 반환한다.`, () => {
+  it('카드사를 선택하지 않은 경우 에러 상태가 true가 된다.', () => {
     const userInput = '';
     const { result } = renderHook(() => useCardCompany());
 
     act(() => {
-      result.current.cardCompany.onChange({
+      result.current.onChange({
         target: { value: userInput },
       } as ChangeEvent<HTMLInputElement>);
     });
 
-    expect(result.current.cardCompany.error.message).toBe(CARD_COMPANY_ERROR_MESSAGE);
+    expect(result.current.error.state).toBeTruthy();
   });
 
   it('올바른 값이 입력되었을 때 유효성 상태가 true가 된다.', () => {
@@ -35,11 +27,11 @@ describe('useCardCompany 테스트', () => {
     const { result } = renderHook(() => useCardCompany());
 
     act(() => {
-      result.current.cardCompany.onChange({
+      result.current.onChange({
         target: { value: userInput },
       } as ChangeEvent<HTMLInputElement>);
     });
 
-    expect(result.current.isCardCompanyValid).toBeTruthy();
+    expect(result.current.isValid).toBeTruthy();
   });
 });
