8719620e30a78894d23e3c33a56839dfeb983147
김진호 <rlawlsgh1227@gmail.com>
2024-05-05 11:10:43 +0900

    chore: eslint prettier extends 배열 제일 마지막으로 이동

--- hooks/.eslintrc.cjs
+++ hooks/.eslintrc.cjs
@@ -2,11 +2,11 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    "plugin:prettier/recommended",
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:react-hooks/recommended",
     "plugin:storybook/recommended",
+    "plugin:prettier/recommended",
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parser: "@typescript-eslint/parser",
