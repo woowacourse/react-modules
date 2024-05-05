d4accd1f7c33d5209430069b0e964591e1a71c7f
ss0526100 <ss0526100@gmail.com>
2024-05-05 15:06:15 +0900

    feat(useExpiryDate):Validator배열을 통한 유효성 검증

--- hooks/src/lib/useExpiryDate/index.ts
+++ hooks/src/lib/useExpiryDate/index.ts
@@ -1,64 +1,122 @@
+import { useMemo, useState } from 'react';
+
 import REGEXPS from '../constants/regExps';
-import { useState } from 'react';
-import useValidation from '../useValidation';
-
-const EXPIRY_MONTH_LENGTH = 2;
-const expiryMonthValidators = [
-  (value: string) => {
-    if (value.length !== EXPIRY_MONTH_LENGTH) {
-      return '유효기간 월(月)은 2자리로 입력해 주세요.';
-    }
-  },
-  (value: string) => {
-    const isValidMonth = REGEXPS.dateMM.test(value);
-    if (!isValidMonth) {
-      return '유효기간 월(月)은 01월부터 12월 중 하나로 입력해 주세요.';
-    }
-  },
-];
-
-const EXPIRY_YEAR_LENGTH = 2;
-const MIN_YEAR = 24;
-const MAX_YEAR = 40;
-const expiryYearValidators = [
-  (value: string) => {
-    if (value.length !== EXPIRY_YEAR_LENGTH) {
-      return '유효기간 년도(年)는 2자리로 입력해 주세요';
-    }
-  },
-  (value: string) => {
-    const isValidYear = Number(value) >= MIN_YEAR && Number(value) <= MAX_YEAR;
-    if (!isValidYear) {
-      return '유효기간 년도(年)는 24년도부터 40년도 중 하나로 입력해 주세요';
-    }
-  },
-];
+import { Validator } from '../type';
 
 export default function useExpiryDate() {
-  const [expiryMonthValue, setExpiryMonthValue] = useState('');
-  const { errorStatus: expiryMonthErrorStatus, validate: validateExpiryMonth } =
-    useValidation<string>(expiryMonthValidators);
+  const [expiryMonth, setExpiryMonth] = useState('');
 
-  const [expiryYearValue, setExpiryYearValue] = useState('');
-  const { errorStatus: expiryYearErrorStatus, validate: validateExpiryYear } =
-    useValidation<string>(expiryYearValidators);
+  const [expiryYear, setExpiryYear] = useState('');
 
-  const setExpiryMonth = (string: string) => {
-    setExpiryMonthValue(string);
-    validateExpiryMonth(string);
-  };
+  const expiryMonthErrorMessage = useMemo(() => {
+    expiryMonthValidators.reduce(
+      (message: MonthErrorMessage | null, validator) => {
+        if (message !== null) return message;
+        if (validator.checkIsValid(expiryMonth)) return message;
+        return validator.message;
+      },
+      null
+    );
+  }, [expiryMonth]);
+  const isValidExpiryMonth = expiryMonthErrorMessage === null;
 
-  const setExpiryYear = (string: string) => {
-    setExpiryYearValue(string);
-    validateExpiryYear(string);
-  };
+  const expiryYearErrorMessage = useMemo(() => {
+    expiryYearValidators.reduce(
+      (message: YearErrorMessage | null, validator) => {
+        if (message !== null) return message;
+        if (validator.checkIsValid(expiryMonth)) return message;
+        return validator.message;
+      },
+      null
+    );
+  }, [expiryMonth]);
+  const isValidExpiryYear = expiryYearErrorMessage === null;
+
+  const expiryDateErrorMessage = expiryDateValidators.reduce(
+    (message: DateErrorMessage | null, validator) => {
+      if (message !== null) return message;
+      if (validator.checkIsValid([expiryMonth, expiryMonth])) return message;
+      return validator.message;
+    },
+    null
+  );
+  const isValidExpiryDate = expiryDateErrorMessage === null;
 
   return {
-    expiryMonth: expiryMonthValue,
+    expiryMonth,
+    expiryYear,
     setExpiryMonth,
-    expiryMonthErrorStatus,
-    expiryYear: expiryYearValue,
     setExpiryYear,
-    expiryYearErrorStatus,
+    expiryMonthErrorMessage,
+    expiryYearErrorMessage,
+    expiryDateErrorMessage,
+    isValidExpiryMonth,
+    isValidExpiryYear,
+    isValidExpiryDate,
   };
 }
+
+const EXPIRY_MONTH_LENGTH = 2;
+const EXPIRY_YEAR_LENGTH = 2;
+
+export const EXPIRY_MONTH_ERROR_MESSAGE = {
+  notDigit: '유효기간 월(月)은 숫자만 포함해야 합니다.',
+  invalidLength: `유효기간 월(月)은 ${EXPIRY_MONTH_LENGTH}자리로 입력해 주세요.`,
+  notDateMM: '유효기간 월(月)은 01월부터 12월 중 하나로 입력해 주세요.',
+} as const;
+
+export const EXPIRY_YEAR_ERROR_MESSAGE = {
+  notDigit: '유효기간 년도(年)는 숫자만 포함해야 합니다.',
+  invalidLength: `유효기간 년도(年)는 ${EXPIRY_YEAR_LENGTH}자리로 입력해 주세요.`,
+} as const;
+
+export const EXPIRY_DATE_ERROR_MESSAGE = {
+  expiredDate: `만료된 유효기간 입니다.`,
+} as const;
+
+type MonthErrorMessage =
+  (typeof EXPIRY_MONTH_ERROR_MESSAGE)[keyof typeof EXPIRY_MONTH_ERROR_MESSAGE];
+
+type YearErrorMessage =
+  (typeof EXPIRY_YEAR_ERROR_MESSAGE)[keyof typeof EXPIRY_YEAR_ERROR_MESSAGE];
+
+type DateErrorMessage =
+  (typeof EXPIRY_DATE_ERROR_MESSAGE)[keyof typeof EXPIRY_DATE_ERROR_MESSAGE];
+const expiryMonthValidators: Validator<string, MonthErrorMessage>[] = [
+  {
+    checkIsValid: (month: string) => REGEXPS.onlyDigitNumber.test(month),
+    message: EXPIRY_MONTH_ERROR_MESSAGE.notDigit,
+  },
+  {
+    checkIsValid: (month: string) => month.length !== EXPIRY_MONTH_LENGTH,
+    message: EXPIRY_MONTH_ERROR_MESSAGE.invalidLength,
+  },
+  {
+    checkIsValid: (month: string) => REGEXPS.dateMM.test(month),
+    message: EXPIRY_MONTH_ERROR_MESSAGE.notDateMM,
+  },
+];
+
+const expiryYearValidators: Validator<string, YearErrorMessage>[] = [
+  {
+    checkIsValid: (year: string) => REGEXPS.onlyDigitNumber.test(year),
+    message: EXPIRY_YEAR_ERROR_MESSAGE.notDigit,
+  },
+  {
+    checkIsValid: (year: string) => year.length === EXPIRY_YEAR_LENGTH,
+    message: EXPIRY_YEAR_ERROR_MESSAGE.notDigit,
+  },
+];
+
+const expiryDateValidators: Validator<[string, string], DateErrorMessage>[] = [
+  {
+    checkIsValid: ([month, year]) => {
+      const inputMonth = Number(month) - 1;
+      const inputYear = Number(year) + 2000;
+      const inputDate = new Date(inputYear, inputMonth);
+
+      return inputDate < new Date();
+    },
+    message: EXPIRY_DATE_ERROR_MESSAGE.expiredDate,
+  },
+];
--- hooks/src/lib/useExpiryDate/useExpiryDate.test.ts
+++ hooks/src/lib/useExpiryDate/useExpiryDate.test.ts
@@ -1,60 +1,32 @@
-import React from "react";
-import { renderHook } from "@testing-library/react";
-import useExpiryDate from ".";
+import React from 'react';
+import { renderHook } from '@testing-library/react';
+import useExpiryDate from '.';
 
-describe("useExpiryDate에 대한 테스트 케이스", () => {
-  describe("expiryMonth 유효성 검증에 실패하는 경우", () => {
-    test.each(["00", "13"])(
-      "월(月)의 최소, 최대 범위를 벗어난 값(%s)을 입력한 경우 유효하지 않은 값으로 판단한다.",
-      (value) => {
+describe('useExpiryDate에 대한 테스트 케이스', () => {
+  describe('expiryMonth 유효성 검증에 실패하는 경우', () => {
+    test.each(['00', '13'])(
+      '월(月)의 최소, 최대 범위를 벗어난 값(%s)을 입력한 경우 유효하지 않은 값으로 판단한다.',
+      month => {
         const { result } = renderHook(() => useExpiryDate());
 
-        React.act(() => result.current.setExpiryMonth(value));
+        React.act(() => result.current.setExpiryMonth(month));
 
-        expect(result.current.expiryMonthErrorStatus.isValid).toBe(false);
-        expect(result.current.expiryMonthErrorStatus.errorMessage).not.toBeNull();
+        expect(result.current.isValidExpiryMonth).toBe(false);
+        expect(result.current.expiryMonthErrorMessage).not.toBeNull();
       }
     );
   });
 
-  describe("expiryYear 유효성 검증에 실패하는 경우", () => {
-    test.each(["23", "41"])(
-      "년(年)의 최소, 최대 범위를 벗어난 값(%s)을 입력한 경우 유효하지 않은 값으로 판단한다.",
-      (value) => {
+  describe('expiryYear 유효성 검증에 실패하는 경우', () => {
+    test.each(['01', '12'])(
+      '월(月)의 범위 내의 값(%s)을 입력한 경우 유효한 값으로 판단한다.',
+      year => {
         const { result } = renderHook(() => useExpiryDate());
 
-        React.act(() => result.current.setExpiryYear(value));
+        React.act(() => result.current.setExpiryYear(year));
 
-        expect(result.current.expiryYearErrorStatus.isValid).toBe(false);
-        expect(result.current.expiryYearErrorStatus.errorMessage).not.toBeNull();
-      }
-    );
-  });
-
-  describe("expiryMonth 유효성 검증에 성공하는 경우", () => {
-    test.each(["01", "12"])(
-      "년(年)의 범위 내의 값(%s)을 입력한 경우 유효한 값으로 판단한다.",
-      (value) => {
-        const { result } = renderHook(() => useExpiryDate());
-
-        React.act(() => result.current.setExpiryMonth(value));
-
-        expect(result.current.expiryMonthErrorStatus.isValid).toBe(true);
-        expect(result.current.expiryMonthErrorStatus.errorMessage).toBeNull();
-      }
-    );
-  });
-
-  describe("expiryYear 유효성 검증에 성공하는 경우", () => {
-    test.each(["24", "40"])(
-      "년(年)의 범위 내의 값(%s)을 입력한 경우 유효한 값으로 판단한다.",
-      (value) => {
-        const { result } = renderHook(() => useExpiryDate());
-
-        React.act(() => result.current.setExpiryYear(value));
-
-        expect(result.current.expiryYearErrorStatus.isValid).toBe(true);
-        expect(result.current.expiryYearErrorStatus.errorMessage).toBeNull();
+        expect(result.current.isValidExpiryYear).toBe(false);
+        expect(result.current.expiryYearErrorMessage).not.toBeNull();
       }
     );
   });
