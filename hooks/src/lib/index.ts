55a8fba3eff16f1e045acf262dd936aeef896b05
Lee sang Yeop <mma7710@naver.com>
2024-05-05 13:40:40 +0900

    feat: 라이브러리 사용자가 useInput, useInputs 도 import 해서 사용할 수 있도록 추가

--- hooks/src/lib/index.ts
+++ hooks/src/lib/index.ts
@@ -4,3 +4,5 @@ export { default as useCVC } from "./useCVC/useCVC";
 export { default as useExpiryDate } from "./useExpiryDate/useExpiryDate";
 export { default as usePassword } from "./usePassword/usePassword";
 export { default as useSelectCardType } from "./useSelectCardType/useSelectCardType";
+export { default as useInput } from "./common/useInput";
+export { default as useInputs } from "./common/useInputs";
