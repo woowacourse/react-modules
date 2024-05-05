5edb65d432306c1a6b0d126f1d21e9a88faf13ad
손진영 <sm099@naver.com>
2024-05-06 01:18:37 +0900

    feat: modal close button 및 button 추가
    1. close button을 위한 image 추가
    
    2. modal color에 대한 객체 추가
    
    3. modal close button 및 modal button에 대한 스타일 추가
    
    4. modal footer에 대한 스타일 수정
    
    5. ButtonColor 및 direction에 대한 타입 추가
    
    6. png 파일에 대한 declare 추가
    
    7. index.css 내 root 스타일 추가
    
    8. clsx와 비슷한 유틸 함수 추가(extendClassNames)
    
    9. modal close button 및 modal button 추가

--- components/src/lib/Modal/Modal.tsx
+++ components/src/lib/Modal/Modal.tsx
@@ -1,60 +1,97 @@
-import { MODAL_POSITION_MAP } from './Modal.constant';
-import { ModalPosition } from './Modal.type';
+import { BUTTON_COLOR_MAP, MODAL_POSITION_MAP } from './Modal.constant';
+import type { ButtonColorType, FlexDirection, ModalPosition } from './Modal.type';
 
 import useModalControl from './hooks/useModalControl';
 
 import { convertPascalCase } from '../../utils/string';
+import { extendClassNames } from '../../utils/extendClassNames';
+import { CloseImage } from '../assets';
 
 import styles from './Modal.module.css';
 
 interface ModalProps {
   isOpen: boolean;
-  onToggle: () => void;
   position: ModalPosition;
+  onToggle: () => void;
 }
 
-type ModalHeaderType = React.FC<React.PropsWithChildren<{ title: string }>>;
-type ModalContentType = React.FC<React.PropsWithChildren>;
-type ModalFooterType = React.FC<React.PropsWithChildren>;
+type ModalHeaderType = React.FC<React.PropsWithChildren<{ title: string } & React.HTMLAttributes<HTMLElement>>>;
+type ModalContentType = React.FC<React.PropsWithChildren<React.HTMLAttributes<HTMLElement>>>;
+type ModalFooterType = React.FC<
+  React.PropsWithChildren<React.HTMLAttributes<HTMLElement> & { direction?: FlexDirection }>
+>;
+type ModalCloseButtonType = React.FC<React.HTMLAttributes<HTMLButtonElement>>;
+type ModalButtonType = React.FC<React.HTMLAttributes<HTMLButtonElement> & { color: ButtonColorType }>;
 
 const Modal: React.FC<React.PropsWithChildren<ModalProps>> & {
   ModalHeader: ModalHeaderType;
   ModalContent: ModalContentType;
   ModalFooter: ModalFooterType;
-} = ({ children, isOpen, onToggle, position = 'center' }) => {
-  const modalContainerStyle = position === 'bottom' ? `modalContainer${convertPascalCase(position)}` : '';
+  ModalCloseButton: ModalCloseButtonType;
+  ModalButton: ModalButtonType;
+} = ({ children, isOpen, position = 'center', onToggle }) => {
+  const modalContainerPositionClassName = position === 'bottom' ? `modalContainer${convertPascalCase(position)}` : '';
 
   useModalControl(isOpen, onToggle);
 
   return (
     isOpen && (
-      <div className={`${styles.modal} ${styles[MODAL_POSITION_MAP[position]]}`}>
+      <div className={extendClassNames(styles.modal, styles[MODAL_POSITION_MAP[position]])}>
         <div className={styles.dimmed} onClick={onToggle} />
-        <div className={`${styles.modalContainer} ${styles[modalContainerStyle]}`}>{children}</div>
+        <div className={extendClassNames(styles.modalContainer, styles[modalContainerPositionClassName])}>
+          {children}
+        </div>
       </div>
     )
   );
 };
 
-const ModalHeader: React.FC<React.PropsWithChildren<{ title: string }>> = ({ children, title }) => {
+const ModalHeader: ModalHeaderType = ({ children, className, title }) => {
   return (
-    <header className={styles.modalHeader}>
+    <header className={extendClassNames(styles.modalHeader, className)}>
       <h2 className={styles.modalTitle}>{title}</h2>
       {children}
     </header>
   );
 };
 
-const ModalContent: React.FC<React.PropsWithChildren> = ({ children }) => {
-  return <section className={styles.modalContent}>{children}</section>;
+const ModalCloseButton: ModalCloseButtonType = ({ className, ...rest }) => {
+  return (
+    <button className={extendClassNames(styles.modalCloseButton, className)} {...rest}>
+      <img className={styles.modalCloseButtonImage} src={CloseImage} alt="close-button" />
+    </button>
+  );
 };
 
-const ModalFooter: React.FC<React.PropsWithChildren> = ({ children }) => {
-  return <footer className={styles.modalFooter}>{children}</footer>;
+const ModalContent: ModalContentType = ({ children, className, ...rest }) => {
+  return (
+    <section className={extendClassNames(styles.modalContent, className)} {...rest}>
+      {children}
+    </section>
+  );
+};
+
+const ModalFooter: ModalFooterType = ({ children, direction = 'row', className }) => {
+  const footerRowClassName = styles[`footer${convertPascalCase(direction)}`];
+  return (
+    <footer className={extendClassNames(styles.modalFooter, footerRowClassName, className)}>
+      <div className={extendClassNames(styles.modalFooterChildrenContainer, styles[direction])}>{children}</div>
+    </footer>
+  );
+};
+
+const ModalButton: ModalButtonType = ({ children, color = 'primary', className, ...rest }) => {
+  return (
+    <button className={extendClassNames(styles.modalButton, styles[BUTTON_COLOR_MAP[color]], className)} {...rest}>
+      {children}
+    </button>
+  );
 };
 
 export default Modal;
 
 Modal.ModalHeader = ModalHeader;
+Modal.ModalCloseButton = ModalCloseButton;
 Modal.ModalContent = ModalContent;
 Modal.ModalFooter = ModalFooter;
+Modal.ModalButton = ModalButton;
