f7408849744b2e58e0263b8c0cb706fa32626cdc
lurgi <wahoo9040@naver.com>
2024-05-05 13:41:51 +0900

    refactor: modal story 수정

--- components/src/storybook/Modal.stories.tsx
+++ components/src/storybook/Modal.stories.tsx
@@ -28,6 +28,8 @@ const meta = {
     theme: { name: "모달의 색상 테마" },
     closeButtonPosition: { name: "닫기 버튼의 위치" },
     hasConfirmButton: { name: "확인 버튼의 유무" },
+    confirmMessage: { name: "확인 버튼의 내부 요소. 메세지" },
+    cancelMessage: { name: "취소 버튼의 내부 요소. 메세지" },
   },
   decorators: [
     (Story) => (
@@ -46,68 +48,51 @@ const Component = () => {
   return <div>child입니다.</div>;
 };
 
+const DEFAULT_PROPS_OBJ = {
+  position: "center",
+  title: "modal title",
+  closeButtonPosition: "top",
+  hasConfirmButton: false,
+  children: <Component />,
+  confirmMessage: "동의하고 계속하기",
+  cancelMessage: "닫기",
+  onConfirm: () => console.log("확인"),
+  onClose: () => console.log("닫기"),
+} as const;
+
 export const Default: Story = {
   name: "기본 모달",
-  args: {
-    position: "center",
-    title: "modal title",
-    closeButtonPosition: "top",
-    hasConfirmButton: false,
-    children: <Component />,
-    onConfirm: () => console.log("확인"),
-    onClose: () => console.log("닫기"),
-  },
+  args: DEFAULT_PROPS_OBJ,
 };
 
 export const ConfirmButton: Story = {
   name: "확인 버튼이 나타난다.",
   args: {
-    position: "center",
-    title: "modal title",
-    closeButtonPosition: "top",
+    ...DEFAULT_PROPS_OBJ,
     hasConfirmButton: true,
-    children: <Component />,
-    onConfirm: () => console.log("확인"),
-    onClose: () => console.log("닫기"),
   },
 };
 
 export const CloseButtonBottom: Story = {
   name: "취소 버튼이 아래에 나타난다",
   args: {
-    position: "center",
-    title: "modal title",
+    ...DEFAULT_PROPS_OBJ,
     closeButtonPosition: "bottom",
-    hasConfirmButton: true,
-    children: <Component />,
-    onConfirm: () => console.log("확인"),
-    onClose: () => console.log("닫기"),
   },
 };
 
 export const LongTitle: Story = {
   name: "타이틀에 긴 문자열이 들어갈 경우",
   args: {
-    position: "center",
+    ...DEFAULT_PROPS_OBJ,
     title: "modal title blahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblah",
-    closeButtonPosition: "top",
-    hasConfirmButton: true,
-    children: <Component />,
-    onConfirm: () => console.log("확인"),
-    onClose: () => console.log("닫기"),
   },
 };
 
 export const CustomWidth: Story = {
   name: "width값을 지정한 경우. position이 bottom일 경우 적용되지 않는다.",
   args: {
-    position: "center",
-    title: "modal title",
+    ...DEFAULT_PROPS_OBJ,
     width: 555,
-    closeButtonPosition: "top",
-    hasConfirmButton: true,
-    children: <Component />,
-    onConfirm: () => console.log("확인"),
-    onClose: () => console.log("닫기"),
   },
 };
