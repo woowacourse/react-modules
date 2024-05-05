f05bbda76b12eb716accad526ba9b818dc0750a6
simhorim <hori5020@gmail.com>
2024-05-05 22:27:18 +0900

    refactor(useExpiryDateValidation): month, year 유효성 검증 기능 분리

--- hooks/src/lib/useExpiryDate/useExpiryDateValidation.ts
+++ hooks/src/lib/useExpiryDate/useExpiryDateValidation.ts
@@ -3,77 +3,23 @@ import { useState } from "react";
 import cardInputValidator from "../validators/cardInputValidator";
 
 import { VALIDATION_MESSAGES } from "../constants/cardCustomHook";
-import { ExpiryDateKeys } from "../types/cardCustomHook";
 
-const useExpiryDateValidation = () => {
-  const [errorState, setErrorState] = useState<Record<ExpiryDateKeys, boolean>>(
-    {
-      month: false,
-      year: false,
-    }
-  );
-
+const useExpiryDateValidation = (
+  expiryMonthSetError: React.Dispatch<React.SetStateAction<boolean>>,
+  expiryYearSetError: React.Dispatch<React.SetStateAction<boolean>>
+) => {
   const [errorText, setErrorText] = useState("");
 
-  const updateErrorState = (name: string, validResult: boolean) => {
-    setErrorState((prevErrorState) => {
-      return {
-        ...prevErrorState,
-        [name]: validResult,
-      };
-    });
-  };
-
   const updateAllErrorState = (validResult: boolean) => {
-    setErrorState({
-      month: validResult,
-      year: validResult,
-    });
+    expiryMonthSetError(validResult);
+    expiryYearSetError(validResult);
   };
 
-  const validateExpiryDate = (
-    name: string,
-    value: string,
-    expiryDate: Record<ExpiryDateKeys, string>
-  ): boolean => {
-    const isNumericInput = cardInputValidator.validateNumericInput(value);
-
-    if (!isNumericInput) {
-      setErrorText(VALIDATION_MESSAGES.onlyNumbersAllowed);
-      updateErrorState(name, true);
-
-      return false;
-    }
-
-    const isOverInputLength = value.length > 2;
-
-    if (isOverInputLength) return false;
-
-    if (name === "month" && !cardInputValidator.validateMonth(value)) {
-      setErrorText(VALIDATION_MESSAGES.invalidMonthRange);
-      updateErrorState(name, true);
-
-      return true;
-    }
-
-    if (name === "year" && !cardInputValidator.validateYear(value)) {
-      setErrorText(VALIDATION_MESSAGES.expiredYear);
-      updateErrorState(name, true);
-
-      return true;
-    }
-
-    if (
-      (name === "month" && expiryDate.year.length !== 2) ||
-      (name === "year" && expiryDate.month.length !== 2)
-    ) {
-      return true;
-    }
-
+  const validateExpiryDate = (month: string, year: string): boolean => {
     if (
       !cardInputValidator.validateFutureDate(
-        name === "month" ? parseInt(value, 10) : parseInt(expiryDate.month, 10),
-        name === "year" ? parseInt(value, 10) : parseInt(expiryDate.year, 10)
+        parseInt(month, 10),
+        parseInt(year, 10)
       )
     ) {
       setErrorText(VALIDATION_MESSAGES.expiredDate);
@@ -89,7 +35,6 @@ const useExpiryDateValidation = () => {
   };
 
   return {
-    errorState,
     errorText,
     validateExpiryDate,
   };
