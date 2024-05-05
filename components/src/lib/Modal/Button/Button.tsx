81f94e47bc3407179d3b6c35d500fe5090dbc535
simhorim <hori5020@gmail.com>
2024-05-06 03:22:39 +0900

    refactor(button): 버튼 속성들의 유연성을 위한 확장

--- components/src/lib/Modal/Button/Button.tsx
+++ components/src/lib/Modal/Button/Button.tsx
@@ -1,16 +1,17 @@
+import { ButtonHTMLAttributes } from "react";
 import * as Styled from "./style";
 
 export type ButtonColor = "dark" | "white";
 
-export interface ButtonProps {
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: JSX.Element;
-  theme: ButtonColor;
+  color: ButtonColor;
   onClick: () => void;
 }
 
-const Button = ({ children, theme, onClick }: ButtonProps) => {
+const Button = ({ children, color, onClick, ...props }: ButtonProps) => {
   return (
-    <Styled.Button $theme={theme} onClick={onClick}>
+    <Styled.Button $color={color} onClick={onClick} {...props}>
       {children}
     </Styled.Button>
   );
