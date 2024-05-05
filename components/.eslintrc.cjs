8043dfc494626f362cc9eb311e87d76c2a565b9b
badahertz52 <badahertz52@gmail.com>
2024-05-06 00:42:52 +0900

    fix: components eslint 오류 수정

--- components/.eslintrc.cjs
+++ components/.eslintrc.cjs
@@ -2,15 +2,14 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'prettier',
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
     'plugin:storybook/recommended',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs', 'import'],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh', '@typescript-eslint'],
+  plugins: ['react-refresh', '@typescript-eslint', 'import', 'prettier'],
   rules: {
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     'max-depth': ['error', 1],
