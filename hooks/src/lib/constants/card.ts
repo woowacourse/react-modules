cc3f9ee8ec709b817a5179b49bc2b27ca158c5cc
badahertz52 <badahertz52@gmail.com>
2024-05-05 16:13:03 +0900

    refactor: useExpiryDate 수정
    - useExpiryDate : props, 반환값 수정에 따른 프로덕션 코드 수정
    - CardExpiryDate: useExpiryDate수정에 따른 수정

--- hooks/src/lib/constants/card.ts
+++ hooks/src/lib/constants/card.ts
@@ -6,5 +6,8 @@ export const MONTH = {
   startNumber: 1,
   endNumber: 12,
 };
+export const DATE_LENGTH = 2;
 
 export const CENTURY_PREFIX = 2000;
+
+export const MAX_CARD_YEARS_FROM_NOW = 5;
--- hooks/src/lib/useExpiryDate.ts
+++ hooks/src/lib/useExpiryDate.ts
@@ -1,106 +1,203 @@
-import { ChangeEvent, FocusEvent, useState } from 'react';
-import useValidation from './useValidation';
-import { CENTURY_PREFIX, MONTH } from './constants';
-import { validateNumber, validateFilledValue } from './utils/validators';
-import { ErrorMessage, UseCardModuleProps, ValidationParam } from './types';
+import { useEffect, useState } from 'react';
+import { DATE_LENGTH, INVALID_INPUT_VALUE, MAX_CARD_YEARS_FROM_NOW, MONTH } from './constants';
+import { validateFilledValue, validateNumber, getNowYearAndMonth } from './utils';
+import { UseCardModuleReturn } from './types';
 
-interface ValidationErrors {
+export interface ExpiryDate {
+  month: string;
+  year: string;
+}
+
+export type ExpiryDateKey = 'month' | 'year' | undefined;
+export interface ExpiryDateErrorMessages {
   empty: string;
   number: string;
-  year: string;
-  month: string;
-  date: string;
+  length: string;
+  yearFormat: string;
+  monthFormat: string;
+  availability: string;
 }
 
-type ExpiryDateKey = 'month' | 'year';
+export type ExpiryDateValidationErrorMessage = null | {
+  month: string | null;
+  year: string | null;
+  availability: string | null;
+};
 
-interface ExpiryDate {
-  month: string;
-  year: string;
-}
-interface ExpiryDateError {
-  month: boolean;
-  year: boolean;
-}
+export type ExpiryDateValidationErrorMessageKey = 'month' | 'year' | 'availability' | null;
 
-interface ExpiryDateValidationParam extends ValidationParam<ExpiryDate> {
-  key: ExpiryDateKey;
+export interface UseExpiryDateProps {
+  expiryDate: ExpiryDate;
+  errorMessages: ExpiryDateErrorMessages;
+  validation: {
+    maxYearsFromNow: number;
+  };
+  isNeedValidValue: boolean;
 }
+export type ExpiryDateError = 'empty' | 'length' | 'number' | 'format' | null;
+export interface UseExpiryDateResult {
+  month: {
+    isValid: boolean;
+    error: ExpiryDateError;
+  };
+  year: {
+    isValid: boolean;
+    error: ExpiryDateError;
+  };
+  isValidAvailability: boolean;
+}
+export type UseExpiryDateReturn = UseCardModuleReturn<
+  ExpiryDateValidationErrorMessage,
+  UseExpiryDateResult,
+  ExpiryDate
+>;
+
 // 연도에 대한 최대 기간 props로 받기
-export default function useExpiryDate({ validationErrorMessages }: UseCardModuleProps<ValidationErrors>) {
-  const [expiryDate, setExpiryDate] = useState<ExpiryDate>({ month: '', year: '' });
-  const [error, setError] = useState<ExpiryDateError>({ month: false, year: false });
-  const [errorMessage, setErrorMessage] = useState<ErrorMessage>(null);
+export default function useExpiryDate(props: UseExpiryDateProps): UseExpiryDateReturn {
+  const { expiryDate, errorMessages, validation, isNeedValidValue } = props;
 
-  const today = new Date();
+  const [monthError, setMonthError] = useState<ExpiryDateError>(null);
+  const [yearError, setYearError] = useState<ExpiryDateError>(null);
+  const [availabilityError, setAvailabilityError] = useState(false);
+  const [validationErrorMessage, setValidationErrorMessage] = useState<ExpiryDateValidationErrorMessage>(null);
 
-  const currentDate = {
-    year: today.getFullYear() - CENTURY_PREFIX,
-    month: today.getMonth() + 1,
+  const currentDate = getNowYearAndMonth();
+
+  const validateDateValue = (
+    value: string,
+    validateFormat: (value: string) => boolean,
+    setError: (error: ExpiryDateError) => void,
+  ) => {
+    if (!validateFilledValue(value)) {
+      return setError('empty');
+    }
+    if (!validateNumber(value)) setError('number');
+
+    if (!validateDateLength(value)) {
+      return setError('length');
+    }
+    if (!validateFormat(value)) {
+      return setError('format');
+    }
+    setError(null);
   };
 
-  const validateExpiryDateFilled = (value: ExpiryDate, key: ExpiryDateKey) => validateFilledValue(value[key]);
-
-  const validateMonth = (value: ExpiryDate) => {
-    const month = Number(value.month);
+  const validateAvailabilityOfMonth = (month: string) => {
+    const monthNumber = Number(month);
     const { startNumber, endNumber } = MONTH;
-    return month >= startNumber && month <= endNumber;
+
+    return monthNumber >= startNumber && monthNumber <= endNumber;
   };
 
-  const validateYear = (value: ExpiryDate) => {
-    return Number(value.year) >= currentDate.year;
+  const validateDateLength = (value: string) => value.length <= DATE_LENGTH;
+
+  const validateMonth = (month: string) => {
+    validateDateValue(month, validateAvailabilityOfMonth, setMonthError);
   };
 
-  const validateDate = (value: ExpiryDate) => {
+  const validateAvailabilityOfYear = (year: string) => {
+    const yearNumber = Number(year);
+    const maxYear = currentDate.year + (validation.maxYearsFromNow || MAX_CARD_YEARS_FROM_NOW);
+
+    return yearNumber >= currentDate.year && yearNumber <= maxYear;
+  };
+
+  const validateYear = (year: string) => {
+    validateDateValue(year, validateAvailabilityOfYear, setYearError);
+  };
+
+  const validateAvailability = (value: ExpiryDate) => {
     const year = Number(value.year);
     const month = Number(value.month);
 
     const isOverYear = year > currentDate.year;
     const isOverMonth = year == currentDate.year && month >= currentDate.month;
 
-    return isOverYear || isOverMonth;
+    const isValid = isOverYear || isOverMonth;
+
+    setAvailabilityError(!isValid);
   };
 
-  const changeEventValidators = (key: ExpiryDateKey) => [
-    { test: (value: ExpiryDate) => validateNumber(value[key]), errorMessage: validationErrorMessages.number },
-  ];
-  const blurEventValidators = (key: ExpiryDateKey) => [
-    { test: (value: ExpiryDate) => validateExpiryDateFilled(value, key), errorMessage: validationErrorMessages.empty },
-    key === 'month'
-      ? { test: validateMonth, errorMessage: validationErrorMessages.month }
-      : { test: validateYear, errorMessage: validationErrorMessages.year },
-    { test: validateDate, errorMessage: validationErrorMessages.date },
-  ];
+  const validateExpiryDate = (value: ExpiryDate) => {
+    validateMonth(value.month);
+    validateYear(value.year);
+    validateAvailability(value);
+  };
+  /**
+   * 현재 입력되는 expiryDate의 key값에 따른 오류 메세지를 반환하는 함수, month와 year에 대한 유효성 검사를 통과하면 카드 기간의 사용성 여부에 대한 검사 결과를 반환하고 아무런 오류가 없을 경우 null을 반환한다.
+   * @returns 오류 메세지
+   */
+  const getValidationErrorMessageKey = (key: ExpiryDateKey) => {
+    const targetError = key === 'month' ? monthError : yearError;
 
-  const handleValidation = ({ value, key, validators }: ExpiryDateValidationParam) => {
-    const result = useValidation<ExpiryDate>({ validators, value });
+    if (targetError === 'empty') return 'empty';
+    if (targetError === 'number') return 'number';
+    if (targetError === 'length') return 'length';
+    if (targetError === 'format') {
+      return targetError === monthError ? 'monthFormat' : 'yearFormat';
+    }
 
-    setExpiryDate((prev) => ({
-      ...prev,
-      [key]: value[key],
-    }));
-
-    setError((prev) => ({
-      ...prev,
-      [key]: !!result.isValid,
-    }));
-
-    setErrorMessage(result.isValid ? null : result.errorMessage);
+    return null;
   };
 
-  const handleChange = (e: ChangeEvent<HTMLInputElement>, key: ExpiryDateKey) => {
-    handleValidation({ value: { ...expiryDate, [key]: e.target.value }, validators: changeEventValidators(key), key });
+  const updateErrorMessages = () => {
+    const monthErrorKey = getValidationErrorMessageKey('month');
+    const yearErrorKey = getValidationErrorMessageKey('year');
+
+    setValidationErrorMessage({
+      month: monthErrorKey ? errorMessages[monthErrorKey] : null,
+      year: yearErrorKey ? errorMessages[yearErrorKey] : null,
+      availability: availabilityError ? errorMessages.availability : null,
+    });
   };
 
-  const handleBlur = (e: FocusEvent<HTMLInputElement>, key: ExpiryDateKey) => {
-    handleValidation({ value: { ...expiryDate, [key]: e.target.value }, validators: blurEventValidators(key), key });
+  const formatValue = (value: string) => {
+    if (!value) return value;
+
+    return value.slice(0, DATE_LENGTH);
   };
 
-  const updateValue = (value: string, key: ExpiryDateKey) => {
-    const validators = [...blurEventValidators(key)].splice(1, 0, changeEventValidators(key)[0]);
+  /**
+   * month,year아 숫자가 아닐 경우 빈문자열을 내보내고 그렇지 않을 경우 isPadZero의 값에 따라 month,year의 형태를 변환해 반환한다.(isPadZero가 true이면 한 자리 숫자 앞에 0을 붙인다.)
+   */
+  const getFormattedValue = () => {
+    const formattedMonth = formatValue(expiryDate.month);
+    const formattedYear = formatValue(expiryDate.year);
 
-    handleValidation({ value: { ...expiryDate, [key]: value }, validators, key });
+    if (isNeedValidValue) {
+      return {
+        month: monthError === 'number' ? INVALID_INPUT_VALUE : formattedMonth,
+        year: yearError === 'number' ? INVALID_INPUT_VALUE : formattedYear,
+      };
+    }
+
+    return {
+      month: formattedMonth,
+      year: formattedYear,
+    };
   };
 
-  return { expiryDate, handleChange, handleBlur, updateValue, errorMessage, error };
+  useEffect(() => {
+    validateExpiryDate(expiryDate);
+  }, [expiryDate]);
+
+  useEffect(() => {
+    updateErrorMessages();
+  }, [monthError, yearError, availabilityError]);
+
+  return {
+    validationErrorMessage,
+    validationResult: {
+      month: {
+        isValid: monthError === null,
+        error: monthError,
+      },
+      year: {
+        isValid: yearError === null,
+        error: yearError,
+      },
+      isValidAvailability: !availabilityError,
+    },
+    formattedValue: getFormattedValue(),
+  };
 }
