47839c5116993f2db869bed3858b23350807fa29
ss0526100 <ss0526100@gmail.com>
2024-05-05 15:04:27 +0900

    feat(useCardNumber):Validator배열을 통한 유효성 검증

--- hooks/src/lib/useCardNumber/index.ts
+++ hooks/src/lib/useCardNumber/index.ts
@@ -1,46 +1,99 @@
-import REGEXPS from '../constants/regExps';
-import { useState } from 'react';
-import useValidation from '../useValidation';
+import { useMemo, useState } from 'react';
 
-const cardNumberPartValidators = [
-  (value: string) => {
-    if (value.length !== 4) {
-      return '카드번호 한 단위는 4자리여야 합니다.';
-    }
+import REGEXPS from '../constants/regExps';
+import { Validator } from '../type';
+
+export default function useCardNumber() {
+  const [firstCardNumberPart, setFirstCardNumberPart] = useState('');
+  const [secondCardNumberPart, setSecondCardNumberPart] = useState('');
+  const [thirdCardNumberPart, setThirdCardNumberPart] = useState('');
+  const [fourthCardNumberPart, setFourthCardNumberPart] = useState('');
+  const cardNumberParts = [
+    firstCardNumberPart,
+    secondCardNumberPart,
+    thirdCardNumberPart,
+    fourthCardNumberPart,
+  ];
+
+  const setCardNumberParts = [
+    setFirstCardNumberPart,
+    setSecondCardNumberPart,
+    setThirdCardNumberPart,
+    setFourthCardNumberPart,
+  ];
+
+  const firstErrorMessage = useMemo(() => {
+    const reducer = getValidateReducer(firstCardNumberPart);
+    return cardNumberPartValidators.reduce(reducer, null);
+  }, [firstCardNumberPart]);
+
+  const secondErrorMessage = useMemo(() => {
+    const reducer = getValidateReducer(secondCardNumberPart);
+    return cardNumberPartValidators.reduce(reducer, null);
+  }, [secondCardNumberPart]);
+
+  const thirdErrorMessage = useMemo(() => {
+    const reducer = getValidateReducer(thirdCardNumberPart);
+    return cardNumberPartValidators.reduce(reducer, null);
+  }, [thirdCardNumberPart]);
+  const fourthErrorMessage = useMemo(() => {
+    const reducer = getValidateReducer(fourthCardNumberPart);
+    return cardNumberPartValidators.reduce(reducer, null);
+  }, [fourthCardNumberPart]);
+
+  const cardPartErrorMessages = [
+    firstErrorMessage,
+    secondErrorMessage,
+    thirdErrorMessage,
+    fourthErrorMessage,
+  ];
+
+  const isValidCardNumberParts = cardPartErrorMessages.map(
+    message => message === null
+  );
+
+  const isValidCardNumber = cardPartErrorMessages.every(
+    message => message === null
+  );
+
+  return {
+    cardNumberParts,
+    setCardNumberParts,
+    cardPartErrorMessages,
+    isValidCardNumber,
+    isValidCardNumberParts,
+  };
+}
+
+const CARD_NUMBER_PART_LENGTH = 4;
+export const CARD_NUMBER_PART_ERROR_MESSAGE = {
+  invalidLength: `카드번호 한 단위는 ${CARD_NUMBER_PART_LENGTH}자리여야 합니다.`,
+  notDigit: '카드번호는 숫자만 포함해야 합니다.',
+} as const;
+
+type ErrorMessage =
+  (typeof CARD_NUMBER_PART_ERROR_MESSAGE)[keyof typeof CARD_NUMBER_PART_ERROR_MESSAGE];
+
+const cardNumberPartValidators: Validator<string, ErrorMessage>[] = [
+  {
+    checkIsValid: (cardNumberPart: string) =>
+      cardNumberPart.length === CARD_NUMBER_PART_LENGTH,
+    message: CARD_NUMBER_PART_ERROR_MESSAGE.invalidLength,
   },
-  (value: string) => {
-    if (!REGEXPS.onlyDigitNumber.test(value)) {
-      return '카드번호는 숫자만 포함해야 합니다.';
-    }
+  {
+    checkIsValid: (cardNumberPart: string) =>
+      REGEXPS.onlyDigitNumber.test(cardNumberPart),
+    message: CARD_NUMBER_PART_ERROR_MESSAGE.notDigit,
   },
 ];
 
-export default function useCardNumber() {
-  const cardNumberPartUseStates = [
-    useState(''),
-    useState(''),
-    useState(''),
-    useState(''),
-  ];
-  const cardNumberPartValidations = [
-    useValidation(cardNumberPartValidators),
-    useValidation(cardNumberPartValidators),
-    useValidation(cardNumberPartValidators),
-    useValidation(cardNumberPartValidators),
-  ];
-  const cardNumberPartValues = cardNumberPartUseStates.map(state => state[0]);
-
-  const setCardNumber = (value: string, index: number) => {
-    const [, setCardNumberPart] = cardNumberPartUseStates[index];
-    const validateCardNumberPart = cardNumberPartValidations[index].validate;
-
-    setCardNumberPart(value);
-    validateCardNumberPart(value);
+const getValidateReducer =
+  (cardNumberPart: string) =>
+  (
+    errorMessage: ErrorMessage | null,
+    validator: Validator<string, ErrorMessage>
+  ) => {
+    if (errorMessage !== null) return errorMessage;
+    if (validator.checkIsValid(cardNumberPart)) return errorMessage;
+    return validator.message;
   };
-
-  const errorStatus = cardNumberPartValidations.map(
-    ({ errorStatus }) => errorStatus
-  );
-
-  return { cardNumber: cardNumberPartValues, setCardNumber, errorStatus };
-}
