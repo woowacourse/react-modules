15e7ce10553f99cf1ed3f5dfd0094a8c38ad7475
jayming66 <mjw8941@gmail.com>
2024-05-06 18:42:32 +0900

    chore(useModalControl): 불필요한 주석 제거

--- components/src/lib/Modal/hooks/useModalControl.ts
+++ components/src/lib/Modal/hooks/useModalControl.ts
@@ -2,11 +2,6 @@ import { useEffect } from 'react';
 import usePressESC from '../../../hooks/usePressESC';
 
 /**
- *
- *
- */
-
-/**
  * useModalControl - modal control에 관한 훅입니다.
  * 1. 사용자가 esc를 눌렀을 때 모달이 닫힌다.
  * 2. 사용자가 모달을 열었을 때 외부 스크롤을 하지 못하도록 막는다.
