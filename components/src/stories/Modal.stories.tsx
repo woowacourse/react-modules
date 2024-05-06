e325b9a99f3c59ffe1fad0bd47f32a19bccbeddc
badahertz52 <badahertz52@gmail.com>
2024-05-06 14:17:10 +0900

    test: 모달 스토리북 테스트 코드 수정
    - BottomModal.stories.tsx 삭제

--- components/src/stories/Modal.stories.tsx
+++ components/src/stories/Modal.stories.tsx
@@ -1,7 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
-import Modal from '../lib/Modal';
 import '../lib/styles/reset.css';
+import { Modal } from '../lib';
 
 const meta: Meta<typeof Modal> = {
   title: 'Modal',
