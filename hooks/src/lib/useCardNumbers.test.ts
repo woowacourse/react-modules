94b74105079475d891ca8c8fb9e483985d55b1e9
Largopie <106071687+Largopie@users.noreply.github.com>
2024-05-06 23:40:38 +0900

    refactor(useCardNumbers): 로직 수정 및 테스트 반영
    Co-authored-by: soosoo22 <lllllh00@naver.com>

--- hooks/src/lib/useCardNumbers.test.ts
+++ hooks/src/lib/useCardNumbers.test.ts
@@ -3,15 +3,37 @@ import { ChangeEvent, act } from 'react';
 import useCardNumbers from './useCardNumbers';
 
 describe('useCardNumbers 테스트', () => {
-  it('입력값이 숫자가 아니라면 입력을 제한한다.', () => {
-    const userInput = 'nakta';
-    const { result } = renderHook(() => useCardNumbers());
+  const initialValue = [
+    { value: '', length: 4 },
+    { value: '', length: 4 },
+    { value: '', length: 4 },
+    { value: '', length: 4 },
+  ];
+
+  it('초기값이 정확하게 설정되어야 한다.', () => {
+    const setInitialValue = [
+      { value: '1234', length: 4 },
+      { value: '1234', length: 4 },
+      { value: '1234', length: 4 },
+      { value: '1234', length: 4 },
+    ];
+
+    const { result } = renderHook(() => useCardNumbers(setInitialValue));
+
+    expect(result.current.cardNumbers.map(({ value }) => value)).toEqual(setInitialValue.map(({ value }) => value));
+  });
+
+  it.each([['nakta'], ['/@[]'], ['한글입력']])('입력값이 숫자가 아니라면 입력을 제한한다.', (userInput) => {
+    const { result } = renderHook(() => useCardNumbers(initialValue));
 
     act(() => {
-      result.current.cardNumbers.forEach((cardNumber) => {
-        cardNumber.onChange({
-          target: { value: userInput },
-        } as ChangeEvent<HTMLInputElement>);
+      Array.from({ length: 4 }, (_, index) => {
+        result.current.onChange(
+          {
+            target: { value: userInput },
+          } as ChangeEvent<HTMLInputElement>,
+          index,
+        );
       });
     });
 
@@ -20,16 +42,19 @@ describe('useCardNumbers 테스트', () => {
     });
   });
 
-  it('카드번호가 4로 시작하는 경우 카드 브랜드를 visa 설정한다.', () => {
+  it('카드번호가 4로 시작하는 16자리 숫자일 때 카드 브랜드를 visa 설정한다.', () => {
     const VISA = 'visa';
     const userInput = ['4000', '0000', '0000', '0000'];
-    const { result } = renderHook(() => useCardNumbers());
+    const { result } = renderHook(() => useCardNumbers(initialValue));
 
     act(() => {
-      result.current.cardNumbers.forEach((cardNumber, index) => {
-        cardNumber.onChange({
-          target: { value: userInput[index] },
-        } as ChangeEvent<HTMLInputElement>);
+      userInput.forEach((input, index) => {
+        result.current.onChange(
+          {
+            target: { value: input },
+          } as ChangeEvent<HTMLInputElement>,
+          index,
+        );
       });
     });
 
@@ -42,15 +67,18 @@ describe('useCardNumbers 테스트', () => {
     [['5300', '0000', '0000', '0000']],
     [['5400', '0000', '0000', '0000']],
     [['5500', '0000', '0000', '0000']],
-  ])('카드번호가 51~55로 시작하는 경우 카드 브랜드를 mastercard 설정한다.', (userInput) => {
+  ])('카드번호가 51~55로 시작하는 16자리 숫자일 때 카드 브랜드를 mastercard 설정한다.', (userInput) => {
     const MASTER_CARD = 'mastercard';
-    const { result } = renderHook(() => useCardNumbers());
+    const { result } = renderHook(() => useCardNumbers(initialValue));
 
     act(() => {
-      result.current.cardNumbers.forEach((cardNumber, index) => {
-        cardNumber.onChange({
-          target: { value: userInput[index] },
-        } as ChangeEvent<HTMLInputElement>);
+      userInput.forEach((input, index) => {
+        result.current.onChange(
+          {
+            target: { value: input },
+          } as ChangeEvent<HTMLInputElement>,
+          index,
+        );
       });
     });
 
@@ -60,17 +88,20 @@ describe('useCardNumbers 테스트', () => {
   it.each([
     [['0000', '0000', '0000', '0000'], true],
     [['0000', '0000', '0000', ''], false],
-  ])('입력 값이 유효하다면 true를 반환하고, 유효하지 않다면 false를 반환한다.', (userInput, isValid) => {
-    const { result } = renderHook(() => useCardNumbers());
+  ])('입력 값이 유효하다면 true를 반환하고, 유효하지 않다면 false를 반환한다.', (userInput: string[], isValid: boolean) => {
+    const { result } = renderHook(() => useCardNumbers(initialValue));
 
     act(() => {
-      result.current.cardNumbers.forEach((cardNumber, index) => {
-        cardNumber.onChange({
-          target: { value: userInput[index] },
-        } as ChangeEvent<HTMLInputElement>);
+      userInput.forEach((input, index) => {
+        result.current.onChange(
+          {
+            target: { value: input },
+          } as ChangeEvent<HTMLInputElement>,
+          index,
+        );
       });
     });
 
-    expect(result.current.isCardNumbersValid).toBe(isValid);
+    expect(result.current.isValid).toBe(isValid);
   });
 });
--- hooks/src/lib/useCardNumbers.ts
+++ hooks/src/lib/useCardNumbers.ts
@@ -1,44 +1,58 @@
-import useInput, { ValidationType } from './useInput';
+import { ChangeEvent, FocusEvent, useState } from 'react';
 import useCardType from './useCardType';
+import Validation from './utils/validation';
 
-export const CARD_NUMBER_LENGTH = 4;
+interface InitialValueProps {
+  value: string;
+  length: number;
+}
 
-type InitialValueType = [string, string, string, string];
+interface CardNumberState extends InitialValueProps {
+  id: number;
+  isError: boolean;
+}
 
-const useCardNumbers = (initialValue: InitialValueType = ['', '', '', '']) => {
-  const isValidLength = (value: string) => {
-    return value.length === CARD_NUMBER_LENGTH;
-  };
-
-  const isNumber = (value: string) => {
-    return /^\d*$/.test(value);
-  };
-
-  const inputValidations: ValidationType[] = [
-    {
-      validate: isValidLength,
-      message: `${CARD_NUMBER_LENGTH}자리의 카드 번호를 입력해주세요.`,
-    },
-  ];
-
-  const preventInputValidations: ValidationType[] = [
-    {
-      validate: isNumber,
-      message: '숫자만 입력 가능합니다.',
-    },
-  ];
-
-  const cardNumbers = [
-    useInput({ initialValue: initialValue[0], inputValidations, preventInputValidations }),
-    useInput({ initialValue: initialValue[1], inputValidations, preventInputValidations }),
-    useInput({ initialValue: initialValue[2], inputValidations, preventInputValidations }),
-    useInput({ initialValue: initialValue[3], inputValidations, preventInputValidations }),
-  ];
-
+const useCardNumbers = (initialValues: InitialValueProps[]) => {
+  const [cardNumbers, setCardNumbers] = useState<CardNumberState[]>(
+    initialValues.map((initialValue, index) => ({ ...initialValue, isError: false, id: index })),
+  );
+  const [errorMessage, setErrorMessage] = useState('');
   const cardBrand = useCardType(cardNumbers.map(({ value }) => value));
-  const isCardNumbersValid = cardNumbers.every(({ value, error }) => value !== '' && !error.state);
+  const isValid = cardNumbers.every(({ value, isError }) => value !== '' && !isError);
 
-  return { cardNumbers, cardBrand, isCardNumbersValid };
+  const onChangeHandler = (e: ChangeEvent<HTMLInputElement>, index: number) => {
+    const newCardNumbers = [...cardNumbers];
+
+    if (!Validation.isNumericPattern(e.target.value)) {
+      newCardNumbers[index].isError = true;
+      setCardNumbers(newCardNumbers);
+      setErrorMessage('숫자만 입력할 수 있습니다.');
+
+      return;
+    }
+
+    if (!Validation.isExactLength(cardNumbers[index].length, e.target.value)) {
+      newCardNumbers[index].isError = true;
+      setErrorMessage(`${cardNumbers[index].length}자리의 카드 번호를 입력해주세요.`);
+    } else {
+      newCardNumbers[index].isError = false;
+      setErrorMessage('');
+    }
+
+    newCardNumbers[index].value = e.target.value;
+    setCardNumbers(newCardNumbers);
+  };
+
+  const onBlurHandler = (e: FocusEvent<HTMLInputElement>, index: number) => {
+    const newCardNumbers = [...cardNumbers];
+
+    if (!Validation.isExactLength(cardNumbers[index].length, e.target.value)) {
+      newCardNumbers[index].isError = true;
+      setErrorMessage(`${cardNumbers[index].length}자리의 카드 번호를 입력해주세요.`);
+    }
+  };
+
+  return { cardNumbers, isValid, cardBrand, errorMessage, onChange: onChangeHandler, onBlur: onBlurHandler };
 };
 
 export default useCardNumbers;
