3f1b808454077902cf8e57668692230f37518a4e
hanyoung <phanyoung@naver.com>
2024-05-07 00:46:48 +0900

    refactor(useCardNumber): 변수명 및 함수 표현 방식 수정

--- hooks/src/lib/useCardNumber/index.ts
+++ hooks/src/lib/useCardNumber/index.ts
@@ -1,33 +1,34 @@
 import { useInputValidation, IErrorStatus } from "../useInputValidation";
 import { cardNumberValidator } from "./validator";
 
-interface CardNumberPartControl {
+interface CardNumberUnitControl {
   value: string;
   errorStatus: IErrorStatus;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
   onBlur: (e: React.FocusEvent<HTMLInputElement>) => void;
 }
 
-export function useCardNumber(): CardNumberPartControl[] {
-  const firstPartControl = useInputValidation(cardNumberValidator);
-  const secondPartControl = useInputValidation(cardNumberValidator);
-  const thirdPartControl = useInputValidation(cardNumberValidator);
-  const fourthPartControl = useInputValidation(cardNumberValidator);
+export function useCardNumber(): CardNumberUnitControl[] {
+  const firstUnitControl = useInputValidation(cardNumberValidator);
+  const secondUnitControl = useInputValidation(cardNumberValidator);
+  const thirdUnitControl = useInputValidation(cardNumberValidator);
+  const fourthUnitControl = useInputValidation(cardNumberValidator);
 
-  const cardNumberPartControls = [
-    firstPartControl,
-    secondPartControl,
-    thirdPartControl,
-    fourthPartControl,
+  const cardNumberUnitControls = [
+    firstUnitControl,
+    secondUnitControl,
+    thirdUnitControl,
+    fourthUnitControl,
   ];
 
-  const result = cardNumberPartControls.map(
+  const result = cardNumberUnitControls.map(
     ({ value, setValueWithValidation, errorStatus, validateOnBlur }) => {
       return {
         value,
         errorStatus,
-        onChange: (e: React.ChangeEvent<HTMLInputElement>) =>
-          setValueWithValidation(e.target.value),
+        onChange: (e: React.ChangeEvent<HTMLInputElement>) => {
+          setValueWithValidation(e.target.value);
+        },
         onBlur: (e: React.FocusEvent<HTMLInputElement>) => validateOnBlur(e.target.value),
       };
     }
