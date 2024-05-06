93450cd39783f099e42cdc1c5ee746ef80f122fe
hwinkr <dnddl8280@naver.com>
2024-05-06 19:14:17 +0900

    refactor(cardInputValidatoor): 사용하지 않는 유효성 검증 함수 삭제, 상수 사용

--- hooks/src/lib/validators/cardInputValidator.ts
+++ hooks/src/lib/validators/cardInputValidator.ts
@@ -18,23 +18,23 @@ const cardInputValidator = {
   },
 
   validateNumberInRange(number: number, min: number, max: number) {
-    return number >= min && number <= max ? true : false;
+    return number >= min && number <= max;
   },
 
   validatePastYear(year: string) {
-    const currentYear = new Date().getFullYear() & 100;
+    const currentYear = new Date().getFullYear() % 100;
 
-    return parseInt(year, 10) <= currentYear;
+    return parseInt(year, 10) >= currentYear;
   },
 
   validateMonth(month: string) {
-    if (month.length !== INPUT_RULES.validExpirationLength) return false;
+    if (month.length !== INPUT_RULES.maxExpirationDateLength) return false;
 
     return this.validateNumberInRange(parseInt(month, 10), 1, 12);
   },
 
   validateYear(year: string) {
-    if (year.length !== INPUT_RULES.validExpirationLength) return false;
+    if (year.length !== INPUT_RULES.maxExpirationDateLength) return false;
 
     return this.validatePastYear(year);
   },
@@ -45,22 +45,6 @@ const cardInputValidator = {
     const enteredDate = new Date(2000 + year, month, 0).getTime();
 
     return enteredDate > currentTime;
-  },
-
-  validateExpiration(month: string, year: string, name: string) {
-    if (name === "month" && !this.validateMonth(month)) return false;
-
-    if (name === "year" && !this.validateYear(year)) return false;
-
-    if (
-      month.length !== INPUT_RULES.validExpirationLength ||
-      year.length !== INPUT_RULES.validExpirationLength
-    )
-      return true;
-
-    return this.validateFutureDate(parseInt(month), parseInt(year))
-      ? true
-      : false;
   },
 };
 
