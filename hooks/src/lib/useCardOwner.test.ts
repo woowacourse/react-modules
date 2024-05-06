116f4772176698067d2e92507fe03c05e5d17557
soosoo22 <lllllh00@naver.com>
2024-05-06 21:30:52 +0900

    test: useCardOwner 테스트에 대한 한글, 기호 테스트 케이스 추가

--- hooks/src/lib/useCardOwner.test.ts
+++ hooks/src/lib/useCardOwner.test.ts
@@ -1,18 +1,20 @@
-import { renderHook } from '@testing-library/react';
-import { ChangeEvent, act } from 'react';
-import useCardOwner from './useCardOwner';
+import { renderHook } from "@testing-library/react";
+import { ChangeEvent, act } from "react";
+import useCardOwner from "./useCardOwner";
 
-describe('useCardOwner 테스트', () => {
-  it('입력한 값이 영어가 아니라면 입력을 제한한다.', () => {
-    const userInput = '12345';
-    const { result } = renderHook(() => useCardOwner());
+describe("useCardOwner 테스트", () => {
+  it.each([["12345"], ["호프짱"], ["!@#$^&"]])(
+    "입력한 값이 영어가 아니라면 입력을 제한한다.",
+    (userInput) => {
+      const { result } = renderHook(() => useCardOwner());
 
-    act(() => {
-      result.current.cardOwner.onChange({
-        target: { value: userInput },
-      } as ChangeEvent<HTMLInputElement>);
-    });
+      act(() => {
+        result.current.cardOwner.onChange({
+          target: { value: userInput },
+        } as ChangeEvent<HTMLInputElement>);
+      });
 
-    expect(result.current.cardOwner.value).toBe('');
-  });
+      expect(result.current.cardOwner.value).toBe("");
+    }
+  );
 });
