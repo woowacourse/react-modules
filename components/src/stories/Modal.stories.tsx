c7d9bdf51452b59fbb1d12c1cf9c105f890a0fd4
rbgksqkr <rbgks1937@naver.com>
2024-05-06 11:40:28 +0900

    test: Modal storybook 합성 컴포넌트로 수정

--- components/src/stories/Modal.stories.tsx
+++ components/src/stories/Modal.stories.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable storybook/prefer-pascal-case */
 import type { Meta, StoryObj } from '@storybook/react';
-import Modal from '../lib/Modal';
-import React from 'react';
+import { Modal } from '../lib';
+import React, { useReducer } from 'react';
 
 const meta = {
   title: 'Modal',
@@ -12,141 +12,202 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const 모달이_열림_center: Story = {
-  args: {
-    isOpen: true,
-    title: '마루',
-    position: 'center',
-    onClose: () => console.log('마루'),
-  },
+const DefaultModal = ({ ...args }) => {
+  const [isOpen, toggleIsOpen] = useReducer(prev => !prev, true);
+
+  return (
+    <>
+      <button onClick={toggleIsOpen}>모달열기</button>
+      <Modal {...args} isOpen={isOpen}>
+        <Modal.Dimmed onDimmedClick={() => toggleIsOpen()} />
+        <Modal.Header>
+          <Modal.Title title="마루" />
+          <Modal.CloseIcon onClose={() => toggleIsOpen()} />
+        </Modal.Header>
+        <Modal.Content>마루와 쿠키</Modal.Content>
+        <Modal.Footer>
+          <Modal.ConfirmButton label="확인" onConfirm={() => toggleIsOpen()} />
+          <Modal.CloseButton label="닫기" onClose={() => toggleIsOpen()} />
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
 };
 
-export const 모달이_열림_bottom: Story = {
+export const 모달_열림_center: Story = {
   args: {
     isOpen: true,
-    title: '마루',
+    position: 'center',
+  },
+  render: args => <DefaultModal {...args} />,
+};
+
+export const 모달_열림_center_animation: Story = {
+  args: {
+    isOpen: true,
+    position: 'center',
+    isAnimation: true,
+    duration: 500,
+  },
+  render: args => <DefaultModal {...args} />,
+};
+
+export const 모달_열림_bottom: Story = {
+  args: {
+    isOpen: true,
     position: 'bottom',
-    onClose: () => console.log('마루'),
   },
+  render: args => <DefaultModal {...args} />,
 };
 
-export const 모달이_닫힘: Story = {
+export const 모달_열림_bottom_animation: Story = {
   args: {
-    isOpen: false,
-    title: '마루',
-    position: 'center',
-    onClose: () => console.log('마루'),
+    isOpen: true,
+    position: 'bottom',
+    isAnimation: true,
+    duration: 500,
   },
+  render: args => <DefaultModal {...args} />,
 };
 
 export const 모달_버튼_1개: Story = {
   args: {
     isOpen: true,
-    title: '마루',
-    position: 'center',
-    onClose: () => console.log('마루'),
-    closeButton: <button style={{ width: '100%' }}>쿠키</button>,
   },
+  render: args => (
+    <Modal {...args}>
+      <Modal.Dimmed />
+      <Modal.Header>
+        <Modal.Title title="모달 버튼 1개" />
+        <Modal.CloseIcon onClose={() => ''} />
+      </Modal.Header>
+      <Modal.Content>마루와 쿠키</Modal.Content>
+      <Modal.Footer>
+        <Modal.CloseButton label="닫기" onClose={() => ''} />
+      </Modal.Footer>
+    </Modal>
+  ),
 };
 
-export const 모달_버튼_2개_ROW: Story = {
+export const 모달_버튼_배치_ROW: Story = {
   args: {
     isOpen: true,
-    title: '마루',
-    position: 'center',
-    onClose: () => console.log('마루'),
-    buttonPosition: 'row',
-    closeButton: <button style={{ width: '100%' }}>쿠키</button>,
-    confirmButton: <button style={{ width: '100%' }}>마루</button>,
   },
+  render: args => (
+    <Modal {...args}>
+      <Modal.Dimmed />
+      <Modal.Header>
+        <Modal.Title title="모달_버튼_배치_ROW" />
+        <Modal.CloseIcon onClose={() => ''} />
+      </Modal.Header>
+      <Modal.Content>마루와 쿠키</Modal.Content>
+      <Modal.Footer position="row">
+        <Modal.ConfirmButton label="확인" onConfirm={() => ''} />
+        <Modal.CloseButton label="닫기" onClose={() => ''} />
+      </Modal.Footer>
+    </Modal>
+  ),
 };
 
-export const 모달_버튼_2개_ROW_REVERSE: Story = {
+export const 모달_버튼_배치_ROW_REVERSE: Story = {
   args: {
     isOpen: true,
-    title: '마루',
-    position: 'center',
-    onClose: () => console.log('마루'),
-    buttonPosition: 'row-reverse',
-    closeButton: <button style={{ width: '100%' }}>쿠키</button>,
-    confirmButton: <button style={{ width: '100%' }}>마루</button>,
   },
+  render: args => (
+    <Modal {...args}>
+      <Modal.Dimmed />
+      <Modal.Header>
+        <Modal.Title title="모달_버튼_배치_ROW_REVERSE" />
+        <Modal.CloseIcon onClose={() => ''} />
+      </Modal.Header>
+      <Modal.Content>마루와 쿠키</Modal.Content>
+      <Modal.Footer position="row-reverse">
+        <Modal.ConfirmButton label="확인" onConfirm={() => ''} />
+        <Modal.CloseButton label="닫기" onClose={() => ''} />
+      </Modal.Footer>
+    </Modal>
+  ),
 };
 
-export const 모달_버튼_2개_COLUMN: Story = {
+export const 모달_버튼_배치_COLUMN_REVERSE: Story = {
   args: {
     isOpen: true,
-    title: '마루',
-    position: 'center',
-    onClose: () => console.log('마루'),
-    buttonPosition: 'column',
-    closeButton: <button style={{ width: '100%' }}>쿠키</button>,
-    confirmButton: <button style={{ width: '100%' }}>마루</button>,
   },
-};
-
-export const 모달_버튼_2개_COLUMN_REVERSE: Story = {
-  args: {
-    isOpen: true,
-    title: '마루',
-    position: 'center',
-    onClose: () => console.log('마루'),
-    buttonPosition: 'column-reverse',
-    closeButton: <button style={{ width: '100%' }}>쿠키</button>,
-    confirmButton: <button style={{ width: '100%' }}>마루</button>,
-  },
+  render: args => (
+    <Modal {...args}>
+      <Modal.Dimmed />
+      <Modal.Header>
+        <Modal.Title title="모달_버튼_배치_COLUMN_REVERSE" />
+        <Modal.CloseIcon onClose={() => ''} />
+      </Modal.Header>
+      <Modal.Content>마루와 쿠키</Modal.Content>
+      <Modal.Footer position="column-reverse">
+        <Modal.ConfirmButton label="확인" onConfirm={() => ''} />
+        <Modal.CloseButton label="닫기" onClose={() => ''} />
+      </Modal.Footer>
+    </Modal>
+  ),
 };
 
 export const 모달_닫기_아이콘_숨김: Story = {
   args: {
     isOpen: true,
-    title: '마루',
-    position: 'center',
-    onClose: () => console.log('마루'),
-    hideCloseIcon: true,
-    buttonPosition: 'row-reverse',
-    closeButton: <button style={{ width: '100%' }}>쿠키</button>,
-    confirmButton: <button style={{ width: '100%' }}>마루</button>,
   },
+  render: args => (
+    <Modal {...args}>
+      <Modal.Dimmed />
+      <Modal.Header>
+        <Modal.Title title="모달_닫기_아이콘_숨김" />
+        <Modal.CloseIcon onClose={() => ''} showCloseIcon={false} />
+      </Modal.Header>
+      <Modal.Content>마루와 쿠키</Modal.Content>
+      <Modal.Footer>
+        <Modal.ConfirmButton label="확인" onConfirm={() => ''} />
+        <Modal.CloseButton label="닫기" onClose={() => ''} />
+      </Modal.Footer>
+    </Modal>
+  ),
 };
 
 export const 모달_닫기_아이콘_커스텀: Story = {
   args: {
     isOpen: true,
-    title: '마루',
-    position: 'center',
-    onClose: () => console.log('마루'),
-    customCloseIcon:
-      'https://github.com/jinhokim98/react-payments/blob/step2/src/assets/image/failure.png?raw=true',
-    buttonPosition: 'row-reverse',
-    closeButton: <button style={{ width: '100%' }}>쿠키</button>,
-    confirmButton: <button style={{ width: '100%' }}>마루</button>,
   },
+  render: args => (
+    <Modal {...args}>
+      <Modal.Dimmed />
+      <Modal.Header>
+        <Modal.Title title="모달_닫기_아이콘_커스텀" />
+        <Modal.CloseIcon
+          onClose={() => ''}
+          customCloseIcon="https://github.com/jinhokim98/react-payments/blob/step2/src/assets/image/failure.png?raw=true"
+        />
+      </Modal.Header>
+      <Modal.Content>마루와 쿠키</Modal.Content>
+      <Modal.Footer>
+        <Modal.ConfirmButton label="확인" onConfirm={() => ''} />
+        <Modal.CloseButton label="닫기" onClose={() => ''} />
+      </Modal.Footer>
+    </Modal>
+  ),
 };
 
 export const 모달_닫기_아이콘_커스텀_오류: Story = {
   args: {
     isOpen: true,
-    title: '마루',
-    position: 'center',
-    onClose: () => console.log('마루'),
-    customCloseIcon: '이미지',
-    buttonPosition: 'row-reverse',
-    closeButton: <button style={{ width: '100%' }}>쿠키</button>,
-    confirmButton: <button style={{ width: '100%' }}>마루</button>,
   },
-};
-
-export const 모달_컨텐츠: Story = {
-  args: {
-    isOpen: true,
-    title: '마루',
-    position: 'center',
-    onClose: () => console.log('마루'),
-    content: <div>안녕 나는 마루랑 쿠키야</div>,
-    customCloseIcon: '이미지',
-    buttonPosition: 'row-reverse',
-    closeButton: <button style={{ width: '100%' }}>쿠키</button>,
-    confirmButton: <button style={{ width: '100%' }}>마루</button>,
-  },
+  render: args => (
+    <Modal {...args}>
+      <Modal.Dimmed />
+      <Modal.Header>
+        <Modal.Title title="모달_닫기_아이콘_커스텀_오류" />
+        <Modal.CloseIcon onClose={() => ''} customCloseIcon="잘못된 이미지 경로를 넣는다면?" />
+      </Modal.Header>
+      <Modal.Content>마루와 쿠키</Modal.Content>
+      <Modal.Footer>
+        <Modal.ConfirmButton label="확인" onConfirm={() => ''} />
+        <Modal.CloseButton label="닫기" onClose={() => ''} />
+      </Modal.Footer>
+    </Modal>
+  ),
 };
