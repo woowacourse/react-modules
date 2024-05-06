59c2c12b19f37d9f90aec8184cfb34224c670022
hanyoung <phanyoung@naver.com>
2024-05-07 00:37:20 +0900

    refactor(useCardholderName): validator 로직을 별도의 파일로 분리

--- hooks/src/lib/useCardholderName/index.ts
+++ hooks/src/lib/useCardholderName/index.ts
@@ -1,4 +1,5 @@
 import { useInputValidation, IErrorStatus } from "../useInputValidation";
+import { cardholderNameValidator } from "./validator";
 
 interface UseCardholderNameReturn {
   value: string;
@@ -8,9 +9,12 @@ interface UseCardholderNameReturn {
 }
 
 export function useCardholderName(): UseCardholderNameReturn {
-  const { value, errorStatus, setValue, validateOnBlur } = useInputValidation(validator);
+  const { value, errorStatus, setValueWithValidation, validateOnBlur } =
+    useInputValidation(cardholderNameValidator);
 
-  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => setValue(e.target.value);
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValueWithValidation(e.target.value);
+  };
 
   const onBlur = (e: React.FocusEvent<HTMLInputElement>) => validateOnBlur(e.target.value);
 
@@ -21,32 +25,3 @@ export function useCardholderName(): UseCardholderNameReturn {
     onBlur,
   };
 }
-
-const TWO_BLANKS = "  ";
-const validator = {
-  onChange: (value: string): IErrorStatus => {
-    if (!/^[A-Z ]+$/.test(value)) {
-      return { isError: true, errorMessage: "소유자명은 영문 대문자만 포함해야 합니다." };
-    }
-
-    return { isError: false, errorMessage: null };
-  },
-
-  onBlur: (value: string): IErrorStatus => {
-    if (!/^[A-Z ]+$/.test(value)) {
-      return { isError: true, errorMessage: "소유자명은 영문 대문자만 포함해야 합니다." };
-    }
-
-    if (value.trim() !== value) {
-      return { isError: true, errorMessage: "소유자명 양 끝에 공백이 포함될 수 없습니다." };
-    }
-
-    if (value.includes(TWO_BLANKS)) {
-      return {
-        isError: true,
-        errorMessage: "소유자명의 사이 공백은 최대 한 칸 입력할 수 있습니다",
-      };
-    }
-    return { isError: false, errorMessage: null };
-  },
-};
