1ce05fa36fb01bda020465b0f3609f39589fdca7
river <jenn0.6n@gmail.com>
2024-05-07 03:09:44 +0900

    refactor: CloseButtonProps에 children 제공 유틸 사용

--- components/src/lib/CloseButton/CloseButton.tsx
+++ components/src/lib/CloseButton/CloseButton.tsx
@@ -2,11 +2,13 @@ import { MouseEventHandler } from "react";
 import { ButtonWrapper } from "./CloseButton.style";
 
 interface CloseButtonProps {
-  children: React.ReactNode | string;
   onClick: MouseEventHandler<HTMLButtonElement>;
 }
 
-const CloseButton: React.FC<CloseButtonProps> = ({ children, onClick }) => {
+const CloseButton: React.FC<React.PropsWithChildren<CloseButtonProps>> = ({
+  children,
+  onClick,
+}) => {
   return <ButtonWrapper onClick={onClick}>{children}</ButtonWrapper>;
 };
 
