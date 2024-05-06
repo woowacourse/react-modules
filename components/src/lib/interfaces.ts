ae5f901997cf874e85838c22675855fbb893aa30
greetings1012 <ahnqwerty@gmail.com>
2024-05-06 22:30:43 +0900

    refactor: 과도한 사용자 선택권 제거

--- components/src/lib/interfaces.ts
+++ components/src/lib/interfaces.ts
@@ -1,30 +1,9 @@
-export interface TitleProps {
-  content: string;
-  position?: 'left' | 'center';
-  color?: string;
-  fontSize?: string;
-}
-
-export interface SubtitleProps {
-  content: string;
-  color?: string;
-  fontSize?: string;
-}
-
-export interface CloseButtonProps {
-  onClose: () => void;
-}
-
 export interface ConfirmButtonProps {
-  backgroundColor?: string;
-  fontColor?: string;
   content: string;
   onConfirm: () => void;
 }
 
 export interface CancelButtonProps {
-  backgroundColor?: string;
-  fontColor?: string;
   content: string;
   onCancel: () => void;
 }
