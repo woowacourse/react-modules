9466070d0b20f35dad2fc8b8be2e66bbb70e7181
hain-tain <hain7ce@gmail.com>
2024-05-06 14:50:52 +0900

    refactor: textColor, hoverColor util 함수로 분리

--- components/src/lib/Button/Button.styled.ts
+++ components/src/lib/Button/Button.styled.ts
@@ -1,4 +1,5 @@
 import { ButtonSize, ButtonStyle, ButtonWidth } from './Button';
+import { hoverColor, textColor } from '../utils/calculateColor';
 
 import styled from 'styled-components';
 
@@ -6,35 +7,6 @@ const BUTTON_SIZE_TABLE = {
   small: { width: '100px', height: '32px', fontSize: '12px' },
   medium: { width: '160px', height: '44px', fontSize: '15px' },
   large: { width: '240px', height: '56px', fontSize: '18px' },
-};
-
-const textColor = (primaryColor: string) => {
-  const bigint = parseInt(primaryColor.slice(1, 7), 16);
-  const r = (bigint >> 16) & 255;
-  const g = (bigint >> 8) & 255;
-  const b = bigint & 255;
-
-  if (r + g + b <= 384) return '#FFFFFF';
-  return '#000000';
-};
-
-const hoverColor = (backgroundColor: string) => {
-  const bigint = parseInt(backgroundColor.slice(1, 7), 16);
-  const r = (bigint >> 16) & 255;
-  const g = (bigint >> 8) & 255;
-  const b = bigint & 255;
-
-  const adjustColor = (color: number, adjustment: number) => {
-    const newColor = color + adjustment;
-    return Math.min(255, Math.max(0, newColor)).toString(16).padStart(2, '0');
-  };
-
-  const adjustment = r + g + b <= 384 ? 24 : -24;
-  const R = adjustColor(r, adjustment);
-  const G = adjustColor(g, adjustment);
-  const B = adjustColor(b, adjustment);
-
-  return '#' + R + G + B;
 };
 
 const BUTTON_STYLE_TABLE = (primaryColor: string) => {
