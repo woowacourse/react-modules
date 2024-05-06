450ef52be73e1702059fda8774ccc4ee2f69967a
hwinkr <dnddl8280@naver.com>
2024-05-06 19:18:32 +0900

    chore(useCardpassword): prettier 설정 변경 적용 (printWidth : 150)

--- hooks/src/lib/useCardPassword/useCardPasswordValidation.ts
+++ hooks/src/lib/useCardPassword/useCardPasswordValidation.ts
@@ -2,10 +2,7 @@ import { useState } from "react";
 
 import cardInputValidator from "../validators/cardInputValidator";
 
-import {
-  INPUT_RULES,
-  VALIDATION_MESSAGES,
-} from "../constants/card-custom-hook";
+import { INPUT_RULES, VALIDATION_MESSAGES } from "../constants/card-custom-hook";
 
 const useCardPasswordValidation = () => {
   const [errorState, setErrorState] = useState(false);
@@ -13,29 +10,23 @@ const useCardPasswordValidation = () => {
   const [errorText, setErrorText] = useState("");
 
   const validateCardPassword = (value: string) => {
-    const isNumericInputValid = cardInputValidator.validateNumericInput(value);
-
-    if (!isNumericInputValid) {
+    if (!cardInputValidator.validateNumericInput(value)) {
       setErrorText(VALIDATION_MESSAGES.onlyNumbersAllowed);
-      setErrorState(!isNumericInputValid);
+      setErrorState(true);
 
       return false;
     }
 
-    const isValidCardPasswordLength = cardInputValidator.validateInputLength(
-      value,
-      INPUT_RULES.validCardPasswordNumberLength
-    );
-
-    if (!isValidCardPasswordLength) {
+    if (!cardInputValidator.validateInputLength(value, INPUT_RULES.validCardPasswordNumberLength)) {
       setErrorText(VALIDATION_MESSAGES.invalidCardPassword);
-      setErrorState(!isValidCardPasswordLength);
+      setErrorState(true);
 
       return true;
     }
 
     setErrorState(false);
     setErrorText("");
+
     return true;
   };
 
