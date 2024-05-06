f41035bed3e98ad14e17e8627b048a702ca775b3
badahertz52 <badahertz52@gmail.com>
2024-05-06 15:09:38 +0900

    chore[components]: vite-plugin-libcss 삭제

--- components/vite.config.ts
+++ components/vite.config.ts
@@ -2,7 +2,6 @@ import react from '@vitejs/plugin-react-swc';
 import * as path from 'path';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
-import libCss from 'vite-plugin-libcss';
 
 export default defineConfig({
   build: {
@@ -30,6 +29,5 @@ export default defineConfig({
       insertTypesEntry: true,
       entryRoot: path.resolve(__dirname, 'src/lib'),
     }),
-    libCss(),
   ],
 });
