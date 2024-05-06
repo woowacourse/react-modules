85a25f2ac0c7bf26ccbf1343eef1dee00f3056a2
greetings1012 <ahnqwerty@gmail.com>
2024-05-06 22:29:44 +0900

    feat: Modal 컴포넌트를 서브 컴포넌트로 분리해서 구현

--- components/src/lib/Modal.tsx
+++ components/src/lib/Modal.tsx
@@ -1,17 +1,18 @@
-import { ReactNode } from 'react';
-import styles from './Modal.module.css';
-import { CloseButtonImage } from './CloseButtonImage';
-import { CancelButtonProps, CloseButtonProps, ConfirmButtonProps, SubtitleProps, TitleProps } from './interfaces';
+import { ReactNode } from "react";
+import styles from "./Modal.module.css";
+import { CloseButtonImage } from "./CloseButtonImage";
+import { CancelButtonProps, ConfirmButtonProps } from "./interfaces";
+import CancelButton from "./CancelButton";
+import TitleField from "./TitleField";
+import ConfirmButton from "./ConfirmButton";
 
 interface ModalProps {
-  title?: TitleProps;
-  subtitle?: SubtitleProps;
-  closeButton: CloseButtonProps;
+  title?: string;
+  subtitle?: string;
+  onClose: () => void;
   confirmButton?: ConfirmButtonProps;
   cancelButton?: CancelButtonProps;
-  backgroundColor?: string;
-  borderRadius?: string;
-  modalPosition: 'center' | 'bottom';
+  modalPosition: "center" | "bottom";
   preventCloseOnOutsideClick?: boolean;
   children: ReactNode;
 }
@@ -19,111 +20,30 @@ interface ModalProps {
 const Modal = ({
   title,
   subtitle,
-  closeButton,
+  onClose,
   confirmButton,
   cancelButton,
-  backgroundColor,
-  borderRadius,
   modalPosition,
   preventCloseOnOutsideClick,
   children,
 }: ModalProps) => {
-  const drawTitleField = () => {
-    if (title) {
-      if (title.position === 'left') {
-        return (
-          <div className={styles['title-field']}>
-            <h1
-              style={{ fontSize: title.fontSize || '18px', color: title.color || 'black' }}
-              className={styles['title']}
-            >
-              {title.content}
-            </h1>
-            {subtitle && (
-              <h2
-                style={{ fontSize: subtitle.fontSize || '12px', color: subtitle.color || 'grey' }}
-                className={styles['subtitle']}
-              >
-                {subtitle.content}
-              </h2>
-            )}
-          </div>
-        );
-      } else {
-        return (
-          <div className={styles['title-field']}>
-            <h1
-              style={{ textAlign: 'center', color: title.color || 'black', fontSize: title.fontSize || '18px' }}
-              className={styles['title']}
-            >
-              {title.content}
-            </h1>
-            {subtitle && (
-              <h2
-                style={{ textAlign: 'center', color: subtitle.color || 'grey', fontSize: subtitle.fontSize || '12px' }}
-                className={styles['subtitle']}
-              >
-                {subtitle.content}
-              </h2>
-            )}
-          </div>
-        );
-      }
-    }
-  };
-
-  const drawConfirmButton = () => {
-    if (confirmButton)
-      return (
-        <button
-          className={styles['button-confirm']}
-          style={{
-            color: `${confirmButton.fontColor || 'white'}`,
-            backgroundColor: `${confirmButton.backgroundColor || 'black'}`,
-          }}
-          onClick={confirmButton.onConfirm}
-        >
-          {confirmButton.content}
-        </button>
-      );
-  };
-
-  const drawCancelButton = () => {
-    if (cancelButton)
-      return (
-        <button
-          className={styles['button-cancel']}
-          style={{
-            color: `${cancelButton.fontColor || 'grey'}`,
-            backgroundColor: `${cancelButton.backgroundColor || 'lightgrey'}`,
-          }}
-          onClick={cancelButton.onCancel}
-        >
-          {cancelButton.content}
-        </button>
-      );
-  };
-
   return (
     <>
-      <div onClick={preventCloseOnOutsideClick ? () => {} : closeButton.onClose} className={styles['backdrop']} />
       <div
-        style={{
-          backgroundColor: `${backgroundColor || 'white'}`,
-          borderRadius: `${borderRadius || '5px'}`,
-        }}
-        className={styles[`container-${modalPosition}`]}
-      >
-        <div className={styles['header']}>
-          <span className={styles['button-close']} onClick={closeButton.onClose}>
+        onClick={preventCloseOnOutsideClick ? () => {} : onClose}
+        className={styles["backdrop"]}
+      />
+      <div className={styles[`container-${modalPosition}`]}>
+        <div className={styles["header"]}>
+          <span className={styles["button-close"]} onClick={onClose}>
             <CloseButtonImage />
           </span>
-          {drawTitleField()}
+          {title && <TitleField title={title} subtitle={subtitle} />}
         </div>
         {children}
-        <div className={styles['button-container']}>
-          {drawCancelButton()}
-          {drawConfirmButton()}
+        <div className={styles["button-container"]}>
+          <CancelButton cancelButton={cancelButton} />
+          <ConfirmButton confirmButton={confirmButton} />
         </div>
       </div>
     </>
