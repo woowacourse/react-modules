4f4a67f3e78eaf17d60c50395395de679299fffe
soosoo22 <lllllh00@naver.com>
2024-05-07 06:21:15 +0900

    chore: 모달 컴포넌트 npm 배포 후 테스트

--- components/src/App.tsx
+++ components/src/App.tsx
@@ -3,23 +3,38 @@ import React, { useState } from "react";
 import { Modal } from "soosoo-react-payments-components";
 
 function App() {
-  const [isOpen, setIsOpen] = useState(true);
+  const [isOpen, setIsOpen] = useState(false);
 
-  const onClose = () => {
+  const handleClose = () => {
     setIsOpen(false);
+  };
+
+  const handleConfirm = () => {
+    console.log("확인");
+  };
+
+  const handleCancel = () => {
+    console.log("취소");
   };
 
   return (
     <>
-      {isOpen && (
-        <Modal
-          position="center"
-          title="제목"
-          children={<></>}
-          onClose={onClose}
-          isCloseButton={true}
-        />
-      )}
+      <button
+        onClick={() => setIsOpen(true)}
+        style={{ background: "blue", width: "200px", height: "100px" }}
+      >
+        모달 오픈
+      </button>
+      <Modal
+        position="center"
+        title={{ position: "left", content: "🍀호프는 몇 살일까?🍀" }}
+        children={<>29살 추정ㅎㅎ</>}
+        isOpen={isOpen}
+        onClose={handleClose}
+        closeButton={{ onClose: handleClose }}
+        confirmButton={{ content: "확인", onConfirm: handleConfirm }}
+        cancelButton={{ content: "취소", onCancel: handleCancel }}
+      />
     </>
   );
 }
