c5832ac5b8626646dfb20db4a86968356de45c13
chysis <chysiss@naver.com>
2024-05-07 23:15:18 +0900

    refactor: 모달에 합성 컴포넌트 적용

--- components/src/lib/index.ts
+++ components/src/lib/index.ts
@@ -1,1 +1,14 @@
-export { default as Modal } from './Modal';
+import ModalMain from './ModalMain/ModalMain';
+import CancelButton from './CancelButton/CancelButton';
+import ConfirmButton from './ConfirmButton/ConfirmButton';
+import ModalContent from './ModalContent/ModalContent';
+import ModalFooter from './ModalFooter/ModalFooter';
+import ModalHeader from './ModalHeader/ModalHeader';
+
+export const Modal = Object.assign(ModalMain, {
+  Header: ModalHeader,
+  Content: ModalContent,
+  Footer: ModalFooter,
+  CancelButton: CancelButton,
+  ConfirmButton: ConfirmButton,
+});
