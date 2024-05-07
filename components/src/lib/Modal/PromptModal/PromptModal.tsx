326dd168c15b526ddfc5e0da723fa164afa764ff
anttiey <hanuuny@gmail.com>
2024-05-07 20:44:44 +0900

    feat(PromptModal): PromptModal 컴포넌트 생성

---
+++ components/src/lib/Modal/PromptModal/PromptModal.tsx
@@ -0,0 +1,33 @@
+import MainModal from '../MainModal/MainModal';
+import Title from '../../components/Title/Title';
+import Input from '../../components/Input/Input';
+import Button from '../../components/Button/Button';
+import { ModalHeader, ModalBody, ModalFooter } from '../../components/ModalLayout/ModalLayout';
+
+import type { ModalProps } from '../../types/common';
+
+interface PromptModalProps extends ModalProps {
+  headerText: string;
+  inputValue: string;
+  onInputChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onConfirm: () => void;
+}
+
+const PromptModal = ({ headerText, inputValue, onInputChange, onConfirm, ...modalProps }: PromptModalProps) => {
+  return (
+    <MainModal {...modalProps}>
+      <ModalHeader>
+        <Title>{headerText}</Title>
+      </ModalHeader>
+      <ModalBody>
+        <Input type="text" value={inputValue} onChange={onInputChange} />
+      </ModalBody>
+      <ModalFooter align="right">
+        <Button type="button" text="취소" mode="secondary" size="sm" onClick={modalProps.close}></Button>
+        <Button type="button" text="확인" size="sm" onClick={onConfirm}></Button>
+      </ModalFooter>
+    </MainModal>
+  );
+};
+
+export default PromptModal;
--- components/src/lib/index.ts
+++ components/src/lib/index.ts
@@ -1,4 +1,5 @@
 import Title from './components/Title/Title';
+import Input from './components/Input/Input';
 import Button from './components/Button/Button';
 import CloseButton from './components/CloseButton/CloseButton';
 import { ModalHeader, ModalBody, ModalFooter } from './components/ModalLayout/ModalLayout';
@@ -6,6 +7,7 @@ import MainModal from './Modal/MainModal/MainModal';
 
 export const Modal = Object.assign(MainModal, {
   Title: Title,
+  Input: Input,
   Button: Button,
   CloseButton: CloseButton,
   Header: ModalHeader,
@@ -15,3 +17,4 @@ export const Modal = Object.assign(MainModal, {
 
 export { default as AlertModal } from './Modal/AlertModal/AlertModal';
 export { default as ConfirmModal } from './Modal/ConfirmModal/ConfirmModal';
+export { default as PromptModal } from './Modal/PromptModal/PromptModal';
