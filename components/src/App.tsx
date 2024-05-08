9aa4d16ced6033dc7f34e59554a4637961fa2535
cys4585 <dev.youngsu@gmail.com>
2024-05-08 22:23:16 +0900

    test: zIndex 적용된 패키지 버전 테스트

--- components/src/App.tsx
+++ components/src/App.tsx
@@ -39,6 +39,7 @@ function App() {
       <Modal
         isOpen={isOpenCenterModal}
         position="center"
+        zIndex={100}
         onClose={() => setIsOpenCenterModal(false)}
       >
         <Modal.Header
