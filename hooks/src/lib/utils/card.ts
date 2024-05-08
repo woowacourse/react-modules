c4403f2c0db309a8a636d2b533777f99ead35120
anttiey <hanuuny@gmail.com>
2024-05-08 17:27:12 +0900

    refactor: 카드 포맷팅 로직 간소화

--- hooks/src/lib/utils/card.ts
+++ hooks/src/lib/utils/card.ts
@@ -28,48 +28,17 @@ export const getCardType = (cardNumber: string): CardType => {
   return 'DEFAULT';
 };
 
-export const getValidCardNumberLength = (cardType: CardType) => {
-  if (cardType === 'DINERS') return 14;
-  if (cardType === 'AMEX') return 15;
-
-  return 16;
-};
-
 export const formatCardNumber = (cardType: CardType, cardNumber: string) => {
-  const cardNumberCopy = cardNumber;
-
   if (cardType === 'DINERS' || cardType === 'AMEX') {
-    if (cardNumberCopy.length > 4 && cardNumberCopy.length <= 10) {
-      return `${cardNumberCopy.slice(0, 4)} ${cardNumberCopy.slice(4, cardNumberCopy.length)}`;
-    }
-
-    if (cardNumberCopy.length > 10) {
-      return `${cardNumberCopy.slice(0, 4)} ${cardNumberCopy.slice(4, 10)} ${cardNumberCopy.slice(
-        10,
-        cardNumberCopy.length
-      )}`;
-    }
-
-    return cardNumberCopy;
+    const formatted = [cardNumber.slice(0, 4), cardNumber.slice(4, 10), cardNumber.slice(10)];
+    return formatted.filter((el) => el).join(' ');
   }
 
-  if (cardNumberCopy.length > 4 && cardNumberCopy.length <= 8) {
-    return `${cardNumberCopy.slice(0, 4)} ${cardNumberCopy.slice(4, cardNumberCopy.length)}`;
-  }
-
-  if (cardNumberCopy.length > 8 && cardNumberCopy.length <= 12) {
-    return `${cardNumberCopy.slice(0, 4)} ${cardNumberCopy.slice(4, 8)} ${cardNumberCopy.slice(
-      8,
-      cardNumberCopy.length
-    )}`;
-  }
-
-  if (cardNumberCopy.length > 12) {
-    return `${cardNumberCopy.slice(0, 4)} ${cardNumberCopy.slice(4, 8)} ${cardNumberCopy.slice(
-      8,
-      12
-    )} ${cardNumberCopy.slice(12, cardNumberCopy.length)}`;
-  }
-
-  return cardNumberCopy;
+  const formatted = [
+    cardNumber.slice(0, 4),
+    cardNumber.slice(4, 8),
+    cardNumber.slice(8, 12),
+    cardNumber.slice(12),
+  ];
+  return formatted.filter((el) => el).join(' ');
 };
