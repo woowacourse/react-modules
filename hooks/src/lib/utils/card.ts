8c8856b0f0975dae83017769c89b99d466d29503
anttiey <hanuuny@gmail.com>
2024-05-08 16:31:51 +0900

    refactor: 카드 타입 판별 로직 간소화

--- hooks/src/lib/utils/card.ts
+++ hooks/src/lib/utils/card.ts
@@ -1,43 +1,28 @@
 export type CardType = 'DEFAULT' | 'VISA' | 'MASTERCARD' | 'DINERS' | 'AMEX' | 'UNIONPAY';
 
-export const getCardType = (cardNumber: string) => {
+export const getCardType = (cardNumber: string): CardType => {
   const firstDigits = cardNumber.charAt(0);
+  const secondDigits = cardNumber.slice(0, 2);
+  const thirdDigits = cardNumber.slice(0, 3);
+  const fourthDigits = cardNumber.slice(0, 4);
+  const sixthDigits = cardNumber.slice(0, 6);
+
+  if (firstDigits === '4') return 'VISA';
 
   if (firstDigits === '3') {
-    if (cardNumber.length < 2) return 'DEFAULT';
-
-    const secondDigits = cardNumber.substring(0, 2);
-
     if (secondDigits === '36') return 'DINERS';
     if (secondDigits === '34' || secondDigits === '37') return 'AMEX';
   }
 
-  if (firstDigits === '4') return 'VISA';
-
-  if (firstDigits === '5') {
-    if (cardNumber.length < 2) return 'DEFAULT';
-
-    const secondDigits = cardNumber.substring(0, 2);
-
-    if (Number(secondDigits) >= 51 && Number(secondDigits) <= 55) return 'MASTERCARD';
-  }
+  if (firstDigits === '5' && Number(secondDigits) >= 51 && Number(secondDigits) <= 55)
+    return 'MASTERCARD';
 
   if (firstDigits === '6') {
-    if (cardNumber.length < 3) return 'DEFAULT';
-
-    const thirdDigits = cardNumber.substring(0, 3);
-
-    if (thirdDigits === '622') {
-      const sixthDigits = cardNumber.substring(0, 6);
-      if (Number(sixthDigits) >= 622126 && Number(sixthDigits) <= 622925) return 'UNIONPAY';
-    }
-
-    if (thirdDigits === '628') {
-      const fourthDigits = cardNumber.substring(0, 4);
-      if (Number(fourthDigits) >= 6282 && Number(fourthDigits) >= 6288) return 'UNIONPAY';
-    }
-
-    if (Number(thirdDigits) >= 624 || Number(thirdDigits) <= 626) return 'UNIONPAY';
+    if (Number(thirdDigits) >= 624 && Number(thirdDigits) <= 626) return 'UNIONPAY';
+    if (thirdDigits === '622' && Number(sixthDigits) >= 622126 && Number(sixthDigits) <= 622925)
+      return 'UNIONPAY';
+    if (thirdDigits === '628' && Number(fourthDigits) >= 6282 && Number(fourthDigits) >= 6288)
+      return 'UNIONPAY';
   }
 
   return 'DEFAULT';
