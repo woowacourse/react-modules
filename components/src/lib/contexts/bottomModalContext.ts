8d868faf5f647a7e434128a79db64f83e52d1f69
badahertz52 <badahertz52@gmail.com>
2024-05-08 10:37:59 +0900

    docs: 불필요한 파일 삭제 및 파일명 변경을 위한 파일 삭제
    - 파일명의 대소문자 변경을 위한 파일 삭제

--- components/src/lib/contexts/bottomModalContext.ts
+++
@@ -1,9 +0,0 @@
-import { createContext } from 'react';
-
-export interface BottomModalContextType {
-  handleCloseModal: () => void;
-}
-
-const BottomModalContext = createContext<BottomModalContextType | null>(null);
-
-export default BottomModalContext;
--- components/src/lib/contexts/modalContainerContext.ts
+++
@@ -1,8 +0,0 @@
-import { createContext } from 'react';
-
-import { ModalContainerProps } from '../types/modal';
-
-interface ModalContainerContextType extends Omit<ModalContainerProps, 'children' | 'openModal'> {}
-const ModalContainerContext = createContext<ModalContainerContextType | null>(null);
-
-export default ModalContainerContext;
