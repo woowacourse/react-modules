480893075c4427f59b6a8a6ace7840d0932d73b6
00kang <kdb070970@gmail.com>
2024-05-08 16:22:46 +0900

    refactor : 커스텀 훅 수정에 따른 테스트 코드 수정

--- hooks/__tests__/useCardIssuer.test.ts
+++ hooks/__tests__/useCardIssuer.test.ts
@@ -6,9 +6,9 @@ describe("useCardIssuer 테스트", () => {
   it("touched 상태인데 입력값이 비어있다면 에러 메시지를 반환해야 한다", () => {
     const { result } = renderHook(() => useCardIssuer());
     act(() => {
-      result.current[1]("");
+      result.current.handleCardIssuerChange("");
     });
-    expect(result.current[2].isValid).toBe(false);
-    expect(result.current[2].errorMessage).toBe(ERROR_MESSAGE.NO_INPUT);
+    expect(result.current.cardIssuerValidation.isValid).toBe(false);
+    expect(result.current.cardIssuerValidation.errorMessage).toBe(ERROR_MESSAGE.NO_INPUT);
   });
 });
--- hooks/__tests__/usePassword.test.ts
+++ hooks/__tests__/usePassword.test.ts
@@ -6,35 +6,39 @@ describe("usePassword 테스트", () => {
   it("touched 상태인데 입력값이 비어있다면 에러 메시지를 반환해야 한다", () => {
     const { result } = renderHook(() => usePassword());
     act(() => {
-      result.current[1]("");
+      result.current.handleCardPasswordChange("");
     });
-    expect(result.current[2].isValid).toBe(false);
-    expect(result.current[2].errorMessage).toBe(ERROR_MESSAGE.NO_INPUT);
+    expect(result.current.cardPasswordValidation.isValid).toBe(false);
+    expect(result.current.cardPasswordValidation.errorMessage).toBe(ERROR_MESSAGE.NO_INPUT);
   });
 
   it("입력값이 숫자가 아니라면 에러 메시지를 반환해야 한다", () => {
     const { result } = renderHook(() => usePassword());
     act(() => {
-      result.current[1]("jo22222");
+      result.current.handleCardPasswordChange("jo22222");
     });
-    expect(result.current[2].isValid).toBe(false);
-    expect(result.current[2].errorMessage).toBe(ERROR_MESSAGE.CARD_PASSWORD.INVALID_CHARACTERS);
+    expect(result.current.cardPasswordValidation.isValid).toBe(false);
+    expect(result.current.cardPasswordValidation.errorMessage).toBe(
+      ERROR_MESSAGE.CARD_PASSWORD.INVALID_CHARACTERS
+    );
   });
 
   it("입력값이 2자리 숫자가 아니라면 에러 메시지를 반환해야 한다", () => {
     const { result } = renderHook(() => usePassword());
     act(() => {
-      result.current[1]("222");
+      result.current.handleCardPasswordChange("222");
     });
-    expect(result.current[2].isValid).toBe(false);
-    expect(result.current[2].errorMessage).toBe(ERROR_MESSAGE.CARD_PASSWORD.MAX_LENGTH_EXCEEDED);
+    expect(result.current.cardPasswordValidation.isValid).toBe(false);
+    expect(result.current.cardPasswordValidation.errorMessage).toBe(
+      ERROR_MESSAGE.CARD_PASSWORD.MAX_LENGTH_EXCEEDED
+    );
   });
 
   it("입력값이 유효하다면 isValid가 true여야 한다", () => {
     const { result } = renderHook(() => usePassword());
     act(() => {
-      result.current[1]("12");
+      result.current.handleCardPasswordChange("12");
     });
-    expect(result.current[2].isValid).toBe(true);
+    expect(result.current.cardPasswordValidation.isValid).toBe(true);
   });
 });
