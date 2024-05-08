bf0e3f532fa6eafa88109561bde89e2a9daa2de8
pakxe <pigkill40@naver.com>
2024-05-08 16:06:15 +0900

    feat: 카드 타입을 반환하는 함수 구현

---
+++ hooks/src/lib/utils/detectCardType.ts
@@ -0,0 +1,14 @@
+import { CARD_TYPE } from '../constants/cardType';
+import { CardType } from '../types/cardType';
+
+const detectCardType = (cardNumber: string): CardType => {
+  if (new RegExp(CARD_TYPE.VISA.REGEX).test(cardNumber)) return 'VISA';
+  if (new RegExp(CARD_TYPE.MASTER.REGEX).test(cardNumber)) return 'MASTER';
+  if (new RegExp(CARD_TYPE.DINERS.REGEX).test(cardNumber)) return 'DINERS';
+  if (new RegExp(CARD_TYPE.AMEX.REGEX).test(cardNumber)) return 'AMEX';
+  if (new RegExp(CARD_TYPE.UNION_PAY.REGEX).test(cardNumber)) return 'UNION_PAY';
+
+  return 'UNKNOWN';
+};
+
+export default detectCardType;
