66a1cc3eda0718ea3659a98c9547f5e26ce03093
이태훈 <rhymint@gmail.com>
2024-05-08 22:44:50 +0900

    feat: Usage를 위한 App.tsx 변경

--- components/src/App.tsx
+++ components/src/App.tsx
@@ -1,10 +1,13 @@
 import Button from './lib/Button/Button';
 
-import Modal from './lib/Modal/Modal';
+import { Modal, AlertModal, ConfirmModal, PromptModal } from './lib/index'
 import React, { useState } from 'react';
 
 const App = () => {
   const [modalOpened, setModalOpened] = useState(false)
+  const [alertModalOpened, setAlertModalOpened] = useState(false)
+  const [confirmModalOpened, setConfirmModalOpened] = useState(false)
+  const [promptModalOpened, setPromptModalOpened] = useState(false)
 
   const handleModalOpen = () => {
     setModalOpened(true)
@@ -14,16 +17,69 @@ const App = () => {
     setModalOpened(false)
   }
 
+  const handleAlertModalOpen = () => {
+    setAlertModalOpened(true)
+  }
+
+  const handleAlertModalClose = () => {
+    setAlertModalOpened(false)
+  }
+
+  const handleConfirmModalOpen = () => {
+    setConfirmModalOpened(true)
+  }
+
+  const handleConfirmModalClose = () => {
+    setConfirmModalOpened(false)
+  }
+
+  const handlePromptModalOpen = () => {
+    setPromptModalOpened(true)
+  }
+
+  const handlePromptModalClose = () => {
+    setPromptModalOpened(false)
+  }
+
+
   return (
     <>
-      <Button
-        text="open modal"
-        onClick={handleModalOpen}
-        size="large"
-        width="full"
-        buttonStyle="primary"
-        primaryColor="#1C77C1"
-      />
+      <div style={{ display: 'flex', flexDirection: 'column', gap: '16px' }}>
+        <Button
+          text="open modal"
+          onClick={handleModalOpen}
+          size="large"
+          width="full"
+          buttonStyle="primary"
+          primaryColor="#1C77C1"
+        />
+        <Button
+          text="open alert modal"
+          onClick={handleAlertModalOpen}
+          size="large"
+          width="full"
+          buttonStyle="primary"
+          primaryColor="#1C77C1"
+        />
+        <Button
+          text="open confirm modal"
+          onClick={handleConfirmModalOpen}
+          size="large"
+          width="full"
+          buttonStyle="primary"
+          primaryColor="#1C77C1"
+        />
+        <Button
+          text="open prompt modal"
+          onClick={handlePromptModalOpen}
+          size="large"
+          width="full"
+          buttonStyle="primary"
+          primaryColor="#1C77C1"
+        />
+      </div>
+
+
       <Modal
         isOpened={modalOpened}
         onClose={handleModalClose}
@@ -56,6 +112,35 @@ const App = () => {
           Children Area
         </div>
       </Modal>
+
+      <AlertModal
+        isOpened={alertModalOpened}
+        onClose={handleAlertModalClose}
+        zIndex={300}
+        title="Todal Modal"
+        description="This is for woowacourse mission"
+        showCloseButton={false}
+      />
+
+      <ConfirmModal
+        isOpened={confirmModalOpened}
+        onClose={handleConfirmModalClose}
+        onConfirm={() => alert('confirmed!')}
+        zIndex={300}
+        title="Todal Modal"
+        description="This is for woowacourse mission"
+        showCloseButton={false}
+      />
+
+      <PromptModal
+        isOpened={promptModalOpened}
+        onClose={handlePromptModalClose}
+        onConfirm={(value) => alert(`value is '${value}'`)}
+        zIndex={300}
+        title="Todal Modal"
+        description="This is for woowacourse mission"
+        showCloseButton={false}
+      />
     </>
   );
 }
