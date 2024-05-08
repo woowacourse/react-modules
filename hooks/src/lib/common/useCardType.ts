cdc28c4e27eb9291d2dc3b978909f842858d02e2
Largopie <106071687+Largopie@users.noreply.github.com>
2024-05-08 15:54:33 +0900

    refactor(useCardType): 길이 검사 로직 삭제

--- hooks/src/lib/common/useCardType.ts
+++ hooks/src/lib/common/useCardType.ts
@@ -1,24 +1,20 @@
 import { useEffect, useState } from 'react';
 import Validation from '../utils/validation';
 
-const VISA_LENGTH = 16;
-
 const useCardType = (cardNumbers: string[]) => {
   const [cardBrand, setCardBrand] = useState('');
 
   useEffect(() => {
     const cardNumber = cardNumbers.join('');
 
-    if (Validation.isExactLength(VISA_LENGTH, cardNumber)) {
-      if (Validation.isVisa(cardNumber)) {
-        setCardBrand('visa');
-        return;
-      }
+    if (Validation.isVisa(cardNumber)) {
+      setCardBrand('visa');
+      return;
+    }
 
-      if (Validation.isMasterCard(cardNumber)) {
-        setCardBrand('mastercard');
-        return;
-      }
+    if (Validation.isMasterCard(cardNumber)) {
+      setCardBrand('mastercard');
+      return;
     }
 
     setCardBrand('');
