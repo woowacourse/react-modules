64924933007b1292e7dd3702ddeba70cd0745e66
이태훈 <rhymint@gmail.com>
2024-05-08 21:18:49 +0900

    feat: ConfirmModal 추가

---
+++ components/src/lib/Modal/storybook/ConfirmModal.size.stories.tsx
@@ -0,0 +1,56 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import ConfirmModal from '../ConfirmModal';
+
+const meta = {
+  title: 'ConfirmModal/size',
+  component: ConfirmModal,
+} satisfies Meta<typeof ConfirmModal>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Small: Story = {
+  args: {
+    isOpened: true,
+    onClose: () => {
+      alert('모달 닫기');
+    },
+    onConfirm: () => {
+      alert('확인')
+    },
+    title: '제목입니다',
+    description: '설명입니다',
+  },
+};
+
+export const Medium: Story = {
+  args: {
+    size: 'medium',
+    isOpened: true,
+    onClose: () => {
+      alert('모달 닫기');
+    },
+    onConfirm: () => {
+      alert('확인')
+    },
+    title: '제목입니다',
+    description: '설명입니다',
+  },
+};
+
+export const Large: Story = {
+  args: {
+    size: 'large',
+    isOpened: true,
+    onClose: () => {
+      alert('모달 닫기');
+    },
+    onConfirm: () => {
+      alert('확인')
+    },
+    title: '제목입니다',
+    description: '설명입니다',
+  },
+};
+
