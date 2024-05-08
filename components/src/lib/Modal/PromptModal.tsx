b0965f5fa38f5f6e6b544c757d7fa7c36036a52e
이태훈 <rhymint@gmail.com>
2024-05-08 22:01:45 +0900

    feat: PromptModal 추가

---
+++ components/src/lib/Modal/PromptModal.tsx
@@ -0,0 +1,103 @@
+import Button from '../Button/Button';
+
+import { useEffect, useState } from 'react';
+import ModalDimmedLayer from './ModalDimmedLayer/ModalDimmedLayer';
+import ModalContainer from './ModalContainer/ModalContainer';
+import ModalHeader from './ModalHeader/ModalHeader';
+import ModalTitle from './ModalTitle/ModalTitle';
+import ModalCloseButton from './ModalCloseButton/ModalCloseButton';
+import ModalBody from './ModalBody/ModalBody';
+import ModalDescription from './ModalDescription/ModalDescription';
+import ModalButtonContainer from './ModalButtonContainer/ModalButtonContainer';
+import ModalInputField from './ModalInputField/ModalInputField';
+
+interface PromptModalProps {
+  size?: ModalSize;
+  isOpened: boolean;
+  onClose: () => void;
+  zIndex?: number;
+  title?: string;
+  description?: string;
+  children?: JSX.Element;
+  modalPosition?: ModalPosition;
+  buttonPosition?: ButtonPosition;
+  primaryColor?: string;
+  showCloseButton?: boolean;
+
+  placeholder?: string;
+  onConfirm: (value: string) => void;
+}
+
+const PromptModal = ({
+  size = 'small',
+  isOpened,
+  onClose,
+  zIndex = 0,
+  title = '',
+  description = '',
+  children,
+  modalPosition = 'center',
+  buttonPosition = 'row',
+  primaryColor,
+  showCloseButton = false,
+  placeholder = '',
+  onConfirm,
+}: PromptModalProps) => {
+
+  useEffect(() => {
+    if (isOpened) document.body.style.overflow = 'hidden';
+    else document.body.style.overflow = 'scroll';
+    document.body.addEventListener('keydown', handleKeyDownEsc);
+    return document.body.removeEventListener('keydown', handleKeyDownEsc);
+  }, [isOpened])
+
+  const handleKeyDownEsc = (e: KeyboardEvent) => {
+    if (e.key === 'Escape' && isOpened) {
+      onClose();
+    }
+  };
+
+  const [value, setValue] = useState('');
+
+  return (
+    <>
+      {isOpened && (
+        <ModalDimmedLayer onClick={onClose} zIndex={zIndex}>
+          <ModalContainer
+            size={size}
+            modalPosition={modalPosition}
+          >
+            <ModalHeader>
+              <ModalTitle title={title} />
+              <ModalCloseButton showCloseButton={showCloseButton} onClick={onClose} />
+            </ModalHeader>
+            <ModalBody>
+              <ModalDescription description={description} />
+              <ModalInputField placeholder={placeholder} value={value} onChange={(e) => setValue(e.target.value)} />
+              <>{children}</>
+            </ModalBody>
+            <ModalButtonContainer buttonPosition={buttonPosition}>
+              <Button
+                text={'확인'}
+                onClick={() => onConfirm(value)}
+                size={'small'}
+                buttonStyle={'primary'}
+                primaryColor={
+                  primaryColor ?? '#333333'
+                }
+              />
+              <Button
+                text={'취소'}
+                onClick={onClose}
+                size={'small'}
+                buttonStyle={'border'}
+              />
+            </ModalButtonContainer>
+          </ModalContainer>
+        </ModalDimmedLayer>
+      )}
+    </>
+  );
+};
+
+export default PromptModal;
---
+++ components/src/lib/Modal/storybook/PromptModal.size.stories.tsx
@@ -0,0 +1,62 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import PromptModal from '../PromptModal';
+
+const meta = {
+  title: 'PromptModal/size',
+  component: PromptModal,
+} satisfies Meta<typeof PromptModal>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Small: Story = {
+  args: {
+    isOpened: true,
+    onClose: () => {
+      alert('모달 닫기');
+    },
+    title: '제목입니다',
+    description: '설명입니다',
+
+    onConfirm: (value: string) => {
+      alert(`${value} 전달됨`)
+    },
+    placeholder: 'placeholder',
+  },
+};
+
+export const Medium: Story = {
+  args: {
+    size: 'medium',
+    isOpened: true,
+    onClose: () => {
+      alert('모달 닫기');
+    },
+    title: '제목입니다',
+    description: '설명입니다',
+
+    onConfirm: (value: string) => {
+      alert(`${value} 전달됨`)
+    },
+    placeholder: 'placeholder',
+  },
+};
+
+export const Large: Story = {
+  args: {
+    size: 'large',
+    isOpened: true,
+    onClose: () => {
+      alert('모달 닫기');
+    },
+    title: '제목입니다',
+    description: '설명입니다',
+
+    onConfirm: (value: string) => {
+      alert(`${value} 전달됨`)
+    },
+    placeholder: 'placeholder',
+  },
+};
+
