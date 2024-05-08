0203229852bd3f556fa943d3a4d499ad999b85d1
pakxe <pigkill40@naver.com>
2024-05-08 16:05:14 +0900

    feat: 라이브러리에 Input 컴포넌트 추가

--- components/src/lib/index.ts
+++ components/src/lib/index.ts
@@ -2,6 +2,7 @@ import Title from './components/Title/Title';
 import Button from './components/Button/Button';
 import CloseButton from './components/CloseButton/CloseButton';
 import { ModalBody, ModalFooter, ModalHeader, ModalMain } from './Modal/Modal';
+import Input from './components/Input/Input';
 
 export const Modal = Object.assign(ModalMain, {
   Title: Title,
@@ -10,4 +11,5 @@ export const Modal = Object.assign(ModalMain, {
   Header: ModalHeader,
   Body: ModalBody,
   Footer: ModalFooter,
+  Input: Input,
 });
