e95d4631d9227091b38b8a81cad3e1010b912637
Lee sang Yeop <mma7710@naver.com>
2024-05-09 20:17:02 +0900

    chore(App): 커스텀훅을 테스트할 수 있는 페이지 추가

--- hooks/src/App.tsx
+++ hooks/src/App.tsx
@@ -1,11 +1,80 @@
 import React from "react";
 import "./App.css";
+import useCardNumber from "./lib/useCardNumber/useCardNumber";
+import useCardHolder from "./lib/useCardHolder/useCardHolder";
+import useCVC from "./lib/useCVC/useCVC";
+import usePassword from "./lib/usePassword/usePassword";
+import useExpiryDate from "./lib/useExpiryDate/useExpiryDate";
 
 function App() {
+  const cardNumber = useCardNumber("");
+  const cardHolder = useCardHolder("");
+  const cardCVC = useCVC("");
+  const cardPassword = usePassword("");
+  const cardExpiryDate = useExpiryDate({ month: "", year: "" });
+
   return (
-    <>
-      <h1>Hooks Modules</h1>
-    </>
+    <div>
+      <div>
+        <h1>카드 번호 입력</h1>
+        <input
+          onChange={cardNumber.handleCardNumberChange}
+          onBlur={cardNumber.handleCardNumberBlur}
+          value={cardNumber.inputValue}
+        />
+        <div>brandType: {cardNumber.brandType}</div>
+        <div>errorMessage: {cardNumber.validationResult.errorMessage}</div>
+      </div>
+      <div>
+        <h1>카드 소유자 입력</h1>
+        <input
+          onChange={cardHolder.handleCardHolderChange}
+          onBlur={cardHolder.handleCardHolderBlur}
+          value={cardHolder.inputValue}
+        />
+        <div>errorStatus: {cardHolder.validationResult.isValid}</div>
+        <div>errorMessage: {cardHolder.validationResult.errorMessage}</div>
+      </div>
+      <div>
+        <h1>카드 CVC 입력</h1>
+        <input
+          onChange={cardCVC.handleCvcChange}
+          onBlur={cardCVC.handleCvcBlur}
+          value={cardCVC.inputValue}
+        />
+        <div>errorStatus: {cardCVC.validationResult.isValid}</div>
+        <div>errorMessage: {cardCVC.validationResult.errorMessage}</div>
+      </div>
+      <div>
+        <h1>카드 소유자 패스워드</h1>
+        <input
+          onChange={cardPassword.handlePasswordChange}
+          onBlur={cardPassword.handlePasswordBlur}
+          value={cardPassword.inputValue}
+        />
+        <div>errorStatus: {cardPassword.validationResult.isValid}</div>
+        <div>errorMessage: {cardPassword.validationResult.errorMessage}</div>
+      </div>
+      <div>
+        <h1>카드 유효기간</h1>
+        <input
+          onChange={cardExpiryDate.handleExpiryChange}
+          onBlur={cardExpiryDate.handleExpiryDateBlur}
+          name="month"
+          placeholder="월"
+          value={cardExpiryDate.inputValue.month}
+        />
+        <input
+          onChange={cardExpiryDate.handleExpiryChange}
+          onBlur={cardExpiryDate.handleExpiryDateBlur}
+          name="year"
+          placeholder="연도"
+          value={cardExpiryDate.inputValue.year}
+        />
+        <div>errorStatus: {cardExpiryDate.validationResult.isValid}</div>
+        <div>errorMessage: {cardExpiryDate.validationResult.errorMessage}</div>
+      </div>
+    </div>
   );
 }
 
