89f69fe720b427eaec8936627fb7ff859fc1d477
useon <yoosk4986@naver.com>
2024-05-09 22:53:36 +0900

    feat: 카드 브랜드에 따라 입력 길이 제한하는 기능

--- hooks/src/lib/useCardNumbers.ts
+++ hooks/src/lib/useCardNumbers.ts
@@ -1,18 +1,34 @@
-import { useState, ChangeEvent } from 'react';
+import { useState } from 'react';
 import { validateCardNumber } from '../validators/newCardInputValidator';
+import CardBrandService from '../domain/CardBrandService';
 
-const useCardNumbers = (cardNumbersLength: number) => {
+const useCardNumbers = () => {
   const [cardNumbersInfo, setCardNumbersInfo] = useState({
     cardNumbers: '',
     errorMessage: '',
   });
-  const handleCardNumbers = (event: ChangeEvent<HTMLInputElement>) => {
-    const { value } = event.target;
-    const message = validateCardNumber(value, cardNumbersLength);
+
+  const handleCardNumbers = (value: string) => {
+    const cardBrand = CardBrandService(value);
+    const { errorMessage, errorType } = validateCardNumber(cardBrand, value);
+
+    if (errorType === 'NonNumeric') {
+      setCardNumbersInfo({
+        cardNumbers: cardNumbersInfo.cardNumbers,
+        errorMessage,
+      });
+      return;
+    }
+
+    if (errorType === 'DinersInvalidLength' && value.length > 14) return;
+
+    if (errorType === 'AMEXInvalidLength' && value.length > 15) return;
+
+    if (value.length > 16) return;
 
     setCardNumbersInfo({
       cardNumbers: value,
-      errorMessage: message,
+      errorMessage: errorMessage,
     });
   };
 
