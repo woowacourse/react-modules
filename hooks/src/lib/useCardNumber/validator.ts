9dbd7caa0a024c3634e67d0395a6fd3f518158e8
hanyoung <phanyoung@naver.com>
2024-05-09 22:35:32 +0900

    feat(useCardNumber/validator): 자리 수 오류 문구 수정

--- hooks/src/lib/useCardNumber/validator.ts
+++ hooks/src/lib/useCardNumber/validator.ts
@@ -1,17 +1,18 @@
+import { CARD_BRAND, CARD_NUMBER_LENGTH } from "./constants/cardBrand";
 import { IErrorStatus } from "../useInputValidation";
 import { hasCardBrand } from "./utils/hasCardBrand";
 
-const CARD_NUMBER_LENGTH = 16;
+const DEFAULT_CARD_NUMBER_LENGTH = 16;
 export const cardNumberValidator = {
   onChange: (value: string): IErrorStatus => {
     if (!/^\d*$/.test(value)) {
       return { isError: true, errorMessage: "카드번호는 숫자만 포함해야 합니다." };
     }
 
-    if (value.length > CARD_NUMBER_LENGTH) {
+    if (value.length > DEFAULT_CARD_NUMBER_LENGTH) {
       return {
         isError: true,
-        errorMessage: `카드번호는 ${CARD_NUMBER_LENGTH}자리로 입력해 주세요.`,
+        errorMessage: `카드번호는 ${DEFAULT_CARD_NUMBER_LENGTH}자리로 입력해 주세요. (${CARD_BRAND.Diners} - ${CARD_NUMBER_LENGTH.Diners}자리, ${CARD_BRAND.AMEX} - ${CARD_NUMBER_LENGTH.AMEX}자리 제외)`,
       };
     }
 
@@ -23,10 +24,10 @@ export const cardNumberValidator = {
       return { isError: true, errorMessage: "카드번호는 숫자만 포함해야 합니다." };
     }
 
-    if (!hasCardBrand(value) && value.length !== CARD_NUMBER_LENGTH) {
+    if (!hasCardBrand(value) && value.length !== DEFAULT_CARD_NUMBER_LENGTH) {
       return {
         isError: true,
-        errorMessage: `카드번호는 ${CARD_NUMBER_LENGTH}자리로 입력해 주세요.`,
+        errorMessage: `카드번호는 ${DEFAULT_CARD_NUMBER_LENGTH}자리로 입력해 주세요. (${CARD_BRAND.Diners} - ${CARD_NUMBER_LENGTH.Diners}자리, ${CARD_BRAND.AMEX} - ${CARD_NUMBER_LENGTH.AMEX}자리 제외)`,
       };
     }
 
