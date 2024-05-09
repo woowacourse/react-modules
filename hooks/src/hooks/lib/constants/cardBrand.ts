35b3f6be5bf217e06eedc314987025e45ba7b044
jinsil <jinsil378900@gmail.com>
2024-05-10 00:56:28 +0900

    feat: 카드 브랜드 판별 커스텀 훅 만들기

---
+++ hooks/src/hooks/lib/constants/cardBrand.ts
@@ -0,0 +1,46 @@
+const CARD_BRAND = {
+  VISA: {
+    name: "VISA",
+    patterns: /^4/,
+    format: [4, 4, 4, 4],
+    matchedLength: 16,
+    maxLength: 19,
+  },
+  MASTER_CARD: {
+    name: "MASTER_CARD",
+    patterns: /^5[1-5]/,
+    format: [4, 4, 4, 4],
+    matchedLength: 16,
+    maxLength: 19,
+  },
+  DINERS: {
+    name: "DINERS",
+    patterns: /^36/,
+    format: [4, 6, 4],
+    matchedLength: 14,
+    maxLength: 16,
+  },
+  AMEX: {
+    name: "AMEX",
+    patterns: /^3(4|7)/,
+    format: [4, 6, 5],
+    matchedLength: 15,
+    maxLength: 17,
+  },
+  UNION_PAY: {
+    name: "UNION_PAY",
+    patterns: /^62(212[6-9]|21[3-9][0-9]|2[2-8][0-9][0-9]|29[0-1][0-9]|292[0-5]|[4-6]|8[2-8])/,
+    format: [4, 4, 4, 4],
+    matchedLength: 16,
+    maxLength: 19,
+  },
+  UNDEFINED: {
+    name: "",
+    patterns: /^/,
+    format: [4, 4, 4, 4],
+    matchedLength: 16,
+    maxLength: 19,
+  },
+};
+
+export default CARD_BRAND;
--- hooks/src/hooks/lib/utils/format.ts
+++ hooks/src/hooks/lib/utils/format.ts
@@ -1,18 +1,16 @@
-export const cardNumberFormatter = {
-  default(value: string) {
-    const v = value.replace(/\s+/g, "").replace(/[^0-9]/gi, "");
-    const matches = v.match(/\d{4,16}/g);
-    const match = (matches && matches[0]) || "";
-    const parts = [];
-    for (let i = 0, len = match.length; i < len; i += 4) {
-      parts.push(match.substring(i, i + 4));
-    }
-    if (parts.length) {
-      return parts.join(" ");
-    } else {
-      return value;
-    }
-  },
+export const cardNumberFormatter = (value: string, format: number[]): string => {
+  const cleaned = value.replace(/\s+/g, "").replace(/[^0-9]/gi, "");
+  const parts = [];
+  let index = 0;
+
+  for (let i = 0; i < format.length && index < cleaned.length; i++) {
+    const partLength = format[i];
+    const end = index + partLength > cleaned.length ? cleaned.length : index + partLength;
+    parts.push(cleaned.substring(index, end));
+    index += partLength;
+  }
+
+  return parts.join(" ");
 };
 
 export const cardHolderFormatter = (value: string) => {
