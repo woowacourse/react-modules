a88560dbc2487a16f178caa653a024d71ff52bd1
lurgi <wahoo9040@naver.com>
2024-05-09 10:51:13 +0900

    chore: alert modal, hook app.tsx에 작성

--- components/src/App.tsx
+++ components/src/App.tsx
@@ -1,14 +1,25 @@
 import "./App.css";
-
-import Modal from "./components/Modal/Modal";
+import { AlertModal, useModalAction } from "./lib/index";
 
 function App() {
+  const { handleOpen } = useModalAction();
+
   return (
     <>
       <h1>Component Modules</h1>
-      <Modal position="center" title="카드사 선택" hasConfirmButton={true} closeButtonPosition="top">
+      <button onClick={handleOpen}></button>
+      {/* <Modal.Small
+        buttonAlign="row"
+        position="center"
+        title="카드사 선택"
+        confirmMessage="확인 메세지"
+        cancelMessage="취소"
+        hasConfirmButton={true}
+        closeButtonPosition="bottom"
+      >
         <div>hello, world</div>
-      </Modal>
+      </Modal.Small> */}
+      <AlertModal.Large title="카드번호를 입력하세요."></AlertModal.Large>
     </>
   );
 }
--- hooks/src/App.tsx
+++ hooks/src/App.tsx
@@ -1,10 +1,59 @@
-import React from "react";
+import React, { ChangeEvent } from "react";
 import "./App.css";
 
+import { useCardNumbers, useRegister } from "./lib";
+
+const DEFAULT_INPUT_LENGTH = 4;
+
 function App() {
+  const {
+    numbers: { firstState, secondState, thirdState, fourthState },
+    errorList: [firstErrorType, secondErrorType, thirdErrorType, fourthErrorType],
+    cardBrand,
+    inputMaxLengthList,
+  } = useCardNumbers();
+
+  const { errorType: firstErrType, ...firstNumberAttrs } = useRegister("firstNumber", {
+    value: firstState[0],
+    onChange: (e: ChangeEvent<HTMLInputElement>) => firstState[1](e.target.value),
+    customType: "number",
+    required: true,
+    maxLength: DEFAULT_INPUT_LENGTH,
+  });
+
+  const { errorType: secondErrType, ...secondNumberAttrs } = useRegister("secondNumber", {
+    value: secondState[0],
+    onChange: (e: ChangeEvent<HTMLInputElement>) => secondState[1](e.target.value),
+    customType: "number",
+    maxLength: inputMaxLengthList ? inputMaxLengthList[1] : DEFAULT_INPUT_LENGTH,
+  });
+
+  const { errorType: thirdErrType, ...thirdNumberAttrs } = useRegister("secondNumber", {
+    value: thirdState[0],
+    onChange: (e: ChangeEvent<HTMLInputElement>) => thirdState[1](e.target.value),
+    customType: "number",
+    maxLength: inputMaxLengthList ? inputMaxLengthList[2] : DEFAULT_INPUT_LENGTH,
+  });
+
+  const { errorType: fourthErrType, ...fourthNumberAttrs } = useRegister("secondNumber", {
+    value: fourthState[0],
+    onChange: (e: ChangeEvent<HTMLInputElement>) => fourthState[1](e.target.value),
+    customType: "number",
+    maxLength: inputMaxLengthList ? inputMaxLengthList[3] : DEFAULT_INPUT_LENGTH,
+  });
+
   return (
     <>
       <h1>Hooks Modules</h1>
+      <div>cardBrand : {cardBrand}</div>
+      <input {...firstNumberAttrs}></input>
+      <div>{firstErrType ?? firstErrorType}</div>
+      <input {...secondNumberAttrs}></input>
+      <div>{secondErrorType ?? secondErrType}</div>
+      <input {...thirdNumberAttrs}></input>
+      <div>{thirdErrType ?? thirdErrorType}</div>
+      <input {...fourthNumberAttrs} disabled={!(inputMaxLengthList && inputMaxLengthList[3])}></input>
+      <div>{fourthErrType ?? fourthErrorType}</div>
     </>
   );
 }
