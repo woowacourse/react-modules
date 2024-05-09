262addb19f427f5a45aab0a52c835633bf0e8b18
rbgksqkr <rbgks1937@naver.com>
2024-05-09 11:47:56 +0900

    refactor: validate 값을 객체로 묶어 복잡한 return 문 제거

--- hooks/src/lib/useCard/useCardBrand.ts
+++ hooks/src/lib/useCard/useCardBrand.ts
@@ -6,63 +6,73 @@ import {
   validateVisa,
 } from '../validator/validateCardBrand';
 
+interface CardChecker {
+  validate: (cardNumber: string) => boolean;
+  format: number[];
+  cardBrand: string;
+}
+
+const cardBrandChecker: Record<string, CardChecker[]> = {
+  14: [
+    {
+      validate: validateDiners,
+      format: [4, 6, 4],
+      cardBrand: 'Diners',
+    },
+  ],
+  15: [
+    {
+      validate: validateAMEX,
+      format: [4, 6, 5],
+      cardBrand: 'AMEX',
+    },
+  ],
+  16: [
+    {
+      validate: validateVisa,
+      format: [4, 4, 4, 4],
+      cardBrand: '비자카드',
+    },
+    {
+      validate: validateMaster,
+      format: [4, 4, 4, 4],
+      cardBrand: '마스터카드',
+    },
+    {
+      validate: validateUnion,
+      format: [4, 4, 4, 4],
+      cardBrand: '유니온페이',
+    },
+  ],
+};
+
+const formatCardNumber = (cardNumber: string, divisionUnits: number[]) => {
+  let start = 0;
+  const formattedCardNumbers = divisionUnits.map(unit => {
+    const digits = cardNumber.slice(start, start + unit);
+    start += unit;
+    return digits;
+  });
+
+  return formattedCardNumbers;
+};
+
 const useCardBrand = (cardNumbers: Record<string, string>) => {
   const fullCardNumber = Object.values(cardNumbers).reduce((acc, cur) => acc + cur);
+  const cardBrandInfo = cardBrandChecker[fullCardNumber.length];
 
-  const isVisa = validateVisa(fullCardNumber);
-  const isMaster = validateMaster(fullCardNumber);
-  const isDiners = validateDiners(fullCardNumber);
-  const isAMEX = validateAMEX(fullCardNumber);
-  const isUnion = validateUnion(fullCardNumber);
+  if (cardBrandInfo) {
+    const validType = cardBrandInfo.find(brand => brand.validate(fullCardNumber));
 
-  if (fullCardNumber.length === 14 && isDiners) {
-    return {
-      cardBrand: 'Diners',
-      formattedCardNumbers: formatCardNumber(fullCardNumber, [4, 6, 4]),
-    };
-  }
-
-  if (fullCardNumber.length === 15 && isAMEX) {
-    return {
-      cardBrand: 'AMEX',
-      formattedCardNumbers: formatCardNumber(fullCardNumber, [4, 6, 5]),
-    };
-  }
-
-  if (fullCardNumber.length === 16) {
-    if (isVisa) {
+    if (validType) {
       return {
-        cardBrand: '비자카드',
-        formattedCardNumbers: formatCardNumber(fullCardNumber, [4, 4, 4, 4]),
-      };
-    }
-
-    if (isMaster) {
-      return {
-        cardBrand: '마스터카드',
-        formattedCardNumbers: formatCardNumber(fullCardNumber, [4, 4, 4, 4]),
-      };
-    }
-
-    if (isUnion) {
-      return {
-        cardBrand: '유니온페이',
-        formattedCardNumbers: formatCardNumber(fullCardNumber, [4, 4, 4, 4]),
+        cardBrand: validType.cardBrand,
+        formattedCardNumbers: formatCardNumber(fullCardNumber, validType.format),
       };
     }
   }
 
   return { cardBrand: '', formattedCardNumbers: [] };
-};
-
-const formatCardNumber = (cardNumber: string, divisionUnits: number[]) => {
-  const formattedCardNumbers = [cardNumber.slice(0, divisionUnits[0])];
-  divisionUnits.reduce((acc, cur) => {
-    formattedCardNumbers.push(cardNumber.slice(acc, acc + cur));
-    return acc + cur;
-  });
-
-  return formattedCardNumbers;
 };
 
 export default useCardBrand;
