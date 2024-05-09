c1987fd09a7099cca74eb95769fac4738ae27471
greetings1012 <ahnqwerty@gmail.com>
2024-05-09 11:39:34 +0900

    storybook: InputForm 스토리북 작성

---
+++ components/src/lib/Input/InputForm.stories.tsx
@@ -0,0 +1,32 @@
+import { StoryFn, Meta } from "@storybook/react";
+import InputForm from "./InputForm";
+
+export default {
+  title: "Components/InputForm",
+  component: InputForm,
+} as Meta;
+
+const Template: StoryFn<{
+  title?: string;
+  placeHolder?: string;
+  size?: "small" | "medium" | "large";
+}> = (args) => <InputForm {...args} />;
+
+export const LargeWithTitle = Template.bind({});
+LargeWithTitle.args = {
+  title: "Title",
+  placeHolder: "Enter your text",
+  size: "large",
+};
+
+export const MediumWithPlaceholder = Template.bind({});
+MediumWithPlaceholder.args = {
+  placeHolder: "Enter your text",
+  size: "medium",
+};
+
+export const SmallWithoutTitle = Template.bind({});
+SmallWithoutTitle.args = {
+  placeHolder: "Enter your text",
+  size: "small",
+};
