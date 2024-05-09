e07b863524e4bbf2041614e930757f0e72a787a1
Yoonkyoung Kim <dev.yoonkyoung@gmail.com>
2024-05-09 17:22:17 +0900

    refactor: ModalContent 컴포넌트 분리

---
+++ components/src/lib/components/Modal/ModalContent.tsx
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+import { StyleDirection } from '../types';
+
+export interface ModalContentProps {
+  message?: string;
+  $direction?: StyleDirection;
+  children?: React.ReactNode;
+}
+
+function ModalContent({
+  message,
+  $direction = 'column',
+  children,
+}: ModalContentProps) {
+  return (
+    <ContentContainer $direction={$direction}>
+      {message && <Message>{message}</Message>}
+      {children}
+    </ContentContainer>
+  );
+}
+
+export default ModalContent;
+
+interface ContentContainerStyleProps {
+  $direction: StyleDirection;
+}
+
+const ContentContainer = styled.div<ContentContainerStyleProps>`
+  display: flex;
+  flex-direction: ${({ $direction }) => $direction};
+  gap: 2rem;
+`;
+
+const Message = styled.div`
+  font-size: var(--font-size-sm);
+`;
