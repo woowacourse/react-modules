ff08eb0531deffd8712d388304f0d22f833ff9b8
river <jenn0.6n@gmail.com>
2024-05-09 13:01:29 +0900

    feat: 피그마 Modal 시안을 Modal 컴포넌트를 사용해 구현

--- components/src/App.tsx
+++ components/src/App.tsx
@@ -8,13 +8,45 @@ function App() {
       <h1>Modal Test</h1>
       <Modal.Provider>
         <Modal.Trigger>열기</Modal.Trigger>
-        <Modal.Content modalPosition="center" closeButtonPosition="top">
+        <Modal.Content modalPosition="center" closeButtonPosition="bottom">
           <Modal.Header title="모달창 타이틀" containClose />
           <Modal.Body>내용ㅋㅋ</Modal.Body>
-          <Modal.Footer>
+          <Modal.Footer align="center">
             <Modal.Close>닫기</Modal.Close>
           </Modal.Footer>
         </Modal.Content>
+      </Modal.Provider>
+
+      <Modal.Provider>
+        <Modal.Trigger> prompt 열기</Modal.Trigger>
+        <Modal.Prompt
+          title="쿠폰 번호를 입력해 주세요."
+          modalPosition="center"
+          closeButtonPosition="top"
+          placeholder="CGEXX46Z"
+        ></Modal.Prompt>
+      </Modal.Provider>
+
+      <Modal.Provider>
+        <Modal.Trigger> alert 열기</Modal.Trigger>
+        <Modal.Alert
+          title="아이디를 입력해 주세요."
+          modalPosition="center"
+          closeButtonPosition="top"
+        >
+          아이디는 필수로 입력해야 합니다.
+        </Modal.Alert>
+      </Modal.Provider>
+
+      <Modal.Provider>
+        <Modal.Trigger> confirm 열기</Modal.Trigger>
+        <Modal.Confirm
+          title="카드를 삭제하시겠습니까?"
+          modalPosition="center"
+          closeButtonPosition="top"
+        >
+          삭제하면 복구하실 수 없습니다.
+        </Modal.Confirm>
       </Modal.Provider>
     </>
   );
