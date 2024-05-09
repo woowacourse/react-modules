5008f906f6fff56a8f8e1fe449d59ace646dc9db
jaeml06 <jaeml0630@gmail.com>
2024-05-09 15:42:22 +0900

    test : 카드 타입 테스트 케이스 추가

--- hooks/src/lib/test/useCardType.test.ts
+++ hooks/src/lib/test/useCardType.test.ts
@@ -5,12 +5,11 @@ import { act } from 'react';
 describe('카드 타입 훅 테스트', () => {
   it('카드 번호 입력 시 첫 글자가 4로 시작할 때 cardType 이 visa 가 된다.', async () => {
     const cardNumberValue = '4444';
-    const cardNumberName = 'cardNumber1';
 
     const { result } = renderHook(() => useCardType());
 
     act(() => {
-      result.current.cardTypeHandler(cardNumberValue, cardNumberName);
+      result.current.cardTypeHandler(cardNumberValue);
     });
 
     expect(result.current.cardType).toBe('Visa');
@@ -18,24 +17,51 @@ describe('카드 타입 훅 테스트', () => {
 
   it('카드 번호 입력 시 앞의 두 글자가 51~55 로 시작할 때 cardType 이 MasterCard 가 된다.', async () => {
     const cardNumberValue = '5526';
-    const cardNumberName = 'cardNumber1';
     const { result } = renderHook(() => useCardType());
 
     act(() => {
-      result.current.cardTypeHandler(cardNumberValue, cardNumberName);
+      result.current.cardTypeHandler(cardNumberValue);
     });
 
     expect(result.current.cardType).toBe('MasterCard');
   });
-
-  it('카드 번호 입력 시 첫 글자에 4 이외의 숫자가 오고, 앞 두글자가 51~55 에 포함되지 않을 경우 cardType 이 Empty 가 된다. ', async () => {
-    const cardNumberValue = '6123';
-    const cardNumberName = 'cardNumber1';
-
+  it('카드 번호 입력 시 앞의 두 글자가 36 로 시작할 때 cardType 이 Diners 가 된다.', async () => {
+    const cardNumberValue = '3612';
     const { result } = renderHook(() => useCardType());
 
     act(() => {
-      result.current.cardTypeHandler(cardNumberValue, cardNumberName);
+      result.current.cardTypeHandler(cardNumberValue);
+    });
+
+    expect(result.current.cardType).toBe('Diners');
+  });
+  it('카드 번호 입력 시 앞의 두 글자가 34, 37 로 시작할 때 cardType 이 MasterCard 가 된다.', async () => {
+    const cardNumberValue = '3412';
+    const { result } = renderHook(() => useCardType());
+
+    act(() => {
+      result.current.cardTypeHandler(cardNumberValue);
+    });
+
+    expect(result.current.cardType).toBe('AMEX');
+  });
+  it('카드 번호 입력 시 앞의 622126~622925로 시작할 때 cardType 이 MasterCard 가 된다.', async () => {
+    const cardNumberValue = '62212612';
+    const { result } = renderHook(() => useCardType());
+
+    act(() => {
+      result.current.cardTypeHandler(cardNumberValue);
+    });
+
+    expect(result.current.cardType).toBe('UnionPay');
+  });
+
+  it('카드 번호 입력 시 첫 글자에 4 이외의 숫자가 오고, 앞 두글자가 51~55 에 포함되지 않을 경우 cardType 이 Empty 가 된다. ', async () => {
+    const cardNumberValue = '6123';
+    const { result } = renderHook(() => useCardType());
+
+    act(() => {
+      result.current.cardTypeHandler(cardNumberValue);
     });
 
     expect(result.current.cardType).toBe('Empty');
