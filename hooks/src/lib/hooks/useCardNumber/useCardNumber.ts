f3242b70f39a8a1e4457b68b0cd3819f76702c1f
anttiey <hanuuny@gmail.com>
2024-05-09 12:57:11 +0900

    refactor: 커스텀 훅 유효성 로직 분리

--- hooks/src/lib/hooks/useCardNumber/useCardNumber.ts
+++ hooks/src/lib/hooks/useCardNumber/useCardNumber.ts
@@ -1,34 +1,38 @@
 import { useState } from 'react';
-
 import useCardType from '../useCardType/useCardType';
-import type { CardType } from '../../types/common.type';
 import { formatCardNumber } from '../../utils/card';
 import { getNumberErrorMessage, isNotNumber } from '../../utils/validation/validation';
-
 import { CARD_TYPE } from '../../constants/Condition';
+
+import type { CardType } from '../../types/common.type';
 
 const useCardNumber = (initialValue: string = '') => {
   const [cardNumber, setCardNumber] = useState(initialValue);
   const [isValidCardNumber, setIsValidCardNumber] = useState(false);
   const [cardNumberErrorMessage, setCardNumberErrorMessage] = useState('');
 
-  const {
-    cardType,
-    handleCardType,
-  }: { cardType: CardType; handleCardType: (cardNumber: string) => void } = useCardType();
+  const { cardType, handleCardType }: { cardType: CardType; handleCardType: (cardNumber: string) => void } =
+    useCardType();
 
-  const handleCardNumberChange = (number: string) => {
-    const numberCopy = number.replace(/\s/g, '');
-
-    handleCardType(numberCopy);
-
-    if (numberCopy.length > CARD_TYPE[cardType].VALID_LENGTH || isNotNumber(numberCopy)) return;
-
-    const errorMessage = getNumberErrorMessage(numberCopy, CARD_TYPE[cardType].VALID_LENGTH);
+  const validateCardNumber = (number: string) => {
+    const errorMessage = getNumberErrorMessage(number, CARD_TYPE[cardType].VALID_LENGTH);
 
     setCardNumberErrorMessage(errorMessage);
     setIsValidCardNumber(!errorMessage);
-    setCardNumber(formatCardNumber(cardType, numberCopy));
+  };
+
+  const handleCardNumberChange = (number: string) => {
+    const cleanNumber = number.replace(/\s/g, '');
+
+    handleCardType(cleanNumber);
+
+    if (cleanNumber.length > CARD_TYPE[cardType].VALID_LENGTH) return;
+
+    validateCardNumber(cleanNumber);
+
+    if (isNotNumber(cleanNumber)) return;
+
+    setCardNumber(formatCardNumber(cardType, cleanNumber));
   };
 
   return {
