fc3cc2fcebceff42ede73e957326151fbf9cf33b
Yoonkyoung Kim <dev.yoonkyoung@gmail.com>
2024-05-09 17:20:36 +0900

    feat: 스타일 상수 파일 생성

---
+++ components/src/lib/components/constants/styles.ts
@@ -0,0 +1,21 @@
+export const MODAL_SIZE = {
+  small: '32rem',
+  medium: '48rem',
+  large: '60rem',
+};
+
+export const BUTTON_SIZE = {
+  small: { width: '30%', height: '3.2rem', fontSize: 'var(--font-size-sm)' },
+  medium: {
+    width: '50%',
+    height: '3.8rem',
+    fontSize: 'var(--font-size-base)',
+  },
+  large: { width: '100%', height: '4.4rem', fontSize: 'var(--font-size-lg)' },
+};
+
+export const JUSTIFY_CONTENT = {
+  start: 'flex-start',
+  center: 'center',
+  end: 'flex-end',
+};
