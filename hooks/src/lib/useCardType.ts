f55dadb628de70372e7279d57a14d373276811f2
jaeml06 <jaeml0630@gmail.com>
2024-05-09 15:43:48 +0900

    feat : 카드 타입에 따른 포맷팅 함수 추가

--- hooks/src/lib/useCardType.ts
+++ hooks/src/lib/useCardType.ts
@@ -33,14 +33,34 @@ const checkCardType = (cardNumber: string) => {
 };
 
 const useCardType = () => {
-  const [cardType, setCardType] = useState<string>('');
+  const [cardType, setCardType] = useState<keyof typeof CARD_NUMBER>('Empty');
 
-  const cardTypeHandler = (value: string, name: CardNumberName) => {
-    if (name === 'cardNumber1') {
-      setCardType(checkCardType(value));
+  const cardTypeHandler = (value: string) => {
+    setCardType(checkCardType(value));
+  };
+
+  const formatCardNumber = (cardType: keyof typeof CARD_NUMBER) => {
+    switch (cardType) {
+      case 'Diners':
+        return (cardNumbers: string) =>
+          cardNumbers.replace(/(\d{4})(\d{6})(\d{4})/, '$1 $2 $3');
+      case 'AMEX':
+        return (cardNumbers: string) =>
+          cardNumbers.replace(/(\d{4})(\d{6})(\d{5})/, '$1 $2 $3');
+      case 'UnionPay':
+      case 'Visa':
+      case 'MasterCard':
+        return (cardNumbers: string) =>
+          cardNumbers.replace(/(\d{4})(\d{4})(\d{4})(\d{4})/, '$1 $2 $3 $4');
+      default:
+        return (cardNumbers: string) => cardNumbers;
     }
   };
 
-  return { cardType, cardTypeHandler };
+  return {
+    cardType,
+    formatCardNumber: formatCardNumber(cardType),
+    cardTypeHandler,
+  };
 };
 export default useCardType;
