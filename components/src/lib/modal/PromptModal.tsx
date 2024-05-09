3788451683f46dc88d15c2176c7312432e31711a
useon <yoosk4986@naver.com>
2024-05-09 20:13:44 +0900

    feat: 입력 모달 컴포넌트 생성

---
+++ components/src/lib/modal/PromptModal.tsx
@@ -0,0 +1,48 @@
+import Modal from './Modal';
+
+export interface PromptModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  title: string;
+  position?: 'top' | 'bottom' | 'center';
+  size?: 'small' | 'medium' | 'large';
+}
+
+const PromptModal: React.FC<PromptModalProps> = ({
+  isOpen,
+  onClose,
+  title,
+  position = 'center',
+  size = 'medium',
+}) => {
+  return (
+    <Modal isOpen={isOpen} position={position} onClose={onClose} size={size}>
+      <Modal.Header>
+        <Modal.Title>{title}</Modal.Title>
+      </Modal.Header>
+      <Modal.Content>
+        <Modal.Input></Modal.Input>
+      </Modal.Content>
+      <Modal.Footer buttonPosition='right' buttonGap='10px'>
+        <Modal.TextButton
+          actionFn={onClose}
+          buttonSize='50px'
+          backgroundColor='#ffffff'
+          fontColor='#333333'
+        >
+          취소
+        </Modal.TextButton>
+        <Modal.TextButton
+          type='submit'
+          actionFn={onClose}
+          buttonSize='50px'
+          fontColor='#ffffff'
+        >
+          제출
+        </Modal.TextButton>
+      </Modal.Footer>
+    </Modal>
+  );
+};
+
+export default PromptModal;
