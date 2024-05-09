a4f81f417766fbde728b97ab1b43cc493b107f41
greetings1012 <ahnqwerty@gmail.com>
2024-05-09 11:36:54 +0900

    storybook: Title 스토리북 구현

---
+++ components/src/lib/Title/Title.stories.tsx
@@ -0,0 +1,26 @@
+import { StoryFn, Meta } from "@storybook/react";
+import Title from "./Title";
+
+export default {
+  title: "Components/Title",
+  component: Title,
+} as Meta;
+
+const Template: StoryFn<{
+  title: string;
+  subtitle?: string;
+  position?: "left" | "center";
+}> = (args) => <Title {...args} />;
+
+export const LeftPositionWithTitleAndSubtitle = Template.bind({});
+LeftPositionWithTitleAndSubtitle.args = {
+  title: "Main Title",
+  subtitle: "Subtitle",
+  position: "left",
+};
+
+export const CenterPositionWithTitle = Template.bind({});
+CenterPositionWithTitle.args = {
+  title: "Centered Title",
+  position: "center",
+};
