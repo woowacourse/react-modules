279c3c60bd6378c78f112dea90eec4f28d9430a9
river <jenn0.6n@gmail.com>
2024-05-09 16:36:59 +0900

    feat: 사용하던 Modal 컴포넌트에 size 추가

--- components/src/App.tsx
+++ components/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <h1>Modal Test</h1>
       <Modal.Provider>
         <Modal.Trigger>열기</Modal.Trigger>
-        <Modal.Content modalPosition="center" closeButtonPosition="bottom">
+        <Modal.Content modalPosition="center" closeButtonPosition="bottom" size="small">
           <Modal.Header title="모달창 타이틀" containClose />
           <Modal.Body>내용ㅋㅋ</Modal.Body>
           <Modal.Footer align="center">
@@ -25,6 +25,7 @@ function App() {
           closeButtonPosition="top"
           placeholder="CGEXX46Z"
           onSubmit={(value) => console.log("input value : ", value)}
+          size="medium"
         ></Modal.Prompt>
       </Modal.Provider>
 
@@ -34,6 +35,7 @@ function App() {
           title="아이디를 입력해 주세요."
           modalPosition="center"
           closeButtonPosition="top"
+          size="small"
         >
           아이디는 필수로 입력해야 합니다.
         </Modal.Alert>
@@ -45,6 +47,7 @@ function App() {
           title="카드를 삭제하시겠습니까?"
           modalPosition="center"
           closeButtonPosition="top"
+          size="medium"
         >
           삭제하면 복구하실 수 없습니다.
         </Modal.Confirm>
