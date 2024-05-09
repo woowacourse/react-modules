5850969722e440d2cb79a16244ccc588443894bb
anttiey <hanuuny@gmail.com>
2024-05-09 11:03:56 +0900

    refactor(Condition): 상수 as const 처리

--- hooks/src/lib/constants/Condition.ts
+++ hooks/src/lib/constants/Condition.ts
@@ -10,27 +10,27 @@ const DEFAULT_PROPERTY: CardTypeProperty = {
   MIN_LENGTH: 6,
   VALID_LENGTH: 16,
   PATTERN: [4, 4, 4, 4],
-};
+} as const;
 
 export const CARD_TYPE: Record<CardType, CardTypeProperty> = {
   VISA: {
     ...DEFAULT_PROPERTY,
     MIN_LENGTH: 1,
-  },
+  } as const,
   MASTERCARD: {
     ...DEFAULT_PROPERTY,
     MIN_LENGTH: 2,
-  },
+  } as const,
   DINERS: {
     MIN_LENGTH: 2,
     VALID_LENGTH: 14,
     PATTERN: [4, 6, 4],
-  },
+  } as const,
   AMEX: {
     MIN_LENGTH: 2,
     VALID_LENGTH: 15,
     PATTERN: [4, 6, 5],
-  },
+  } as const,
   UNIONPAY: DEFAULT_PROPERTY,
   DEFAULT: DEFAULT_PROPERTY,
-};
+} as const;
