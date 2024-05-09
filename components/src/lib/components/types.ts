07eca60f65bbd7d506ae12661132a4964e5953f6
Yoonkyoung Kim <dev.yoonkyoung@gmail.com>
2024-05-09 17:19:42 +0900

    type: types.ts 파일 생성 (Style- 타입 정의)

---
+++ components/src/lib/components/types.ts
@@ -0,0 +1,4 @@
+export type StyleSize = 'small' | 'medium' | 'large';
+export type StyleDirection = 'row' | 'column';
+export type StyleAlign = 'start' | 'center' | 'end';
+export type StylePosition = 'center' | 'bottom';
