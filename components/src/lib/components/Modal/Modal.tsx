ccb0866cefcb9625eb878867289baf66c25618af
손진영 <sm099@naver.com>
2024-05-09 16:28:44 +0900

    refactor: Modal 구성 요소 component들 각 디렉터리 내 분리

--- components/src/lib/components/Modal/Modal.tsx
+++ components/src/lib/components/Modal/Modal.tsx
@@ -1,18 +1,34 @@
-import { forwardRef } from 'react';
+import {
+  ModalHeader,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalButton,
+  ModalInput,
+  ModalLabel,
+} from './@common';
+
+import type {
+  ModalHeaderType,
+  ModalCloseButtonType,
+  ModalContentType,
+  ModalFooterType,
+  ModalButtonType,
+  ModalInputType,
+  ModalLabelType,
+} from './@common';
 
 import {
-  BUTTON_COLOR_MAP,
   MODAL_DEVICE_CLASS_NAME_MAP,
   MODAL_POSITION_CLASS_NAME_MAP,
   MODAL_SIZE_CLASS_NAME_MAP,
 } from './Modal.constant';
-import type { ButtonColorType, FlexDirection, ModalDevice, ModalPosition, ModalSize } from './Modal.type';
+
+import type { ModalDevice, ModalPosition, ModalSize } from './Modal.type';
 
 import useModalControl from './@hooks/useModalControl';
 
-import { convertPascalCase } from '../../../utils/string';
 import { extendClassNames } from '../../../utils/extendClassNames';
-import { CloseImage } from '../../assets';
 
 import styles from './Modal.module.css';
 
@@ -23,18 +39,6 @@ export interface ModalProps {
   device?: ModalDevice;
   onToggle: () => void;
 }
-
-export type ModalInputType = React.ForwardRefExoticComponent<
-  React.InputHTMLAttributes<HTMLInputElement> & React.RefAttributes<HTMLInputElement>
->;
-type ModalHeaderType = React.FC<React.PropsWithChildren<{ title: string } & React.HTMLAttributes<HTMLElement>>>;
-type ModalContentType = React.FC<React.PropsWithChildren<React.HTMLAttributes<HTMLElement>>>;
-type ModalFooterType = React.FC<
-  React.PropsWithChildren<React.HTMLAttributes<HTMLElement> & { direction?: FlexDirection }>
->;
-type ModalCloseButtonType = React.FC<React.ButtonHTMLAttributes<HTMLButtonElement>>;
-type ModalButtonType = React.FC<React.ButtonHTMLAttributes<HTMLButtonElement> & { color: ButtonColorType }>;
-type ModalLabelType = React.FC<React.PropsWithChildren>;
 
 const Modal: React.FC<React.PropsWithChildren<ModalProps>> & {
   ModalHeader: ModalHeaderType;
@@ -69,60 +73,6 @@ const Modal: React.FC<React.PropsWithChildren<ModalProps>> & {
       </div>
     )
   );
-};
-
-const ModalHeader: ModalHeaderType = ({ children, className, title }) => {
-  return (
-    <header className={extendClassNames(styles.modalHeader, className)}>
-      <h2 className={styles.modalTitle}>{title}</h2>
-      {children}
-    </header>
-  );
-};
-
-const ModalCloseButton: ModalCloseButtonType = ({ className, type = 'submit', ...rest }) => {
-  return (
-    <button type={type} className={extendClassNames(styles.modalCloseButton, className)} {...rest}>
-      <img className={styles.modalCloseButtonImage} src={CloseImage} alt="close-button" />
-    </button>
-  );
-};
-
-const ModalContent: ModalContentType = ({ children, className, ...rest }) => {
-  return (
-    <section className={extendClassNames(styles.modalContent, className)} {...rest}>
-      {children}
-    </section>
-  );
-};
-
-const ModalFooter: ModalFooterType = ({ children, direction = 'row', className }) => {
-  const footerRowClassName = styles[`footer${convertPascalCase(direction)}`];
-  return (
-    <footer className={extendClassNames(styles.modalFooter, footerRowClassName, className)}>
-      <div className={extendClassNames(styles.modalFooterChildrenContainer, styles[direction])}>{children}</div>
-    </footer>
-  );
-};
-
-const ModalButton: ModalButtonType = ({ children, type = 'submit', color = 'primary', className, ...rest }) => {
-  return (
-    <button
-      type={type}
-      className={extendClassNames(styles.modalButton, styles[BUTTON_COLOR_MAP[color]], className)}
-      {...rest}
-    >
-      {children}
-    </button>
-  );
-};
-
-const ModalInput = forwardRef<HTMLInputElement, React.InputHTMLAttributes<HTMLInputElement>>(({ ...rest }, ref) => {
-  return <input className={styles.modalInput} ref={ref} {...rest} />;
-});
-
-const ModalLabel: ModalLabelType = ({ children }) => {
-  return <p className={styles.modalLabel}>{children}</p>;
 };
 
 export default Modal;
