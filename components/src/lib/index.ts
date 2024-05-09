484c5e81c6337807ce848ff3cc59fadd653af185
greetings1012 <ahnqwerty@gmail.com>
2024-05-09 11:40:44 +0900

    feat: 합성 컴포넌트 모듈화를 위한 index.ts 작성

--- components/src/lib/index.ts
+++ components/src/lib/index.ts
@@ -1,1 +1,14 @@
-export { default as Modal } from './Modal';
+import CancelButton from "./Buttons/CancelButton/CancelButton";
+import CloseButton from "./Buttons/CloseButton/CloseButton";
+import ConfirmButton from "./Buttons/ConfirmButton/ConfirmButton";
+import Container from "./Container/Container";
+import InputForm from "./Input/InputForm";
+import Title from "./Title/Title";
+
+export const Modal = Object.assign(Container, {
+  Title: Title,
+  CloseButton: CloseButton,
+  ConfirmButton: ConfirmButton,
+  CancelButton: CancelButton,
+  InputForm: InputForm,
+});
