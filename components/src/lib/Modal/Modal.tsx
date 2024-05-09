1e9c2082c7c962ad4cb839e2245e379b7d3a5adb
jayming66 <mjw8941@gmail.com>
2024-05-09 21:41:06 +0900

    feat(Modal.tsx): modalButton 컴포넌트 추가

--- components/src/lib/Modal/Modal.tsx
+++ components/src/lib/Modal/Modal.tsx
@@ -1,10 +1,10 @@
 import { MODAL_POSITION_MAP } from './Modal.constant';
-import { ModalPosition } from './Modal.type';
+import { ButtonStyle, ModalPosition } from './Modal.type';
 
 import useModalControl from './hooks/useModalControl';
 
 import { convertPascalCase } from '../../utils/string';
-
+import closeButtonImg from '../../asset/CloseButton.png';
 import styles from './Modal.module.css';
 
 interface ModalProps {
@@ -13,14 +13,27 @@ interface ModalProps {
   position: ModalPosition;
 }
 
-type ModalHeaderType = React.FC<React.PropsWithChildren<{ title: string }>>;
-type ModalContentType = React.FC<React.PropsWithChildren>;
-type ModalFooterType = React.FC<React.PropsWithChildren>;
+interface ModalButtonProps {
+  variant: ButtonStyle;
+  onClick?: () => void;
+}
+
+interface ModalCloseButtonProps {
+  onClick: () => void;
+}
+
+type ModalHeaderType = React.FC<React.PropsWithChildren<React.HTMLAttributes<HTMLElement> & { title: string }>>;
+type ModalContentType = React.FC<React.PropsWithChildren<React.HTMLAttributes<HTMLElement>>>;
+type ModalFooterType = React.FC<React.PropsWithChildren<React.HTMLAttributes<HTMLElement>>>;
+type ModalButtonType = React.FC<React.PropsWithChildren<ModalButtonProps>>;
+type ModalCloseButtonType = React.FC<React.PropsWithChildren<ModalCloseButtonProps>>;
 
 const Modal: React.FC<React.PropsWithChildren<ModalProps>> & {
   ModalHeader: ModalHeaderType;
   ModalContent: ModalContentType;
   ModalFooter: ModalFooterType;
+  ModalButton: ModalButtonType;
+  ModalCloseButton: ModalCloseButtonType;
 } = ({ children, isOpen, onToggle, position = 'center' }) => {
   const modalContainerStyle = position === 'bottom' ? `modalContainer${convertPascalCase(position)}` : '';
 
@@ -36,21 +49,45 @@ const Modal: React.FC<React.PropsWithChildren<ModalProps>> & {
   );
 };
 
-const ModalHeader: React.FC<React.PropsWithChildren<{ title: string }>> = ({ children, title }) => {
+const ModalHeader: ModalHeaderType = ({ children, title, ...rest }) => {
   return (
-    <header className={styles.modalHeader}>
+    <header className={styles.modalHeader} {...rest}>
       <h2 className={styles.modalTitle}>{title}</h2>
       {children}
     </header>
   );
 };
 
-const ModalContent: React.FC<React.PropsWithChildren> = ({ children }) => {
-  return <section className={styles.modalContent}>{children}</section>;
+const ModalContent: ModalContentType = ({ children, ...rest }) => {
+  return (
+    <section className={styles.modalContent} {...rest}>
+      {children}
+    </section>
+  );
 };
 
-const ModalFooter: React.FC<React.PropsWithChildren> = ({ children }) => {
-  return <footer className={styles.modalFooter}>{children}</footer>;
+const ModalFooter: ModalFooterType = ({ children, ...rest }) => {
+  return (
+    <footer className={styles.modalFooter} {...rest}>
+      {children}
+    </footer>
+  );
+};
+
+const ModalButton: ModalButtonType = ({ children, variant = 'primary', onClick, ...rest }) => {
+  return (
+    <button className={`${styles.modalButton} ${styles[variant]}`} onClick={onClick} {...rest}>
+      {children}
+    </button>
+  );
+};
+
+const ModalCloseButton: ModalCloseButtonType = ({ onClick, ...rest }) => {
+  return (
+    <button className={styles.modalCloseButton} onClick={onClick} {...rest}>
+      <img src={closeButtonImg} className={styles.modalCloseButton} />
+    </button>
+  );
 };
 
 export default Modal;
@@ -58,3 +95,5 @@ export default Modal;
 Modal.ModalHeader = ModalHeader;
 Modal.ModalContent = ModalContent;
 Modal.ModalFooter = ModalFooter;
+Modal.ModalButton = ModalButton;
+Modal.ModalCloseButton = ModalCloseButton;
