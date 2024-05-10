24069173af8afde0ec82f1ab7958a3ae40d99638
hwinkr <dnddl8280@naver.com>
2024-05-10 15:36:12 +0900

    docs: Modal 기능 요구 사항 완성

--- components/src/lib/Modal/Modal.tsx
+++ components/src/lib/Modal/Modal.tsx
@@ -1,30 +1,38 @@
 import styled from "styled-components";
 
-import ModalContent from "./ModalContent";
 import ModalBackDrop from "./ModalBackDrop";
+import ModalContent from "./ModalContent";
 import ModalHeader from "./ModalHeader";
 import ModalTitle from "./ModalTitle";
 import ModalCloseButton from "./ModalCloseButton";
+import ModalForm from "./ModalForm";
+import ModalInput from "./ModalInput";
 import ModalFooter from "./ModalFooter";
 import ModalButton from "./ModalButton";
 
 import useBlockScroll from "../hooks/useBlockScroll";
 import useKeyDown from "../hooks/useKeyDown";
 
+import { DEVICE_WIDTH, MODAL_BORDER_RADIUS, MODAL_POSITION } from "../constants/modal";
+
+import { Device, ModalPosition } from "../types/modal";
+
 export interface ModalProps extends React.HTMLAttributes<HTMLElement> {
   children: React.ReactNode;
   isOpen: boolean;
+  position?: ModalPosition;
+  device?: Device;
   onClose: () => void;
 }
 
-const Modal = ({ children, isOpen, onClose }: ModalProps) => {
+const Modal = ({ children, isOpen, position = "center", device = "desktop", onClose }: ModalProps) => {
   useKeyDown("Escape", onClose);
 
   useBlockScroll(isOpen);
 
   return (
     isOpen && (
-      <StyledModal>
+      <StyledModal $position={position} $device={device}>
         <ModalBackDrop onClose={onClose} />
         {children}
       </StyledModal>
@@ -36,13 +44,26 @@ Modal.ModalContent = ModalContent;
 Modal.ModalHeader = ModalHeader;
 Modal.ModalTitle = ModalTitle;
 Modal.ModalCloseButton = ModalCloseButton;
+Modal.ModalForm = ModalForm;
+Modal.ModalInput = ModalInput;
 Modal.ModalFooter = ModalFooter;
 Modal.ModalButton = ModalButton;
 
 export default Modal;
 
-const StyledModal = styled.section`
-  position: relative;
-
+const StyledModal = styled.section<{ $position: ModalPosition; $device: Device }>`
   height: 100vh;
+  width: 100%;
+  max-width: ${({ $device }) => DEVICE_WIDTH[$device]};
+  position: fixed;
+  inset: 0;
+
+  display: flex;
+  justify-content: center;
+  align-items: ${({ $position }) => MODAL_POSITION[$position]};
+  margin: auto;
+
+  & > div:nth-child(2) {
+    border-radius: ${({ $position }) => MODAL_BORDER_RADIUS[$position]};
+  }
 `;
