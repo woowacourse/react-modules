ce7b6882dee3fc116964c35498dfa3cbd7010949
손진영 <sm099@naver.com>
2024-05-10 15:49:09 +0900

    refactor(ConfirmModal): Props 형태 변경
    ModalImpletationProps와 겹치는 이유로 ConfirmModalProps 제거

--- components/src/lib/components/ConfirmModal/ConfirmModal.tsx
+++ components/src/lib/components/ConfirmModal/ConfirmModal.tsx
@@ -4,26 +4,25 @@ import ModalFooterContainer from '../Modal/@container/ModalFooterContainer/Modal
 import { MODAL_CUSTOM_STYLES } from '../Modal/Modal.constant';
 import { ModalImplementationProps } from '../Modal/Modal.type';
 
-interface ConfirmModalProps {
-  onConfirm: (...args: unknown[]) => void;
-}
-
-const ConfirmModal: React.FC<React.PropsWithChildren<ConfirmModalProps & ModalImplementationProps & ModalProps>> = ({
+const ConfirmModal: React.FC<React.PropsWithChildren<ModalImplementationProps & ModalProps>> = ({
   children,
   cancelButtonText = '취소',
   confirmButtonText = '확인',
+  title,
+  onConfirm,
+  onToggle,
   ...rest
 }) => {
   return (
-    <Modal {...rest}>
-      <Modal.ModalHeader title={rest.title} />
+    <Modal onToggle={onToggle} {...rest}>
+      <Modal.ModalHeader title={title} />
       <Modal.ModalContent style={{ ...MODAL_CUSTOM_STYLES.modalContent }}>{children}</Modal.ModalContent>
       <Modal.ModalFooter>
         <ModalFooterContainer>
-          <Modal.ModalButton onClick={rest.onConfirm} color="secondary" style={{ ...MODAL_CUSTOM_STYLES.cancelButton }}>
+          <Modal.ModalButton onClick={onConfirm} color="secondary" style={{ ...MODAL_CUSTOM_STYLES.cancelButton }}>
             {cancelButtonText}
           </Modal.ModalButton>
-          <Modal.ModalButton onClick={rest.onToggle} color="primary" style={{ ...MODAL_CUSTOM_STYLES.confirmButton }}>
+          <Modal.ModalButton onClick={onToggle} color="primary" style={{ ...MODAL_CUSTOM_STYLES.confirmButton }}>
             {confirmButtonText}
           </Modal.ModalButton>
         </ModalFooterContainer>
