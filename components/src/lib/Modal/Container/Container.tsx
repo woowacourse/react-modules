6c22f5ae73485460f3a5ceffb3b6ba2b16b30ca7
simhorim <hori5020@gmail.com>
2024-05-10 21:03:08 +0900

    design(Container): size prop 추가 및 size에 따른 width 변화 UI 구현

--- components/src/lib/Modal/Container/Container.tsx
+++ components/src/lib/Modal/Container/Container.tsx
@@ -1,14 +1,19 @@
 import * as Styled from "./style";
 
 export type ContainerPositionType = "top" | "bottom" | "center";
-
+export type ModalContainerSizeType = "small" | "medium" | "large";
 export interface ContainerProps {
   position: ContainerPositionType;
+  size: ModalContainerSizeType;
   children: JSX.Element;
 }
 
-const Container = ({ position, children }: ContainerProps) => {
-  return <Styled.Container $position={position}>{children}</Styled.Container>;
+const Container = ({ position, size, children }: ContainerProps) => {
+  return (
+    <Styled.Container $position={position} $size={size}>
+      {children}
+    </Styled.Container>
+  );
 };
 
 export default Container;
--- components/src/lib/Modal/Container/style.tsx
+++ components/src/lib/Modal/Container/style.tsx
@@ -1,13 +1,11 @@
 import styled from "styled-components";
-import { ContainerPositionType } from "./Container";
+import { ContainerPositionType, ModalContainerSizeType } from "./Container";
 
 const POSITION_STYLES = {
   top: `
     top: 0;
     left: 50%;
     transform: translate(-50%, 0);
-
-    width: 100%;
     
     border-radius: 0px 0px 10px 10px;
     `,
@@ -15,9 +13,7 @@ const POSITION_STYLES = {
     top: 50%;
     left: 50%;
     transform: translate(-50%,-50%);
-    
-    width: 80%;
-    
+
     border-radius: 8px;
     `,
   bottom: `
@@ -25,19 +21,32 @@ const POSITION_STYLES = {
     left: 50%;
     transform: translate(-50%,-100%);
 
-    width: 100%;
-  
     border-radius: 10px 10px 0px 0px;
     `,
 };
 
-export const Container = styled.div<{ $position: ContainerPositionType }>`
+export const Container = styled.div<{
+  $size: ModalContainerSizeType;
+  $position: ContainerPositionType;
+}>`
   position: fixed;
   padding: 32px 24px;
   display: flex;
   flex-direction: column;
   gap: 1rem;
 
+  width: ${({ $size }) =>
+    $size === "large" ? "60%" : $size === "medium" ? "40%" : "30%"};
+
   background-color: ${(props) => props.theme.color.white};
+
   ${({ $position }) => POSITION_STYLES[$position]};
+
+  @media (min-width: 376px) {
+    min-width: 376px;
+  }
+
+  @media (max-width: 376px) {
+    width: ${({ $position }) => ($position === "center" ? "80%" : "100%")};
+  }
 `;
