514dca9760010865229fe047d06a3bebc2c706ec
jinsil <jinsil378900@gmail.com>
2024-05-11 00:23:57 +0900

    feat: CVC input 커스텀 훅 생성

---
+++ hooks/src/hooks/lib/useCVCInput/useCVCInput.ts
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import validator from "../utils/validate";
+import ERROR_MESSAGE from "../constants/errorMessage";
+
+interface CVCState {
+  value: string;
+  isValid: boolean;
+  errorMessage: string;
+}
+
+interface Props {
+  CVCState: CVCState;
+  handleCVCChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const MAX_CVC_LENGTH = 3;
+
+const useCVCInput = (): Props => {
+  const [cvcState, setCVCState] = useState<CVCState>({
+    value: "",
+    isValid: false,
+    errorMessage: "",
+  });
+
+  const handleCVCChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    let isValid = true;
+    let errorMessage = "";
+
+    if (!validator.isValidEmptyValue(value)) {
+      isValid = false;
+      errorMessage = ERROR_MESSAGE.EMPTY_VALUE;
+    } else if (!validator.isValidDigit(value)) {
+      isValid = false;
+      errorMessage = ERROR_MESSAGE.ONLY_NUMBER;
+    } else if (!validator.isValidLength({ value: value, matchedLength: MAX_CVC_LENGTH })) {
+      isValid = false;
+      errorMessage = ERROR_MESSAGE.INVALID_CVC_LENGTH;
+    }
+
+    setCVCState({
+      value: value,
+      isValid,
+      errorMessage,
+    });
+  };
+
+  return { CVCState: cvcState, handleCVCChange };
+};
+
+export default useCVCInput;
