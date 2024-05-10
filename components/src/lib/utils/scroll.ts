78982c84470f4b889aad69c3774a902b9a438937
pakxe <pigkill40@naver.com>
2024-05-10 21:11:43 +0900

    fix: 스크롤을 막는 함수가 스크롤 바를 사라지게 하는 오류 수정

--- components/src/lib/utils/scroll.ts
+++ components/src/lib/utils/scroll.ts
@@ -1,19 +1,23 @@
 export const preventScroll = () => {
-  const currentScrollY = window.scrollY;
+  // 현재 스크롤 위치 저장
+  const scrollY = window.scrollY;
 
+  // 스크롤을 고정시키고, 화면을 현재 스크롤 위치에 고정
   document.body.style.position = 'fixed';
+  document.body.style.top = `-${scrollY}px`;
   document.body.style.width = '100%';
-  document.body.style.top = `-${currentScrollY}px`;
-  document.body.style.overflowY = 'scroll';
+  document.body.style.overflowY = 'scroll'; // hidden으로 하면 스크롤바 사라져서 이상함
 
-  return currentScrollY;
+  // 현재 스크롤 위치를 반환하여 나중에 복원할 수 있게 함
+  return scrollY;
 };
 
-export const allowScroll = (prevScrollY: number) => {
+export const allowScroll = (scrollY: number) => {
   document.body.style.position = '';
-  document.body.style.width = '';
   document.body.style.top = '';
+  document.body.style.width = '';
   document.body.style.overflowY = '';
 
-  window.scrollTo(0, prevScrollY);
+  // 저장된 스크롤 위치로 이동
+  window.scrollTo(0, scrollY);
 };
