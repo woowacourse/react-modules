e22ad4e8c2f772fc1e32e2862fcf94cb1568cf4c
Largopie <106071687+Largopie@users.noreply.github.com>
2024-05-10 22:47:21 +0900

    refactor(Alert): storybook render 로직 수정

--- components/src/lib/Modal/AlertModal/AlertModal.stories.tsx
+++ components/src/lib/Modal/AlertModal/AlertModal.stories.tsx
@@ -1,7 +1,7 @@
 import '../../../index.css';
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { AlertModal, Modal } from '../../';
-import type { Meta } from '@storybook/react';
+import type { Meta, StoryObj } from '@storybook/react';
 
 const meta: Meta<typeof AlertModal> = {
   title: 'AlertModal',
@@ -16,102 +16,109 @@ const meta: Meta<typeof AlertModal> = {
   parameters: {
     layout: 'center',
   },
+  render: (args) => {
+    const [isOpen, setIsOpen] = useState(true);
+
+    const onClose = () => setIsOpen(false);
+    const onConfirm = () => {
+      console.log('confirm');
+    };
+
+    return (
+      <>
+        <Modal.Button onClick={() => setIsOpen(true)} size='small' backgroundColor='primary'>
+          모달열기
+        </Modal.Button>
+        <AlertModal {...args} isOpen={isOpen} onClose={onClose} onConfirm={onConfirm} />
+      </>
+    );
+  },
 };
 
 export default meta;
+type Story = StoryObj<typeof AlertModal>;
 
-const Template = (args) => {
-  const [isOpen, setIsOpen] = useState(true);
-
-  const onClose = () => setIsOpen(false);
-  const onConfirm = () => {
-    console.log('confirm');
-  };
-
-  return (
-    <>
-      <Modal.Button onClick={() => setIsOpen(true)} size='small' backgroundColor='primary'>
-        모달열기
-      </Modal.Button>
-      <AlertModal {...args} isOpen={isOpen} onClose={onClose} onConfirm={onConfirm} />
-    </>
-  );
+export const Default: Story = {
+  args: {
+    position: 'center',
+    size: 'medium',
+    title: '제목을 입력해주세요',
+    label: '설명을 입력해주세요.',
+    existCloseButton: true,
+    isOpen: true,
+  },
 };
 
-export const Default = Template.bind({});
-Default.args = {
-  position: 'center',
-  size: 'medium',
-  title: '제목을 입력해주세요',
-  label: '설명을 입력해주세요.',
-  existCloseButton: true,
-  isOpen: true,
+export const Bottom: Story = {
+  args: {
+    position: 'bottom',
+    size: 'medium',
+    title: '제목을 입력해주세요',
+    label: '설명을 입력해주세요.',
+    existCloseButton: true,
+    isOpen: true,
+  },
 };
 
-export const Bottom = Template.bind({});
-Bottom.args = {
-  position: 'bottom',
-  size: 'medium',
-  title: '제목을 입력해주세요',
-  label: '설명을 입력해주세요.',
-  existCloseButton: true,
-  isOpen: true,
+export const Center: Story = {
+  args: {
+    position: 'center',
+    size: 'medium',
+    title: '제목을 입력해주세요',
+    label: '설명을 입력해주세요.',
+    existCloseButton: true,
+    isOpen: true,
+  },
 };
 
-export const Center = Template.bind({});
-Center.args = {
-  position: 'center',
-  size: 'medium',
-  title: '제목을 입력해주세요',
-  label: '설명을 입력해주세요.',
-  existCloseButton: true,
-  isOpen: true,
+export const Small: Story = {
+  args: {
+    position: 'center',
+    size: 'small',
+    title: '제목을 입력해주세요',
+    label: '설명을 입력해주세요.',
+    existCloseButton: true,
+    isOpen: true,
+  },
 };
 
-export const Small = Template.bind({});
-Small.args = {
-  position: 'center',
-  size: 'small',
-  title: '제목을 입력해주세요',
-  label: '설명을 입력해주세요.',
-  existCloseButton: true,
-  isOpen: true,
+export const Medium: Story = {
+  args: {
+    position: 'center',
+    size: 'medium',
+    title: '제목을 입력해주세요',
+    label: '설명을 입력해주세요.',
+    existCloseButton: true,
+    isOpen: true,
+  },
 };
 
-export const Medium = Template.bind({});
-Medium.args = {
-  position: 'center',
-  size: 'medium',
-  title: '제목을 입력해주세요',
-  label: '설명을 입력해주세요.',
-  existCloseButton: true,
-  isOpen: true,
+export const Large: Story = {
+  args: {
+    position: 'center',
+    size: 'large',
+    title: '제목을 입력해주세요',
+    label: '설명을 입력해주세요.',
+    existCloseButton: true,
+    isOpen: true,
+  },
 };
 
-export const Large = Template.bind({});
-Large.args = {
-  position: 'center',
-  size: 'large',
-  title: '제목을 입력해주세요',
-  label: '설명을 입력해주세요.',
-  existCloseButton: true,
-  isOpen: true,
+export const ExistCloseButton: Story = {
+  args: {
+    position: 'center',
+    size: 'medium',
+    title: '제목을 입력해주세요',
+    label: '설명을 입력해주세요.',
+    existCloseButton: true,
+  },
 };
-
-export const ExistCloseButton = Template.bind({});
-ExistCloseButton.args = {
-  position: 'center',
-  size: 'medium',
-  title: '제목을 입력해주세요',
-  label: '설명을 입력해주세요.',
-  existCloseButton: true,
-};
-
-export const ExcludeCloseButton = Template.bind({});
-ExcludeCloseButton.args = {
-  position: 'center',
-  size: 'medium',
-  title: '제목을 입력해주세요',
-  label: '설명을 입력해주세요.',
-  existCloseButton: false,
+export const ExcludeCloseButton: Story = {
+  args: {
+    position: 'center',
+    size: 'medium',
+    title: '제목을 입력해주세요',
+    label: '설명을 입력해주세요.',
+    existCloseButton: false,
+  },
 };
