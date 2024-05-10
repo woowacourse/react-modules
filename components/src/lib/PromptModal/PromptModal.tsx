334b0fb580749dd75d6aba1bb593c5d0c17720d1
jayming66 <mjw8941@gmail.com>
2024-05-10 18:09:03 +0900

    feat(PromptModal.tsx): Modal 컴포넌트를 조합한 PromptModal 컴포넌트 추가

---
+++ components/src/lib/PromptModal/PromptModal.tsx
@@ -0,0 +1,51 @@
+import React, { useRef } from 'react';
+import Modal, { ModalProps } from '../Modal/Modal';
+
+import styles from './PromptModal.module.css';
+import { convertPascalCase } from '../../utils/string';
+
+interface PromptModalProps extends ModalProps, Pick<React.InputHTMLAttributes<HTMLInputElement>, 'value' | 'onChange'> {
+  onSubmit: (value: string) => void;
+  title: string;
+}
+
+const PromptModal: React.FC<PromptModalProps> = ({
+  onToggle,
+  isOpen,
+  position,
+  size,
+  title,
+  value,
+  onChange,
+  onSubmit,
+  ...rest
+}) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const inputClass = `modalInput${convertPascalCase(size)}`;
+
+  const handleSubmit = () => {
+    if (inputRef.current) {
+      onSubmit(inputRef.current.value);
+      onToggle();
+    }
+  };
+
+  return (
+    <Modal className={styles.promptModal} isOpen={isOpen} onToggle={onToggle} position={position} size={size} {...rest}>
+      <Modal.ModalHeader title={title} />
+      <Modal.ModalContent>
+        <Modal.ModalInput className={styles[inputClass]} value={value} onChange={onChange} ref={inputRef} />
+      </Modal.ModalContent>
+      <Modal.ModalFooter className={styles.promptModalFooter}>
+        <Modal.ModalButton variant="secondary" onClick={onToggle}>
+          취소
+        </Modal.ModalButton>
+        <Modal.ModalButton variant="primary" onClick={handleSubmit}>
+          확인
+        </Modal.ModalButton>
+      </Modal.ModalFooter>
+    </Modal>
+  );
+};
+
+export default PromptModal;
--- components/src/lib/index.ts
+++ components/src/lib/index.ts
@@ -1,2 +1,3 @@
 export { default as Modal } from './Modal/Modal';
 export { default as AlertModal } from './AlertModal/AlertModal';
+export { default as PromptModal } from './PromptModal/PromptModal';
