167b4ae6ef350baccb4072b8ef1712dcf8ee78f4
anttiey <hanuuny@gmail.com>
2024-05-10 15:50:21 +0900

    refactor: 필요하지 않은 상수 제거

--- hooks/src/lib/constants/Condition.ts
+++ hooks/src/lib/constants/Condition.ts
@@ -1,13 +1,11 @@
 import type { CardType } from '../utils/card/card.type';
 
 interface CardTypeProperty {
-  MIN_LENGTH: number;
   VALID_LENGTH: number;
   PATTERN: number[];
 }
 
 const DEFAULT_PROPERTY: CardTypeProperty = {
-  MIN_LENGTH: 6,
   VALID_LENGTH: 16,
   PATTERN: [4, 4, 4, 4],
 } as const;
@@ -15,19 +13,15 @@ const DEFAULT_PROPERTY: CardTypeProperty = {
 export const CARD_TYPE: Record<CardType, CardTypeProperty> = {
   VISA: {
     ...DEFAULT_PROPERTY,
-    MIN_LENGTH: 1,
   } as const,
   MASTERCARD: {
     ...DEFAULT_PROPERTY,
-    MIN_LENGTH: 2,
   } as const,
   DINERS: {
-    MIN_LENGTH: 2,
     VALID_LENGTH: 14,
     PATTERN: [4, 6, 4],
   } as const,
   AMEX: {
-    MIN_LENGTH: 2,
     VALID_LENGTH: 15,
     PATTERN: [4, 6, 5],
   } as const,
