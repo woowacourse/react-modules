3e4578f7d2efb4b09cd4d06b2f8eb4921546fe26
pakxe <pigkill40@naver.com>
2024-05-10 21:33:13 +0900

    feat: 스크롤 필요성에 따라 스크롤 width를 두는지 안두는지 적용

--- components/src/lib/utils/scroll.ts
+++ components/src/lib/utils/scroll.ts
@@ -1,3 +1,17 @@
+function isPageLongerThanViewport() {
+  const pageHeight = Math.max(
+    document.body.scrollHeight,
+    document.body.offsetHeight,
+    document.documentElement.clientHeight,
+    document.documentElement.scrollHeight,
+    document.documentElement.offsetHeight,
+  );
+
+  const viewportHeight = window.innerHeight;
+
+  return pageHeight > viewportHeight;
+}
+
 export const preventScroll = () => {
   // 현재 스크롤 위치 저장
   const scrollY = window.scrollY;
@@ -6,7 +20,8 @@ export const preventScroll = () => {
   document.body.style.position = 'fixed';
   document.body.style.top = `-${scrollY}px`;
   document.body.style.width = '100%';
-  document.body.style.overflowY = 'scroll'; // hidden으로 하면 스크롤바 사라져서 이상함
+  document.body.style.height = '100%';
+  document.body.style.overflowY = isPageLongerThanViewport() ? 'scroll' : 'hidden'; // scroll은 스크롤 width만큼을 남긴다. hidden은 스크롤 width도 아예 없음. 스크롤이 없는 상태(viewport height < page)에서는 스크롤 만큼의 width가 생기면 width가 늘어나고 줄어들어 아이템이 움직이는 문제 존재.
 
   // 현재 스크롤 위치를 반환하여 나중에 복원할 수 있게 함
   return scrollY;
@@ -16,6 +31,7 @@ export const allowScroll = (scrollY: number) => {
   document.body.style.position = '';
   document.body.style.top = '';
   document.body.style.width = '';
+  document.body.style.height = '';
   document.body.style.overflowY = '';
 
   // 저장된 스크롤 위치로 이동
