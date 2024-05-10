402de3eb0f926fecd86ed04f8e18081a8a5463ed
hwinkr <dnddl8280@naver.com>
2024-05-10 15:51:48 +0900

    chore: 카드 브랜드 별 입력, 포매팅 규칙 상수 객체 추가

--- hooks/src/lib/types/card-custom-hook.ts
+++ hooks/src/lib/types/card-custom-hook.ts
@@ -1,3 +1,12 @@
+import { CARD_BRAND } from "../constants/card-custom-hook";
+
 export type ExpiryDateKeys = "month" | "year";
 
 export type CardNumberKeys = "first" | "second" | "third" | "fourth";
+
+export type CardBrand = keyof typeof CARD_BRAND;
+
+export interface FormattedCardNumber {
+  formattedResult: string[];
+  totalLength: number;
+}
