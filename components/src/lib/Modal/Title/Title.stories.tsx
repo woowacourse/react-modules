9c53583bc911d01987290cf2ae529df543f9f158
Largopie <106071687+Largopie@users.noreply.github.com>
2024-05-10 13:51:48 +0900

    feat(Title): storybook 구현

---
+++ components/src/lib/Modal/Title/Title.stories.tsx
@@ -0,0 +1,21 @@
+import '../../../index.css';
+import type { Meta, StoryObj } from '@storybook/react';
+import { Modal } from '../../';
+
+const meta = {
+  title: 'Title',
+  component: Modal.Title,
+  parameters: {
+    layout: 'centered',
+  },
+} satisfies Meta<typeof Modal>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    children: '제목',
+  },
+};
