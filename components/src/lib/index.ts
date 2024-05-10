79af5b1aa3f9fc3db2ec9f9c379c097941c4b3fb
soosoo22 <lllllh00@naver.com>
2024-05-10 15:07:13 +0900

    chore: AlertModal, ConfirmModal, PromptModal export함

--- components/src/lib/index.ts
+++ components/src/lib/index.ts
@@ -1,4 +1,4 @@
 export { default as Modal } from "./Modal/Modal";
-export { default as AlertModal } from "./Modal/AlertModal";
-export { default as ConfirmModal } from "./Modal/ConfirmModal";
-export { default as PromptModal } from "./Modal/PromptModal";
+export { default as AlertModal } from "./Modal/AlertModal/AlertModal";
+export { default as ConfirmModal } from "./Modal/ConfirmModal/ConfirmModal";
+export { default as PromptModal } from "./Modal/PromptModal/PromptModal";
