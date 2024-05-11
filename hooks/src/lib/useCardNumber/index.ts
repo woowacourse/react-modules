e65edf3745e04cd98bb2eeefe7c698d841cd81c0
ss0526100 <ss0526100@gmail.com>
2024-05-12 03:36:22 +0900

    feat(hooks/useCardNumber): 원하는 카드사만 가져올 수 있도록 변경

--- hooks/src/lib/useCardNumber/index.ts
+++ hooks/src/lib/useCardNumber/index.ts
@@ -1,26 +1,38 @@
-import { Validator, ValueOf } from '../type';
-import { useCallback, useState } from 'react';
+import { useCallback, useMemo, useState } from 'react';
 
-import REGEXPS from '../constants/regExps';
-import getCardBrand from './getCardBrand';
+import { CARD_BRAND_NAME } from '../constants/cardBrandRule';
+import getCardNumberValidator from './getCardNumberValidator';
+import getCheckCardBrandRule from './getCheckCardBrand';
 import getErrorMessage from '../utils/getErrorMessage';
 import getFormattedCardNumber from './getFormattedCardNumber';
 import getOnChange from '../utils/getOnChange';
 
-export default function useCardNumber() {
+export default function useCardNumber(
+  targetBrandNames: (typeof CARD_BRAND_NAME)[number][] = [...CARD_BRAND_NAME]
+) {
   const [cardNumber, setCardNumber] = useState('');
 
   const onChange = useCallback(() => getOnChange(setCardNumber), []);
 
-  const errorMessage = getErrorMessage(cardNumber, cardNumberPartValidators);
+  const checkCardBrandRule = useCallback(
+    getCheckCardBrandRule(targetBrandNames),
+    targetBrandNames
+  );
+
+  const cardNumberValidators = useMemo(
+    () => getCardNumberValidator(targetBrandNames),
+    targetBrandNames
+  );
+
+  const errorMessage = getErrorMessage(cardNumber, cardNumberValidators);
 
   const isValid = errorMessage === null;
 
-  const cardBrand = getCardBrand(cardNumber);
+  const cardBrandRule = checkCardBrandRule(cardNumber);
 
   const formattedCardNumber =
-    cardBrand && cardBrand.numberLength === cardNumber.length
-      ? getFormattedCardNumber(cardNumber, cardBrand.formatArray)
+    cardBrandRule && cardBrandRule.numberLength === cardNumber.length
+      ? getFormattedCardNumber(cardNumber, cardBrandRule.formatArray)
       : null;
 
   return {
@@ -29,44 +41,8 @@ export default function useCardNumber() {
     onChange,
     errorMessage,
     isValid,
-    cardBrand: cardBrand ?? null,
+    cardBrandName: cardBrandRule?.name ?? null,
+    cardBrandRule: cardBrandRule ?? null,
     formattedCardNumber,
   };
 }
-
-const CARD_NUMBER_LENGTH = 16;
-export const CARD_NUMBER_ERROR_MESSAGE = {
-  overLength: `카드번호는 ${CARD_NUMBER_LENGTH}자리 이하여야 합니다.`,
-  notDigit: '카드번호는 숫자만 포함해야 합니다.',
-  notSupportedBrand: '지원하지 않는 카드 브랜드입니다.',
-  invalidCardLength: '해당 카드 브랜드와는 맞지 않는 번호 길이입니다.',
-} as const;
-
-type ErrorMessage = ValueOf<typeof CARD_NUMBER_ERROR_MESSAGE>;
-
-const cardNumberPartValidators: Validator<string, ErrorMessage>[] = [
-  {
-    checkIsValid: (cardNumber: string) =>
-      cardNumber.length <= CARD_NUMBER_LENGTH,
-    message: CARD_NUMBER_ERROR_MESSAGE.overLength,
-  },
-  {
-    checkIsValid: (cardNumber: string) =>
-      REGEXPS.onlyDigitNumber.test(cardNumber),
-    message: CARD_NUMBER_ERROR_MESSAGE.notDigit,
-  },
-  {
-    checkIsValid: (cardNumber: string) => {
-      return getCardBrand(cardNumber) !== null;
-    },
-    message: CARD_NUMBER_ERROR_MESSAGE.notSupportedBrand,
-  },
-  {
-    checkIsValid: (cardNumber: string) => {
-      const cardBrand = getCardBrand(cardNumber);
-      if (cardBrand === null) return false;
-      return cardBrand?.numberLength === cardNumber.length;
-    },
-    message: CARD_NUMBER_ERROR_MESSAGE.notSupportedBrand,
-  },
-];
