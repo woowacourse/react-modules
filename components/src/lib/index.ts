d7a0e9c89b3a1ba5d53882509b7f5e523e4e4e4b
chysis <chysiss@naver.com>
2024-05-11 22:02:42 +0900

    style(index): 오타 수정

--- components/src/lib/index.ts
+++ components/src/lib/index.ts
@@ -1,4 +1,4 @@
 export { default as Modal } from './Modal/Modal';
 export { default as AlertModal } from './AlertModal/AlertModal';
-export { default as ConfirmMoal } from './ConfirmModal/ConfirmModal';
+export { default as ConfirmModal } from './ConfirmModal/ConfirmModal';
 export { default as PromptModal } from './PromptModal/PromptModal';
