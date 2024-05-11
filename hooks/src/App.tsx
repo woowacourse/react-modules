cc2797b3402ff1d10d762bff112ec2fa2d0dc693
GimGayeon <kky1373@naver.com>
2024-05-11 16:12:01 +0900

    feat(App): 카드 번호 입력 테스트 문서 작성

--- hooks/src/App.tsx
+++ hooks/src/App.tsx
@@ -1,12 +1,26 @@
 import React from "react";
 import "./App.css";
+import useCardNumberInput from "./lib/useCardNumberInput";
+import styled from "styled-components";
 
 function App() {
+  const { cardNumber, cardType, handleCardNumberChange } = useCardNumberInput();
   return (
-    <>
-      <h1>Hooks Modules</h1>
-    </>
+    <DefaultStyle>
+      <input
+        value={cardNumber.value.cardNumber}
+        onChange={(e) => handleCardNumberChange(e)}
+        name="cardNumber"
+      ></input>
+      <p>에러메세지: {cardNumber.errorMessage.cardNumber}</p>
+      <p>값: {cardNumber.value.cardNumber}</p>
+      <p>카드 타입: {cardType}</p>
+    </DefaultStyle>
   );
 }
 
+const DefaultStyle = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 export default App;
