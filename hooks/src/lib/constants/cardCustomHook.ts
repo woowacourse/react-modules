1ec65b544fb32e2032ebe0b37ff500e43def5276
simhorim <hori5020@gmail.com>
2024-05-11 18:00:34 +0900

    feat(cardCustomHook): validCardNumberLength, invalidCardNumberLength 함수로 수정 및 Diners, AMEX 상수 선언

--- hooks/src/lib/constants/cardCustomHook.ts
+++ hooks/src/lib/constants/cardCustomHook.ts
@@ -1,9 +1,19 @@
+import cardInputValidator from "../validators/cardInputValidator";
+
 export const INPUT_RULES = {
   minMonth: 1,
   maxMonth: 12,
   minCardHolderNameLength: 1,
   maxCardHolderNameLength: 15,
-  validCardNumberLength: 4,
+  validDinersCardNumber: "36",
+  validAMEXCardNumbers: ["34", "37"],
+  validCardNumberLength: (value: string) => {
+    if (cardInputValidator.validateDinersCardNumber(value)) return 14;
+
+    if (cardInputValidator.validateAMEXCardNumber(value)) return 15;
+
+    return 16;
+  },
   validExpiryDateLength: 2,
   validCVCLength: 3,
   validCardPasswordLength: 2,
@@ -12,7 +22,15 @@ export const INPUT_RULES = {
 export const VALIDATION_MESSAGES = {
   onlyNumbersAllowed: "숫자를 입력해 주세요.",
   onlyEnglishAllowed: "영문자를 입력해 주세요.",
-  invalidCardNumberLength: "4자리의 숫자를 입력해 주세요.",
+  invalidCardNumberLength: (value: string) => {
+    if (cardInputValidator.validateDinersCardNumber(value))
+      return "Diners 카드번호는 14자리 숫자여야 합니다.";
+
+    if (cardInputValidator.validateAMEXCardNumber(value))
+      return "AMEX 카드번호는 15자리 숫자여야 합니다.";
+
+    return "카드번호는 16자리 숫자여야 합니다.";
+  },
   invalidMonthRange: "월 입력은 01 ~ 12 사이의 입력이어야해요",
   expiredYear: "이미 지난 년도는 입력할 수 없어요.",
   expiredDate: "이미 지난 유효기간이에요",
