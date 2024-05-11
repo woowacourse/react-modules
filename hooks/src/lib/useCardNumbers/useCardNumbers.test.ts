f5d004eaaf5deb88e85d8747fc3927f628026e34
이태훈 <rhymint@gmail.com>
2024-05-12 02:38:09 +0900

    test: useCardNumbers, useCardExpirationDate 테스트 코드 작성

--- hooks/src/lib/useCardNumbers/useCardNumbers.test.ts
+++ hooks/src/lib/useCardNumbers/useCardNumbers.test.ts
@@ -2,50 +2,119 @@ import { act } from 'react';
 import { renderHook } from '@testing-library/react';
 import useCardNumbers from './useCardNumbers';
 
-describe('useCardNumbers custom hook', () => {
-  const maxLength = 4;
-
-  test('hook이 초기화될 때 주어진 initValue로 설정된다.', () => {
-    const { result } = renderHook(() =>
-      useCardNumbers(['1234', '5678', '9012', '3456'], maxLength),
-    );
-    expect(result.current.value).toEqual(['1234', '5678', '9012', '3456']);
-  });
-
-  test(`카드번호의 각 input에서 ${maxLength}를 초과하는 값을 입력하면 "카드번호는 ${maxLength}글자 까지만 입력이 가능해요." 라는 errorMessage를 반환한다.`, () => {
-    const { result } = renderHook(() =>
-      useCardNumbers(['', '', '', ''], maxLength),
-    );
-    act(() => {
-      result.current.onChangeHandler({ target: { value: '12345' } } as any, 0);
-    });
-    expect(result.current.errorMessage).toBe(
-      `카드번호는 ${maxLength}글자 까지만 입력이 가능해요.`,
-    );
-  });
-
-  test('모든 input에서 포커스가 해제됐을 때, 전체 카드 번호가 16글자가 아니라면 "카드번호는 16글자로 입력해 주세요." 라는 errorMessage를 반환한다.', () => {
-    const { result } = renderHook(() =>
-      useCardNumbers(['1234', '567', '9012', ''], maxLength),
-    );
-    act(() => {
-      result.current.onBlurHandler(0);
-      result.current.onBlurHandler(1);
-    });
-    expect(result.current.errorMessage).toBe(
-      '카드번호는 16글자로 입력해 주세요.',
-    );
-  });
-
-  test('모든 input에서 포커스가 해제됐을 때, 전체 카드 번호가 16글자라면 isCompleted가 true로 설정된다.', () => {
-    const { result } = renderHook(() =>
-      useCardNumbers(['1234', '5678', '9012', '3456'], maxLength),
-    );
-    act(() => {
-      result.current.onBlurHandler(0);
-      result.current.onBlurHandler(1);
-    });
-    expect(result.current.isCompleted).toBe(true);
+describe('useCardNumbers Hook', () => {
+  it('initializes correctly with empty values', () => {
+    const { result } = renderHook(() => useCardNumbers(''));
+    expect(result.current.value).toBe('');
+    expect(result.current.cardBrand).toBe('none');
     expect(result.current.errorMessage).toBe('');
+  });
+
+  describe('카드 브랜드 별 포맷팅 테스트', () => {
+    it.each([
+      {
+        type: 'Visa',
+        number: '4111111111111111',
+        formatted: '4111-1111-1111-1111',
+      },
+      {
+        type: 'MasterCard',
+        number: '5555555555554444',
+        formatted: '5555-5555-5555-4444',
+      },
+      {
+        type: 'Diners',
+        number: '36777777777777',
+        formatted: '3677-777777-7777',
+      },
+      {
+        type: 'AMEX',
+        number: '341111111111111',
+        formatted: '3411-111111-11111',
+      },
+      {
+        type: 'UnionPay',
+        number: '6221288888888888',
+        formatted: '6221-2888-8888-8888',
+      },
+      {
+        type: 'UnionPay',
+        number: '6221925888888888',
+        formatted: '6221-9258-8888-8888',
+      },
+    ])(
+      '$type 카드의 번호는 $formatted와 같이 포맷팅 되어야 한다.',
+      async ({ number, formatted }) => {
+        const { result } = renderHook(() => useCardNumbers(''));
+        act(() => {
+          result.current.onChangeHandler({
+            target: { value: number },
+          } as React.ChangeEvent<HTMLInputElement>);
+        });
+
+        expect(result.current.value).toBe(formatted);
+      },
+    );
+  });
+
+  describe('error 처리 테스트', () => {
+    it('숫자가 아닌 값이 입력됐을 때, 에러가 발생하고 값이 변경되지 않아야 한다.', () => {
+      const { result } = renderHook(() => useCardNumbers(''));
+      act(() => {
+        result.current.onChangeHandler({
+          target: { value: 'abc' },
+        } as React.ChangeEvent<HTMLInputElement>);
+      });
+
+      expect(result.current.value).toBe('');
+      expect(result.current.errorMessage).toContain(
+        '카드번호는 숫자만 입력이 가능해요.',
+      );
+    });
+  });
+
+  describe('카드 브랜드별 카드 번호 최대 길이 테스트', () => {
+    it.each([
+      {
+        type: 'Visa',
+        number: '4111-1111-1111-1111',
+        maxLength: 16,
+      },
+      {
+        type: 'MasterCard',
+        number: '5555-5555-5555-4444',
+        maxLength: 16,
+      },
+      {
+        type: 'Diners',
+        number: '3677-777777-7777',
+        maxLength: 14,
+      },
+      {
+        type: 'AMEX',
+        number: '3411-111111-11111',
+        maxLength: 15,
+      },
+      {
+        type: 'UnionPay',
+        number: '6221-2888-8888-8888',
+        maxLength: 16,
+      },
+    ])('formats $type card correctly', ({ number, maxLength }) => {
+      const { result } = renderHook(() => useCardNumbers(''));
+      act(() =>
+        result.current.onChangeHandler({
+          target: { value: number },
+        } as React.ChangeEvent<HTMLInputElement>),
+      );
+      act(() =>
+        result.current.onChangeHandler({
+          target: { value: number + '1' },
+        } as React.ChangeEvent<HTMLInputElement>),
+      );
+      expect(result.current.errorMessage).toBe(
+        `카드번호는 ${maxLength}글자 까지만 입력이 가능해요.`,
+      );
+    });
   });
 });
