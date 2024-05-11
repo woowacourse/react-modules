566a57a8b2f3a8acda46da18a9e009ef82b4e0f6
Yoonkyoung Kim <dev.yoonkyoung@gmail.com>
2024-05-11 12:28:26 +0900

    test: 사이즈별 모달 스토리 추가 (small, medium, large)

--- components/src/stories/Modal.stories.tsx
+++ components/src/stories/Modal.stories.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Meta, StoryObj } from '@storybook/react';
 import { Modal, useModal } from '../lib/index';
-import CardCompanySelector from '../components/CardCompanySelector';
+import { action } from '@storybook/addon-actions';
+import Input from '../lib/components/common/Input';
 
 import '../styles/index.css';
 
@@ -9,46 +10,110 @@ const meta: Meta<typeof Modal> = {
   title: 'Modal',
   component: Modal,
   argTypes: {
-    title: { control: 'text' },
-    size: {
+    title: {
+      control: 'text',
+    },
+    message: {
+      control: 'text',
+    },
+    closeIcon: {
+      control: 'boolean',
+    },
+    $size: {
       control: { type: 'select', options: ['small', 'medium', 'large'] },
     },
-    position: {
-      control: { type: 'select', options: ['center', 'bottom'] },
+    $position: {
+      control: { type: 'radio', options: ['center', 'bottom'] },
     },
-    closeOption: {
-      control: { type: 'select', options: ['icon', 'button'] },
-    },
+
+    children: { table: { disable: true } },
   },
 };
 
 export default meta;
 
-const Template: StoryObj<typeof meta.args> = (args) => {
+const DefaultModal: StoryObj<typeof meta.args> = (args) => {
   const { isModalOpen, openModal, closeModal } = useModal();
 
   return (
     <>
       <button onClick={openModal}>모달 열기</button>
       {isModalOpen && (
-        <Modal {...args} closeModal={closeModal}>
-          <CardCompanySelector />
+        <Modal
+          $size={args.$size}
+          $position={args.$position}
+          onCloseModal={closeModal}
+        >
+          <Modal.Header
+            title={args.title}
+            closeIcon={args.closeIcon}
+            onCloseModal={closeModal}
+          ></Modal.Header>
+          <Modal.Content message={args.message} />
+          <Modal.Footer>
+            <Modal.Button
+              type="button"
+              $size="small"
+              onButtonClick={closeModal}
+            >
+              닫기
+            </Modal.Button>
+          </Modal.Footer>
         </Modal>
       )}
     </>
   );
 };
 
-export const IconCloseModal = Template.bind({});
-IconCloseModal.args = {
-  title: '아이콘으로 닫기',
-  position: 'center',
-  closeOption: 'icon',
+export const SmallModal = DefaultModal.bind({});
+SmallModal.args = {
+  title: '작은 모달',
+  message: '이것은 작은 사이즈의 모달입니다.',
+  closeIcon: true,
+  $size: 'small',
+  $position: 'center',
+};
+SmallModal.argTypes = {
+  $size: { table: { disable: true } },
 };
 
-export const ButtonCloseModal = Template.bind({});
+export const MediumModal = DefaultModal.bind({});
+MediumModal.args = {
+  title: '중간 모달',
+  message: '이것은 중간 사이즈의 모달입니다.',
+  closeIcon: true,
+  $size: 'medium',
+  $position: 'center',
+};
+MediumModal.argTypes = {
+  $size: { table: { disable: true } },
+};
+
+export const LargeModal = DefaultModal.bind({});
+LargeModal.args = {
+  title: '큰 모달',
+  message: '이것은 큰 사이즈의 모달입니다.',
+  closeIcon: true,
+  $size: 'large',
+  $position: 'center',
+};
+LargeModal.argTypes = {
+  $size: { table: { disable: true } },
+};
+
+export const IconCloseModal = DefaultModal.bind({});
+IconCloseModal.args = {
+  title: '아이콘으로 닫기',
+  message: '아이콘을 클릭하면 모달이 닫힙니다.',
+  closeIcon: true,
+  $position: 'center',
+};
+
+export const ButtonCloseModal = DefaultModal.bind({});
 ButtonCloseModal.args = {
   title: '버튼으로 닫기',
-  position: 'bottom',
-  closeOption: 'button',
+  message: '닫기 버튼을 클릭하면 모달이 닫힙니다.',
+  closeIcon: false,
+  $size: 'medium',
+  $position: 'center',
 };
