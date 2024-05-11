a2b99439b5d58e03f019af34d8ad185f7644cc50
hain-tain <hain7ce@gmail.com>
2024-05-11 18:08:11 +0900

    chore: hain-tain-hooks 모듈 버전 업 및 배포
    Co-authored-by: Todari <rhymint@gmail.com>

--- hooks/src/lib/index.ts
+++ hooks/src/lib/index.ts
@@ -4,5 +4,6 @@ export { default as useCardCompany } from './useCardCompany/useCardCompany';
 export { default as useCardCVC } from './useCardCVC/useCardCVC';
 export { default as useCardExpirationDate } from './useCardExpirationDate/useCardExpirationDate';
 export { default as useCardHolderName } from './useCardHolderName/useCardHolderName';
+export { default as useCardNumber } from './useCardNumber/useCardNumber';
 export { default as useCardNumbers } from './useCardNumbers/useCardNumbers';
 export { default as useCardPassword } from './useCardPassword/useCardPassword';
