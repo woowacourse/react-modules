c558babf012268d4dd38e53e60db3801c5e6b2fc
bmj0053 <bmj05378@gmail.com>
2024-05-11 21:00:26 +0900

    feat: maxLength 판별 기능 추가

--- hooks/src/lib/hooks/useCardNumbers/useCardNumbers.ts
+++ hooks/src/lib/hooks/useCardNumbers/useCardNumbers.ts
@@ -5,73 +5,73 @@ import { MAX_LENGTH_ERROR_MESSAGE } from "../../constants/errorMessage";
 
 function useCardNumbers() {
   const [cardNumbers, setCardNumbers] = useState("");
-  const [cardNumberErrors, setCardNumberErrors] = useState(false);
+  const [cardNumberError, setCardNumberError] = useState(false);
+  const [cardBrand, setCardBrand] = useState("domestic");
+  const [cardNumberMaxLength, setCardNumberMaxLength] = useState(0);
 
-  const checkCardBrand = (
-    value: string
-  ): { cardBrand: string; maxLength: number } => {
-    const inputLength = value.length;
+  const checkCardBrand = (value: string) => {
     const twoDigitValue = Number(value.substring(0, 2));
-    const valueToNumber = Number(value.substring(0, inputLength));
+    const valueToNumber = Number(value.substring(0, 3));
 
     const inputValue = {
       cardBrand: "domestic",
-      maxLength: 16,
+      // maxLength: 16,
     };
 
     if (value.startsWith("4")) {
       inputValue.cardBrand = "visa";
-      inputValue.maxLength = 16;
+      setCardNumberMaxLength(16);
+      // inputValue.maxLength = 16;
     } else if (twoDigitValue > 50 && twoDigitValue < 56) {
       inputValue.cardBrand = "masterCard";
-      inputValue.maxLength = 16;
+      setCardNumberMaxLength(16);
+      // inputValue.maxLength = 16;
     } else if (twoDigitValue === 36) {
       inputValue.cardBrand = "diners";
-      inputValue.maxLength = 14;
+      setCardNumberMaxLength(14);
+      // inputValue.maxLength = 14;
     } else if (twoDigitValue === 34 || twoDigitValue === 37) {
       inputValue.cardBrand = "amex";
-      inputValue.maxLength = 15;
+      setCardNumberMaxLength(15);
+      // inputValue.maxLength = 15;
     } else if (
       (valueToNumber >= 624 && valueToNumber <= 626) ||
       (valueToNumber >= 6282 && valueToNumber <= 6288) ||
       (valueToNumber >= 622126 && valueToNumber <= 622925)
     ) {
       inputValue.cardBrand = "unionPay";
-      inputValue.maxLength = 16;
+      setCardNumberMaxLength(16);
+      // inputValue.maxLength = 16;
     }
     return inputValue;
   };
 
   const handleCardNumbersChange = (value: string) => {
     // 1. 입력 받을 때마다 카드 브랜드 검사 - 앞쪽 글자 확인
-    const { cardBrand, maxLength } = checkCardBrand(value);
-    console.log(value, cardBrand, maxLength);
+    const { cardBrand } = checkCardBrand(value);
+    setCardBrand(cardBrand);
 
     // 2. 카드 브랜드에 맞게 maxLength 설정
-
+    const isValidNumber =
+      INPUT_REGEX_PARAMS.cardNumber(cardNumberMaxLength).test(value);
+    setCardNumberError(!isValidNumber);
+    setCardNumbers(value);
+    
     // 3. 포맷팅
-
-    // const isValidNumber = INPUT_REGEX_PARAMS.cardNumber(maxLength).test(value);
-
-    //   const updatedErrors = [...cardNumberErrors];
-    //   updatedErrors[inputIndex] = !isValidNumber;
-    //   setCardNumberErrors(updatedErrors);
-
-    //   const updatedNumbers = [...cardNumbers];
-    //   updatedNumbers[inputIndex] = value;
-    //   setCardNumbers(updatedNumbers);
   };
 
-  // const getCardNumbersErrorMessage = () => {
-  //   return cardNumberErrors.some((isError) => isError)
-  //     ? MAX_LENGTH_ERROR_MESSAGE(maxLength)
-  //     : undefined;
-  // };
+  const getCardNumbersErrorMessage = () => {
+    return cardNumberError
+      ? MAX_LENGTH_ERROR_MESSAGE(cardNumberMaxLength)
+      : null;
+  };
 
   return {
     cardNumbers,
-    // cardNumberErrors,
-    // getCardNumbersErrorMessage,
+    cardNumberMaxLength,
+    cardBrand,
+    // cardNumberError,
+    getCardNumbersErrorMessage,
     handleCardNumbersChange,
   };
 }
