0380e735e7808773b1d23d51c5f38e95afce20b8
Yoonkyoung Kim <dev.yoonkyoung@gmail.com>
2024-05-11 20:00:19 +0900

    feat: AlertModal 컴포넌트 구현

--- components/src/lib/components/types.ts
+++ components/src/lib/components/types.ts
@@ -2,3 +2,18 @@ export type StyleSize = 'small' | 'medium' | 'large';
 export type StyleDirection = 'row' | 'column';
 export type StyleAlign = 'start' | 'center' | 'end';
 export type StylePosition = 'center' | 'bottom';
+
+export interface BasicModal {
+  title: string;
+  message?: string;
+  isCloseIcon?: boolean;
+  $modalSize?: StyleSize;
+  $position?: StylePosition;
+  $contentDirection?: StyleDirection;
+  $footerSize?: StyleSize;
+  $footerDirection?: StyleDirection;
+  $algin?: StyleAlign;
+  $buttonSize?: StyleSize;
+  $buttonBackgroundColor?: string;
+  $buttonColor?: string;
+}
