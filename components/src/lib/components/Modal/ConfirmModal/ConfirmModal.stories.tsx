87068e752baa3d93e48df8f568b35fe0e9b6882f
Yoonkyoung Kim <dev.yoonkyoung@gmail.com>
2024-05-11 20:39:07 +0900

    test: Confirm 모달 스토리 추가

---
+++ components/src/lib/components/Modal/ConfirmModal/ConfirmModal.stories.tsx
@@ -0,0 +1,31 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+
+import ConfirmModal from './index';
+
+import '../../../../styles/index.css';
+
+const meta: Meta<typeof ConfirmModal> = {
+  title: 'Modal/Examples',
+  component: ConfirmModal,
+  argTypes: {
+    children: { table: { disable: true } },
+  },
+};
+
+export default meta;
+
+export const Confirm = ConfirmModal.bind({});
+Confirm.args = {
+  title: '확인/취소(Confirm) 모달',
+  message: '사용자에게 선택지를 제공하고 확인 및 취소 버튼을 제공',
+  isCloseIcon: false,
+  $size: 'medium',
+  $position: 'center',
+  $buttonSize: 'small',
+  $buttonBackgroundColor: '#FFFFFF',
+  $buttonColor: '#000000',
+  onModalClose: action('modalClose'),
+  onConfirmButtonClick: action('confirmButtonClick'),
+  onCancelButtonClick: action('cancelButtonClick'),
+};
--- components/src/lib/components/Modal/ConfirmModal/index.tsx
+++ components/src/lib/components/Modal/ConfirmModal/index.tsx
@@ -1,4 +1,3 @@
-import useModal from '../../../hooks/useModal';
 import Modal from '../../common/Modal';
 import { BasicModal } from '../../types';
 
@@ -8,43 +7,36 @@ interface ConfirmModalProps extends BasicModal {
 }
 
 function ConfirmModal({ ...rest }: ConfirmModalProps) {
-  const { isModalOpen, closeModal } = useModal();
   return (
-    <>
-      {isModalOpen && (
-        <Modal onCloseModal={closeModal}>
-          <Modal.Header
-            title={rest.title}
-            isCloseIcon={rest.isCloseIcon}
-            onCloseModal={closeModal}
-          ></Modal.Header>
-          <Modal.Content
-            message={rest.message}
-            $direction={rest.$contentDirection}
-          />
-          <Modal.Footer $direction={rest.$footerDirection} $align={rest.$algin}>
-            <Modal.Button
-              type="button"
-              onButtonClick={rest.onCancelButtonClick}
-              $size={rest.$buttonSize}
-              $backgroundColor={rest.$buttonBackgroundColor}
-              $color={rest.$buttonColor}
-            >
-              취소
-            </Modal.Button>
-            <Modal.Button
-              type="button"
-              onButtonClick={rest.onConfirmButtonClick}
-              $size={rest.$buttonSize}
-              $backgroundColor={rest.$buttonBackgroundColor}
-              $color={rest.$buttonColor}
-            >
-              확인
-            </Modal.Button>
-          </Modal.Footer>
-        </Modal>
-      )}
-    </>
+    <Modal onCloseModal={rest.onModalClose}>
+      <Modal.Header
+        title={rest.title}
+        isCloseIcon={rest.isCloseIcon}
+        onCloseModal={rest.onModalClose}
+      ></Modal.Header>
+      <Modal.Content
+        message={rest.message}
+        $direction={rest.$contentDirection}
+      />
+      <Modal.Footer $direction={rest.$footerDirection} $align={rest.$algin}>
+        <Modal.Button
+          type="button"
+          onButtonClick={rest.onCancelButtonClick}
+          $size={rest.$buttonSize}
+          $backgroundColor={rest.$buttonBackgroundColor}
+          $color={rest.$buttonColor}
+        >
+          취소
+        </Modal.Button>
+        <Modal.Button
+          type="button"
+          onButtonClick={rest.onConfirmButtonClick}
+          $size={rest.$buttonSize}
+        >
+          확인
+        </Modal.Button>
+      </Modal.Footer>
+    </Modal>
   );
 }
 
