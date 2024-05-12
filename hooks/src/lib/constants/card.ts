496121d63fd43ffbd34d98df617cdb82d02013c0
badahertz52 <badahertz52@gmail.com>
2024-05-12 16:35:32 +0900

    refactor [hooks] ; 카드 브랜드 포멧팅 리팩토링
     - 카드 브랜드들은 CARD_BRAND라는 하나의 상수에서 관리하도록 수정
    - 유니온 카드처럼 브랜드를 판별하는 숫자의 자릿수가 다양한 경우 이중 배열을 사용
    - BRAND_LENGTH 도 CARD_BRAND라는 것을 사용해 생성되도록 함
    - detectCardBrand에서 CARD_BRAND를 사용해 바로 카드 판별하도록 함

--- hooks/src/lib/constants/card.ts
+++ hooks/src/lib/constants/card.ts
@@ -1,4 +1,4 @@
-import { Brand, BrandInfo } from '../types/card';
+import { BrandInfo } from '../types/card';
 import createNumbers from '../utils/numberArrayUtils';
 
 export const MONTH = {
@@ -13,64 +13,26 @@ export const MAX_CARD_YEARS_FROM_NOW = 5;
 
 export const PASSWORD_LENGTH = 2;
 
-export const VISA: BrandInfo<number[]> = {
-  name: 'visa',
-  numbers: [4],
-  length: 16,
-};
-export const MASTER_NUMBERS = {
-  start: 51,
-  end: 55,
-};
-export const MASTER: BrandInfo<number[]> = {
-  name: 'master',
-  numbers: createNumbers(MASTER_NUMBERS.start, MASTER_NUMBERS.end),
-  length: 16,
-};
+export const CARD_BRANDS: BrandInfo<number[] | number[][]>[] = [
+  { name: 'visa', numbers: [4], length: 16 },
+  { name: 'master', numbers: createNumbers(51, 55), length: 16 },
+  { name: 'diners', numbers: [36], length: 14 },
+  { name: 'amex', numbers: [34, 35], length: 15 },
+  {
+    name: 'union',
+    numbers: [createNumbers(622126, 622925), createNumbers(6282, 6288), createNumbers(624, 626)],
+    length: 16,
+  },
+];
 
-export const DINERS: BrandInfo<number[]> = {
-  name: 'diners',
-  numbers: [36],
-  length: 14,
-};
-
-export const AMEX: BrandInfo<number[]> = {
-  name: 'amex',
-  numbers: [34, 35],
-  length: 15,
-};
-export const UNION_NUMBERS = {
-  sixNumbers: {
-    start: 622126,
-    end: 622925,
+export const BRAND_LENGTH = CARD_BRANDS.reduce(
+  (acc, brand) => {
+    if (!brand.name) return acc;
+    acc[brand.name] = brand.length;
+    return acc;
   },
-  fourNumbers: {
-    start: 6282,
-    end: 6288,
-  },
-  threeNumbers: {
-    start: 624,
-    end: 626,
-  },
-};
+  {} as Record<string, number>,
+);
 
 export const MIN_CARD_NUMBERS_LENGTH = 14;
 export const MAX_CARD_NUMBERS_LENGTH = 16;
-
-export const UNION: BrandInfo<{ sixNumbers: number[]; fourNumbers: number[]; threeNumbers: number[] }> = {
-  name: 'union',
-  numbers: {
-    sixNumbers: createNumbers(UNION_NUMBERS.sixNumbers.start, UNION_NUMBERS.sixNumbers.end),
-    fourNumbers: createNumbers(UNION_NUMBERS.fourNumbers.start, UNION_NUMBERS.fourNumbers.end),
-    threeNumbers: createNumbers(UNION_NUMBERS.threeNumbers.start, UNION_NUMBERS.threeNumbers.end),
-  },
-  length: 16,
-};
-
-export const BRAND_LENGTH: { [key in Exclude<Brand, null>]: number } = {
-  visa: VISA.length,
-  master: MASTER.length,
-  diners: DINERS.length,
-  amex: AMEX.length,
-  union: UNION.length,
-};
--- hooks/src/lib/hooks/useCardBrand.ts
+++ hooks/src/lib/hooks/useCardBrand.ts
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 
-import { AMEX, DINERS, MASTER, UNION, VISA } from '../constants';
-import { Brand, BrandInfo } from '../types/card';
+import { CARD_BRANDS } from '../constants';
+import { Brand } from '../types/card';
 
 export default function useCardBrand({ cardNumbers }: { cardNumbers: string }) {
   const [brand, setBrand] = useState<Brand>(null);
@@ -15,43 +15,27 @@ export default function useCardBrand({ cardNumbers }: { cardNumbers: string }) {
 
     return brandNumbers.includes(prefix);
   };
-
   /**
-   * 카드 번호에 맞는 카드 브랜드를 반환, 만약 해당하는 카드 브랜드가 없으면 undefined를 반환
-   * @param brandInfo : 카드 브랜드 정보
-   * @param isValidatedNumbers  : 카드 번호가 카드 브랜드 번호에 속하는 지 여부
+   * number[] 인지 판단
    */
-  const getBrand = <T>(brandInfo: BrandInfo<T>, isValidatedNumbers: boolean): Brand | undefined => {
-    const { length, name } = brandInfo;
+  function isNumberArray(arr: number[] | number[][]): arr is number[] {
+    return Array.isArray(arr) && arr.every((item) => typeof item === 'number');
+  }
 
-    if (cardNumbers.length !== length) return;
-    if (isValidatedNumbers) {
-      return name;
-    }
+  const detectCardBrand = (): Brand | undefined => {
+    return CARD_BRANDS.find(({ numbers, length }) => {
+      if (cardNumbers.length !== length) return false;
+      // 브랜드를 판별하는 숫자가 number[]인 경우에 isBrandNumbers로 바로 판단
+      if (isNumberArray(numbers)) return isBrandNumbers(numbers);
+      //  브랜드를 판별하는 숫자의 자릿수가 다양한 유니온의 경우, 이중배열이라서 이중 배열을 돌면서 판단
+      return numbers.some((array) => isBrandNumbers(array));
+    })?.name;
   };
-
-  /**
-   * 카드 브랜드별, 판독 메서드를 가지고 있는 Map 객체
-   */
-  const brandDetecterMap: Map<Exclude<Brand, null>, () => Brand | undefined> = new Map([
-    ['visa', (): Brand | undefined => getBrand(VISA, isBrandNumbers(VISA.numbers))],
-    ['master', (): Brand | undefined => getBrand(MASTER, isBrandNumbers(MASTER.numbers))],
-    ['diners', (): Brand | undefined => getBrand(DINERS, isBrandNumbers(DINERS.numbers))],
-    ['amex', (): Brand | undefined => getBrand(AMEX, isBrandNumbers(AMEX.numbers))],
-    [
-      'union',
-      (): Brand | undefined => {
-        const isUnionNumbers = Object.values(UNION.numbers).some((numbers) => isBrandNumbers(numbers));
-        return getBrand(UNION, isUnionNumbers);
-      },
-    ],
-  ]);
-
-  const detectCardBrand = () => Array.from(brandDetecterMap.entries()).find(([_, value]) => !!value())?.[0];
 
   const validateCardBrand = () => {
     // 카드 번호에 맞는 카드 번호 판단
     const cardBrand = detectCardBrand();
+
     setBrand(cardBrand || null);
   };
 
