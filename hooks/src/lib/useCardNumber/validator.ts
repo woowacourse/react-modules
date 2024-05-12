1b8b0222c49b748cfda80eadb5cf3527569b6e3f
hanyoung <phanyoung@naver.com>
2024-05-12 13:08:24 +0900

    feat(useCardNumber/validator): 카드 브랜드 별로 카드번호 자리수를 다르게 검증하도록 세분화

--- hooks/src/lib/useCardNumber/validator.ts
+++ hooks/src/lib/useCardNumber/validator.ts
@@ -1,7 +1,7 @@
 import { CARD_BRAND, CARD_NUMBER_LENGTH } from "./constants/cardBrand";
 import { IErrorStatus } from "../useInputValidation";
-import { hasCardBrand } from "./utils/hasCardBrand";
 import { REGEX } from "../constants/regex";
+import { identifyCardBrand } from "./utils/identifyCardBrand";
 
 const DEFAULT_CARD_NUMBER_LENGTH = 16;
 export const cardNumberValidator = {
@@ -10,10 +10,27 @@ export const cardNumberValidator = {
       return { isError: true, errorMessage: "카드번호는 숫자만 포함해야 합니다." };
     }
 
-    if (value.length > DEFAULT_CARD_NUMBER_LENGTH) {
+    const cardBrand = identifyCardBrand(value);
+    const isDiners = cardBrand === CARD_BRAND.Diners;
+    const isAmex = cardBrand === CARD_BRAND.AMEX;
+    if (isDiners && value.length > CARD_NUMBER_LENGTH.Diners) {
       return {
         isError: true,
-        errorMessage: `카드번호는 ${DEFAULT_CARD_NUMBER_LENGTH}자리로 입력해 주세요. (${CARD_BRAND.Diners} - ${CARD_NUMBER_LENGTH.Diners}자리, ${CARD_BRAND.AMEX} - ${CARD_NUMBER_LENGTH.AMEX}자리 제외)`,
+        errorMessage: `Diners 카드번호는 ${CARD_NUMBER_LENGTH.Diners}자리로 입력해 주세요.`,
+      };
+    }
+
+    if (isAmex && value.length > CARD_NUMBER_LENGTH.AMEX) {
+      return {
+        isError: true,
+        errorMessage: `AMEX 카드번호는 ${CARD_NUMBER_LENGTH.AMEX}자리로 입력해 주세요.`,
+      };
+    }
+
+    if (!isAmex && !isDiners && value.length > DEFAULT_CARD_NUMBER_LENGTH) {
+      return {
+        isError: true,
+        errorMessage: `카드번호는 ${DEFAULT_CARD_NUMBER_LENGTH}자리로 입력해 주세요.`,
       };
     }
 
@@ -29,10 +46,27 @@ export const cardNumberValidator = {
       return { isError: true, errorMessage: "카드번호는 숫자만 포함해야 합니다." };
     }
 
-    if (!hasCardBrand(value) && value.length !== DEFAULT_CARD_NUMBER_LENGTH) {
+    const cardBrand = identifyCardBrand(value);
+    const isDiners = cardBrand === CARD_BRAND.Diners;
+    const isAmex = cardBrand === CARD_BRAND.AMEX;
+    if (isDiners && value.length !== CARD_NUMBER_LENGTH.Diners) {
       return {
         isError: true,
-        errorMessage: `카드번호는 ${DEFAULT_CARD_NUMBER_LENGTH}자리로 입력해 주세요. (${CARD_BRAND.Diners} - ${CARD_NUMBER_LENGTH.Diners}자리, ${CARD_BRAND.AMEX} - ${CARD_NUMBER_LENGTH.AMEX}자리 제외)`,
+        errorMessage: `Diners 카드번호는 ${CARD_NUMBER_LENGTH.Diners}자리로 입력해 주세요.`,
+      };
+    }
+
+    if (isAmex && value.length !== CARD_NUMBER_LENGTH.AMEX) {
+      return {
+        isError: true,
+        errorMessage: `AMEX 카드번호는 ${CARD_NUMBER_LENGTH.AMEX}자리로 입력해 주세요.`,
+      };
+    }
+
+    if (!isAmex && !isDiners && value.length !== DEFAULT_CARD_NUMBER_LENGTH) {
+      return {
+        isError: true,
+        errorMessage: `카드번호는 ${DEFAULT_CARD_NUMBER_LENGTH}자리로 입력해 주세요.`,
       };
     }
 
