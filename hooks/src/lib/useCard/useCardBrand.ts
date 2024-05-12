91018e0135dd7b7939447cc06b20c88969c34c49
rbgksqkr <rbgks1937@naver.com>
2024-05-12 11:19:16 +0900

    feat: 입력할 때마다 cardBrand를 판단하는 로직으로 수정

--- hooks/src/lib/useCard/useCardBrand.ts
+++ hooks/src/lib/useCard/useCardBrand.ts
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import {
   validateAMEX,
   validateDiners,
@@ -5,74 +6,38 @@ import {
   validateUnion,
   validateVisa,
 } from '../validator/validateCardBrand';
+import { cardBrandChecker } from '../constants';
 
-interface CardChecker {
-  validate: (cardNumber: string) => boolean;
-  format: number[];
-  cardBrand: string;
-}
+const useCardBrand = () => {
+  const [cardBrand, setCardBrand] = useState('');
 
-const cardBrandChecker: Record<string, CardChecker[]> = {
-  14: [
-    {
-      validate: validateDiners,
-      format: [4, 6, 4],
-      cardBrand: 'Diners',
-    },
-  ],
-  15: [
-    {
-      validate: validateAMEX,
-      format: [4, 6, 5],
-      cardBrand: 'AMEX',
-    },
-  ],
-  16: [
-    {
-      validate: validateVisa,
-      format: [4, 4, 4, 4],
-      cardBrand: '비자카드',
-    },
-    {
-      validate: validateMaster,
-      format: [4, 4, 4, 4],
-      cardBrand: '마스터카드',
-    },
-    {
-      validate: validateUnion,
-      format: [4, 4, 4, 4],
-      cardBrand: '유니온페이',
-    },
-  ],
-};
+  const determineCardBrand = (value: string) => {
+    const isDiners = validateDiners(value);
+    const isAMEX = validateAMEX(value);
+    const isVisa = validateVisa(value);
+    const isMaster = validateMaster(value);
+    const isUnion = validateUnion(value);
 
-const formatCardNumber = (cardNumber: string, divisionUnits: number[]) => {
-  let start = 0;
-  const formattedCardNumbers = divisionUnits.map(unit => {
-    const digits = cardNumber.slice(start, start + unit);
-    start += unit;
-    return digits;
-  });
+    const brands = {
+      DINERS: isDiners,
+      AMEX: isAMEX,
+      VISA: isVisa,
+      MASTER: isMaster,
+      UNION_PAY: isUnion,
+    };
 
-  return formattedCardNumbers;
-};
+    const trueKey = Object.entries(brands).filter(([, value]) => value === true);
+    const validBrand = trueKey.length > 0 ? trueKey[0][0] : '';
+    setCardBrand(validBrand);
+    return validBrand;
+  };
 
-const useCardBrand = (cardNumbers: Record<string, string>) => {
-  const fullCardNumber = Object.values(cardNumbers).reduce((acc, cur) => acc + cur);
-  const cardBrandInfo = cardBrandChecker[fullCardNumber.length];
-
-  if (cardBrandInfo) {
-    const validType = cardBrandInfo.find(brand => brand.validate(fullCardNumber));
-
-    if (validType) {
-      return {
-        cardBrand: validType.cardBrand,
-        formattedCardNumbers: formatCardNumber(fullCardNumber, validType.format),
-      };
-    }
-  }
-
-  return { cardBrand: '', formattedCardNumbers: [] };
+  return {
+    cardBrand,
+    setCardBrand,
+    validMaxLength: cardBrandChecker[cardBrand].validMaxLength,
+    determineCardBrand,
+  };
 };
 
 export default useCardBrand;
