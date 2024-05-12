73b03e1e373693245568f3d8ab851e1a1d9a289c
greetings1012 <ahnqwerty@gmail.com>
2024-05-12 20:26:40 +0900

    refactor: 각 hooks 리팩터링

--- hooks/src/lib/useCVC.ts
+++ hooks/src/lib/useCVC.ts
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 import { REGEX } from "./constants";
 
 interface ValidationResult {
@@ -6,32 +6,37 @@ interface ValidationResult {
   errorMessages: string[];
 }
 
-const useCardPasswordValidation = () => {
+const MAX_LENGTH = 3;
+
+const useCVC = () => {
+  const [cvc, setCVC] = useState("");
   const [validationResult, setValidationResult] = useState<ValidationResult>({
     isValid: false,
     errorMessages: [],
   });
 
-  const handleCardPasswordChange = (value: string, maxLength: number) => {
+  const handleCVCChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
     const errors: string[] = [];
     const isNumericInput = REGEX.onlyNumber.test(value);
-    const isValidLength = value.length === maxLength;
+    const isValidLength = value.length === MAX_LENGTH;
 
     if (!isNumericInput) {
       errors.push("숫자로 입력해주세요.");
     }
 
     if (!isValidLength) {
-      errors.push(`${maxLength}자리 비밀번호를 입력해주세요.`);
+      errors.push(`${MAX_LENGTH}자로 입력해주세요.`);
     }
-
     setValidationResult({
       isValid: isNumericInput && isValidLength,
       errorMessages: errors,
     });
+    if (!isNumericInput) return;
+    setCVC(value);
   };
 
-  return { validationResult, handleCardPasswordChange };
+  return { validationResult, cvc, handleCVCChange };
 };
 
-export default useCardPasswordValidation;
+export default useCVC;
