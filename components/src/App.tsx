4d8607d40e1921e1df5c915840f50b80f2bdfa48
jayming66 <mjw8941@gmail.com>
2024-05-12 21:54:59 +0900

    refactor(App): 로컬 예시 주석 제거

--- components/src/App.tsx
+++ components/src/App.tsx
@@ -1,7 +1,6 @@
 import React, { ChangeEvent } from 'react';
 import { useState } from 'react';
-import { AlertModal, ConfirmModal, Modal, PromptModal } from '@jaymyong66/simple-modal';
-// import { AlertModal, ConfirmModal, Modal, PromptModal } from './lib/index';
+import { PromptModal } from '@jaymyong66/simple-modal';
 import './App.css';
 function App() {
   const [isOpen, setIsOpen] = useState(false);
@@ -12,21 +11,8 @@ function App() {
   return (
     <div className="app">
       <div>
-        <button onClick={handleToggle}>zz</button>
+        <button onClick={handleToggle}>Open</button>
       </div>
-      {/* <Modal position="center" size="medium" isOpen={isOpen} onToggle={handleToggle}>
-        <Modal.ModalHeader title="카드사 선택">
-          <Modal.ModalCloseButton onClick={handleToggle} />
-        </Modal.ModalHeader>
-        <Modal.ModalContent>
-          <div style={{ height: '100px', width: '100px' }}>zz</div>
-        </Modal.ModalContent>
-        <Modal.ModalFooter style={{ backgroundColor: 'red' }}>
-          <Modal.ModalButton variant="primary" onClick={handleToggle}>
-            확인
-          </Modal.ModalButton>
-        </Modal.ModalFooter>
-      </Modal> */}
       <PromptModal
         position="bottom"
         size="medium"
@@ -37,22 +23,6 @@ function App() {
         onChange={handleChange}
         onSubmit={handleSubmit}
       />
-      {/* <AlertModal
-        position="center"
-        size="small"
-        isOpen={isOpen}
-        onToggle={handleToggle}
-        title="쿠폰 번호를 입력해 주세요."
-        caption="아이디를 입력해주세요."
-      /> */}
-      {/* <ConfirmModal
-        position="center"
-        size="small"
-        isOpen={isOpen}
-        onToggle={handleToggle}
-        title="쿠폰 번호를 입력해 주세요."
-        caption="아이디를 입력해주세요."
-      /> */}
     </div>
   );
 }
