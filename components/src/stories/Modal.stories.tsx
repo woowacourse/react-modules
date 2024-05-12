b37f71f0a101ebd9c020b0b10dc2288e97cfc7a2
simhorim <hori5020@gmail.com>
2024-05-13 00:34:21 +0900

    storybook(Modal): 모바일 환경에서 포지션에 따른 스토리북 구현

--- components/src/stories/Modal.stories.tsx
+++ components/src/stories/Modal.stories.tsx
@@ -835,3 +835,147 @@ export const PromptModal: Story = {
     ),
   },
 };
+
+export const MobilePositionCenter: Story = {
+  parameters: {
+    viewport: {
+      defaultViewport: "mobile1",
+    },
+  },
+  args: {
+    isOpen: true,
+    children: (
+      <>
+        <ThemeProvider theme={theme}>
+          <Modal.BackDrop
+            onClose={() => {
+              console.log("backdrop clicked");
+            }}
+          />
+          <Modal.Container position="center" size="medium">
+            <>
+              <Modal.Header>
+                <Modal.Title text="쿠폰 번호를 입력해 주세요." />
+              </Modal.Header>
+              <input type="text" placeholder="CGEXX46Z" />
+              <Modal.ButtonContainer direction="row" position="right">
+                <>
+                  <Modal.Button
+                    color="light"
+                    size="small"
+                    onClick={() => console.log("closeButton clicked")}
+                  >
+                    <span>취소</span>
+                  </Modal.Button>
+                  <Modal.Button
+                    color="dark"
+                    size="small"
+                    onClick={() => console.log("closeButton clicked")}
+                  >
+                    <span>확인</span>
+                  </Modal.Button>
+                </>
+              </Modal.ButtonContainer>
+            </>
+          </Modal.Container>
+        </ThemeProvider>
+      </>
+    ),
+  },
+};
+
+export const MobilePositionTop: Story = {
+  parameters: {
+    viewport: {
+      defaultViewport: "mobile1",
+    },
+  },
+  args: {
+    isOpen: true,
+    children: (
+      <>
+        <ThemeProvider theme={theme}>
+          <Modal.BackDrop
+            onClose={() => {
+              console.log("backdrop clicked");
+            }}
+          />
+          <Modal.Container position="top" size="medium">
+            <>
+              <Modal.Header>
+                <Modal.Title text="쿠폰 번호를 입력해 주세요." />
+              </Modal.Header>
+              <input type="text" placeholder="CGEXX46Z" />
+              <Modal.ButtonContainer direction="row" position="right">
+                <>
+                  <Modal.Button
+                    color="light"
+                    size="small"
+                    onClick={() => console.log("closeButton clicked")}
+                  >
+                    <span>취소</span>
+                  </Modal.Button>
+                  <Modal.Button
+                    color="dark"
+                    size="small"
+                    onClick={() => console.log("closeButton clicked")}
+                  >
+                    <span>확인</span>
+                  </Modal.Button>
+                </>
+              </Modal.ButtonContainer>
+            </>
+          </Modal.Container>
+        </ThemeProvider>
+      </>
+    ),
+  },
+};
+
+export const MobilePositionBottom: Story = {
+  parameters: {
+    viewport: {
+      defaultViewport: "mobile1",
+    },
+  },
+  args: {
+    isOpen: true,
+    children: (
+      <>
+        <ThemeProvider theme={theme}>
+          <Modal.BackDrop
+            onClose={() => {
+              console.log("backdrop clicked");
+            }}
+          />
+          <Modal.Container position="bottom" size="medium">
+            <>
+              <Modal.Header>
+                <Modal.Title text="쿠폰 번호를 입력해 주세요." />
+              </Modal.Header>
+              <input type="text" placeholder="CGEXX46Z" />
+              <Modal.ButtonContainer direction="row" position="right">
+                <>
+                  <Modal.Button
+                    color="light"
+                    size="small"
+                    onClick={() => console.log("closeButton clicked")}
+                  >
+                    <span>취소</span>
+                  </Modal.Button>
+                  <Modal.Button
+                    color="dark"
+                    size="small"
+                    onClick={() => console.log("closeButton clicked")}
+                  >
+                    <span>확인</span>
+                  </Modal.Button>
+                </>
+              </Modal.ButtonContainer>
+            </>
+          </Modal.Container>
+        </ThemeProvider>
+      </>
+    ),
+  },
+};
