6d07f5bdca17b22dd80f62e6803f84d508ea123a
Soyeon Choe <soy2302ten@gmail.com>
2024-05-12 21:20:27 +0900

    refactor: 불 필요한 변수 제거

--- hooks/src/lib/utils/formattingCardNumber.ts
+++ hooks/src/lib/utils/formattingCardNumber.ts
@@ -1,17 +1,13 @@
 import { REGEX } from "../constants";
 
 const formattingCardNumber = (cardBrand: CardBrandType, value: string): string => {
-  let formattedCardNumber: string;
-
   if (cardBrand === "Diners") {
-    formattedCardNumber = value.replace(REGEX.cardNumberLength14, "$1 $2 $3");
+    return value.replace(REGEX.cardNumberLength14, "$1 $2 $3");
   } else if (cardBrand === "AMEX") {
-    formattedCardNumber = value.replace(REGEX.cardNumberLength15, "$1 $2 $3");
+    return value.replace(REGEX.cardNumberLength15, "$1 $2 $3");
   } else {
-    formattedCardNumber = value.replace(REGEX.cardNumberLength16, "$1 $2 $3 $4");
+    return value.replace(REGEX.cardNumberLength16, "$1 $2 $3 $4");
   }
-
-  return formattedCardNumber;
 };
 
 export default formattingCardNumber;
