680ee4488c41ec8ecc6b4d693bc6ff25728b872e
jinsil <jinsil378900@gmail.com>
2024-05-13 21:10:19 +0900

    Comment: 카드 브랜드에 대한 주석 추가

--- hooks/src/hooks/lib/constants/cardBrand.ts
+++ hooks/src/hooks/lib/constants/cardBrand.ts
@@ -1,4 +1,5 @@
 const CARD_BRAND = {
+  // VISA: 4로 시작하는 16자리 숫자
   VISA: {
     name: "VISA",
     patterns: /^4/,
@@ -6,6 +7,8 @@ const CARD_BRAND = {
     matchedLength: 16,
     maxLength: 19,
   },
+
+  // masterCard: 51~55로 시작하는 16자리 숫자
   MASTER_CARD: {
     name: "MASTER_CARD",
     patterns: /^5[1-5]/,
@@ -13,6 +16,8 @@ const CARD_BRAND = {
     matchedLength: 16,
     maxLength: 19,
   },
+
+  // Diners: 36으로 시작하는 14자리 숫자
   DINERS: {
     name: "DINERS",
     patterns: /^36/,
@@ -20,6 +25,8 @@ const CARD_BRAND = {
     matchedLength: 14,
     maxLength: 16,
   },
+
+  // AMEX: 34, 37로 시작하는 15자리 숫자
   AMEX: {
     name: "AMEX",
     patterns: /^3(4|7)/,
@@ -27,6 +34,8 @@ const CARD_BRAND = {
     matchedLength: 15,
     maxLength: 17,
   },
+
+  // Union Pay: 622126~622925 또는 624~626 또는 6282~6288로 시작하는 16자리 숫자
   UNION_PAY: {
     name: "UNION_PAY",
     patterns: /^62(212[6-9]|21[3-9][0-9]|2[2-8][0-9][0-9]|29[0-1][0-9]|292[0-5]|[4-6]|8[2-8])/,
@@ -34,6 +43,8 @@ const CARD_BRAND = {
     matchedLength: 16,
     maxLength: 19,
   },
+
+  // 그 외 카드: 16자리 숫자
   UNKNOWN: {
     name: "",
     patterns: /^/,
