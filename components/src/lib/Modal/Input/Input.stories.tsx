a43dd9d769d9cf8b3084cc005d676a0cd296bd69
Largopie <106071687+Largopie@users.noreply.github.com>
2024-05-13 22:28:55 +0900

    refactor: Input label description 속성으로 변경

--- components/src/lib/Modal/Input/Input.stories.tsx
+++ components/src/lib/Modal/Input/Input.stories.tsx
@@ -18,7 +18,7 @@ type Story = StoryObj<typeof meta>;
 export const Default: Story = {
   args: {
     value: '',
-    label: 'input label',
+    description: 'input label',
     onChange: action('onChange'),
   },
 };
