554a76b20ff2a1c08a271c5eaaa69b27360590f7
Largopie <106071687+Largopie@users.noreply.github.com>
2024-05-13 21:36:01 +0900

    refactor(Label): 미입력 시 기본 색상 설정

--- components/src/lib/Modal/Label/Label.tsx
+++ components/src/lib/Modal/Label/Label.tsx
@@ -2,10 +2,10 @@ import { PropsWithChildren } from 'react';
 import styles from './label.module.css';
 
 interface LabelProps {
-  color: 'basic' | 'lightGray';
+  color?: 'basic' | 'lightGray';
 }
 
-export default function Label({ color, children }: PropsWithChildren<LabelProps>) {
+export default function Label({ color = 'basic', children }: PropsWithChildren<LabelProps>) {
   return (
     <div className={styles.labelContainer}>
       <span className={`${styles.label} ${styles[color]}`}>{children}</span>
