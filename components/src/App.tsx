2957eff0c050c4eb2aa11372a29a7bdedffb8cc7
Yoonkyoung Kim <dev.yoonkyoung@gmail.com>
2024-05-13 17:06:11 +0900

    build: woowacourse-6th-react-modal-component 라이브러리에 배포 및 App.tsx에 적용
    version: 0.0.14

--- components/src/App.tsx
+++ components/src/App.tsx
@@ -11,15 +11,19 @@ function App() {
 
   return (
     <>
-      <Modal.Button onButtonClick={openModal}>Prompt 모달 열기</Modal.Button>
+      <Modal.Button onClick={openModal}>Prompt 모달 열기</Modal.Button>
       {isModalOpen && (
         <PromptModal
           title="prompt 모달"
+          basicDescription="모달 설명입니다."
           label="이름을 입력하세요."
           onModalClose={closeModal}
           onConfirmButtonClick={closeModal}
           onCancelButtonClick={closeModal}
           onInputChange={(event) => console.log(event.target.value)}
+          $buttonSize="small"
+          $buttonBackgroundColor="white"
+          $buttonColor="black"
         />
       )}
     </>
