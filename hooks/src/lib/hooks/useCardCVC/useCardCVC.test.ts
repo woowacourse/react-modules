9e986d3939b3c990127f8c6ab40630e4f8a35563
pakxe <pigkill40@naver.com>
2024-05-14 01:06:04 +0900

    test: 같은 동작을 하는 로직 모듈화

--- hooks/src/lib/hooks/useCardCVC/useCardCVC.test.ts
+++ hooks/src/lib/hooks/useCardCVC/useCardCVC.test.ts
@@ -4,43 +4,98 @@ import { NUMBER_ERROR_MESSAGES } from '../../utils/validation/validation';
 import { act } from 'react';
 
 describe('useCardCVC 커스텀 훅 테스트', () => {
-  it('올바른 CVC 번호를 입력하면 유효하다', () => {
-    const { result } = renderHook(() => useCardCVC());
-    act(() => {
-      result.current.handleCVCNumberChange('123');
+  const setup = (initialCVCNumber?: string) => {
+    const { result } = renderHook(() => useCardCVC({ initialCVCNumber }));
+    return result;
+  };
+
+  const setCVCNumber = (result: any, cvcNumber: string) => {
+    act(() => result.current.handleCVCNumberChange(cvcNumber));
+  };
+
+  describe('올바른 CVC 번호를 입력한 경우', () => {
+    it('올바른 CVC 번호를 입력하면 유효하다', () => {
+      const result = setup();
+
+      setCVCNumber(result, '123');
+
+      expect(result.current.isValidCVCNumber).toBe(true);
     });
-    expect(result.current.isValidCVCNumber).toBe(true);
+
+    it('올바른 CVC 번호를 입력하면 해당 값을 저장한다.', () => {
+      const cvcNumber = '123';
+      const result = setup();
+
+      setCVCNumber(result, cvcNumber);
+
+      expect(result.current.cvcNumber).toBe(cvcNumber);
+    });
   });
 
-  it('숫자 외의 값을 입력하면 유효하지 않다.', () => {
-    const { result } = renderHook(() => useCardCVC());
-    act(() => {
-      result.current.handleCVCNumberChange('v');
+  describe('숫자 외의 값을 입력하는 경우', () => {
+    it('문자를 입력하면 유효하지 않다.', () => {
+      const invalidCVCNumber = 'v';
+      const result = setup();
+
+      setCVCNumber(result, invalidCVCNumber);
+
+      expect(result.current.isValidCVCNumber).toBe(false);
     });
-    expect(result.current.isValidCVCNumber).toBe(false);
+
+    it('문자를 입력하면 에러 메세지를 표시한다.', () => {
+      const invalidCVCNumber = 'v';
+      const result = setup();
+
+      setCVCNumber(result, invalidCVCNumber);
+
+      expect(result.current.cvcNumberErrorMessage).toBe(NUMBER_ERROR_MESSAGES.NOT_NUMBER);
+    });
+
+    it('공백을 입력하면 유효하지 않다.', () => {
+      const invalidCVCNumber = ' ';
+      const result = setup();
+
+      setCVCNumber(result, invalidCVCNumber);
+
+      expect(result.current.isValidCVCNumber).toBe(false);
+    });
+
+    it('공백을 입력하면 에러 메세지를 표시한다.', () => {
+      const invalidCVCNumber = ' ';
+      const result = setup();
+
+      setCVCNumber(result, invalidCVCNumber);
+
+      expect(result.current.cvcNumberErrorMessage).toBe(NUMBER_ERROR_MESSAGES.NOT_NUMBER);
+    });
+
+    it('공백을 입력하면 갱신되지 않고 값이 유지된다.', () => {
+      const invalidCVCNumber = ' ';
+      const result = setup();
+
+      setCVCNumber(result, invalidCVCNumber);
+
+      expect(result.current.cvcNumber).toBe('');
+    });
   });
 
-  it('숫자 외의 값을 입력하면 에러 메세지를 표시한다.', () => {
-    const { result } = renderHook(() => useCardCVC());
-    act(() => {
-      result.current.handleCVCNumberChange('v');
-    });
-    expect(result.current.cvcNumberErrorMessage).toBe(NUMBER_ERROR_MESSAGES.NOT_NUMBER);
-  });
+  describe('필요 길이보다 적게 입력되었을 경우', () => {
+    it('3자 미만의 CVC 번호를 입력하면 유효하지 않다.', () => {
+      const cvcNumber = '1';
+      const result = setup();
 
-  it('3자 미만의 CVC 번호를 입력하면 유효하지 않다.', () => {
-    const { result } = renderHook(() => useCardCVC());
-    act(() => {
-      result.current.handleCVCNumberChange('1');
-    });
-    expect(result.current.isValidCVCNumber).toBe(false);
-  });
+      setCVCNumber(result, cvcNumber);
 
-  it('3자 미만의 CVC 번호를 입력하면 에러 메시지를 표시한다.', () => {
-    const { result } = renderHook(() => useCardCVC());
-    act(() => {
-      result.current.handleCVCNumberChange('1');
+      expect(result.current.isValidCVCNumber).toBe(false);
     });
-    expect(result.current.cvcNumberErrorMessage).toBe(NUMBER_ERROR_MESSAGES.MAX_LENGTH(VALID_CVC_NUMBER_LENGTH));
+
+    it('3자 미만의 CVC 번호를 입력하면 에러 메시지를 표시한다.', () => {
+      const cvcNumber = '1';
+      const result = setup();
+
+      setCVCNumber(result, cvcNumber);
+
+      expect(result.current.cvcNumberErrorMessage).toBe(NUMBER_ERROR_MESSAGES.MAX_LENGTH(VALID_CVC_NUMBER_LENGTH));
+    });
   });
 });
