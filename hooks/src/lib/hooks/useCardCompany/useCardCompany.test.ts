627d0de474d698500bc6c5ec82ca17153f1ab151
pakxe <pigkill40@naver.com>
2024-05-14 01:05:49 +0900

    test: 같은 동작을 하는 로직 모듈화

--- hooks/src/lib/hooks/useCardCompany/useCardCompany.test.ts
+++ hooks/src/lib/hooks/useCardCompany/useCardCompany.test.ts
@@ -14,43 +14,77 @@ const CARD_COMPANY_LIST = [
 ];
 
 describe('useCardCompany 커스텀 훅 테스트', () => {
-  it('올바른 카드사를 선택했다면 유효하다.', () => {
-    const { result } = renderHook(() => useCardCompany(CARD_COMPANY_LIST));
-    act(() => {
-      result.current.handleCardCompanyChange('BC카드');
-    });
-    expect(result.current.isValidCardCompany).toBe(true);
+  const setup = (initialCompany?: string) => {
+    const { result } = renderHook(() => useCardCompany(CARD_COMPANY_LIST, initialCompany));
+    return result;
+  };
+
+  const setCompany = (result: any, company: string) => {
+    act(() => result.current.handleCardCompanyChange(company));
+  };
+
+  it('초기값을 전달해주면 해당 초기값을 저장한다.', () => {
+    const company = '신한카드';
+
+    const result = setup();
+    setCompany(result, company);
+
+    expect(result.current.cardCompany).toBe(company);
   });
 
-  it('올바르지 않은 카드사라면 유효하지 않다.', () => {
-    const { result } = renderHook(() => useCardCompany(CARD_COMPANY_LIST));
-    act(() => {
-      result.current.handleCardCompanyChange('AB카드');
-    });
+  it('아무 것도 선택하지 않았다면 유효하지 않다.', () => {
+    const result = setup();
+
     expect(result.current.isValidCardCompany).toBe(false);
   });
 
-  it('올바르지 않은 카드사라면 에러 메세지를 표시한다.', () => {
-    const { result } = renderHook(() => useCardCompany(CARD_COMPANY_LIST));
-    act(() => {
-      result.current.handleCardCompanyChange('AB카드');
+  describe('올바른 카드사를 선택한 경우', () => {
+    it('올바른 카드사를 선택했다면 유효하다.', () => {
+      const result = setup();
+      setCompany(result, 'BC카드');
+
+      expect(result.current.isValidCardCompany).toBe(true);
     });
-    expect(result.current.cardCompanyErrorMessage).toBe(CARD_COMPANY_ERROR_MESSAGES.INVALID_CARD_COMPANY);
+
+    it('올바른 카드사를 선택했다면 해당 값을 저장한다.', () => {
+      const company = 'BC카드';
+
+      const result = setup();
+      setCompany(result, company);
+
+      expect(result.current.cardCompany).toBe(company);
+    });
   });
 
-  it('카드사가 입력되지 않으면 유효하지 않다.', () => {
-    const { result } = renderHook(() => useCardCompany(CARD_COMPANY_LIST));
-    act(() => {
-      result.current.handleCardCompanyChange('');
+  describe('초기 인자로 전달해준 리스트에 없는 항목이 선택된 경우', () => {
+    it('올바르지 않은 카드사라면 유효하지 않다.', () => {
+      const result = setup();
+      setCompany(result, 'AB카드');
+
+      expect(result.current.isValidCardCompany).toBe(false);
     });
-    expect(result.current.isValidCardCompany).toBe(false);
+
+    it('올바르지 않은 카드사라면 에러 메세지를 표시한다.', () => {
+      const result = setup();
+      setCompany(result, 'AB카드');
+
+      expect(result.current.cardCompanyErrorMessage).toBe(CARD_COMPANY_ERROR_MESSAGES.INVALID_CARD_COMPANY);
+    });
   });
 
-  it('카드사가 입력되지 않으면 에러 메세지를 표시한다.', () => {
-    const { result } = renderHook(() => useCardCompany(CARD_COMPANY_LIST));
-    act(() => {
-      result.current.handleCardCompanyChange('');
+  describe('카드사가 입력되지 않은 경우', () => {
+    it('카드사가 입력되지 않으면 유효하지 않다.', () => {
+      const result = setup();
+      setCompany(result, '');
+
+      expect(result.current.isValidCardCompany).toBe(false);
     });
-    expect(result.current.cardCompanyErrorMessage).toBe(CARD_COMPANY_ERROR_MESSAGES.NO_CARD_COMPANY);
+
+    it('카드사가 입력되지 않으면 에러 메세지를 표시한다.', () => {
+      const result = setup();
+      setCompany(result, '');
+
+      expect(result.current.cardCompanyErrorMessage).toBe(CARD_COMPANY_ERROR_MESSAGES.NO_CARD_COMPANY);
+    });
   });
 });
