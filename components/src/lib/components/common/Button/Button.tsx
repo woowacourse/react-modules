4db94e39508b8f3fe20b250b007f0c2572f15658
pakxe <pigkill40@naver.com>
2024-05-15 03:28:09 +0900

    feat: Button컴포넌트의 text에 default value 추가

--- components/src/lib/components/common/Button/Button.tsx
+++ components/src/lib/components/common/Button/Button.tsx
@@ -2,14 +2,21 @@ import { ButtonHTMLAttributes } from 'react';
 import styles from './Button.module.css';
 
 export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  text: string;
+  text?: string;
   color?: 'default' | 'none';
   size?: 'sm' | 'md' | 'lg';
   fullWidth?: boolean;
   variants?: 'normal' | 'border';
 }
 
-const Button = ({ text, color = 'default', size = 'lg', fullWidth, variants = 'normal', ...rest }: ButtonProps) => {
+const Button = ({
+  text = '',
+  color = 'default',
+  size = 'lg',
+  fullWidth,
+  variants = 'normal',
+  ...rest
+}: ButtonProps) => {
   return (
     <button
       className={`${styles.modalButton} ${styles[color]} ${styles[size]} ${fullWidth ? styles['full-width'] : ''} ${styles[variants]}`}
