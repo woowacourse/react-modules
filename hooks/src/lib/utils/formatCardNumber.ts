c6ef5ee0c02c6281fdc2abbf7815812f5c234986
pakxe <pigkill40@naver.com>
2024-05-15 01:47:37 +0900

    feat: 카드 번호를 최대 길이만큼만 자르는 로직을 카드 번호 포맷 로직에 융합

--- hooks/src/lib/utils/formatCardNumber.ts
+++ hooks/src/lib/utils/formatCardNumber.ts
@@ -1,10 +1,12 @@
 // TODO: 리펙터링
-const formatCardNumber = (number: string, pattern: number[], separator: string = ' ') => {
+const formatCardNumber = (number: string, pattern: number[], maxLength: number, separator: string = ' ') => {
+  const slicedNumber = number.slice(0, maxLength);
+
   let formattedNumber = '';
   let cursorIndex = 0;
 
   pattern.forEach((unitLength) => {
-    const unitNumber = number.slice(cursorIndex, cursorIndex + unitLength);
+    const unitNumber = slicedNumber.slice(cursorIndex, cursorIndex + unitLength);
     formattedNumber += unitNumber + separator;
     cursorIndex += unitLength;
   });
