ee274c11416b9623253f938122a4aa2f6d5f8dca
pakxe <pigkill40@naver.com>
2024-05-15 00:51:29 +0900

    refactor: 읽는 흐름이 부자연스러운 부분을 수정

--- hooks/src/lib/hooks/useCardCVC/useCardCVC.ts
+++ hooks/src/lib/hooks/useCardCVC/useCardCVC.ts
@@ -3,20 +3,28 @@ import { getNumberErrorMessage, isNotNumber } from '../../utils/validation/valid
 
 export const VALID_CVC_NUMBER_LENGTH = 3;
 
-const useCardCVC = (validLength: number = VALID_CVC_NUMBER_LENGTH, initialCVCNumber: string = '') => {
+type useCardCVCType = {
+  validLength?: number;
+  initialCVCNumber?: string;
+};
+
+const useCardCVC = ({ validLength = VALID_CVC_NUMBER_LENGTH, initialCVCNumber = '' }: useCardCVCType = {}) => {
   const [cvcNumber, setCVCNumber] = useState(initialCVCNumber);
   const [isValidCVCNumber, setIsValidCVCNumber] = useState(false);
   const [cvcNumberErrorMessage, setCVCNumberErrorMessage] = useState('');
 
   const handleCVCNumberChange = (number: string) => {
-    if (number.length > validLength) return;
+    if (number.length > validLength) return; // 입력이 초과로 들어오는 경우 오류 메세지 없이 그냥 무시
 
     const errorMessage = getNumberErrorMessage(number, validLength);
+
+    if (isNotNumber(number)) {
+      setCVCNumberErrorMessage(errorMessage);
+      return;
+    }
+
     setCVCNumberErrorMessage(errorMessage);
-
-    if (isNotNumber(number)) return;
-
-    setIsValidCVCNumber(!errorMessage);
+    setIsValidCVCNumber(errorMessage === '');
     setCVCNumber(number);
   };
 
