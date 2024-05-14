c45269f1baf579359b0024de30e43ecb0d66f5df
hwinkr <dnddl8280@naver.com>
2024-05-14 13:58:30 +0900

    test(Modal): 모달 스토리 수정
    - AlertModal, ConfirmModal, PromptModal 분리
    - 타이틀, 모달 텍스트, 버튼 텍스트 수정할 수 있도록 argTypes 수정

--- components/src/stories/Modal.stories.tsx
+++ components/src/stories/Modal.stories.tsx
@@ -9,9 +9,11 @@ import Modal from "../lib/Modal/Modal";
 import { ElementDirection, ModalButtonTheme, ModalPosition, ModalSize } from "../lib/types/modal";
 
 const meta = {
-  title: "Modal",
+  title: "Modal/Playground",
 
   component: Modal,
+
+  tags: ["autodocs"],
 
   parameters: {
     layout: "centered",
@@ -57,6 +59,30 @@ const meta = {
         type: "radio",
       },
     },
+
+    modalTitle: {
+      control: {
+        type: "text",
+      },
+    },
+
+    modalText: {
+      control: {
+        type: "text",
+      },
+    },
+
+    primaryButtonText: {
+      control: {
+        type: "text",
+      },
+    },
+
+    secondButtonText: {
+      control: {
+        type: "text",
+      },
+    },
   },
 
   args: {
@@ -65,12 +91,16 @@ const meta = {
     size: "medium",
     direction: "row",
     onClose: fn(),
+    modalTitle: "테스트 모달 제목입니다.",
+    modalText: "테스트 모달입니다.",
+    primaryButtonText: "동의하고 저장하기",
+    secondButtonText: "닫기",
   },
 };
 
 export default meta;
 
-interface ModalProps {
+interface ModalStoryProps {
   isOpen: boolean;
   position: ModalPosition;
   size: ModalSize;
@@ -78,9 +108,13 @@ interface ModalProps {
   formDirection: ElementDirection;
   theme: ModalButtonTheme;
   onClose: () => void;
+  modalTitle?: string;
+  modalText?: string;
+  primaryButtonText?: string;
+  secondButtonText?: string;
 }
 
-type Story = StoryObj<ModalProps>;
+type Story = StoryObj<ModalStoryProps>;
 
 export const Modal_Story_Playground: Story = {
   render: () => {
@@ -103,94 +137,17 @@ export const Modal_Story_Playground: Story = {
         <Modal isOpen={args.isOpen} onClose={onClose} device={args.device} position={args.position}>
           <Modal.ModalContent size={args.size}>
             <Modal.ModalHeader>
-              <Modal.ModalTitle text="약관에 동의해 주세요." />
+              <Modal.ModalTitle text={args.modalTitle} />
               <Modal.ModalCloseButton onCloseButtonClick={onClose} />
             </Modal.ModalHeader>
-            <h1>테스트 모달입니다</h1>
+            <h1>{args.modalText}</h1>
             <Modal.ModalFooter direction={args.direction}>
-              <Modal.ModalButton theme="dark">동의하고 저장하기</Modal.ModalButton>
-              <Modal.ModalButton onClick={onClose}>닫기</Modal.ModalButton>
+              <Modal.ModalButton theme="dark">{args.primaryButtonText}</Modal.ModalButton>
+              <Modal.ModalButton onClick={onClose}>{args.secondButtonText}</Modal.ModalButton>
             </Modal.ModalFooter>
           </Modal.ModalContent>
         </Modal>
       </div>
-    );
-  },
-};
-
-export const AlertModal: Story = {
-  render: (args) => {
-    return (
-      <div
-        style={{
-          width: "375px",
-          margin: "auto",
-        }}
-      >
-        <Modal isOpen={true} onClose={args.onClose}>
-          <Modal.ModalContent size={args.size}>
-            <Modal.ModalHeader>
-              <Modal.ModalTitle text="아이디를 입력해 주세요." />
-            </Modal.ModalHeader>
-            <span>아이디는 필수로 입력해야 합니다.</span>
-            <Modal.ModalFooter direction={args.direction} justify="end">
-              <Modal.ModalButton theme="dark" size="medium" width="fixed">
-                확인
-              </Modal.ModalButton>
-            </Modal.ModalFooter>
-          </Modal.ModalContent>
-        </Modal>
-      </div>
-    );
-  },
-};
-
-export const ConfirmModal: Story = {
-  render: (args) => {
-    return (
-      <Modal isOpen={true} onClose={args.onClose}>
-        <Modal.ModalContent size={args.size}>
-          <Modal.ModalHeader>
-            <Modal.ModalTitle text="카드를 삭제하시겠습니까?" />
-          </Modal.ModalHeader>
-          <span>삭제하면 복구할 수 없습니다.</span>
-          <Modal.ModalFooter direction={args.direction} justify="end">
-            <Modal.ModalButton theme="white" size="medium" width="fixed">
-              취소
-            </Modal.ModalButton>
-            <Modal.ModalButton theme="dark" size="medium" width="fixed">
-              확인
-            </Modal.ModalButton>
-          </Modal.ModalFooter>
-        </Modal.ModalContent>
-      </Modal>
-    );
-  },
-};
-
-export const PromptModal: Story = {
-  render: (args) => {
-    return (
-      <Modal isOpen={true} onClose={args.onClose}>
-        <Modal.ModalContent size={args.size}>
-          <Modal.ModalHeader>
-            <Modal.ModalTitle text="쿠폰 번호를 입력해 주세요." />
-          </Modal.ModalHeader>
-
-          <Modal.ModalForm>
-            <Modal.ModalInput />
-          </Modal.ModalForm>
-
-          <Modal.ModalFooter direction={args.direction} justify="end">
-            <Modal.ModalButton theme="white" size="medium" width="fixed">
-              취소
-            </Modal.ModalButton>
-            <Modal.ModalButton theme="dark" size="medium" width="fixed">
-              확인
-            </Modal.ModalButton>
-          </Modal.ModalFooter>
-        </Modal.ModalContent>
-      </Modal>
     );
   },
 };
