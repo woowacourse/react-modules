7e1e716f8c2345740b94e27829a06375d235ad1f
Largopie <106071687+Largopie@users.noreply.github.com>
2024-05-14 10:24:55 +0900

    refactor: Object.freeze -> as const로 수정

--- hooks/src/lib/useCardCompany/useCardCompany.ts
+++ hooks/src/lib/useCardCompany/useCardCompany.ts
@@ -3,7 +3,7 @@ import useValidation, { ValidationType } from '../common/useValidation';
 
 type CardType = 'BC카드' | '신한카드' | '카카오뱅크' | '현대카드' | '우리카드' | '롯데카드' | '하나카드' | '국민카드' | '';
 
-const CARD_COMPANY = Object.freeze({
+const CARD_COMPANY = {
   '': '카드사를 선택해주세요',
   BC카드: 'BC카드',
   신한카드: '신한카드',
@@ -13,7 +13,7 @@ const CARD_COMPANY = Object.freeze({
   롯데카드: '롯데카드',
   하나카드: '하나카드',
   국민카드: '국민카드',
-});
+} as const;
 
 const inputValidations: ValidationType<CardType>[] = [
   {
