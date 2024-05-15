b2dddec093004e7505f08ffc48c4f958179afe2f
cys4585 <dev.youngsu@gmail.com>
2024-05-15 12:05:32 +0900

    refactor(Modal): Modal Config prop 적용 및 해당 prop의 default 셋팅

--- components/src/lib/Modal/Modal.tsx
+++ components/src/lib/Modal/Modal.tsx
@@ -8,24 +8,36 @@ import { ModalZIndex } from './constants';
 
 export type ModalPositionType = 'center' | 'bottom';
 export type ModalSizeType = 'small' | 'medium' | 'large';
+type ModalConfig =
+  | { position: ModalPositionType }
+  | { size: ModalSizeType }
+  | { position: 'center'; size: ModalSizeType };
 
 export interface ModalProps {
   isOpen: boolean;
-  position?: ModalPositionType;
-  size?: ModalSizeType;
+  config?: ModalConfig;
   zIndex?: ModalZIndex | number;
   children: React.ReactNode;
   onClose: () => void;
 }
 
-function Modal({
-  isOpen,
-  position = 'center',
-  size = 'medium',
-  zIndex = ModalZIndex.High,
-  children,
-  onClose,
-}: ModalProps) {
+const getDefaultModalConfig = (
+  config?: ModalConfig,
+): { position: ModalPositionType; size: ModalSizeType } => {
+  if (!config) {
+    return { position: 'center', size: 'medium' };
+  }
+  if ('position' in config && !('size' in config)) {
+    return { position: config.position, size: 'medium' };
+  }
+  if (!('position' in config) && 'size' in config) {
+    return { position: 'center', size: config.size };
+  }
+  return config;
+};
+
+function Modal({ isOpen, config, zIndex = ModalZIndex.High, children, onClose }: ModalProps) {
+  const { position, size } = getDefaultModalConfig(config);
   const modalRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
