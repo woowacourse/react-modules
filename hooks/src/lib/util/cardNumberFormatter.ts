e9ac956cdab6af3dcbeb11a9c08f122f9df92c57
river <jenn0.6n@gmail.com>
2024-05-17 10:45:21 +0900

    refactor: 변수명 직관적으로 수정

--- hooks/src/lib/util/cardNumberFormatter.ts
+++ hooks/src/lib/util/cardNumberFormatter.ts
@@ -1,12 +1,12 @@
 const cardNumberFormatter = (cardNumbers: string, cardIdentifier?: string) => {
-  const cleanedValue = cardNumbers.replace(/\D/g, "");
+  const dehyphenatedCardNumberString = cardNumbers.replace(/\D/g, "");
 
   if (cardIdentifier === "Diners") {
-    return cleanedValue.replace(/(\d{4})(\d{6})(\d{4})/, "$1 $2 $3").trim();
+    return dehyphenatedCardNumberString.replace(/(\d{4})(\d{6})(\d{4})/, "$1 $2 $3").trim();
   } else if (cardIdentifier === "AMEX") {
-    return cleanedValue.replace(/(\d{4})(\d{6})(\d{5})/, "$1 $2 $3").trim();
+    return dehyphenatedCardNumberString.replace(/(\d{4})(\d{6})(\d{5})/, "$1 $2 $3").trim();
   } else {
-    return cleanedValue.replace(/(\d{4})(?=\d)/g, "$1 ").trim();
+    return dehyphenatedCardNumberString.replace(/(\d{4})(?=\d)/g, "$1 ").trim();
   }
 };
 
