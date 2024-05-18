4c2ebf932bcb077ed80ea371cef0f739884713d6
simhorim <hori5020@gmail.com>
2024-05-18 19:32:05 +0900

    refactor(preview): themprovider 추가

--- components/.storybook/preview.tsx
+++ components/.storybook/preview.tsx
@@ -1,6 +1,16 @@
+import React from "react";
 import type { Preview } from "@storybook/react";
+import { ThemeProvider } from "styled-components";
+import theme from "../src/theme";
 
 const preview: Preview = {
+  decorators: [
+    (Story) => (
+      <ThemeProvider theme={theme}>
+        <Story />
+      </ThemeProvider>
+    ),
+  ],
   parameters: {
     controls: {
       matchers: {
