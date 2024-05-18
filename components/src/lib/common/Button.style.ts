2eaf9b5a3c6d499b0de176ab1d8cacdbd85b71c1
river <jenn0.6n@gmail.com>
2024-05-18 15:53:00 +0900

    feat: style props에서 색상코드 검증 추가

--- components/src/lib/common/Button.style.ts
+++ components/src/lib/common/Button.style.ts
@@ -1,12 +1,18 @@
 import styled from "@emotion/styled";
 import { ButtonProps } from "./Button";
 
+const isValidColorCode = (color: string): boolean => {
+  const colorCodeRegex = /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/;
+  return colorCodeRegex.test(color);
+};
+
 export const ButtonWrapper = styled.button<ButtonProps>`
   padding: 6px 21px;
   border-radius: 5px;
   border: none;
   background: ${({ backgroundColor }) => backgroundColor};
-  ${({ borderColor }) => borderColor && `border : 1px solid ${borderColor};`}
+  ${({ borderColor }) =>
+    borderColor && isValidColorCode(borderColor) && `border : 1px solid ${borderColor};`}
   cursor: pointer;
   font-size: 15px;
   font-weight: 700;
