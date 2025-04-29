9508c5a6fadf9ea3bdbb190ccbb979b533bf094f
keemsebeen <sebin0580@gmail.com>
2025-04-29 14:54:39 +0900

    feat: Modal Props 정의
    Co-Authored-By: 신종욱 <66900221+dlsxjzld@users.noreply.github.com>

--- components/src/lib/Portal.tsx
+++ components/src/lib/Portal.tsx
@@ -29,3 +29,43 @@ const Portal = ({ isOpen, children }: Props) => {
 
   return <PortalWrapper>{isOpen && children}</PortalWrapper>;
 };
+
+type ModalProps = {
+  /**
+   * If true, the modal will be open
+   */
+  isOpen: boolean;
+  /**
+   * position of the modal
+   * @default 'center'
+   */
+  position?: 'top' | 'center' | 'bottom';
+  /**
+   * The title of the modal
+   */
+  title: string;
+  /**
+   * Indicate close button visibility
+   * @default true
+   */
+  showCloseButton?: boolean;
+  /**
+   * close function to be called when the close button is clicked
+   * @default 'Close'
+   */
+  onClose: VoidFunction;
+  /**
+   * The content of the modal
+   */
+  children: React.ReactNode;
+};
+
+// export const Modal = ({ isOpen ,position, title, showCloseButton } :ModalProps) => {
+//   return (
+//     <Portal isOpen={isOpen}>
+//       <StyledBackDrop />
+//       <StyledModalContainer>
+//         <StyledModalContent>
+//     </Portal>
+//   );
+// };
