b368440338a8fc0f30d29f8cd276ba8295065edd
Beomtae <95863810+Beomtae@users.noreply.github.com>
2025-04-29 15:22:36 +0900

    🎨 design: 모달 css 작성
    Co-Authored-By: Minje Sung <minje813@naver.com>

--- components/src/lib/Modal.tsx
+++ components/src/lib/Modal.tsx
@@ -7,7 +7,10 @@ interface ModalProps {
 function Modal({ isOpen }: ModalProps) {
   return (
     <ModalContainer isOpen={isOpen}>
-      <p>모달열림</p>
+      <ModalOverlay />
+      <ModalContent>
+        <p>모달열림</p>
+      </ModalContent>
     </ModalContainer>
   );
 }
@@ -16,4 +19,26 @@ export default Modal;
 
 const ModalContainer = styled.div<{ isOpen: boolean }>`
   display: ${({ isOpen }) => (isOpen ? "block" : "none")};
+`;
+
+const ModalContent = styled.div`
+  width: 304px;
+  height: 216px;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: white;
+  padding: 24px 32px;
+  border-radius: 8px;
+  color: #000;
+`;
+
+const ModalOverlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgba(0, 0, 0, 0.35);
 `;
