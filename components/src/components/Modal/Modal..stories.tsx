a32be9f4665f339f1db03a9980f11fd978ffa43b
bunju20 <ghkd2271@naver.com>
2025-04-29 16:00:54 +0900

    feat(story) : 모달 컴포넌트 스토리북 추가 - 열림, 닫힘 - 센터 바텀

---
+++ components/src/components/Modal/Modal..stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import Modal from "./index";
+import "../../styles/reset.css";
+
+const meta = {
+  title: "Component/Modal",
+  component: Modal,
+  args: {
+    isOpen: true,
+    title: "Modal Title",
+    onClose: () => {},
+    position: "center",
+  },
+} satisfies Meta<typeof Modal>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
