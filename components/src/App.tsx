72989d4322c9315539dfb999068bf6e04e73fb24
sooyeoniya <tndus1306403@naver.com>
2025-04-29 19:22:15 +0900

    feat: global.d.ts에 외부 라이브러리 타입 선언 및 Modal 컴포넌트 적용

--- components/src/App.tsx
+++ components/src/App.tsx
@@ -1,9 +1,13 @@
 import "./App.css";
+import { Modal } from "@sooyeoniya/components";
 
 function App() {
   return (
     <>
       <h1>Component Modules</h1>
+      <Modal title="Title" isOpen={true} onClose={() => {}}>
+        Contents
+      </Modal>
     </>
   );
 }
