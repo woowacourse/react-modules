51e06347042ff42e6ede9f232885857d85b91493
Beomtae <95863810+Beomtae@users.noreply.github.com>
2025-04-29 19:10:54 +0900

    ✅ test: 모달 종류에 따른 테스트 작성
    Co-Authored-By: Minje Sung <minje813@naver.com>

--- components/src/stories/Modal.stories.tsx
+++ components/src/stories/Modal.stories.tsx
@@ -22,3 +22,65 @@ export const Default = () => {
     />
   );
 };
+
+export const CenterModal = () => {
+  const [isOpen, setIsOpen] = useState(true);
+
+  return (
+    <Modal
+      title="테스트"
+      content={<p>테스트</p>}
+      isOpen={isOpen}
+      onClose={() => setIsOpen(false)}
+      position="center"
+    />
+  );
+};
+
+export const BottomModal = () => {
+  const [isOpen, setIsOpen] = useState(true);
+
+  return (
+    <Modal
+      title="테스트"
+      content={<p>테스트</p>}
+      isOpen={isOpen}
+      onClose={() => setIsOpen(false)}
+      position="bottom"
+    />
+  );
+};
+
+export const ConfirmButtonModal = () => {
+  const [isOpen, setIsOpen] = useState(true);
+
+  return (
+    <Modal
+      title="테스트"
+      content={<p>테스트</p>}
+      isOpen={isOpen}
+      onClose={() => setIsOpen(false)}
+      onConfirm={() => setIsOpen(false)}
+      hasConfirmButton={true}
+      position="center"
+    />
+  );
+};
+
+export const CloseButtonModal = () => {
+  const [isOpen, setIsOpen] = useState(true);
+
+  return (
+    <Modal
+      title="테스트"
+      content={<p>테스트</p>}
+      isOpen={isOpen}
+      onClose={() => setIsOpen(false)}
+      onConfirm={() => setIsOpen(false)}
+      hasConfirmButton={true}
+      hasTopCloseButton={false}
+      hasBottomCloseButton={true}
+      position="center"
+    />
+  );
+};
