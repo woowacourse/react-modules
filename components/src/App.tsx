3ff73015a6aac12a2f402f9c6483adafae8fc05f
장정안 <wjddks96@naver.com>
2025-04-29 16:01:11 +0900

    feat: 오버레이 클릭 & esc 입력 시 모달 닫기 기능 구현

--- components/src/App.tsx
+++ components/src/App.tsx
@@ -1,12 +1,30 @@
+import { useState } from 'react';
 import Modal from './shared/components/modal/Modal';
 
 function App() {
+  const [isOpen, setIsOpen] = useState(true);
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  function handleEscClick(e: React.KeyboardEvent<HTMLDivElement>) {
+    if (e.key === 'Escape') {
+      closeModal();
+    }
+    console.log('esc 클릭!');
+  }
+
   return (
     <>
       <h1>Component Modules</h1>
-      <Modal>
-        <p>Modal Content</p>
-      </Modal>
+      <button onClick={() => setIsOpen(true)}>Open Modal</button>
+      {isOpen && (
+        <Modal closeModal={closeModal}>
+          <p>Modal Content</p>
+          <button>X</button>
+        </Modal>
+      )}
     </>
   );
 }
--- components/src/shared/components/modal/Modal.tsx
+++ components/src/shared/components/modal/Modal.tsx
@@ -1,21 +1,43 @@
 import { createPortal } from 'react-dom';
 import { ModalContainer, ModalLayout } from './Modal.css';
-import { PropsWithChildren } from 'react';
+import { PropsWithChildren, useEffect, useId } from 'react';
 
-function Modal(props: PropsWithChildren) {
-  const { children, ...rest } = props;
+interface ModalProps extends PropsWithChildren {
+  closeModal: () => void;
+}
+
+function Modal(props: ModalProps) {
+  const { children, closeModal, ...rest } = props;
+  const id = useId();
+
+  function handleClickOverlay(e: React.MouseEvent<HTMLDivElement, MouseEvent>) {
+    if (e.target instanceof HTMLElement && e.target.id === id) {
+      closeModal();
+    }
+  }
+
+  useEffect(() => {
+    function handleKeyDown(e: KeyboardEvent) {
+      if (e.key === 'Escape') {
+        closeModal();
+      }
+    }
+
+    window.addEventListener('keydown', handleKeyDown);
+    return () => window.removeEventListener('keydown', handleKeyDown);
+  }, [closeModal]);
 
   return (
-    <>
+    <div>
       {createPortal(
-        <div css={ModalLayout}>
+        <div css={ModalLayout} id={id} onClick={handleClickOverlay}>
           <div css={ModalContainer} {...rest}>
             {children}
           </div>
         </div>,
         document.body
       )}
-    </>
+    </div>
   );
 }
 
