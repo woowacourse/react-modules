b52074d8df87d5c4cdcfd0f35a4ef23faaff5afa
장정안 <wjddks96@naver.com>
2025-04-29 20:20:40 +0900

    feat: Modal 컴포넌트 구현

---
+++ components/src/shared/components/modal/ui/Modal.tsx
@@ -0,0 +1,66 @@
+import { createPortal } from 'react-dom';
+
+import { PropsWithChildren, useId } from 'react';
+
+import {
+  bottom,
+  center,
+  CloseButton,
+  ModalHeader,
+  ModalLayout,
+} from '../css/Modal.css';
+import useEscapeModal from '../../../../hooks/useEscapeModal';
+import { closeButton } from '../../../../assets';
+
+export interface ModalProps extends PropsWithChildren {
+  closeModal: () => void;
+  position?: 'center' | 'bottom';
+  maxWidth?: number;
+  title?: string;
+  isVisibleCloseButton?: boolean;
+}
+
+function Modal(props: ModalProps) {
+  const {
+    children,
+    closeModal,
+    position = 'center',
+    maxWidth = 500,
+    title,
+    isVisibleCloseButton = true,
+    ...rest
+  } = props;
+  const id = useId();
+
+  const { handleClickOverlay } = useEscapeModal(closeModal);
+
+  return (
+    <section>
+      {createPortal(
+        <div
+          css={ModalLayout}
+          id={id}
+          onClick={(e) => handleClickOverlay(e, id)}
+        >
+          <div
+            css={position === 'center' ? center(maxWidth) : bottom(maxWidth)}
+            {...rest}
+          >
+            <div css={ModalHeader}>
+              {title && <h2>{title}</h2>}
+              {isVisibleCloseButton && (
+                <button onClick={closeModal} css={CloseButton}>
+                  <img src={closeButton} alt="close" />
+                </button>
+              )}
+            </div>
+            {children}
+          </div>
+        </div>,
+        document.body
+      )}
+    </section>
+  );
+}
+
+export default Modal;
