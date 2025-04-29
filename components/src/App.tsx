8b454dec20b9597131d68eb4691086560aa6dd7c
eunoia-jaxson <eunoia@handong.ac.kr>
2025-04-29 17:01:40 +0900

    feat: App 모달 테스트

--- components/src/App.tsx
+++ components/src/App.tsx
@@ -1,17 +1,27 @@
+import { useState } from 'react';
 import './App.css';
 import Modal from './modal/Modal';
 
 function App() {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const handleOpen = () => {
+    console.log('모달이 열렸습니다.');
+  };
+
   return (
     <>
-      <Modal
-        position="center"
-        title="알림"
-        content="이것은 모달 내용입니다."
-        onConfirm={() => {}}
-        onClose={() => {}}
-        onOpen={() => {}}
-      />
+      <button onClick={() => setIsOpen(true)}>열기</button>
+      {isOpen && (
+        <Modal
+          position="center"
+          title="알림"
+          content="모달 내용"
+          onConfirm={() => {}}
+          onClose={() => setIsOpen(false)}
+          onOpen={handleOpen}
+        />
+      )}
     </>
   );
 }
