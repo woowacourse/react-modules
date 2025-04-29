06cf3fb40699f6982859e6aa922f07d7f1d66d96
Jeongeun Lee <leeju1009@gmail.com>
2025-04-29 15:39:46 +0900

    feat: 모달을 열었을 때 dim 처리되는 기능 구현

--- components/src/modal/Modal.tsx
+++ components/src/modal/Modal.tsx
@@ -1,18 +1,19 @@
 import { useEffect, useRef } from 'react';
+import styles from './Modal.module.css';
 
 function Modal({ isOpen, onClose }: { isOpen: boolean; onClose: () => void }) {
   const modalRef = useRef<HTMLDialogElement>(null);
 
   useEffect(() => {
     if (isOpen) {
-      modalRef.current?.show();
+      modalRef.current?.showModal();
     } else {
       modalRef.current?.close();
     }
   }, [isOpen]);
 
   return (
-    <dialog ref={modalRef}>
+    <dialog className={styles.modal} ref={modalRef}>
       <button onClick={onClose}>닫기</button>
     </dialog>
   );
