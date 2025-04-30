e1d701d335a6f7843783c7ce118cecac2556c6b4
spoyodevelop <kjy9505@gmail.com>
2025-04-30 20:45:42 +0900

    feat: 커스텀 훅 완성

--- hooks/src/lib/hooks/useCardValidation.ts
+++ hooks/src/lib/hooks/useCardValidation.ts
@@ -1,8 +1,12 @@
 import useCardNumber from "./useCardNumber";
 import useCVCNumber from "./useCVCNumber";
+import useExpiryDateNumber from "./useExpiryDateNumber";
+import usePasswordNumber from "./usePasswordNumber";
 
 export default function useCardValidation() {
   const card = useCardNumber();
   const cvc = useCVCNumber();
-  return { card, cvc };
+  const expiry = useExpiryDateNumber();
+  const password = usePasswordNumber();
+  return { card, cvc, expiry, password };
 }
