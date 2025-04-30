e1cd7bba28d8ab156cf87b2a8280f9bcd4364993
keemsebeen <sebin0580@gmail.com>
2025-04-30 15:01:33 +0900

    feat: useCardNumber hook 구현
    Co-Authored-By: 신종욱 <66900221+dlsxjzld@users.noreply.github.com>

---
+++ hooks/src/lib/hooks/useCardNumber/useCardNumber.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { useState } from 'react';
+import { validateCardInput } from '../../validation/cardNumberLengthInfo';
+
+import { CardInputItem } from '../../types/cardInputItem.types';
+
+const CARD_NUMBER_INPUTS_LENGTH = 4;
+
+export const useCardNumber = () => {
+  const [cardNumbers, setCardNumbers] = useState<CardInputItem[]>(
+    Array.from({ length: CARD_NUMBER_INPUTS_LENGTH }, () => ({ value: '', isValid: true }))
+  );
+
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleCardNumberChange = (event: React.ChangeEvent<HTMLInputElement>, index: number) => {
+    const newValue = event.target.value;
+
+    const { isValid, errorMessage } = validateCardInput(newValue, CARD_NUMBER_INPUTS_LENGTH);
+
+    setCardNumbers((prev) => {
+      const newCardNumbers = [...prev];
+      newCardNumbers[index].value = newValue;
+      newCardNumbers[index].isValid = isValid;
+      return newCardNumbers;
+    });
+
+    if (!isValid) {
+      setErrorMessage(errorMessage);
+    }
+  };
+
+  return {
+    cardNumbers,
+    errorMessage,
+    handleCardNumberChange,
+  };
+};
