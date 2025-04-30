a87b8b65875d336823a6f111fd3a41f6b1066437
장정안 <wjddks96@naver.com>
2025-04-30 14:26:31 +0900

    feat: useError 커스텀 훅 작성
    Co-authored-by: yeongipark <yeongipark@users.noreply.github.com>

---
+++ hooks/src/lib/types.ts
@@ -0,0 +1,20 @@
+export type ValidationFn = {
+  condition: () => boolean;
+  errorMsg: string;
+};
+
+export type CheckValidationType<T extends Record<string, string>> = {
+  length: number;
+  value: string;
+  type: keyof T;
+};
+
+export type GetValidationFnsType = (
+  length: number,
+  value: string
+) => ValidationFn[];
+
+export type UseErrorProps<T extends Record<string, string>> = {
+  initError: T;
+  getValidationFns: GetValidationFnsType;
+};
---
+++ hooks/src/lib/useInputError.ts
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+import { NO_ERROR } from './constants';
+import { CheckValidationType, UseErrorProps } from './types';
+
+export default function useError<T extends Record<string, string>>({
+  initError,
+  getValidationFns,
+}: UseErrorProps<T>) {
+  const [error, setError] = useState<T>(initError);
+
+  function checkValidation({ length, value, type }: CheckValidationType<T>) {
+    const validationFns = getValidationFns(length, value);
+    const validation = validationFns.find((v) => v.condition());
+    setError((prev: T) => {
+      return {
+        ...prev,
+        [type]: validation ? validation.errorMsg : NO_ERROR,
+      };
+    });
+  }
+
+  function findFirstError(errorObj: Record<string, string>) {
+    for (const key in errorObj) {
+      const typedKey = key as keyof typeof errorObj;
+      if (errorObj[typedKey] !== NO_ERROR) {
+        return { key: typedKey, value: errorObj[typedKey] };
+      }
+    }
+    return null;
+  }
+
+  function getErrorMessage() {
+    const result = findFirstError(error);
+    return result?.value;
+  }
+
+  function isError() {
+    return !!findFirstError(error);
+  }
+
+  function resetErrors() {
+    setError(initError);
+  }
+
+  return { error, checkValidation, getErrorMessage, isError, resetErrors };
+}
