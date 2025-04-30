8dc66ce0882f65d5bc33c872bb6b331acf147b1b
eunwoo-levi <eunwoo1341@gmail.com>
2025-04-30 19:39:18 +0900

    test: useCardNumber 테스트코드 추가

--- hooks/_test_/useCardNumber.test.tsx
+++ hooks/_test_/useCardNumber.test.tsx
@@ -64,4 +64,25 @@ describe('useCardNumber 테스트', () => {
     });
     expect(result.current.errorMessage).toBe('카드 번호는 4자리로 입력해주세요.');
   });
+
+  it('4자리가 아닌 숫자와 숫자가 아닌 값이 포함된 경우, 해당 2개의 input만 false가 되어야 하며 에러 메시지가 설정되어야 한다', () => {
+    const { result } = renderHook(() => useCardNumber());
+
+    act(() => {
+      result.current.handleCardNumber({
+        input1: 'levi',
+        input2: '5678',
+        input3: '902',
+        input4: '3456',
+      });
+    });
+
+    expect(result.current.isValid).toEqual({
+      input1: false,
+      input2: true,
+      input3: false,
+      input4: true,
+    });
+    expect(result.current.errorMessage).toBe('카드 번호는 숫자만 입력해주세요.');
+  });
 });
