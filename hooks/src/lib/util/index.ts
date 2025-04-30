0076a2b8b96a6f8722637f3a6063e5e08dbb2818
spoyodevelop <kjy9505@gmail.com>
2025-04-30 15:45:32 +0900

    feat: isNumber util 작성 Co-authored-by: mun-kyeong <mun-kyeong@users.noreply.github.com>

---
+++ hooks/src/lib/util/index.ts
@@ -0,0 +1,10 @@
+/**
+ * 주어진 값이 숫자 타입이면서 NaN인지 확인합니다.
+ * @param value - 확인할 값
+ * @returns 값이 숫자 타입이면서 NaN이면 true, 그렇지 않으면 false를 반환합니다.
+ */
+
+export function isNumber(value: unknown) {
+  const parsedNumber = Number(value);
+  return typeof value !== "number" && Number.isNaN(parsedNumber);
+}
