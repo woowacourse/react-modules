46caef7b5da10d6f489f89888ba27def99107c8a
Oliy <eunsoa0310@gmail.com>
2025-04-30 20:28:24 +0900

    feat: 모달 컴포넌트 구조 개선 및 ModalBox, ModalComponent 추가
    Co-authored-by: dev-dino22 <dev-dino22@users.noreply.github.com>

--- components/src/lib/Modal.tsx
+++ components/src/lib/Modal.tsx
@@ -1,21 +1,47 @@
-import ModalHeader from './ModalHeader';
-import { ModalContainer } from './ModalStyle';
-
-interface ModalProps {
-  modalType: 'center' | 'bottom';
-  titleText?: string;
-  children: any;
-  closeType: 'top' | 'bottom';
-  onClose?: () => void;
-}
+import { useContext, useEffect } from 'react';
+import ModalBox from './ModalBox';
+import { ModalContainer, ModalBackdrop, Container, Wrapper } from './styles/ModalStyle';
+import { ModalContext, useModal } from './contexts/ModalContext';
+import { ModalProps } from './types/modalTypes';
 
 const Modal = ({ modalType, titleText = '', children, closeType, onClose }: ModalProps) => {
-  const hasHeaderCloseButton = closeType === 'top' ? true : false;
+  const context = useContext(ModalContext);
+  const { closeModalHandler } = useModal();
+
+  const handleKeyDown = (event: KeyboardEvent) => {
+    if (event.key === 'Escape') {
+      closeModalHandler();
+    }
+  };
+
+  useEffect(() => {
+    if (context && context.isModalOpened) {
+      window.addEventListener('keydown', handleKeyDown);
+      return () => {
+        window.removeEventListener('keydown', handleKeyDown);
+      };
+    }
+  }, [closeModalHandler, context]);
+
+  if (!context) return null;
+  const { isModalOpened } = context;
+  if (!isModalOpened) return null;
+
   return (
-    <ModalContainer>
-      <ModalHeader titleText={titleText} hasCloseButton={hasHeaderCloseButton}></ModalHeader>
-      {children}
-      {!hasHeaderCloseButton && <div>닫기</div>}
+    <ModalContainer modalType={modalType}>
+      <ModalBackdrop onClick={closeModalHandler} />
+      <Container>
+        <Wrapper>
+          <ModalBox
+            modalType={modalType}
+            titleText={titleText}
+            closeType={closeType}
+            onClose={onClose}
+          >
+            {children}
+          </ModalBox>
+        </Wrapper>
+      </Container>
     </ModalContainer>
   );
 };
--- components/src/lib/ModalHeader.tsx
+++ components/src/lib/ModalHeader.tsx
@@ -1,17 +1,23 @@
-import CloseIconSVG from '../assets/CloseIconSVG';
-import { ModalHeaderContainer } from './ModalStyle';
-import { ModalTitle } from './ModalTextStyle';
+import CloseIconSVG from './assets/CloseIconSVG';
+import { ModalHeaderContainer } from './styles/ModalStyle';
+import { ModalTitle } from './styles/ModalTextStyle';
+import { useModal } from './contexts/ModalContext';
+import { ModalHeaderProps } from './types/modalTypes';
 
-interface ModalHeaderProps {
-  titleText: string;
-  hasCloseButton: boolean;
-}
-
-const ModalHeader = ({ titleText, hasCloseButton }: ModalHeaderProps) => {
+const ModalHeader = ({ titleText, hasCloseButton, onClose }: ModalHeaderProps) => {
+  const { closeModalHandler } = useModal();
+  function closeHandler() {
+    if (onClose) onClose();
+    closeModalHandler();
+  }
   return (
     <ModalHeaderContainer>
       <ModalTitle>{titleText}</ModalTitle>
-      {hasCloseButton && <CloseIconSVG sizeName={'md'}></CloseIconSVG>}
+      {hasCloseButton && (
+        <button style={{ cursor: 'pointer' }} type="button" onClick={closeHandler}>
+          <CloseIconSVG sizeName={'md'}></CloseIconSVG>
+        </button>
+      )}
     </ModalHeaderContainer>
   );
 };
