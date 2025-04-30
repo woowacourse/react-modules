ef330462f3da8357d41fbeb144fcf8f171921bfa
SuHwa Lee <tnghk9611@naver.com>
2025-04-30 21:57:35 +0900

    refactor: 커스텀 훅 디렉터리 구조 변경
    Co-authored-by: Luna <ShinjungOh@users.noreply.github.com>

--- hooks/src/lib/index.ts
+++ hooks/src/lib/index.ts
@@ -1,8 +1,8 @@
-export { useCardCompany } from './hooks/useCardCompany';
-export { useCardCVC } from './hooks/useCardCVC';
-export { useCardExpiryPeriod } from './hooks/useCardExpiryPeriod';
-export { useCardNumber } from './hooks/useCardNumber';
-export { useCardPassword } from './hooks/useCardPassword';
+export { useCardCompany } from './useCardCompany/useCardCompany';
+export { useCardCVC } from './useCardCVC/useCardCVC';
+export { useCardExpiryPeriod } from './useCardExpiryPeriod/useCardExpiryPeriod';
+export { useCardNumber } from './useCardNumber/useCardNumber';
+export { useCardPassword } from './useCardPassword/useCardPassword';
 
 export * from './types/cardTypes';
 export { isOnlyDigits } from './utils/validateNumber';
