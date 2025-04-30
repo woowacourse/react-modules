c4289e1e88ae19cdb833fb71f27ce70194035a09
sooyeoniya <tndus1306403@naver.com>
2025-04-30 16:07:12 +0900

    refactor: useCvcValidation 훅 구조 변경
    - useState로 에러 상태 및 에러 메시지 관리
    - useEffect로 value값이 변경되었을 때 유효성 검증 및 에러 상태 및 메시지 업데이트

--- hooks/src/lib/hooks/useCvcValidation.ts
+++ hooks/src/lib/hooks/useCvcValidation.ts
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { ValidationType } from "../../types/validation";
 import { ERROR_MESSAGE } from "../constants/error";
 import isLengthEqual from "../utils/isLengthEqual";
@@ -5,25 +6,32 @@ import isPositiveInteger from "../utils/isPositiveInteger";
 
 const MAX_LENGTH = 3;
 
+const defaultErrorState = {
+  isError: false,
+  errorMessage: null,
+};
+
 const useCvcValidation = (value: string): ValidationType => {
-  if (!isPositiveInteger(value)) {
-    return {
-      isError: true,
-      errorMessage: ERROR_MESSAGE.INVALID_NUMBER,
-    };
-  }
+  const [cvcValidationResult, setCvcValidationResult] =
+    useState<ValidationType>(defaultErrorState);
 
-  if (!isLengthEqual(value, MAX_LENGTH)) {
-    return {
-      isError: true,
-      errorMessage: `${MAX_LENGTH}${ERROR_MESSAGE.INVALID_LENGTH}`,
-    };
-  }
+  useEffect(() => {
+    if (!isPositiveInteger(value)) {
+      setCvcValidationResult({
+        isError: true,
+        errorMessage: ERROR_MESSAGE.INVALID_NUMBER,
+      });
+    }
 
-  return {
-    isError: false,
-    errorMessage: null,
-  };
+    if (!isLengthEqual(value, MAX_LENGTH)) {
+      setCvcValidationResult({
+        isError: true,
+        errorMessage: `${MAX_LENGTH}${ERROR_MESSAGE.INVALID_LENGTH}`,
+      });
+    }
+  }, [value]);
+
+  return cvcValidationResult;
 };
 
 export default useCvcValidation;
