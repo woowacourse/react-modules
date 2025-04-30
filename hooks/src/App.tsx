303086125335deb15664111a2beb454e85c5faca
eunoia-jaxson <eunoia@handong.ac.kr>
2025-04-30 17:47:53 +0900

    feat: 데모 App 작성

--- hooks/src/App.tsx
+++ hooks/src/App.tsx
@@ -1,10 +1,22 @@
 import './App.css';
+import { useCvcInput } from '@eunoia-jaxson/card-field-hooks';
 
 function App() {
+  const { cvc, handleCvcChange, cvcError } = useCvcInput();
+
   return (
-    <>
-      <h1>Hooks Modules</h1>
-    </>
+    <div>
+      <label htmlFor="cvc">cvc</label>
+      <input
+        type="text"
+        name="cvc"
+        value={cvc}
+        onChange={(e) => handleCvcChange(e.target.value)}
+        maxLength={3}
+        placeholder="cvc 번호를 입력해주세요."
+      />
+      <p>{cvcError}</p>
+    </div>
   );
 }
 
