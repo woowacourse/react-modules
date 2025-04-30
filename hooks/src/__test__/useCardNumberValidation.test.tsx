fdb1760c066cddd9199826a96d3a51d5556c2cc3
Beomtae <95863810+Beomtae@users.noreply.github.com>
2025-04-30 13:51:37 +0900

    ✅ test: 카드 번호 유효성 검증 테스트 작성
    Co-Authored-By: Minje Sung <minje813@naver.com>

--- hooks/src/__test__/useCardNumberValidation.test.tsx
+++ hooks/src/__test__/useCardNumberValidation.test.tsx
@@ -1,3 +1,38 @@
+import { renderHook } from "@testing-library/react";
+import useCardNumberValidation from "../lib/useCardNumberValidation";
+
 describe("useCardNumberValidation 테스트", () => {
-  it("CardNumber가 유효한 형태인지 확인한다.", () => {});
+  it("CardNumber가 유효한 형태인지 확인한다.", () => {
+    const cardNumbers = {
+      first: "1234",
+      second: "1234",
+      third: "1234",
+      fourth: "1234",
+    };
+
+    const { result } = renderHook(() => useCardNumberValidation(cardNumbers));
+    expect(result.current.isCardNumberError).toEqual([
+      false,
+      false,
+      false,
+      false,
+    ]);
+  });
+
+  it("CardNumber중 유효하지 않은 형태가 있는 지 확인한다.", () => {
+    const cardNumbers = {
+      first: "1234",
+      second: "asdf",
+      third: "1234",
+      fourth: "1234",
+    };
+
+    const { result } = renderHook(() => useCardNumberValidation(cardNumbers));
+    expect(result.current.isCardNumberError).toEqual([
+      false,
+      true,
+      false,
+      false,
+    ]);
+  });
 });
