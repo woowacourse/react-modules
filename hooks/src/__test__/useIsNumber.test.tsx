539ac2d7537fe3a87c73f011d03020f408ed5964
Beomtae <95863810+Beomtae@users.noreply.github.com>
2025-04-30 13:42:08 +0900

    ✅ test: 입력값 숫자 검증 테스트 작성
    Co-Authored-By: Minje Sung <minje813@naver.com>

---
+++ hooks/src/__test__/useIsNumber.test.tsx
@@ -0,0 +1,19 @@
+import { renderHook } from "@testing-library/react";
+import useIsNumber from "../lib/useIsNumber";
+
+describe("useIsNumber 테스트", () => {
+  it.each(["mingo", "-1", "3.4"])(
+    "들어온 값이 숫자가 아닌 경우를 확인한다.",
+    (value) => {
+      const { result } = renderHook(() => useIsNumber(value));
+
+      expect(result.current).toBe(false);
+    }
+  );
+
+  it("들어온 값이 숫자인 경우를 확인한다.", () => {
+    const { result } = renderHook(() => useIsNumber("1234"));
+
+    expect(result.current).toBe(true);
+  });
+});
