6fb1aa48ea0040a8cc34343ca324076318f1fae2
Oliy <eunsoa0310@gmail.com>
2025-04-30 14:13:15 +0900

    feat: App 컴포넌트에 모달 추가 및 레이아웃 구성
    Co-authored-by: dev-dino22 <dev-dino22@users.noreply.github.com>

--- components/src/App.tsx
+++ components/src/App.tsx
@@ -1,11 +1,20 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import { Modal } from './lib';
+import { ModalBackdrop, Container, Wrapper } from './lib';
+import './App.css';
 
 function App() {
   return (
-    <>
-      <h1>Component Modules</h1>
-    </>
+    <div style={{ width: '100%', height: '100%', position: 'relative' }}>
+      <ModalBackdrop />
+      <Container>
+        <Wrapper>
+          <Modal modalType="center" titleText="카드사 선택" closeType="top">
+            카드사 선택
+          </Modal>
+        </Wrapper>
+      </Container>
+    </div>
   );
 }
 
