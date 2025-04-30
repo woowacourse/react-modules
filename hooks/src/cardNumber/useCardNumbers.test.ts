375102f048a8d26a6d83ae99e51e6a5ff7fd61e0
Jeongeun Lee <leeju1009@gmail.com>
2025-04-30 22:22:08 +0900

    test: useCardNumber 테스트 코드 추가

---
+++ hooks/src/cardNumber/useCardNumbers.test.ts
@@ -0,0 +1,41 @@
+import { renderHook, act } from '@testing-library/react';
+import useCardNumbers from './useCardNumbers';
+import { ChangeEvent } from 'react';
+
+describe('useCardNumbers', () => {
+  it('입력값이 정확히 업데이트 되어야 한다.', () => {
+    const { result } = renderHook(() => useCardNumbers());
+
+    act(() => {
+      result.current.handleCardNumbersChange({
+        target: { name: 'part1', value: '1234' },
+      } as ChangeEvent<HTMLInputElement>);
+    });
+
+    expect(result.current.cardNumbers.part1).toBe('1234');
+  });
+
+  it('입력값이 숫자가 아닐 때 isValid로 false를 반환하고 에러 메시지를 반환한다.', () => {
+    const { result } = renderHook(() => useCardNumbers());
+
+    act(() => {
+      result.current.validateCardNumbers('part1', 'aaaa');
+    });
+
+    expect(result.current.isValid.part1).toBe(false);
+    expect(result.current.errorMessage).toBe('숫자만 입력해주세요.');
+  });
+
+  it('입력값이 네 자리가 아닐 때 isValid로 false를 반환하고 에러 메시지를 반환한다.', () => {
+    const { result } = renderHook(() => useCardNumbers());
+
+    act(() => {
+      result.current.validateCardNumbers('part1', '12345');
+    });
+
+    expect(result.current.isValid.part1).toBe(false);
+    expect(result.current.errorMessage).toBe(
+      '카드 번호는 네 자리만 입력해야 합니다.'
+    );
+  });
+});
