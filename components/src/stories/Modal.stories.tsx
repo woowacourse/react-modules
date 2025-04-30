d13b7a5eee9b627ba72c784358bff38feb1d6c53
youjeong Jeong <wjddbwjd1205@naver.com>
2025-04-30 16:39:12 +0900

    test: Modal Component 스토리북 제작
    Co-authored-by: 호이초이 <hoyyChoi@users.noreply.github.com>

--- components/src/stories/Modal.stories.tsx
+++ components/src/stories/Modal.stories.tsx
@@ -4,15 +4,43 @@ import Modal from "../lib/Modal";
 const meta = {
   title: "Modal",
   component: Modal,
+  parameters: {
+    docs: {
+      description: {
+        component:
+          "Modal은 위치와 배경 설정이 가능하며, 하위 컴포넌트로 유연하게 구성할 수 있는 재사용 가능한 오버레이 UI입니다.",
+      },
+    },
+  },
 } satisfies Meta<typeof Modal>;
 
 export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
+export const ModalComponent: Story = {
   args: {
+    show: true,
+    onHide: () => {},
     background: true,
-    children: <div>hih</div>,
+    position: "center",
+    gap: 16,
+    children: (
+      <>
+        <Modal.Header closeButton>
+          <Modal.Title>Title</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Body</Modal.Body>
+        <Modal.Footer>Footer</Modal.Footer>
+      </>
+    ),
+  },
+
+  render: (args) => {
+    return (
+      <Modal background={args.background} show={args.show} onHide={args.onHide} position={args.position} gap={args.gap}>
+        {args.children}
+      </Modal>
+    );
   },
 };
