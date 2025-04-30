9e3aace37fa46a77a022647684492d9fdd05837f
spoyodevelop <kjy9505@gmail.com>
2025-04-30 11:41:18 +0900

    feat: 모달 컴포넌트 추가 및 상태 관리 구현, 타입 선언 파일 생성

--- components/src/App.tsx
+++ components/src/App.tsx
@@ -1,10 +1,23 @@
-import React from "react";
+import { useState } from "react";
 import "./App.css";
+import { Modal } from "compoents-modal-test-spoy";
 
 function App() {
+  const [isOpen, setIsOpen] = useState(true);
+
   return (
     <>
-      <h1>Component Modules</h1>
+      <button onClick={() => setIsOpen((prev) => !prev)}>asdfasfdlk</button>
+      <Modal
+        position="center"
+        title="모달 제목"
+        isOpen={isOpen}
+        onClose={() => {
+          setIsOpen(false);
+        }}
+      >
+        {<p>모달 내용asdada</p>}
+      </Modal>
     </>
   );
 }
--- components/src/lib/index.ts
+++ components/src/lib/index.ts
@@ -1,2 +1,2 @@
 export { default as Modal } from "./Modal";
-export type { ModalProps } from "./Modal";
+// export type { ModalProps } from "./Modal";
