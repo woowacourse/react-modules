5df3036ee2a217e87963bc06e26c77e75cc56ad1
AHHYUNJU <wndkgus0225@naver.com>
2025-04-30 19:11:57 +0900

    feat: useCardNumbers 훅 구현
    Co-authored-by: thgml05 <thgml05@users.noreply.github.com>

--- hooks/src/App.tsx
+++ hooks/src/App.tsx
@@ -2,6 +2,7 @@ import "./App.css";
 // import useCvcNumber from "./lib/useCvcNumber";
 import usePassword from "./lib/usePassword";
 import useExpiryDate from "./lib/useExpiryDate";
+import useCardNumbers from "./lib/useCardNumber";
 
 function App() {
   const {
@@ -10,6 +11,11 @@ function App() {
     validate: passwordValidate,
   } = usePassword();
   const { date, error: dateError, validate: dateValidate } = useExpiryDate();
+  const {
+    numbers,
+    error: cardNumbersError,
+    validate: cardNumbersValidate,
+  } = useCardNumbers();
 
   const handlePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
     passwordValidate(e.target.value);
@@ -23,6 +29,12 @@ function App() {
     }
   };
 
+  const handleCardNumber = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    index: number
+  ) => {
+    cardNumbersValidate(e.target.value, index);
+  };
   return (
     <>
       <div>
@@ -45,6 +57,21 @@ function App() {
           onChange={handleExpiryDate}
         />
         <p>{dateError[1].errorMessage}</p>
+      </div>
+      <div>
+        <h1>CardNumbers</h1>
+        {numbers.map((number, index) => (
+          <input
+            key={index}
+            type="text"
+            value={number}
+            onChange={(e) => handleCardNumber(e, index)}
+          />
+        ))}
+        <p>
+          {cardNumbersError.find((error) => error.errorMessage !== "")
+            ?.errorMessage ?? ""}
+        </p>
       </div>
     </>
   );
---
+++ hooks/src/lib/useCardNumber.tsx
@@ -0,0 +1,65 @@
+import { useState } from "react";
+
+const CARD_NUMBER_RULE = {
+  INVALID_LENGTH_ERROR: "카드 번호는 4자리로 입력해 주세요.",
+  NOT_A_NUMBER: "카드 번호는 숫자로 입력해 주세요.",
+  MAX_LENGTH: 4,
+} as const;
+
+type ValitationResult = {
+  numbers: string[];
+  error: errorType[];
+  validate: (value: string, index: number) => void;
+};
+
+type errorType = {
+  isValidate: boolean;
+  errorMessage: string;
+};
+
+const initialDate = {
+  isValidate: false,
+  errorMessage: "",
+};
+
+export default function useCardNumbers(): ValitationResult {
+  const [numbers, setNumbers] = useState(["", "", "", ""]);
+  const [error, setError] = useState<errorType[]>(
+    Array.from({ length: 4 }, () => initialDate)
+  );
+
+  const updateCardNumber = (
+    index: number,
+    isError: boolean,
+    message: string
+  ) => {
+    setError((prev) => {
+      prev[index] = {
+        isValidate: isError,
+        errorMessage: message,
+      };
+      return prev;
+    });
+  };
+
+  const validate = (value: string, index: number) => {
+    if (value.length > CARD_NUMBER_RULE.MAX_LENGTH) return;
+    setNumbers((prev) => {
+      const newNumbers = [...prev];
+      newNumbers[index] = value;
+      return newNumbers;
+    });
+
+    if (!/^\d*$/.test(value)) {
+      updateCardNumber(index, true, CARD_NUMBER_RULE.NOT_A_NUMBER);
+      return;
+    }
+    if (value.length < CARD_NUMBER_RULE.MAX_LENGTH) {
+      updateCardNumber(index, true, CARD_NUMBER_RULE.INVALID_LENGTH_ERROR);
+      return;
+    }
+    updateCardNumber(index, false, "");
+  };
+
+  return { numbers, error, validate };
+}
