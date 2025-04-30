af21e6f830a085164c9648afdb4831380eac324a
hanheel <hanhee630630@gmail.com>
2025-04-30 16:18:33 +0900

    feat : useCardNumber 훅 구현
    1. cardNumber 받음
    2. validation 실행
    3. isValid, errorMessage 반환

--- hooks/src/App.tsx
+++ hooks/src/App.tsx
@@ -1,18 +1,35 @@
+import { useState } from "react";
 import "./App.css";
-import useCardCVC from "./lib/useCardCVC";
+import useCardNumber from "./lib/useCardNumber";
 
 function App() {
-  const { handleCVCValidate, isValid, errorMessage } = useCardCVC();
+  const [cardNumbers, setCardNumbers] = useState({
+    input1: "",
+    input2: "",
+    input3: "",
+    input4: "",
+  });
+  const { handleCardNumber, isValid, errorMessage } = useCardNumber();
 
   const handleBlur = (e) => {
-    handleCVCValidate(e.target.value);
+    const { name, value } = e.target;
+
+    setCardNumbers((prev) => ({ ...prev, [name]: value }));
+    handleCardNumber({
+      ...cardNumbers,
+      [name]: value,
+    });
   };
 
   console.log(isValid, errorMessage);
   return (
     <div>
+      {/* <input name="month" onBlur={handleBlur}></input> */}
       {errorMessage}
-      <input onBlur={handleBlur}></input>
+      <input name="input1" onBlur={handleBlur}></input>
+      <input name="input2" onBlur={handleBlur}></input>
+      <input name="input3" onBlur={handleBlur}></input>
+      <input name="input4" onBlur={handleBlur}></input>
     </div>
   );
 }
---
+++ hooks/src/lib/useCardNumber.ts
@@ -0,0 +1,49 @@
+import { useState } from "react";
+
+interface CardNumberInput {
+  input1: string;
+  input2: string;
+  input3: string;
+  input4: string;
+}
+
+const useCardNumber = () => {
+  const [isValid, setIsValid] = useState({
+    input1: true,
+    input2: true,
+    input3: true,
+    input4: true,
+  });
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleCardNumber = (numbers: CardNumberInput) => {
+    validateCardNumber(numbers);
+  };
+
+  const validateCardNumber = (numbers: CardNumberInput) => {
+    Object.entries(numbers).forEach(([key, value]) => {
+      if (isNumber(value) && isFourDigits(value)) {
+        setIsValid((prev) => ({ ...prev, [key]: true }));
+        setErrorMessage("");
+      } else if (!isNumber(value)) {
+        setIsValid((prev) => ({ ...prev, [key]: false }));
+        setErrorMessage("카드 번호는 숫자만 입력해주세요.");
+      } else if (!isFourDigits(value)) {
+        setIsValid((prev) => ({ ...prev, [key]: false }));
+        setErrorMessage("카드 번호는 4자리로 입력해주세요.");
+      }
+    });
+
+    return { handleCardNumber, isValid, errorMessage };
+  };
+};
+
+const isNumber = (value: string) => {
+  return !Number.isNaN(Number(value));
+};
+
+const isFourDigits = (value: string) => {
+  return value.length === 4;
+};
+
+export default useCardNumber;
