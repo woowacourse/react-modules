a6ad1bd724b35185114a1612032d06300e6bd1b3
SuHwa Lee <tnghk9611@naver.com>
2025-04-30 20:18:42 +0900

    feat: 카드 매직 넘버 및 에러 메세지 상수화
    Co-authored-by: Luna <ShinjungOh@users.noreply.github.com>

---
+++ hooks/src/lib/constants/cardConfig.ts
@@ -0,0 +1,21 @@
+export const CARD_NUMBER = {
+  maxLength: 4,
+  fieldCount: 4,
+};
+
+export const CARD_EXPIRATION = {
+  minMonth: 1,
+  maxMonth: 12,
+  minYear: 25,
+  monthLength: 2,
+  yearLength: 2,
+  maxLength: 2,
+};
+
+export const CARD_CVC = {
+  maxLength: 3,
+};
+
+export const CARD_PASSWORD = {
+  maxLength: 2,
+};
