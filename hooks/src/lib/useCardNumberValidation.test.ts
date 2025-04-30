e248d70e24abc3d79c2fb920ab33ce6e110ded5a
youjeong Jeong <wjddbwjd1205@naver.com>
2025-04-30 22:24:31 +0900

    test: useCardNumberValidation 훅 테스트 코드 작성
    Co-authored-by: 호이초이 <hoyyChoi@users.noreply.github.com>

---
+++ hooks/src/lib/useCardNumberValidation.test.ts
@@ -0,0 +1,38 @@
+import { renderHook, act } from '@testing-library/react';
+import useCardNumberValidation from './useCardNumberValidation';
+
+describe('useCardNumberValidation', () => {
+  it('useCardNumberValidation의 초기 에러 상태가 반환된다.', () => {
+    const initialErrors = [false, false, false, false];
+    const { result } = renderHook(
+      (): { errors: boolean[]; errorMessage: string } =>
+        useCardNumberValidation()
+    );
+
+    expect(result.current.errors).toEqual(initialErrors);
+  });
+
+  it('숫자가 아닌 값이 들어오면 에러메시지를 반환한다.', () => {
+    const userInput = 'hi';
+    const index = 0; // input 위치
+    const { result } = renderHook(() => useCardNumberValidation());
+
+    act(() => {
+      result.current.validateInput(index, userInput);
+    });
+
+    expect(result.current.errorMessage).toBe('숫자만 입력 가능합니다.');
+  });
+
+  it('현재 에러 상태에 에러가 없다면 noError가 true를 반환한다.', () => {
+    const userInput = '1243';
+    const index = 0; // input 위치
+    const { result } = renderHook(() => useCardNumberValidation());
+
+    act(() => {
+      result.current.validateInput(index, userInput);
+    });
+
+    expect(result.current.noError).toBeTruthy();
+  });
+});
