7769680935f359e09da82d57c13c88f4b8e01633
youjeong Jeong <wjddbwjd1205@naver.com>
2025-04-30 11:44:16 +0900

    style: prettier 설정 추가

--- components/src/lib/Modal.tsx
+++ components/src/lib/Modal.tsx
@@ -1,22 +1,15 @@
-import React from 'react';
-import closeIcon from './assets/close.svg';
+import React from "react";
+import closeIcon from "./assets/close.svg";
 
 const Modal = ({ children }: { children: React.ReactNode }) => {
   return <div>{children}</div>;
 };
 
-
-Modal.Header = ({
-  closeButton = false,
-  children,
-}: {
-  closeButton: boolean;
-  children: React.ReactNode;
-}) => {
+Modal.Header = ({ closeButton = false, children }: { closeButton: boolean; children: React.ReactNode }) => {
   return (
     <div>
       <span>{children}</span>
-      {closeButton && <img src={closeIcon} alt='X' />}
+      {closeButton && <img src={closeIcon} alt="X" />}
     </div>
   );
 };
