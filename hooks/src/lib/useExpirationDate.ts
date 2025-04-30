0df23c5dc13ffa387b9ebb48fd40492a2545b675
eunwoo-levi <eunwoo1341@gmail.com>
2025-04-30 14:34:33 +0900

    feat: ExpirationDate 유효성 검사하는 hook 구현

---
+++ hooks/src/lib/useExpirationDate.ts
@@ -0,0 +1,80 @@
+import { useState } from 'react';
+
+const useExpirationDate = () => {
+  const [isValid, setIsValid] = useState({ month: true, year: true });
+  const [errorMessage, setErrorMessage] = useState('');
+
+  interface ExpirationDateInput {
+    month: string;
+    year: string;
+  }
+
+  const handleExpirationDate = (date: ExpirationDateInput) => {
+    validateExpirationDate(date);
+  };
+
+  const validateExpirationDate = (date: ExpirationDateInput) => {
+    const { month, year } = date;
+
+    if (!validateMonth(month)) return;
+
+    if (!validateYear(year)) return;
+
+    setErrorMessage('');
+  };
+
+  const validateMonth = (month: string) => {
+    if (!isNumber(month)) {
+      setIsValid((prev) => ({ ...prev, month: false }));
+      setErrorMessage('숫자만 입력해 주세요.');
+      return false;
+    }
+    if (!isTwoDigits(month)) {
+      setIsValid((prev) => ({ ...prev, year: false }));
+      setErrorMessage('2자리의 숫자를 입력해 주세요.');
+      return false;
+    }
+
+    if (!isValidMonth(month)) {
+      setIsValid((prev) => ({ ...prev, month: false }));
+      setErrorMessage('1~12 사이의 숫자를 입력해 주세요.');
+      return false;
+    }
+
+    setIsValid((prev) => ({ ...prev, month: true }));
+    return true;
+  };
+
+  const validateYear = (year: string) => {
+    if (!isNumber(year)) {
+      setIsValid((prev) => ({ ...prev, year: false }));
+      setErrorMessage('숫자만 입력해 주세요.');
+      return false;
+    }
+    if (!isTwoDigits(year)) {
+      setIsValid((prev) => ({ ...prev, year: false }));
+      setErrorMessage('2자리의 숫자를 입력해 주세요.');
+      return false;
+    }
+
+    setIsValid((prev) => ({ ...prev, year: true }));
+    return true;
+  };
+
+  return { handleExpirationDate, isValid, errorMessage };
+};
+
+const isNumber = (value: string) => {
+  return !Number.isNaN(Number(value));
+};
+
+const isTwoDigits = (value: string) => {
+  return value.length === 2;
+};
+
+const isValidMonth = (month: string) => {
+  const monthNumber = Number(month);
+  return monthNumber >= 1 && monthNumber <= 12;
+};
+
+export default useExpirationDate;
