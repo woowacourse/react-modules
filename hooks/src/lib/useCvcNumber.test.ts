ee482907908fb3c9978d4600f5c62832663b8c53
guesung <gueit214@naver.com>
2025-04-30 15:58:44 +0900

    test: CVCNumber 테스트 코드 작성

---
+++ hooks/src/lib/useCvcNumber.test.ts
@@ -0,0 +1,60 @@
+import "@testing-library/jest-dom";
+import { renderHook, act } from "@testing-library/react";
+import useCvcNumber from "./useCvcNumber";
+
+test("사용자가 정상적인 값을 입력하면 에러가 발생하지 않는다.", async () => {
+  const { result } = renderHook(() => useCvcNumber());
+  const { errorMessage, isValid, cvcNumber, handleCvcNumberChange } =
+    result.current;
+
+  const event = {
+    target: {
+      value: "111",
+    },
+  };
+
+  act(() =>
+    // @ts-ignore
+    handleCvcNumberChange(event)
+  );
+  expect(isValid).toBeTruthy();
+});
+
+test("사용자가 두 글자를 입력하면 에러가 발생한다.", async () => {
+  const { result } = renderHook(() => useCvcNumber());
+  const { errorMessage, isValid, cvcNumber, handleCvcNumberChange } =
+    result.current;
+
+  const event = {
+    target: {
+      value: "11",
+    },
+  };
+  act(() => {
+    // @ts-ignore
+    handleCvcNumberChange(event);
+  });
+
+  expect(result.current.cvcNumber).toBe("11");
+  expect(result.current.isValid).toBeFalsy();
+  expect(result.current.errorMessage).toBe("3글자를 입력해 주세요.");
+});
+
+test("사용자가 문자열을 입력하면 값이 바뀌지 않는다.", async () => {
+  const { result } = renderHook(() => useCvcNumber());
+  const { errorMessage, isValid, cvcNumber, handleCvcNumberChange } =
+    result.current;
+
+  const event = {
+    target: {
+      value: "안녕",
+    },
+  };
+
+  act(() => {
+    // @ts-ignore
+    handleCvcNumberChange(event);
+  });
+
+  expect(result.current.cvcNumber).toBe("");
+});
