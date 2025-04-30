020936de6a2206693f25e3bd97688fb4cd60cd8d
Beomtae <95863810+Beomtae@users.noreply.github.com>
2025-04-30 14:35:19 +0900

    ✨ feat: 카드 정보 유효성 검증 로직 구현
    Co-Authored-By: Minje Sung <minje813@naver.com>

---
+++ hooks/src/lib/useCardInfoValidation.ts
@@ -0,0 +1,25 @@
+import { CardInfo } from "../types/Card";
+import useCardCvcValidation from "./useCardCvcValidation";
+import useCardExpirationValidation from "./useCardExpirationValidation";
+import useCardNumberValidation from "./useCardNumberValidation";
+import useCardPasswordValidation from "./useCardPasswordValidation";
+
+export function useCardInfoValidation(cardInfo: CardInfo) {
+  const { isCardNumberError } = useCardNumberValidation(cardInfo.number);
+  const { isCardExpirationError } = useCardExpirationValidation(
+    cardInfo.expiration.month,
+    cardInfo.expiration.year
+  );
+  const { isCvcError } = useCardCvcValidation(cardInfo.cvc);
+  const { isPasswordError } = useCardPasswordValidation(cardInfo.passwordFront);
+
+  return (
+    isCardNumberError.some((isError) => isError) ||
+    isCardExpirationError.some((isError) => isError) ||
+    isCvcError ||
+    isPasswordError ||
+    cardInfo.company === ""
+  );
+}
+
+export default useCardInfoValidation;
