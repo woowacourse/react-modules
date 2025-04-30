96896f1658555f0e358de741a194b9fcc77d3e58
Oliy <eunsoa0310@gmail.com>
2025-04-30 22:52:49 +0900

    feat: 카드 입력 유효성 검사 및 오류 메시지 처리 기능 추가
    Co-authored-by: dev-dino22 <dev-dino22@users.noreply.github.com>

---
+++ hooks/src/lib/validator/getFirstErrorMessage.ts
@@ -0,0 +1,23 @@
+import { ERROR_MESSAGE } from './constants/errorMessage';
+
+export function getExpirationFirstErrorMessage(
+  errorResult: Record<string, boolean>,
+  fieldKey: 'MONTH' | 'YEAR',
+): string {
+  const firstError = Object.entries(errorResult).find(([, isValid]) => !isValid);
+  if (!firstError) return '';
+
+  const [errorCode] = firstError;
+  return (ERROR_MESSAGE.EXPIRATION[fieldKey] as Record<string, string>)[errorCode] ?? '';
+}
+
+export function getFirstErrorMessage(
+  errorResult: Record<string, boolean>,
+  fieldKey: 'CVC' | 'NUMBER' | 'PASSWORD',
+): string {
+  const firstError = Object.entries(errorResult).find(([, isValid]) => !isValid);
+  if (!firstError) return '';
+
+  const [errorCode] = firstError;
+  return (ERROR_MESSAGE[fieldKey] as Record<string, string>)[errorCode] ?? '';
+}
