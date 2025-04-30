eaf949baf450e4000cb9b968ae139975f07ba0a6
Ko Daeun <drechsu@gmail.com>
2025-04-30 20:57:58 +0900

    feat : useCardBrand 훅 구현

---
+++ hooks/src/useCardBrand/useCardBrand.ts
@@ -0,0 +1,65 @@
+import { useState } from 'react';
+import useError from '../useError/useError';
+const KEY = 'cardBrand';
+
+const CARD_BRAND_ERROR_MESSAGE = {
+  NOT_SELECTED: '카드 브랜드를 선택해주세요.',
+};
+
+type useCardBrandProps = {
+  userCardBrand?: string;
+  optionValues: string[];
+};
+
+export default function useCardBrand({
+  userCardBrand = '',
+  optionValues,
+}: useCardBrandProps) {
+  const [cardBrand, setCardBrand] = useState(userCardBrand);
+  const { error, changeError, clearError } = useError({
+    [KEY]: false,
+  });
+
+  function handleCardBrandChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const input = event.target.value.trim();
+    const { inputError, inputErrorMessage } = getCardBrandError({
+      input,
+      optionValues,
+    });
+
+    if (inputError) {
+      changeError(KEY, inputErrorMessage);
+      return;
+    }
+
+    clearError(KEY);
+    setCardBrand(input);
+  }
+
+  return {
+    cardBrand,
+    isError: error.isError[KEY],
+    errorMessage: error.errorMessage[KEY],
+    handleCardBrandChange,
+  };
+}
+
+function getCardBrandError({
+  input,
+  optionValues,
+}: {
+  input: string;
+  optionValues: string[];
+}) {
+  if (!optionValues.includes(input)) {
+    return {
+      inputError: true,
+      inputErrorMessage: CARD_BRAND_ERROR_MESSAGE.NOT_SELECTED,
+    };
+  }
+
+  return {
+    inputError: false,
+    inputErrorMessage: '',
+  };
+}
