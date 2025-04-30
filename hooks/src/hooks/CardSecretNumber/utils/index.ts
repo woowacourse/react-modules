3dd31ed7dfea83c68a16ef782137b167db8ff1d2
wo-o29 <woogur29@gmail.com>
2025-04-30 17:30:14 +0900

    feat: useCardSecretNumber 훅 구현

---
+++ hooks/src/hooks/CardSecretNumber/utils/index.ts
@@ -0,0 +1,24 @@
+import { ValidationResult } from "../../../types";
+
+export const validateCardSecretNumber = (
+  secretNumber: string
+): ValidationResult => {
+  if (isNaN(Number(secretNumber))) {
+    return {
+      isValid: false,
+      errorMessage: "숫자만 입력해주세요.",
+    };
+  }
+
+  if (secretNumber.length !== 2) {
+    return {
+      isValid: false,
+      errorMessage: "비밀번호는 2자리여야 합니다.",
+    };
+  }
+
+  return {
+    isValid: true,
+    errorMessage: "",
+  };
+};
