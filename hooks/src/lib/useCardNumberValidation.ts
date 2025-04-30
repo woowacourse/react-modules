3a6f82638caee6a1558772b2b0d7e3192eb2dab3
Beomtae <95863810+Beomtae@users.noreply.github.com>
2025-04-30 13:51:51 +0900

    ✨ feat: 카드 번호 유효성 검증 로직 작성
    Co-Authored-By: Minje Sung <minje813@naver.com>

--- hooks/src/lib/useCardNumberValidation.ts
+++ hooks/src/lib/useCardNumberValidation.ts
@@ -1,3 +1,13 @@
-function useCardNumberValidation() {}
+import { CardNumber } from "../types/Card";
+import useIsNumber from "./useIsNumber";
+import useIsValidLength from "./useIsValidLength";
+
+function useCardNumberValidation(cardNumbers: CardNumber) {
+  const isCardNumberError = Object.values(cardNumbers).map((number) => {
+    return !useIsValidLength(number, 0, 4) || !useIsNumber(number);
+  });
+
+  return { isCardNumberError };
+}
 
 export default useCardNumberValidation;
