b874390768430edefb65411008f93265d6c16182
youjeong Jeong <wjddbwjd1205@naver.com>
2025-04-30 22:46:06 +0900

    feat: 월, 년 검증 함수 구현
    Co-authored-by: 호이초이 <hoyyChoi@users.noreply.github.com>

--- hooks/src/lib/utils/validation.ts
+++ hooks/src/lib/utils/validation.ts
@@ -4,3 +4,23 @@ export const isNumber = (value: string) => {
   if (error) return { error, message: '숫자만 입력 가능합니다.' };
   return { error, message: '' };
 };
+
+export const isExpirationDate = (type: 'month' | 'year', value: string) => {
+  const isNumberError = isNumber(value);
+  if (isNumberError.error) return isNumberError;
+
+  if (type === 'month') {
+    const month = parseInt(value, 10);
+    if (month < 1 || month > 12) {
+      return { error: true, message: '유효하지 않은 월입니다.' };
+    }
+  } else if (type === 'year') {
+    const year = parseInt(value, 10);
+    const currentYear = new Date().getFullYear() % 100;
+    if (year < currentYear) {
+      return { error: true, message: '유효하지 않은 연도입니다.' };
+    }
+  }
+
+  return { error: false, message: '' };
+};
