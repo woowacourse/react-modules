1f3641ab98bfb9b8aebe3fff3052b238c20af8be
aydenote <dslgpgh@gmail.com>
2025-04-30 13:53:34 +0900

    feat: 확인, 닫기 버튼 기본 스타일 제공

---
+++ components/src/lib/Modal/Modal.tsx
@@ -0,0 +1,75 @@
+import { ReactNode } from "react";
+import styled from "@emotion/styled";
+
+interface ModalProps {
+	position: "center" | "bottom";
+	isOpen: boolean;
+	isCloseButton?: boolean;
+	isConfirmButton?: boolean;
+	children: ReactNode;
+	onClose: () => void;
+}
+
+const Modal = ({ position, isOpen, children = null, onClose, isCloseButton = false, isConfirmButton = false }: ModalProps) => {
+	return (
+		<>
+			<ModalLayout isOpen={isOpen}>
+				<ModalContainer position={position}>
+					{children}
+					{isConfirmButton && <ConfirmButton onClick={onClose}>확인</ConfirmButton>}
+					{isCloseButton && <CloseButton onClick={onClose}>닫기</CloseButton>}
+				</ModalContainer>
+			</ModalLayout>
+		</>
+	);
+};
+
+export default Modal;
+
+const ModalLayout = styled.div<{ isOpen: boolean }>`
+	position: absolute;
+	z-index: 2;
+	display: ${(props) => (props.isOpen ? "flex" : "none")};
+	width: 100vw;
+	height: 100vh;
+	background-color: rgba(0, 0, 0, 0.35);
+`;
+
+const ModalContainer = styled.div<{ position: string }>`
+	z-index: 3;
+	position: relative;
+	display: flex;
+	flex-direction: column;
+	width: ${(props) => (props.position === "bottom" ? "100%" : "60%")};
+	margin: ${(props) => (props.position === "bottom" ? "auto 0 0 0" : "auto")};
+	height: auto;
+	padding: 24px 32px;
+	border-radius: 8px;
+	background-color: #fff;
+`;
+
+const CloseButton = styled.button`
+	width: 100%;
+	padding: 8px 0px;
+	border: 1px solid #8b95a1;
+	text-align: center;
+	font-size: 15px;
+	font-weight: 700;
+	border-radius: 5px;
+	background: #fff;
+	color: #8b95a1;
+	cursor: pointer;
+`;
+
+const ConfirmButton = styled.button`
+	width: 100%;
+	padding: 8px 0px;
+	border: none;
+	text-align: center;
+	font-size: 15px;
+	font-weight: 700;
+	border-radius: 5px;
+	color: #fff;
+	background: #333;
+	cursor: pointer;
+`;
