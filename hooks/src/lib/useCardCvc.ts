1d9de305b84fc00713e4c430b27483a2219f4f79
aydenote <dslgpgh@gmail.com>
2025-04-30 17:22:08 +0900

    feat: 카드 CVC 입력 유효성 검증 커스텀 훅 구현
    Co-authored-by: Deokbin Kim <Db0111@users.noreply.github.com>

---
+++ hooks/src/lib/useCardCvc.ts
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+const CVC_VALID_LENGTH = 3;
+
+const ERROR_MESSAGE = {
+	INVALID_NUMBER: "숫자만 입력 가능합니다.",
+	INPUT_LENGTH_LIMIT: `${CVC_VALID_LENGTH}자리를 입력해주세요.`,
+};
+
+const useCardCvc = () => {
+	const [isValid, setIsValid] = useState<boolean>(true);
+	const [errorMessage, setErrorMessage] = useState<string[]>([]);
+
+	const checkNumber = (value: string) => {
+		if (!/^\d+$/.test(value)) return false;
+	};
+
+	const checkLength = (value: string) => {
+		if (value.length < CVC_VALID_LENGTH) return false;
+	};
+
+	const validate = (value: string) => {
+		const isNumber = checkNumber(value);
+		const isValidLength = checkLength(value);
+
+		if (!isNumber) {
+			setErrorMessage([ERROR_MESSAGE.INVALID_NUMBER]);
+			setIsValid(false);
+		}
+
+		if (!isValidLength) {
+			setErrorMessage([...errorMessage, ERROR_MESSAGE.INPUT_LENGTH_LIMIT]);
+		}
+
+		setIsValid(errorMessage.length === 0);
+	};
+
+	return { isValid, errorMessage, validate };
+};
+
+export default useCardCvc;
