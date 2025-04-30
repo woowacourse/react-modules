e6d16a0bb3fb0a8198938b7dddc99fc2c8b63903
eunwoo-levi <eunwoo1341@gmail.com>
2025-04-30 14:32:41 +0900

    refactor: useCardCVC 더 정확한 로직으로 리팩토링

--- hooks/src/lib/useCardCVC.ts
+++ hooks/src/lib/useCardCVC.ts
@@ -1,27 +1,31 @@
-import { useState } from "react";
+import { useState } from 'react';
 
 const useCardCVC = () => {
   const [isValid, setIsValid] = useState(true);
-  const [errorMessage, setErrorMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState('');
 
   const handleCVCValidate = (input: string) => {
-    validateCVC(input);
+    if (!validateCVC(input)) return;
+    setIsValid(true);
+    setErrorMessage('');
   };
 
   const validateCVC = (input: string) => {
     if (Number.isNaN(Number(input))) {
       setIsValid(false);
-      setErrorMessage("숫자만 입력해 주세요.");
-      return;
+      setErrorMessage('숫자만 입력해 주세요.');
+      return false;
     }
     if (input.length !== 3) {
       setIsValid(false);
-      setErrorMessage("3글자의 숫자를 입력해 주세요.");
-      return;
+      setErrorMessage('3글자의 숫자를 입력해 주세요.');
+      return false;
     }
+
+    return true;
   };
 
-  return { handleCVCValidate, isValid, errorMessage, validateCVC };
+  return { handleCVCValidate, isValid, errorMessage };
 };
 
 export default useCardCVC;
