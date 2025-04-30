ab62707c9d36fe912d96276c13ca6c8ce07a834b
aydenote <dslgpgh@gmail.com>
2025-04-30 17:21:56 +0900

    feat: 카드사 입력 유효성 검증 커스텀 훅 구현
    Co-authored-by: Deokbin Kim <Db0111@users.noreply.github.com>

---
+++ hooks/src/lib/useCardCompany.ts
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+const ERROR_MESSAGE = {
+	EMPTY_CARD_COMPANY: "카드사를 선택해주세요.",
+};
+
+const useCardCompany = () => {
+	const [isValid, setIsValid] = useState<boolean>(true);
+	const [errorMessage, setErrorMessage] = useState<string>("");
+
+	const validate = (value: string) => {
+		if (!value) {
+			setIsValid(false);
+			setErrorMessage(ERROR_MESSAGE.EMPTY_CARD_COMPANY);
+		}
+	};
+
+	return { isValid, errorMessage, validate };
+};
+
+export default useCardCompany;
