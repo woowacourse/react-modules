fc77e2ef2c43883e2d686113d5d885e3cf37b8cc
spoyodevelop <kjy9505@gmail.com>
2025-04-30 20:45:32 +0900

    feat: password 와, Expirydate 상수 추가

--- hooks/src/lib/constants/index.ts
+++ hooks/src/lib/constants/index.ts
@@ -2,8 +2,13 @@ export const ERROR_MESSAGE = {
   INVALID_LENGTH: (maxLength: number) =>
     `${maxLength} 자리의 공백 없는 숫자만 입력 가능합니다.`,
   NOT_NUMERIC: "입력은 숫자만 입력 가능합니다.",
+  INVALID_MONTH: "1부터 12까지의 월수를 입력해주세요.",
 } as const;
 
 export const CARD_NUMBER_LENGTH = 16;
 
 export const CVC_LENGTH = 3;
+
+export const PASSWORD_LENGTH = 2;
+
+export const EXPIRY_DATE_LENGTH = 4;
