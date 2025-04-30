77ea5fc374f4cf0d64e7a8015351bfb5272ab9a3
youjeong Jeong <wjddbwjd1205@naver.com>
2025-04-30 22:45:29 +0900

    feat: useExpirationDateValidation 훅 구현
    Co-authored-by: 호이초이 <hoyyChoi@users.noreply.github.com>

---
+++ hooks/src/lib/useExpirationDateValidation.ts
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+import { isExpirationDate } from './utils/validation';
+import checkNoError from './utils/checkNoError';
+
+const useExpirationDateValidation = () => {
+  const [errors, setErrors] = useState([false, false]);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const validateInput = (index: number, value: string) => {
+    const inputType = index === 0 ? 'month' : 'year';
+    const { error, message } = isExpirationDate(inputType, value);
+
+    setErrors((prev) => {
+      const updated = [...prev];
+      updated[index] = error;
+      return updated;
+    });
+
+    setErrorMessage(message);
+  };
+
+  const noError = checkNoError(errors);
+
+  return {
+    errors,
+    errorMessage,
+    validateInput,
+    noError,
+  };
+};
+
+export default useExpirationDateValidation;
