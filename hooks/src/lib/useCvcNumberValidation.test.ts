dbc111c33542c9d34174fa0a4fd65682efc46548
youjeong Jeong <wjddbwjd1205@naver.com>
2025-05-01 11:06:10 +0900

    test: useCvcNumberValidation 테스트 코드 작성
    Co-authored-by: 호이초이 <hoyyChoi@users.noreply.github.com>

---
+++ hooks/src/lib/useCvcNumberValidation.test.ts
@@ -0,0 +1,35 @@
+import { renderHook, act } from '@testing-library/react';
+import useCvcNumberValidation from './useCvcNumberValidation';
+
+describe('useCvcNumberValidation', () => {
+  it('useCvcNumberValidation 초기 에러 상태가 반환된다.', () => {
+    const initialErrors = false;
+    const { result } = renderHook(
+      (): { errors: boolean; errorMessage: string } => useCvcNumberValidation()
+    );
+
+    expect(result.current.errors).toEqual(initialErrors);
+  });
+
+  it('숫자가 아닌 값이 들어오면 에러메시지를 반환한다.', () => {
+    const userInput = 'hi';
+    const { result } = renderHook(() => useCvcNumberValidation());
+
+    act(() => {
+      result.current.validateInput(userInput);
+    });
+
+    expect(result.current.errorMessage).toBe('숫자만 입력 가능합니다.');
+  });
+
+  it('현재 에러 상태에 에러가 없다면 noError가 true를 반환한다.', () => {
+    const userInput = '124';
+    const { result } = renderHook(() => useCvcNumberValidation());
+
+    act(() => {
+      result.current.validateInput(userInput);
+    });
+
+    expect(result.current.noError).toBeTruthy();
+  });
+});
