d77211ba821e8a648987a08807c31f2214a26d1d
SuHwa Lee <tnghk9611@naver.com>
2025-05-01 12:42:43 +0900

    chore: 기존 라이브러리 삭제
    Co-authored-by: Luna <ShinjungOh@users.noreply.github.com>

--- components/src/App.tsx
+++ components/src/App.tsx
@@ -1,26 +1,5 @@
-import { useState } from 'react';
-import './App.css';
-import { Modal } from 'lume_modal';
-
 function App() {
-  const [isShowModal, setIsShowModal] = useState(false);
-
-  return (
-    <div>
-      {isShowModal && (
-        <Modal
-          position="bottom"
-          title="모달"
-          onClose={() => {
-            setIsShowModal(false);
-          }}
-        >
-          <div>모달 내용</div>
-        </Modal>
-      )}
-      <button onClick={() => setIsShowModal(true)}>모달 열기</button>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
