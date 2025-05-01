b327fcab4e44e18ae88c1e2d36f4c32a3d1222c4
youjeong Jeong <wjddbwjd1205@naver.com>
2025-05-01 12:10:33 +0900

    feat: constants 처리
    Co-authored-by: 호이초이 <hoyyChoi@users.noreply.github.com>

---
+++ hooks/src/lib/constants/index.ts
@@ -0,0 +1,9 @@
+export const NUMBER_REGEX = /^[0-9]*$/;
+export const ERROR_MESSAGE = {
+  NUMBER_ONLY: '숫자만 입력 가능합니다.',
+  MONTH_VALID: '유효하지 않은 월입니다.',
+  YEAR_VALID: '유효하지 않은 연도입니다.',
+};
+export const MIN_MONTH = 1;
+export const MAX_MONTH = 12;
+export const CURRENT_YEAR = new Date().getFullYear() % 100;
