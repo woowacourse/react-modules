1c5acb29762c15bc7d1bd7de43f7da58b49e6659
spoyodevelop <kjy9505@gmail.com>
2025-05-03 04:03:24 +0900

    feat: 필드 유효성 검사 규칙 및 검증기 구현

---
+++ hooks/src/lib/validator/validators.ts
@@ -0,0 +1,47 @@
+import { createValidator, ValidationRule } from "./index";
+import { ValidateField } from "./constants";
+import { validationRules } from "./validation-rules";
+import { FieldErrorCode } from "./constants/error-messages";
+type ValidationRuleObj = {
+  check: (value: string) => boolean;
+  message: string;
+};
+
+function createFieldRules<T extends ValidateField>(
+  field: T,
+  rules: Record<string, ValidationRuleObj>
+): ValidationRule<T>[] {
+  return Object.entries(rules).map(([code, { check }]) => ({
+    check,
+    errorMeta: {
+      field,
+      code: code as FieldErrorCode[T],
+    },
+  }));
+}
+
+const cvcRules = createFieldRules("cvc", validationRules.cvc);
+
+const cardNumberRules = createFieldRules(
+  "cardNumber",
+  validationRules.cardNumber
+);
+
+const passwordRules = createFieldRules("password", validationRules.password);
+
+const expiryDateRules = createFieldRules(
+  "expiryDate",
+  validationRules.expiryDate
+);
+
+const validateCVC = createValidator<"cvc">(cvcRules);
+const validateCardNumber = createValidator<"cardNumber">(cardNumberRules);
+const validatePassword = createValidator<"password">(passwordRules);
+const validateExpiryDate = createValidator<"expiryDate">(expiryDateRules);
+
+export {
+  validateCVC,
+  validateCardNumber,
+  validatePassword,
+  validateExpiryDate,
+};
