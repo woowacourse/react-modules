33baef70926a5b299f26f7a4ebda0e952ac33620
guesung <gueit214@naver.com>
2025-05-03 16:33:02 +0900

    fix: 빌드 이슈

--- hooks/vite.config.ts
+++ hooks/vite.config.ts
@@ -1,32 +1,37 @@
-import * as path from "path";
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import dts from "vite-plugin-dts";
+import * as path from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
   build: {
     lib: {
-      entry: path.resolve(__dirname, "src/lib/index.ts"),
-      name: "index",
-      fileName: "index",
+      entry: path.resolve(__dirname, 'src/lib/index.js'),
+      name: 'index',
+      fileName: 'index',
     },
     rollupOptions: {
-      external: ["react"],
+      external: ['react'],
       output: {
         globals: {
-          react: "React",
+          react: 'React',
         },
+        entryFileNames: 'lib/index.js',
+        assetFileNames: 'lib/index.css',
+        sourcemap: true,
       },
     },
     commonjsOptions: {
-      esmExternals: ["react"],
+      esmExternals: ['react'],
     },
+    outDir: 'dist',
+    sourcemap: true,
   },
   plugins: [
     react(),
     dts({
-      include: ["src"],
-      tsconfigPath: "./tsconfig.app.json",
+      include: ['src/lib'],
+      tsconfigPath: './tsconfig.app.json',
     }),
   ],
 });
