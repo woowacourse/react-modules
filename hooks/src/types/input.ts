190ac844b9bf5c5dcffe59c01aaa3d50796f18f1
guesung <gueit214@naver.com>
2025-05-03 15:25:46 +0900

    chore: 타입 추가

---
+++ hooks/src/types/input.ts
@@ -0,0 +1,11 @@
+import { CardCompany } from "./card";
+
+import { CardExpirationDate } from "./card";
+
+import { CardNumberSequence } from "./card";
+
+export type CardNumberInput = Record<CardNumberSequence, string>;
+export type CardExpirationDateInput = Record<CardExpirationDate, string>;
+export type CardCVCNumberInput = Record<"cvc", string>;
+export type CardPasswordInput = Record<"password", string>;
+export type CardCompanyInput = Record<"company", CardCompany>;
