63fc54784d7c1a5dc3cf11a27c25d77d4c9b30a1
keemsebeen <sebin0580@gmail.com>
2025-05-04 03:13:02 +0900

    refactor: useCardNumber JSDoc 설명 수정

--- hooks/src/lib/hooks/useCardNumber/useCardNumber.ts
+++ hooks/src/lib/hooks/useCardNumber/useCardNumber.ts
@@ -6,13 +6,11 @@ import { CardInputItem } from '../../types/cardInputItem.types';
 const CARD_NUMBER_INPUTS_LENGTH = 4;
 
 /**
- *
  * @description
  * useCardNumber 훅은 카드 번호 입력을 관리하는 커스텀 훅입니다.
  * 카드 번호의 유효성을 검사하고, 입력값을 상태로 관리합니다.
- * @param  validLength 카드 번호의 유효 길이
  * @returns 카드 번호 입력 상태와 핸들러를 반환합니다.
- * @returns cardNumbers 카드 번호 입력 상태
+ * @returns cardNumbers - 카드 번호 입력 상태 배열 (4개 항목)
  * @returns errorMessage 카드 번호 입력 오류 메시지
  * @returns handleCardNumberChange 카드 번호 입력 핸들러
  * @example
