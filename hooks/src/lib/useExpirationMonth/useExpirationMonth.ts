a354b52778da7fc583203d35dc453e24f9973ea9
deokbin <deokbin@naver.com>
2025-05-03 13:39:07 +0900

    refactor: 불필요한 변수 할당 제거

--- hooks/src/lib/useExpirationMonth/useExpirationMonth.ts
+++ hooks/src/lib/useExpirationMonth/useExpirationMonth.ts
@@ -3,60 +3,47 @@ import { useState } from "react";
 const MONTH_VALID_LENGTH = 2;
 
 const ERROR_MESSAGE = {
-	INVALID_NUMBER: "숫자만 입력 가능합니다.",
-	INVALID_MONTH_RANGE: "1~12까지의 범위만 입력 가능합니다.",
-	INVALID_MONTH_FORMAT: "MM형태로 입력해주세요.",
+  INVALID_NUMBER: "숫자만 입력 가능합니다.",
+  INVALID_MONTH_RANGE: "1~12까지의 범위만 입력 가능합니다.",
+  INVALID_MONTH_FORMAT: "MM형태로 입력해주세요.",
 };
 
 const useExpirationMonth = () => {
-	const [isValid, setIsValid] = useState<boolean>(true);
-	const [errorMessage, setErrorMessage] = useState<string>("");
+  const [isValid, setIsValid] = useState<boolean>(true);
+  const [errorMessage, setErrorMessage] = useState<string>("");
 
-	const checkLength = (value: string) => {
-		if (value.length < MONTH_VALID_LENGTH) {
-			return false;
-		}
-		return true;
-	};
+  const isValidLength = (value: string) => {
+    return value.length < MONTH_VALID_LENGTH;
+  };
 
-	const checkNumber = (value: string) => {
-		if (!/^\d+$/.test(value)) {
-			return false;
-		}
-		return true;
-	};
+  const isNumber = (value: string) => {
+    return !/^\d+$/.test(value);
+  };
 
-	const checkMonthRange = (value: string) => {
-		if (Number(value) < 1 || Number(value) > 12) {
-			return false;
-		}
-		return true;
-	};
+  const isValidRange = (value: string) => {
+    return Number(value) < 1 || Number(value) > 12;
+  };
 
-	const validate = (value: string) => {
-		const isValidLength = checkLength(value);
-		const isNumber = checkNumber(value);
-		const isValidRange = checkMonthRange(value);
+  const validate = () => {
+    if (!isValidLength) {
+      setErrorMessage(ERROR_MESSAGE.INVALID_MONTH_FORMAT);
+      setIsValid(false);
+      return;
+    }
+    if (!isNumber) {
+      setErrorMessage(ERROR_MESSAGE.INVALID_NUMBER);
+      setIsValid(false);
+      return;
+    }
 
-		if (!isValidLength) {
-			setErrorMessage(ERROR_MESSAGE.INVALID_MONTH_FORMAT);
-			setIsValid(false);
-			return;
-		}
-		if (!isNumber) {
-			setErrorMessage(ERROR_MESSAGE.INVALID_NUMBER);
-			setIsValid(false);
-			return;
-		}
+    if (!isValidRange) {
+      setErrorMessage(ERROR_MESSAGE.INVALID_MONTH_RANGE);
+      setIsValid(false);
+      return;
+    }
+  };
 
-		if (!isValidRange) {
-			setErrorMessage(ERROR_MESSAGE.INVALID_MONTH_RANGE);
-			setIsValid(false);
-			return;
-		}
-	};
-
-	return { isValid, errorMessage, validate };
+  return { isValid, errorMessage, validate };
 };
 
 export default useExpirationMonth;
