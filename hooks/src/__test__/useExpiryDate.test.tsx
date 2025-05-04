b877a141ebfb325e3650a24af6cf1be8954f355f
keemsebeen <sebin0580@gmail.com>
2025-05-04 14:07:43 +0900

    test: useExpiryDate 카드 만료기간 테스트 추가

--- hooks/src/__test__/useExpiryDate.test.tsx
+++ hooks/src/__test__/useExpiryDate.test.tsx
@@ -4,6 +4,8 @@ import { useExpiryDate } from '../lib/hooks/useExpiryDate/useExpiryDate';
 describe('유효기간 검증 테스트입니다.', () => {
   const expiryDateLength = 2;
   const currentYear = new Date().getFullYear() % 100;
+  const currentMonth = new Date().getMonth() + 1;
+
   test(`사용자가 입력한 value값의 길이와 validLength(${expiryDateLength})가 같다면, errorMessage를 빈 값으로 반환한다.`, () => {
     const { result } = renderHook(() => useExpiryDate());
 
@@ -59,5 +61,26 @@ describe('유효기간 검증 테스트입니다.', () => {
     expect(result.current.errorMessage).toBe(
       `유효기간의 년도는 현재 년도(${currentYear})보다 크거나 같아야 합니다.`
     );
+  });
+
+  test(`사용자가 입력한 month 값이 현재 년도(${currentYear})와 같고, month 값이 현재 월(${currentMonth})보다 작다면 errorMessage를 반환준다.`, () => {
+    const { result } = renderHook(() => useExpiryDate());
+
+    const monthMockEvent = {
+      target: { value: '04' },
+    } as React.ChangeEvent<HTMLInputElement>;
+    act(() => {
+      result.current.handleExpiryDateChange(monthMockEvent, 0);
+    });
+
+    const yearMockEvent = {
+      target: { value: currentYear.toString() },
+    } as React.ChangeEvent<HTMLInputElement>;
+
+    act(() => {
+      result.current.handleExpiryDateChange(yearMockEvent, 1);
+    });
+
+    expect(result.current.errorMessage).toBe(`카드가 만료되었습니다. 유효기간을 확인해주세요.`);
   });
 });
