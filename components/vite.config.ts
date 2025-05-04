857c58d3356cd74d0d8e59f9dae5c35f9c951e59
AHHYUNJU <wndkgus0225@naver.com>
2025-05-04 23:44:05 +0900

    fix: 불필요한 주석 제거

--- components/vite.config.ts
+++ components/vite.config.ts
@@ -1,40 +1,3 @@
-// import * as path from "path";
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react-swc";
-// import dts from "vite-plugin-dts";
-
-// export default defineConfig({
-//   build: {
-//     lib: {
-//       entry: path.resolve(__dirname, "src/lib/index.ts"),
-//       name: "index",
-//       fileName: "index",
-//     },
-//     rollupOptions: {
-//       external: ["react"],
-//       output: {
-//         globals: {
-//           react: "React",
-//         },
-//       },
-//     },
-//     commonjsOptions: {
-//       esmExternals: ["react"],
-//     },
-//   },
-//   plugins: [
-//     react(),
-//     dts({
-//       include: ["src/lib"],
-//       tsconfigPath: "./tsconfig.app.json",
-//     }),
-//   ],
-//   server: {
-//     host: "0.0.0.0",
-//     port: 5173,
-//   },
-// });
-
 import * as path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
@@ -46,8 +9,6 @@ export default defineConfig({
     react(),
     dts({
       tsconfigPath: "./tsconfig.app.json",
-      // tsconfigPath: "./tsconfig.build.json",
-      // insertTypesEntry: true,
       include: ["src/lib"],
     }),
     cssInjectedByJs({
