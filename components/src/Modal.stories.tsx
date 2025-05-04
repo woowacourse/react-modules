3d98a80c8986b80c57ede8045066b07822f838d9
hanheel <hanhee630630@gmail.com>
2025-05-04 22:31:49 +0900

    refactor: 스토리북 문서화를 위해 args 기반 스토리 구조로 변경

--- components/src/Modal.stories.tsx
+++ components/src/Modal.stories.tsx
@@ -6,12 +6,6 @@ import React from "react";
 const meta: Meta<typeof Modal> = {
   title: "Components/Modal",
   component: Modal,
-  argTypes: {
-    position: {
-      control: { type: "radio" },
-      options: ["center", "bottom"],
-    },
-  },
 };
 export default meta;
 
@@ -19,32 +13,34 @@ type Story = StoryObj<typeof Modal>;
 
 export const Default: Story = {
   render: (args) => {
-    const [isOpen, setIsOpen] = useState(true);
-
-    const handleCloseModal = () => {
-      setIsOpen(false);
-    };
-
     return (
-      <div style={{ minHeight: "700px" }}>
-        <button
-          style={{
-            border: "1px solid #000",
-            backgroundColor: "#cccccc",
-            cursor: "pointer",
-          }}
-          onClick={() => setIsOpen(true)}
-        >
-          Open Modal
-        </button>
-        <Modal {...args} isOpen={isOpen} handleCloseModal={handleCloseModal}>
-          <div style={{ padding: "20px" }}>모달 content 입니다</div>
-        </Modal>
+      <div style={{ minHeight: "700px", minWidth: "100%" }}>
+        <Modal {...args} />
       </div>
     );
   },
   args: {
-    title: "모달 타이틀",
+    isOpen: true,
+    title: "기본 모달",
     position: "center",
+    handleCloseModal: () => alert("모달 닫힘"),
+    children: <div style={{ padding: "20px" }}>모달 콘텐츠입니다</div>,
+  },
+};
+
+export const BottomModal: Story = {
+  render: (args) => {
+    return (
+      <div style={{ minHeight: "700px", minWidth: "100%" }}>
+        <Modal {...args} />
+      </div>
+    );
+  },
+  args: {
+    isOpen: true,
+    title: "하단 모달",
+    position: "bottom",
+    handleCloseModal: () => alert("모달 닫기"),
+    children: <div style={{ padding: "20px" }}>하단 모달 내용입니다</div>,
   },
 };
