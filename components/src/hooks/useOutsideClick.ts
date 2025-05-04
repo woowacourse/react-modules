f7fd007774572924d5f20b86d0b05e4332004c3a
bunju20 <ghkd2271@naver.com>
2025-05-04 16:02:32 +0900

    fix: callback 의존성 추가

--- components/src/hooks/useOutsideClick.ts
+++ components/src/hooks/useOutsideClick.ts
@@ -1,36 +1,42 @@
 import { useEffect } from "react";
 
 const useModalCloseEvent = (callback: () => void) => {
-  useEffect(function outsideClick() {
-    const handleDocumentClick = ({ target }: MouseEvent | TouchEvent) => {
-      const currentTarget = target as HTMLElement;
-      if (currentTarget.id !== "backdrop") {
-        return;
-      }
+  useEffect(
+    function outsideClick() {
+      const handleDocumentClick = ({ target }: MouseEvent | TouchEvent) => {
+        const currentTarget = target as HTMLElement;
+        if (currentTarget.id !== "backdrop") {
+          return;
+        }
 
-      callback();
-    };
-
-    document.addEventListener("click", handleDocumentClick);
-
-    return () => {
-      document.removeEventListener("click", handleDocumentClick);
-    };
-  }, []);
-
-  useEffect(function escKeyDown() {
-    const handleDocumentKeyDown = ({ key }: KeyboardEvent) => {
-      if (key === "Escape") {
         callback();
-      }
-    };
+      };
 
-    document.addEventListener("keydown", handleDocumentKeyDown);
+      document.addEventListener("click", handleDocumentClick);
 
-    return () => {
-      document.removeEventListener("keydown", handleDocumentKeyDown);
-    };
-  }, []);
+      return () => {
+        document.removeEventListener("click", handleDocumentClick);
+      };
+    },
+    [callback]
+  );
+
+  useEffect(
+    function escKeyDown() {
+      const handleDocumentKeyDown = ({ key }: KeyboardEvent) => {
+        if (key === "Escape") {
+          callback();
+        }
+      };
+
+      document.addEventListener("keydown", handleDocumentKeyDown);
+
+      return () => {
+        document.removeEventListener("keydown", handleDocumentKeyDown);
+      };
+    },
+    [callback]
+  );
 };
 
 export default useModalCloseEvent;
