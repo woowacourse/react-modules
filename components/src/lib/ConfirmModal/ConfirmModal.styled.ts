8651016f4b08b1d502ec340b4a8db627e0752f74
wo-o29 <woogur29@gmail.com>
2025-05-05 01:29:32 +0900

    feat: ConfirmModal 컴포넌트 스토리북 구현

--- components/src/lib/ConfirmModal/ConfirmModal.styled.ts
+++ components/src/lib/ConfirmModal/ConfirmModal.styled.ts
@@ -3,7 +3,7 @@ import styled from "@emotion/styled";
 export const ConfirmText = styled.p`
   color: #0a0d13;
   font-family: "Noto Sans";
-  font-size: 0.75rem;
+  font-size: 0.875rem;
   font-weight: 500;
   line-height: 0.9375rem;
 `;
