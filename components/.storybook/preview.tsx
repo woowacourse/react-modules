b4b61a811cc0c7f3eaeb223448e7c4df04563d7d
wo-o29 <woogur29@gmail.com>
2025-05-05 00:19:49 +0900

    feat: 스토리북 뷰포트 설정 추가

--- components/.storybook/preview.tsx
+++ components/.storybook/preview.tsx
@@ -1,6 +1,10 @@
 import type { Preview } from "@storybook/react";
 import { Global } from "@emotion/react";
 import { resetCss } from "@/styles/reset";
+import {
+  MINIMAL_VIEWPORTS,
+  INITIAL_VIEWPORTS,
+} from "@storybook/addon-viewport";
 
 export const decorators = [
   (Story) => (
@@ -11,11 +15,45 @@ export const decorators = [
   ),
 ];
 
+const customViewports = {
+  largePC: {
+    name: "Large PC",
+    styles: {
+      width: "1920px",
+      height: "1080px",
+    },
+    type: "desktop",
+  },
+  mediumPC: {
+    name: "Medium PC",
+    styles: {
+      width: "1366px",
+      height: "768px",
+    },
+    type: "desktop",
+  },
+  tabletLandscape: {
+    name: "Tablet Landscape",
+    styles: {
+      width: "1024px",
+      height: "768px",
+    },
+    type: "tablet",
+  },
+};
+
 const preview: Preview = {
   parameters: {
     docs: {
       story: {
         height: "700px",
+      },
+    },
+    viewport: {
+      viewports: {
+        ...customViewports,
+        ...MINIMAL_VIEWPORTS,
+        ...INITIAL_VIEWPORTS,
       },
     },
     controls: {
