436be704561ec33e779242348eef5dfe3672d484
thgml05 <aisohii@naver.com>
2025-05-05 11:54:37 +0900

    refactor: useExpiryDate 훅 수정
    - dateName 이름 dateUnit으로 변경
    - dateUnit 타입 'month' | 'year'로 수정
    - 월/년도 validate 함수 분리

--- hooks/src/lib/useExpiryDate.test.ts
+++ hooks/src/lib/useExpiryDate.test.ts
@@ -1,92 +1,92 @@
-import { renderHook, act } from "@testing-library/react";
-import useExpiryDate from "./useExpiryDate";
+import { renderHook, act } from '@testing-library/react';
+import useExpiryDate from './useExpiryDate';
 
-test("월 입력에 1에서 12 사이의 숫자를 입력하면 정상 작동한다. ", () => {
+test('월 입력에 1에서 12 사이의 숫자를 입력하면 정상 작동한다. ', () => {
   const { result } = renderHook(() => useExpiryDate());
 
   act(() => {
-    result.current.validate("11", "month");
+    result.current.updateExpiryDate('11', 'month');
   });
 
-  expect(result.current.error[0].errorMessage).toBe("");
+  expect(result.current.error[0].errorMessage).toBe('');
 });
 
-test("월 입력에 숫자가 아닌 값을 validate 하면 에러 메시지가 세팅된다", () => {
+test('월 입력에 숫자가 아닌 값을 validate 하면 에러 메시지가 세팅된다', () => {
   const { result } = renderHook(() => useExpiryDate());
 
   act(() => {
-    result.current.validate("ab", "month");
+    result.current.updateExpiryDate('ab', 'month');
   });
 
   expect(result.current.error[0].errorMessage).toBe(
-    "월은 숫자로 입력해 주세요."
+    '월은 숫자로 입력해 주세요.'
   );
 });
 
-test("월 입력에 2자리 이하의 숫자를 validate 하면 에러 메시지가 세팅된다", () => {
+test('월 입력에 2자리 이하의 숫자를 validate 하면 에러 메시지가 세팅된다', () => {
   const { result } = renderHook(() => useExpiryDate());
 
   act(() => {
-    result.current.validate("1", "month");
+    result.current.updateExpiryDate('1', 'month');
   });
 
   expect(result.current.error[0].errorMessage).toBe(
-    "월은 2자리로 입력해 주세요."
+    '월은 2자리로 입력해 주세요.'
   );
 });
 
-test("유효하지 않은 월을 validate 하면 에러 메시지가 세팅된다", () => {
+test('유효하지 않은 월을 validate 하면 에러 메시지가 세팅된다', () => {
   const { result } = renderHook(() => useExpiryDate());
 
   act(() => {
-    result.current.validate("13", "month");
+    result.current.updateExpiryDate('13', 'month');
   });
 
-  expect(result.current.error[0].errorMessage).toBe("유효하지 않은 월입니다.");
+  expect(result.current.error[0].errorMessage).toBe('유효하지 않은 월입니다.');
 });
 
-test("연도 입력에 25 이상의 숫자를 입력하면 정상 작동한다. ", () => {
+test('연도 입력에 25 이상의 숫자를 입력하면 정상 작동한다. ', () => {
   const { result } = renderHook(() => useExpiryDate());
 
   act(() => {
-    result.current.validate("25", "year");
+    result.current.updateExpiryDate('25', 'year');
   });
 
-  expect(result.current.error[1].errorMessage).toBe("");
+  expect(result.current.error[1].errorMessage).toBe('');
 });
 
-test("연도 입력에 숫자가 아닌 값을 validate 하면 에러 메시지가 세팅된다", () => {
+test('연도 입력에 숫자가 아닌 값을 validate 하면 에러 메시지가 세팅된다', () => {
   const { result } = renderHook(() => useExpiryDate());
 
   act(() => {
-    result.current.validate("ab", "year");
+    result.current.updateExpiryDate('ab', 'year');
   });
 
   expect(result.current.error[1].errorMessage).toBe(
-    "연도는 숫자로 입력해 주세요."
+    '연도는 숫자로 입력해 주세요.'
   );
 });
 
-test("연도 입력에 2자리 이하의 숫자를 validate 하면 에러 메시지가 세팅된다", () => {
+test('연도 입력에 2자리 이하의 숫자를 validate 하면 에러 메시지가 세팅된다', () => {
   const { result } = renderHook(() => useExpiryDate());
 
   act(() => {
-    result.current.validate("1", "year");
+    result.current.updateExpiryDate('1', 'year');
   });
 
   expect(result.current.error[1].errorMessage).toBe(
-    "연도는 2자리로 입력해 주세요."
+    '연도는 2자리로 입력해 주세요.'
   );
 });
 
-test("유효하지 않은 연도를 validate 하면 에러 메시지가 세팅된다", () => {
+test('유효하지 않은 연도를 validate 하면 에러 메시지가 세팅된다', () => {
   const { result } = renderHook(() => useExpiryDate());
 
   act(() => {
-    result.current.validate("24", "year");
+    result.current.updateExpiryDate('24', 'year');
   });
 
   expect(result.current.error[1].errorMessage).toBe(
-    "유효하지 않은 연도입니다."
+    '유효하지 않은 연도입니다.'
   );
 });
--- hooks/src/lib/useExpiryDate.ts
+++ hooks/src/lib/useExpiryDate.ts
@@ -7,16 +7,16 @@ const EXPIRYDATE_RULE = {
   MONTH_IS_NOT_A_NUMBER: '월은 숫자로 입력해 주세요.',
   INVALID_YEAR: '유효하지 않은 연도입니다.',
   INVALID_MONTH: '유효하지 않은 월입니다.',
-  DATE_MAX_LENGTH: 2,
-  DATE_MONTH_MIN: 1,
-  DATE_MONTH_MAX: 12,
-  DATE_YEAR_MIN: 25,
+  MAX_LENGTH: 2,
+  MONTH_MIN: 1,
+  MONTH_MAX: 12,
+  YEAR_MIN: 25,
 } as const;
 
 type ValitationResult = {
   date: dateType;
   error: errorType[];
-  validate: (value: string, dateName: string) => void;
+  updateExpiryDate: (value: string, dateName: 'month' | 'year') => void;
 };
 
 type dateType = {
@@ -29,20 +29,16 @@ type errorType = {
   errorMessage: string;
 };
 
-const initialError = [
-  {
-    isValidate: false,
-    errorMessage: '',
-  },
-  {
-    isValidate: false,
-    errorMessage: '',
-  },
-];
+const initialError = {
+  isValidate: false,
+  errorMessage: '',
+};
 
 export default function useExpiryDate(): ValitationResult {
   const [date, setDate] = useState<dateType>({ month: '', year: '' });
-  const [error, setError] = useState<errorType[]>(initialError);
+  const [error, setError] = useState<errorType[]>(
+    Array.from({ length: EXPIRYDATE_RULE.MAX_LENGTH }, () => initialError)
+  );
 
   const updateError = (index: number, isError: boolean, message: string) => {
     setError((prev) => {
@@ -54,58 +50,64 @@ export default function useExpiryDate(): ValitationResult {
     });
   };
 
-  const validate = (value: string, dateName: string) => {
-    if (dateName === 'month') {
-      if (value.length > EXPIRYDATE_RULE.DATE_MAX_LENGTH) return;
+  const updateExpiryDate = (value: string, dateUnit: 'month' | 'year') => {
+    if (value.length > EXPIRYDATE_RULE.MAX_LENGTH) return;
+
+    if (dateUnit === 'month') {
+      validateMonth(value);
+
       setDate((prev) => ({ ...prev, month: value }));
+    } else if (dateUnit === 'year') {
+      validateYear(value);
 
-      if (value === '') {
-        updateError(0, false, '');
-        return;
-      }
-
-      if (!/^\d*$/.test(value)) {
-        updateError(0, true, EXPIRYDATE_RULE.MONTH_IS_NOT_A_NUMBER);
-        return;
-      }
-      if (value.length < EXPIRYDATE_RULE.DATE_MAX_LENGTH) {
-        updateError(0, true, EXPIRYDATE_RULE.INVALID_MONTH_LENGTH_ERROR);
-        return;
-      }
-      if (
-        Number(value) < EXPIRYDATE_RULE.DATE_MONTH_MIN ||
-        Number(value) > EXPIRYDATE_RULE.DATE_MONTH_MAX
-      ) {
-        updateError(0, true, EXPIRYDATE_RULE.INVALID_MONTH);
-        return;
-      }
-      updateError(0, false, '');
-    }
-
-    if (dateName === 'year') {
-      if (value.length > EXPIRYDATE_RULE.DATE_MAX_LENGTH) return;
       setDate((prev) => ({ ...prev, year: value }));
-
-      if (value === '') {
-        updateError(1, false, '');
-        return;
-      }
-
-      if (!/^\d*$/.test(value)) {
-        updateError(1, true, EXPIRYDATE_RULE.YEAR_IS_NOT_A_NUMBER);
-        return;
-      }
-      if (value.length < EXPIRYDATE_RULE.DATE_MAX_LENGTH) {
-        updateError(1, true, EXPIRYDATE_RULE.INVALID_YEAR_LENGTH_ERROR);
-        return;
-      }
-      if (Number(value) < EXPIRYDATE_RULE.DATE_YEAR_MIN) {
-        updateError(1, true, EXPIRYDATE_RULE.INVALID_YEAR);
-        return;
-      }
-      updateError(1, false, '');
     }
   };
 
-  return { date, error, validate };
+  const validateMonth = (value: string) => {
+    if (value === '') {
+      updateError(0, false, '');
+      return;
+    }
+
+    if (!/^\d*$/.test(value)) {
+      updateError(0, true, EXPIRYDATE_RULE.MONTH_IS_NOT_A_NUMBER);
+      return;
+    }
+    if (value.length < EXPIRYDATE_RULE.MAX_LENGTH) {
+      updateError(0, true, EXPIRYDATE_RULE.INVALID_MONTH_LENGTH_ERROR);
+      return;
+    }
+    if (
+      Number(value) < EXPIRYDATE_RULE.MONTH_MIN ||
+      Number(value) > EXPIRYDATE_RULE.MONTH_MAX
+    ) {
+      updateError(0, true, EXPIRYDATE_RULE.INVALID_MONTH);
+      return;
+    }
+    updateError(0, false, '');
+  };
+
+  const validateYear = (value: string) => {
+    if (value === '') {
+      updateError(1, false, '');
+      return;
+    }
+
+    if (!/^\d*$/.test(value)) {
+      updateError(1, true, EXPIRYDATE_RULE.YEAR_IS_NOT_A_NUMBER);
+      return;
+    }
+    if (value.length < EXPIRYDATE_RULE.MAX_LENGTH) {
+      updateError(1, true, EXPIRYDATE_RULE.INVALID_YEAR_LENGTH_ERROR);
+      return;
+    }
+    if (Number(value) < EXPIRYDATE_RULE.YEAR_MIN) {
+      updateError(1, true, EXPIRYDATE_RULE.INVALID_YEAR);
+      return;
+    }
+    updateError(1, false, '');
+  };
+
+  return { date, error, updateExpiryDate };
 }
