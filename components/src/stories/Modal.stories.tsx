925a9261db22cbe44c1301908bc16aa4bdd16352
thgml05 <aisohii@naver.com>
2025-05-05 16:43:26 +0900

    feat: Modal 스토리북 'center'와 'bottom'일 때 수동 동작 테스트 코드 추가

--- components/src/stories/Modal.stories.tsx
+++ components/src/stories/Modal.stories.tsx
@@ -1,10 +1,11 @@
-import type { Meta, StoryObj } from "@storybook/react";
-import Modal from "../lib/Modal.tsx";
+import type { Meta, StoryObj } from '@storybook/react';
+import Modal from '../lib/Modal.tsx';
+import { useState } from 'react';
 
 const meta: Meta<typeof Modal> = {
-  title: "Components/Modal",
+  title: 'Components/Modal',
   component: Modal,
-  tags: ["autodocs"],
+  tags: ['autodocs'],
 };
 
 export default meta;
@@ -13,20 +14,70 @@ type Story = StoryObj<typeof Modal>;
 
 export const centerOpen: Story = {
   args: {
-    isModalOpen: true,
-    position: "center",
-    title: "제목",
-    children: "이것은 모달 내용입니다.",
+    isOpen: true,
+    position: 'center',
+    title: '제목',
+    children: '이것은 모달 내용입니다.',
     onClose: () => {},
   },
 };
 
 export const bottomOpen: Story = {
   args: {
-    isModalOpen: true,
-    position: "bottom",
-    title: "제목",
-    children: "이것은 모달 내용입니다.",
+    isOpen: true,
+    position: 'bottom',
+    title: '제목',
+    children: '이것은 모달 내용입니다.',
     onClose: () => {},
+  },
+};
+
+export const InteractiveModalCenter = {
+  render: () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const handleBackdropClick = (e: React.MouseEvent<HTMLDivElement>) => {
+      if (e.target === e.currentTarget) {
+        setIsOpen(false);
+      }
+    };
+    return (
+      <>
+        <button onClick={() => setIsOpen(true)}>Open Modal</button>
+        <Modal
+          isOpen={isOpen}
+          position="center"
+          title="제목"
+          onClose={() => setIsOpen(false)}
+          onBackdropClick={handleBackdropClick}
+        >
+          <p>이것은 모달 내용입니다.</p>
+        </Modal>
+      </>
+    );
+  },
+};
+
+export const InteractiveModalBottom = {
+  render: () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const handleBackdropClick = (e: React.MouseEvent<HTMLDivElement>) => {
+      if (e.target === e.currentTarget) {
+        setIsOpen(false);
+      }
+    };
+    return (
+      <>
+        <button onClick={() => setIsOpen(true)}>Open Modal</button>
+        <Modal
+          isOpen={isOpen}
+          position="bottom"
+          title="제목"
+          onClose={() => setIsOpen(false)}
+          onBackdropClick={handleBackdropClick}
+        >
+          <p>이것은 모달 내용입니다.</p>
+        </Modal>
+      </>
+    );
   },
 };
