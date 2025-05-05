0aef810b905929887201d1ca88eaf63e37566aee
hoyyChoi <hoyeon8165@gmail.com>
2025-05-05 19:33:43 +0900

    test: useCardNumber 훅 테스트 수정

--- hooks/src/lib/Hooks/useCardNumberValidation/useCardNumberValidation.test.ts
+++ hooks/src/lib/Hooks/useCardNumberValidation/useCardNumberValidation.test.ts
@@ -1,16 +1,47 @@
-import { renderHook, act } from '@testing-library/react';
-import useCardNumberValidation from '.';
+import { renderHook, act } from "@testing-library/react";
+import useCardNumberValidation from ".";
 
-describe('useCardNumberValidation', () => {
-  it('useCardNumberValidation의 초기 에러 상태가 반환된다.', () => {
+describe("useCardNumber", () => {
+  it("useCardNumber 초기 상태가 반환된다.", () => {
+    const initialState = {
+      first: "",
+      second: "",
+      third: "",
+      forth: "",
+    };
+    const { result } = renderHook(() => useCardNumberValidation());
+
+    expect(result.current.state).toEqual(initialState);
+  });
+
+  it("useCardNumber 상태를 변경할 수 있다.", () => {
+    const initialState = {
+      first: "1234",
+      second: "",
+      third: "",
+      forth: "",
+    };
+    const userInput = "1234";
+    const index = 0; // input 위치
+
+    const { result } = renderHook(() => useCardNumberValidation());
+
+    act(() => {
+      result.current.onChange(userInput, index);
+    });
+
+    expect(result.current.state.first).toEqual(initialState.first);
+  });
+
+  it("useCardNumber 초기 에러 상태가 반환된다.", () => {
     const initialErrors = [false, false, false, false];
     const { result } = renderHook(() => useCardNumberValidation());
 
     expect(result.current.errors).toEqual(initialErrors);
   });
 
-  it('숫자가 아닌 값이 들어오면 에러메시지를 반환한다.', () => {
-    const userInput = 'hi';
+  it("숫자가 아닌 값이 들어오면 에러메시지를 반환한다.", () => {
+    const userInput = "hi";
     const index = 0; // input 위치
     const { result } = renderHook(() => useCardNumberValidation());
 
@@ -18,11 +49,11 @@ describe('useCardNumberValidation', () => {
       result.current.validateInput(userInput, index);
     });
 
-    expect(result.current.errorMessage).toBe('숫자만 입력 가능합니다.');
+    expect(result.current.errorMessage).toBe("숫자만 입력 가능합니다.");
   });
 
-  it('현재 에러 상태에 에러가 없다면 noError가 true를 반환한다.', () => {
-    const userInput = '1243';
+  it("현재 에러 상태에 에러가 없다면 noError가 true를 반환한다.", () => {
+    const userInput = "1243";
     const index = 0; // input 위치
     const { result } = renderHook(() => useCardNumberValidation());
 
