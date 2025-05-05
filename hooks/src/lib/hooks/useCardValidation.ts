a1d6c44c3b7f74b93dffd5c469cd6e9b7604d334
spoyodevelop <kjy9505@gmail.com>
2025-05-05 23:00:37 +0900

    refactor: useCardValidation 함수에서 반환 값에 카드 포맷 추가 및 불필요한 인터페이스 제거

--- hooks/src/lib/hooks/useCardValidation.ts
+++ hooks/src/lib/hooks/useCardValidation.ts
@@ -1,26 +1,25 @@
+// hooks/useCardValidation.ts
 import useCardNumber from "./useCardNumber";
+import useStrictCardNumber from "./useStrictCardNumber";
 import useCVCNumber from "./useCVCNumber";
 import useExpiryDateNumber from "./useExpiryDateNumber";
 import usePasswordNumber from "./usePasswordNumber";
 import useCardNetwork from "./useCardNetwork";
-import useStrictCardNumber from "./useStrictCardNumber";
+import useCardFormat from "./useCardFormat";
+import type { UseCardValidationOptions } from "./useCardFormat";
+export default function useCardValidation(
+  options: UseCardValidationOptions = {}
+) {
+  const { format: formatOptions = {} } = options;
+  const { splitter = " " } = formatOptions;
 
-interface UseCardValidationReturn {
-  card: ReturnType<typeof useCardNumber>;
-  cvc: ReturnType<typeof useCVCNumber>;
-  expiry: ReturnType<typeof useExpiryDateNumber>;
-  password: ReturnType<typeof usePasswordNumber>;
-  network: ReturnType<typeof useCardNetwork>;
-  strictCard: ReturnType<typeof useStrictCardNumber>;
-}
-
-export default function useCardValidation(): UseCardValidationReturn {
   const card = useCardNumber();
   const strictCard = useStrictCardNumber();
   const cvc = useCVCNumber();
   const expiry = useExpiryDateNumber();
   const password = usePasswordNumber();
   const network = useCardNetwork();
+  const format = useCardFormat(splitter);
 
-  return { card, cvc, expiry, password, network, strictCard };
+  return { card, cvc, expiry, password, network, strictCard, format };
 }
