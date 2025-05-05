3df47922b31b9b0049929d3437300eab81b27d73
spoyodevelop <kjy9505@gmail.com>
2025-05-05 22:00:00 +0900

    fix: 카드 번호 길이 상수에 DEFAULT 브랜드 추가

--- hooks/src/lib/validator/constants/card-number-length.ts
+++ hooks/src/lib/validator/constants/card-number-length.ts
@@ -1,10 +1,10 @@
 import { CardBrand } from "../../constants/CardBrand";
 
-type KnownBrand = Exclude<CardBrand, "DEFAULT">;
-export const CARD_NUMBER_LENGTH: Record<KnownBrand, number[]> = {
+export const CARD_NUMBER_LENGTH: Record<CardBrand, number[]> = {
   VISA: [13, 16, 19],
   MASTERCARD: [16],
   AMEX: [15],
   DINERS: [14, 15, 16, 17, 18, 19],
   UNIONPAY: [16, 17, 18, 19],
+  DEFAULT: [16],
 };
