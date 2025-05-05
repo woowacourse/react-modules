4a7fd22d02d0d2b6cc118e25fb3f4ede9093a1aa
hoyyChoi <hoyeon8165@gmail.com>
2025-05-05 16:50:40 +0900

    refactor: (모달) svg 아이콘 컴포넌트 분리

---
+++ components/src/lib/IconClose.tsx
@@ -0,0 +1,10 @@
+export default function IconClose(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg width="15" height="14" viewBox="0 0 15 14" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+      <path
+        d="M14.4922 1.41L13.0822 0L7.49219 5.59L1.90219 0L0.492188 1.41L6.08219 7L0.492188 12.59L1.90219 14L7.49219 8.41L13.0822 14L14.4922 12.59L8.90219 7L14.4922 1.41Z"
+        fill="currentColor"
+      />
+    </svg>
+  );
+}
--- components/src/lib/Modal.tsx
+++ components/src/lib/Modal.tsx
@@ -1,6 +1,7 @@
 import React, { createContext, useContext } from "react";
 import { css } from "@emotion/react";
 import useKeyEscClose from "./useKeyEscClose";
+import IconClose from "./IconClose";
 
 export interface ChildrenProps {
   /** 자식 요소 (JSX.Element 또는 문자열 등) */
@@ -54,16 +55,7 @@ Modal.Header = ({ closeButton = false, children }: ModalHeaderProps) => {
   return (
     <div css={ModalHeaderStyle}>
       <span>{children}</span>
-      {closeButton && (
-        <div css={ModalCloseStyle} onClick={onHide}>
-          <svg width="15" height="14" viewBox="0 0 15 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path
-              d="M14.4922 1.41L13.0822 0L7.49219 5.59L1.90219 0L0.492188 1.41L6.08219 7L0.492188 12.59L1.90219 14L7.49219 8.41L13.0822 14L14.4922 12.59L8.90219 7L14.4922 1.41Z"
-              fill="black"
-            />
-          </svg>
-        </div>
-      )}
+      {closeButton && <IconClose css={ModalCloseStyle} onClick={onHide} />}
     </div>
   );
 };
