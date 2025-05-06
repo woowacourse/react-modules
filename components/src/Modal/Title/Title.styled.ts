749818f7fe6cc8e69d57d16b58dd3caa7e0bad4c
H0ngJu <hongju4385@gmail.com>
2025-05-06 15:03:37 +0900

    refactor(Title): Title 컴포넌트 분리 및 className, styled 속성을 받을 수 있도록 수정

---
+++ components/src/Modal/Title/Title.styled.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const ModalTitle = styled.div`
+  font-size: 24px;
+  font-weight: 900;
+  position: absolute;
+  top: 20px;
+  left: 20px;
+  text-align: start;
+`;
