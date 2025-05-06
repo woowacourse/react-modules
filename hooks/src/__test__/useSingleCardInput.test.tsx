52a6ff45044bdc1824189d379d2d97aafcad1364
dlsxjzld <sinjuk1@naver.com>
2025-05-07 03:00:57 +0900

    test: useSingleCardInput 훅의 테스트에서 PassWord의 유효한 입력 길이에 대한 경계값 테스트 추가

--- hooks/src/__test__/useSingleCardInput.test.tsx
+++ hooks/src/__test__/useSingleCardInput.test.tsx
@@ -29,7 +29,7 @@ describe('카드 CVC 검증 테스트입니다.', () => {
     expect(result.current.errorMessage).toBe('숫자(0~9)만 입력 가능합니다.');
   });
 
-  test('사용자가 입력한 value값의 길이가 validLength(3)보다 작다면 errorMessage를 반환준다.', () => {
+  test('사용자가 입력한 value값의 길이가 validLength(3)보다 작다면 errorMessage를 반환한다.', () => {
     const { result } = renderHook(() => useSingleCardInput(3));
 
     const mockEvent = {
@@ -49,7 +49,7 @@ describe('카드 PassWord 검증 테스트입니다.', () => {
     const { result } = renderHook(() => useSingleCardInput(2));
 
     const mockEvent = {
-      target: { value: '1234' },
+      target: { value: '12' },
     } as React.ChangeEvent<HTMLInputElement>;
 
     act(() => {
@@ -72,11 +72,25 @@ describe('카드 PassWord 검증 테스트입니다.', () => {
     expect(result.current.errorMessage).toBe('숫자(0~9)만 입력 가능합니다.');
   });
 
-  test('사용자가 입력한 value값의 길이가 validLength(2)보다 작다면 errorMessage를 반환준다.', () => {
+  test('사용자가 입력한 value값의 길이가 validLength(2)보다 작다면 errorMessage를 반환한다.', () => {
     const { result } = renderHook(() => useSingleCardInput(2));
 
     const mockEvent = {
       target: { value: '1' },
+    } as React.ChangeEvent<HTMLInputElement>;
+
+    act(() => {
+      result.current.handleSingleCardInputChange(mockEvent);
+    });
+
+    expect(result.current.errorMessage).toBe('숫자 2자리를 입력해주세요.');
+  });
+
+  test('사용자가 입력한 value값의 길이가 validLength(2)보다 크다면 errorMessage를 반환한다.', () => {
+    const { result } = renderHook(() => useSingleCardInput(2));
+
+    const mockEvent = {
+      target: { value: '123' },
     } as React.ChangeEvent<HTMLInputElement>;
 
     act(() => {
