d4f6cadfc8108d68b23e9adbe4766947494b0320
spoyodevelop <kjy9505@gmail.com>
2025-05-06 17:01:17 +0900

    feat: 요구 사항에 있는 card length 그대로를 만족하는 CLIENT_CARD_NUMBER_LENGTH 추가

--- hooks/src/lib/validator/constants/card-number-length.ts
+++ hooks/src/lib/validator/constants/card-number-length.ts
@@ -8,3 +8,12 @@ export const CARD_NUMBER_LENGTH: Record<CardBrand, number[]> = {
   UNIONPAY: [16, 17, 18, 19],
   DEFAULT: [16],
 };
+
+export const CLIENT_CARD_NUMBER_LENGTH: Record<CardBrand, number[]> = {
+  VISA: [16],
+  MASTERCARD: [16],
+  AMEX: [15],
+  DINERS: [14],
+  UNIONPAY: [16],
+  DEFAULT: [16],
+};
