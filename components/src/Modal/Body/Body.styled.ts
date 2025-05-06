db502a0514fd8bdcf86afd8a3a5e780a1892b7e6
H0ngJu <hongju4385@gmail.com>
2025-05-06 16:58:08 +0900

    refactor(Body): 긴 내용의 경우 스크롤 가능하도록 수정

--- components/src/Modal/Body/Body.styled.ts
+++ components/src/Modal/Body/Body.styled.ts
@@ -2,8 +2,13 @@ import styled from "styled-components";
 
 const ModalBody = styled.div`
   display: flex;
+  flex-direction: column;
   margin-top: 56px;
   height: 100%;
+  max-width: 800px;
+  max-height: 300px;
+  overflow-y: auto;
+  overflow-x: hidden;
 `;
 
 export { ModalBody };
