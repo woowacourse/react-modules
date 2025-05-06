8616f977553091a7666753b586850b90f636aed2
H0ngJu <hongju4385@gmail.com>
2025-05-06 16:58:03 +0900

    refactor(Frame): 하단 모달의 border-radius 조절

--- components/src/Modal/Frame/Frame.styled.ts
+++ components/src/Modal/Frame/Frame.styled.ts
@@ -10,7 +10,8 @@ export const ModalFrame = styled.div<PositionProps>`
 
   z-index: 500;
   background-color: white;
-  border-radius: 8px;
+  border-radius: ${({ $position }) =>
+    $position === "bottom" ? "8px 8px 0 0 " : "8px"};
   padding: 20px;
   box-sizing: border-box;
   display: flex;
