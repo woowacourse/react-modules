c2e1eb92a896ec89beaa107c0c2c509a68fc3244
guesung <gueit214@naver.com>
2025-05-07 13:46:05 +0900

    chore: 기본값으로 모바일 사이즈 제거

--- pesu-components/.storybook/preview.ts
+++ pesu-components/.storybook/preview.ts
@@ -9,6 +9,7 @@ const preview: Preview = {
         date: /Date$/i,
       },
     },
+    layout: 'fullscreen',
     backgrounds: {
       values: [
         { name: 'Dark', value: '#333' },
@@ -19,7 +20,6 @@ const preview: Preview = {
     },
     viewport: {
       viewports: INITIAL_VIEWPORTS,
-      defaultViewport: 'iphone12',
     },
   },
 };
