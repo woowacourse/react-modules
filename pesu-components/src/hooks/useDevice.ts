7b41d465f707a4f60b627519546c400d581e1132
guesung <gueit214@naver.com>
2025-05-07 13:52:03 +0900

    feat: 디바이스 식별하는 훅

---
+++ pesu-components/src/hooks/useDevice.ts
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react';
+
+const DEVICES = ['mobile', 'tablet', 'desktop'] as const;
+
+export type Device = (typeof DEVICES)[number];
+
+export default function useDevice() {
+  const [device, setDevice] = useState<Device>('desktop');
+
+  useEffect(() => {
+    const handleResize = () => {
+      console.log(window.innerWidth);
+      if (window.innerWidth < 768) {
+        setDevice('mobile');
+      } else if (window.innerWidth >= 768 && window.innerWidth < 1024) {
+        setDevice('tablet');
+      } else {
+        setDevice('desktop');
+      }
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  return device;
+}
