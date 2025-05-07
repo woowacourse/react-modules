92153e6efd8c1ff0ec540db5b1d40d04de4d8463
minji2219 <aabb0219@naver.com>
2025-05-07 23:40:35 +0900

    test: 훅 변경 사항에 맞춰 테스트 코드 변경

--- hooks/src/__test__/useExpirationDateValidation.test.ts
+++ hooks/src/__test__/useExpirationDateValidation.test.ts
@@ -1,72 +1,56 @@
-import { renderHook } from "@testing-library/react";
+import {renderHook} from '@testing-library/react';
 import {
   ERROR_MESSAGE,
   defaultValidationValue,
-} from "../lib/constants/validation";
-import useExpirationDateValidation from "../lib/hooks/useExpirationDateValidation";
+} from '../lib/constants/validation';
+import useExpirationDate from '../lib/hooks/useExpirationDate';
+import {act} from 'react';
 
 const defaultExpirationDateValidationValue = {
   month: defaultValidationValue,
   year: defaultValidationValue,
 };
 
-describe("useExpirationDateValidation", () => {
-  it("사용자 입력 값이 없는 경우 기본 유효성 검사 결과를 반환한다.", () => {
-    // given
+describe('useExpirationDateValidation', () => {
+  it('유효 기간이 올바른 경우 에러가 발생하지 않는다.', () => {
     // when
-    const { result } = renderHook(() => useExpirationDateValidation());
+    const {result} = renderHook(() => useExpirationDate());
+
+    act(() => {
+      result.current.onChange('month', '04');
+      result.current.onChange('year', '25');
+    });
 
     // then
-    expect(result.current).toEqual(defaultExpirationDateValidationValue);
-  });
-
-  it("유효 기간이 올바른 경우 에러가 발생하지 않는다.", () => {
-    // given
-    const initialValue = {
-      month: "04",
-      year: "25",
-    };
-
-    // when
-    const { result } = renderHook(() =>
-      useExpirationDateValidation(initialValue)
+    expect(result.current.expirationDateValidationResult).toEqual(
+      defaultExpirationDateValidationValue
     );
-
-    // then
-    expect(result.current).toEqual(defaultExpirationDateValidationValue);
   });
 
-  it("유효 기간이 숫자가 아닌 경우 에러가 발생한다.", () => {
+  it('유효 기간이 숫자가 아닌 경우 에러가 발생한다.', () => {
     // given
-    const initialValue = {
-      month: "ㄱ",
-      year: "ㄴ",
-    };
-
     const expectedErrorResult = {
       isError: true,
       errorMessage: ERROR_MESSAGE.INVALID_NUMBER,
     };
 
     // when
-    const { result } = renderHook(() =>
-      useExpirationDateValidation(initialValue)
-    );
+    const {result} = renderHook(() => useExpirationDate());
+    act(() => {
+      result.current.onChange('month', 'ㄱ');
+      result.current.onChange('year', 'ㄴ');
+    });
 
     // then
-    expect(result.current).toEqual({
+    expect(result.current.expirationDateValidationResult).toEqual({
       month: expectedErrorResult,
       year: expectedErrorResult,
     });
   });
 
-  it("유효 기간의 각 섹션의 자릿수가 2가 아닌 경우 에러가 발생한다.", () => {
+  it('유효 기간의 각 섹션의 자릿수가 2가 아닌 경우 에러가 발생한다.', () => {
     // given
     const MAX_LENGTH = 2;
-    const initialValue = {
-      month: "1",
-      year: "2",
-    };
 
     const expectedErrorResult = {
       isError: true,
@@ -74,32 +58,32 @@ describe("useExpirationDateValidation", () => {
     };
 
     // when
-    const { result } = renderHook(() =>
-      useExpirationDateValidation(initialValue)
-    );
+    const {result} = renderHook(() => useExpirationDate());
+
+    act(() => {
+      result.current.onChange('month', '1');
+      result.current.onChange('year', '2');
+    });
 
     // then
-    expect(result.current).toEqual({
+    expect(result.current.expirationDateValidationResult).toEqual({
       month: expectedErrorResult,
       year: expectedErrorResult,
     });
   });
 
-  it.each(["00", "13"])(
+  it.each(['00', '13'])(
     "월 값이 '%s'인 경우: 월의 범위(01~12)를 벗어나면 에러가 발생한다.",
     (monthValue: string) => {
-      // given
-      const initialValue = {
-        month: monthValue,
-      };
-
       // when
-      const { result } = renderHook(() =>
-        useExpirationDateValidation(initialValue)
-      );
+      const {result} = renderHook(() => useExpirationDate());
+
+      act(() => {
+        result.current.onChange('month', monthValue);
+      });
 
       // then
-      expect(result.current).toEqual({
+      expect(result.current.expirationDateValidationResult).toEqual({
         ...defaultExpirationDateValidationValue,
         month: {
           isError: true,
@@ -109,21 +93,19 @@ describe("useExpirationDateValidation", () => {
     }
   );
 
-  it("연도가 현재 연도보다 작은 경우 에러가 발생한다.", () => {
+  it('연도가 현재 연도보다 작은 경우 에러가 발생한다.', () => {
     // given
     const currentYear = new Date().getFullYear() % 100;
 
-    const initialValue = {
-      year: String(currentYear - 1),
-    };
-
     // when
-    const { result } = renderHook(() =>
-      useExpirationDateValidation(initialValue)
-    );
+    const {result} = renderHook(() => useExpirationDate());
+
+    act(() => {
+      result.current.onChange('year', String(currentYear - 1));
+    });
 
     // then
-    expect(result.current).toEqual({
+    expect(result.current.expirationDateValidationResult).toEqual({
       ...defaultExpirationDateValidationValue,
       year: {
         isError: true,
