b42bb453915c013500216b11c73f9ffb4ce75e65
Jeongeun Lee <leeju1009@gmail.com>
2025-05-07 19:32:17 +0900

    test: prompt 모달 테스트 추가

--- components/src/modal/Modal.stories.tsx
+++ components/src/modal/Modal.stories.tsx
@@ -125,8 +125,48 @@ export const ShowAllButtons: Story = {
   },
 };
 
+export const Prompt: Story = {
+  args: {
+    open: true,
+    onClose: () => {},
+  },
+
+  render: function App(args) {
+    const [modalOpen, setModalOpen] = useState(false);
+
+    const onClose = () => {
+      setModalOpen(false);
+    };
+
+    return (
+      <>
+        <button onClick={() => setModalOpen(true)}>열기</button>
+        <Modal.Container {...args} onClose={onClose} open={modalOpen}>
+          <Modal.Title>쿠폰 번호를 입력해 주세요.</Modal.Title>
+          <Modal.Input />
+          <PromptButtonWrapper>
+            <Modal.SecondaryButton label="취소" onClick={onClose} />
+            <Modal.PrimaryButton
+              label="확인"
+              onClick={() => alert('확인 클릭')}
+            />
+          </PromptButtonWrapper>
+        </Modal.Container>
+      </>
+    );
+  },
+};
+
 const ButtonWrapper = styled.div`
   display: flex;
   flex-direction: column;
+  gap: 12px;
+`;
+
+const PromptButtonWrapper = styled.div`
+  width: 50%;
+  margin-left: auto;
+
+  display: flex;
   gap: 12px;
 `;
