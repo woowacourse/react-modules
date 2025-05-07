e42786404c827ba1d584a2d2befb6d429f9ca7f9
minji2219 <aabb0219@naver.com>
2025-05-07 23:44:47 +0900

    test: 훅 변경 사항에 맞춰 테스트 코드 변경

--- hooks/src/__test__/useCvcValidation.test.ts
+++ hooks/src/__test__/useCvcValidation.test.ts
@@ -1,46 +1,59 @@
-import { renderHook } from "@testing-library/react";
+import {renderHook} from '@testing-library/react';
 import {
   ERROR_MESSAGE,
   defaultValidationValue,
-} from "../lib/constants/validation";
-import useCvcValidation from "../lib/hooks/useCvcValidation";
+} from '../lib/constants/validation';
+import useCvc from '../lib/hooks/useCvc';
+import {act} from 'react';
 
-describe("useCvcValidation", () => {
-  it("CVC번호가 올바른 경우 에러가 발생하지 않는다.", () => {
+describe('useCvcValidation', () => {
+  it('CVC번호가 올바른 경우 에러가 발생하지 않는다.', () => {
     // given
-    const initialValue = "123";
+    const value = '123';
 
     // when
-    const { result } = renderHook(() => useCvcValidation(initialValue));
+    const {result} = renderHook(() => useCvc());
+
+    act(() => {
+      result.current.onChange(value);
+    });
 
     // then
-    expect(result.current).toEqual(defaultValidationValue);
+    expect(result.current.cvcValidationResult).toEqual(defaultValidationValue);
   });
 
-  it("CVC번호가 숫자가 아닌 경우 에러가 발생한다.", () => {
+  it('CVC번호가 숫자가 아닌 경우 에러가 발생한다.', () => {
     // given
-    const initialValue = "ㄱ";
+    const value = 'ㄱ';
 
     // when
-    const { result } = renderHook(() => useCvcValidation(initialValue));
+    const {result} = renderHook(() => useCvc());
+
+    act(() => {
+      result.current.onChange(value);
+    });
 
     // then
-    expect(result.current).toEqual({
+    expect(result.current.cvcValidationResult).toEqual({
       isError: true,
       errorMessage: ERROR_MESSAGE.INVALID_NUMBER,
     });
   });
 
-  it("CVC번호의 자릿수가 3이 아닌 경우 에러가 발생한다.", () => {
+  it('CVC번호의 자릿수가 3이 아닌 경우 에러가 발생한다.', () => {
     // given
     const MAX_LENGTH = 3;
-    const initialValue = "12";
+    const value = '12';
 
     // when
-    const { result } = renderHook(() => useCvcValidation(initialValue));
+    const {result} = renderHook(() => useCvc());
+
+    act(() => {
+      result.current.onChange(value);
+    });
 
     // then
-    expect(result.current).toEqual({
+    expect(result.current.cvcValidationResult).toEqual({
       isError: true,
       errorMessage: `${MAX_LENGTH}${ERROR_MESSAGE.INVALID_LENGTH}`,
     });
