88e35e6f0d73647e4738d34cc56b869562df6c97
spoyodevelop <kjy9505@gmail.com>
2025-05-07 18:18:21 +0900

    feat: ModalContext에 useModalContext 훅 추가 및 dialogType 프로퍼티 수정

--- components/src/lib/Components/Modal/ModalContext.tsx
+++ components/src/lib/Components/Modal/ModalContext.tsx
@@ -1,8 +1,17 @@
-import { createContext } from "react";
+import { createContext, useContext } from "react";
 
 export interface ModalContextType {
   onClose: () => void;
   position: "bottom" | "center";
+  dialogType: "alert" | "confirm" | "prompt" | "default";
 }
 
 export const ModalContext = createContext<ModalContextType | null>(null);
+
+export const useModalContext = () => {
+  const context = useContext(ModalContext);
+  if (!context) {
+    throw new Error("useModalContext must be used within a ModalProvider");
+  }
+  return context;
+};
