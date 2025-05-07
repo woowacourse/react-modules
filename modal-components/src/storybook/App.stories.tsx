769c290a5b70cbaff339eed6deb3218620853d0d
dev-dino22 <wlsskdud02@naver.com>
2025-05-07 21:52:35 +0900

    feat: 패키지 버전 업데이트 및 의존성 추가, 스타일 개선

--- modal-components/src/storybook/App.stories.tsx
+++ modal-components/src/storybook/App.stories.tsx
@@ -1,8 +1,8 @@
-import type { Meta, StoryObj } from '@storybook/react';
-import App from '../App';
+import type { Meta, StoryObj } from "@storybook/react";
+import App from "../App";
 
 const meta = {
-  title: 'App',
+  title: "App",
   component: App,
 } satisfies Meta<typeof App>;
 
@@ -12,25 +12,25 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
   args: {
-    modalType: 'center',
-    closeType: 'top',
-    titleText: 'text',
-    children: 'children',
+    modalPosition: "center",
+    closeType: "top",
+    titleText: "text",
+    children: "children",
   },
 };
 export const BottomTypeA: Story = {
   args: {
-    modalType: 'bottom',
-    closeType: 'top',
-    titleText: 'text',
-    children: 'children',
+    modalPosition: "bottom",
+    closeType: "top",
+    titleText: "text",
+    children: "children",
   },
 };
 export const BottomTypeB: Story = {
   args: {
-    modalType: 'bottom',
-    closeType: 'bottom',
-    titleText: 'text',
-    children: 'children',
+    modalPosition: "bottom",
+    closeType: "bottom",
+    titleText: "text",
+    children: "children",
   },
 };
