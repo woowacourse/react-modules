6943846976b21a0397456a1251fc2956dbf90c75
eunwoo-levi <eunwoo1341@gmail.com>
2025-05-07 20:29:13 +0900

    refactor: matchCardBrand 유틸함수 로직 수정

--- hooks/src/lib/utils/matchCardBrand.ts
+++ hooks/src/lib/utils/matchCardBrand.ts
@@ -1,7 +1,7 @@
 import { CardBrandType } from '../type/card';
 
-export const matchCardBrand = (cardNumber: string): CardBrandType => {
-  const twoDigits = cardNumber.slice(0, 2);
+export const matchCardBrand = (input1: string, input2: string): CardBrandType => {
+  const twoDigits = input1.slice(0, 2);
   if (twoDigits[0] === '4') {
     return 'Visa';
   } else if (Number(twoDigits) >= 51 && Number(twoDigits) <= 55) {
@@ -11,10 +11,11 @@ export const matchCardBrand = (cardNumber: string): CardBrandType => {
   } else if (twoDigits === '34' || twoDigits === '37') {
     return 'AMEX';
   } else if (twoDigits === '62') {
-    const threeDigits = Number(cardNumber.slice(0, 3));
-    const fourDigits = Number(cardNumber.slice(0, 4));
+    const sixDigits = Number(input1 + input2.slice(0, 2));
+    const threeDigits = Number(input1.slice(0, 3));
+    const fourDigits = Number(input1.slice(0, 4));
     if (
-      (Number(cardNumber) >= 622126 && Number(cardNumber) <= 622925) ||
+      (sixDigits >= 622126 && sixDigits <= 622925) ||
       (threeDigits >= 624 && threeDigits <= 626) ||
       (fourDigits >= 6282 && fourDigits <= 6288)
     ) {
