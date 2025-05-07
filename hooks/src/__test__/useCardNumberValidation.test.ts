cec584b4234fc71533d08aa2edf5c11bc2fb2284
minji2219 <aabb0219@naver.com>
2025-05-07 23:35:14 +0900

    test: 훅 변경 사항에 맞춰 테스트 코드 변경

--- hooks/src/__test__/useCardNumberValidation.test.ts
+++ hooks/src/__test__/useCardNumberValidation.test.ts
@@ -1,9 +1,10 @@
-import { renderHook } from "@testing-library/react";
+import {renderHook} from '@testing-library/react';
 import {
   ERROR_MESSAGE,
   defaultValidationValue,
-} from "../lib/constants/validation";
-import useCardNumberValidation from "../lib/hooks/useCardNumberValidation";
+} from '../lib/constants/validation';
+import useCardNumber from '../lib/hooks/useCardNumber';
+import {act} from 'react';
 
 const defaultCardNumberValidationValue = {
   first: defaultValidationValue,
@@ -12,75 +13,49 @@ const defaultCardNumberValidationValue = {
   fourth: defaultValidationValue,
 };
 
-describe("useCardNumberValidation", () => {
-  it("사용자 입력 값이 없는 경우 기본 유효성 검사 결과를 반환한다.", () => {
-    // given
+describe('useCardNumberValidation', () => {
+  it('카드 번호가 올바른 경우 에러가 발생하지 않는다.', () => {
     // when
-    const { result } = renderHook(() => useCardNumberValidation());
+    const {result} = renderHook(() => useCardNumber());
+
+    act(() => {
+      result.current.onChange('first', '1111');
+    });
 
     // then
-    expect(result.current).toEqual(defaultCardNumberValidationValue);
+    expect(result.current.cardNumberValidationResult).toEqual(
+      defaultCardNumberValidationValue
+    );
   });
 
-  it("카드 번호가 올바른 경우 에러가 발생하지 않는다.", () => {
-    // given
-    const initialValue = {
-      first: "1111",
-      second: "2222",
-      third: "3333",
-      fourth: "4444",
-    };
-
+  it('카드 번호가 숫자가 아닌 경우 에러가 발생한다.', () => {
     // when
-    const { result } = renderHook(() => useCardNumberValidation(initialValue));
+    const {result} = renderHook(() => useCardNumber());
+
+    act(() => {
+      result.current.onChange('first', 'ㄱ');
+    });
 
     // then
-    expect(result.current).toEqual(defaultCardNumberValidationValue);
-  });
-
-  it("사용자는 카드 번호 4자리 중 선택적으로 유효성 검사를 수행할 수 있다.", () => {
-    // given
-    const initialValue = {
-      first: "",
-      second: "2222",
-      third: "3333",
-    };
-
-    // when
-    const { result } = renderHook(() => useCardNumberValidation(initialValue));
-
-    // then
-    expect(result.current).toEqual(defaultCardNumberValidationValue);
-  });
-
-  it("카드 번호가 숫자가 아닌 경우 에러가 발생한다.", () => {
-    // given
-    const initialValue = {
-      first: "ㄱ",
-    };
-
-    // when
-    const { result } = renderHook(() => useCardNumberValidation(initialValue));
-
-    // then
-    expect(result.current).toEqual({
+    expect(result.current.cardNumberValidationResult).toEqual({
       ...defaultCardNumberValidationValue,
-      first: { isError: true, errorMessage: ERROR_MESSAGE.INVALID_NUMBER },
+      first: {isError: true, errorMessage: ERROR_MESSAGE.INVALID_NUMBER},
     });
   });
 
-  it("카드 번호의 각 섹션의 자릿수가 4가 아닌 경우 에러가 발생한다.", () => {
+  it('카드 번호의 각 섹션의 자릿수가 4가 아닌 경우 에러가 발생한다.', () => {
     // given
     const MAX_LENGTH = 4;
-    const initialValue = {
-      first: "123",
-    };
 
     // when
-    const { result } = renderHook(() => useCardNumberValidation(initialValue));
+    const {result} = renderHook(() => useCardNumber());
+
+    act(() => {
+      result.current.onChange('first', '123');
+    });
 
     // then
-    expect(result.current).toEqual({
+    expect(result.current.cardNumberValidationResult).toEqual({
       ...defaultCardNumberValidationValue,
       first: {
         isError: true,
@@ -89,19 +64,20 @@ describe("useCardNumberValidation", () => {
     });
   });
 
-  it("여러 에러가 발생하는 경우 각 섹션마다 에러가 발생한다.", () => {
+  it('여러 에러가 발생하는 경우 각 섹션마다 에러가 발생한다.', () => {
     // given
     const MAX_LENGTH = 4;
-    const initialValue = {
-      first: "123",
-      second: "ㄱ",
-    };
 
     // when
-    const { result } = renderHook(() => useCardNumberValidation(initialValue));
+    const {result} = renderHook(() => useCardNumber());
+
+    act(() => {
+      result.current.onChange('first', '123');
+      result.current.onChange('second', 'ㄱㄱ');
+    });
 
     // then
-    expect(result.current).toEqual({
+    expect(result.current.cardNumberValidationResult).toEqual({
       ...defaultCardNumberValidationValue,
       first: {
         isError: true,
