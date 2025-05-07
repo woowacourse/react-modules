5c0a12c0b27a3fe3101bba5579c22d5d224af1ab
minji2219 <aabb0219@naver.com>
2025-05-07 23:43:20 +0900

    test: 훅 변경 사항에 맞춰 테스트 변경

--- hooks/src/__test__/usePasswordValidation.test.ts
+++ hooks/src/__test__/usePasswordValidation.test.ts
@@ -1,46 +1,61 @@
-import { renderHook } from "@testing-library/react";
+import {renderHook} from '@testing-library/react';
 import {
   ERROR_MESSAGE,
   defaultValidationValue,
-} from "../lib/constants/validation";
-import usePasswordValidation from "../lib/hooks/usePasswordValidation";
+} from '../lib/constants/validation';
+import usePassword from '../lib/hooks/usePassword';
+import {act} from 'react';
 
-describe("usePasswordValidation", () => {
-  it("비밀번호가 올바른 경우 에러가 발생하지 않는다.", () => {
+describe('usePasswordValidation', () => {
+  it('비밀번호가 올바른 경우 에러가 발생하지 않는다.', () => {
     // given
-    const initialValue = "12";
+    const value = '12';
 
     // when
-    const { result } = renderHook(() => usePasswordValidation(initialValue));
+    const {result} = renderHook(() => usePassword());
+
+    act(() => {
+      result.current.onChange(value);
+    });
 
     // then
-    expect(result.current).toEqual(defaultValidationValue);
+    expect(result.current.passwordValidationResult).toEqual(
+      defaultValidationValue
+    );
   });
 
-  it("비밀번호가 숫자가 아닌 경우 에러가 발생한다.", () => {
+  it('비밀번호가 숫자가 아닌 경우 에러가 발생한다.', () => {
     // given
-    const initialValue = "ㄱ";
+    const value = 'ㄱ';
 
     // when
-    const { result } = renderHook(() => usePasswordValidation(initialValue));
+    const {result} = renderHook(() => usePassword());
+
+    act(() => {
+      result.current.onChange(value);
+    });
 
     // then
-    expect(result.current).toEqual({
+    expect(result.current.passwordValidationResult).toEqual({
       isError: true,
       errorMessage: ERROR_MESSAGE.INVALID_NUMBER,
     });
   });
 
-  it("비밀번호의 자릿수가 2가 아닌 경우 에러가 발생한다.", () => {
+  it('비밀번호의 자릿수가 2가 아닌 경우 에러가 발생한다.', () => {
     // given
     const MAX_LENGTH = 2;
-    const initialValue = "1";
+    const value = '1';
 
     // when
-    const { result } = renderHook(() => usePasswordValidation(initialValue));
+    const {result} = renderHook(() => usePassword());
+
+    act(() => {
+      result.current.onChange(value);
+    });
 
     // then
-    expect(result.current).toEqual({
+    expect(result.current.passwordValidationResult).toEqual({
       isError: true,
       errorMessage: `${MAX_LENGTH}${ERROR_MESSAGE.INVALID_LENGTH}`,
     });
