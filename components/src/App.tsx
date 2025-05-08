8e80ee416fde71585ce216ee3225a55c2f27aea8
Minje Sung <minje813@dgu.ac.kr>
2025-05-08 20:20:05 +0900

    feat: App에 모달 열기 데모 버튼 추가

--- components/src/App.tsx
+++ components/src/App.tsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import { useState } from 'react';
 import './App.css';
+import Modal from './lib/Modal';
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <>
       <h1>Component Modules</h1>
+      <button onClick={() => setIsOpen(true)}>모달열기</button>
+      <Modal isOpen={isOpen} />
     </>
   );
 }
