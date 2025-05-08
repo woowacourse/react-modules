0c98f073e684d77002f22d47f11bf39226a2ab9c
Oliy <eunsoa0310@gmail.com>
2025-05-08 09:39:11 +0900

    chore(component): ModalComponent에서 스타일 관련 import 제거

--- components/src/lib/index.ts
+++ components/src/lib/index.ts
@@ -1,4 +1,2 @@
 export { default as ModalComponent } from './ModalComponent';
 export { ModalProvider, useModal } from './contexts/ModalContext';
-export * from './styles/ModalStyle';
-export * from './styles/ModalTextStyle';
