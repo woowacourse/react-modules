a0c8d5cf7966aa5f032e791e467fb20232465741
Minje Sung <minje813@dgu.ac.kr>
2025-05-08 20:16:46 +0900

    feat: 모달 열림 기능 구현

--- components/src/lib/Modal.tsx
+++ components/src/lib/Modal.tsx
@@ -1,5 +1,17 @@
-const Modal = () => {
-  return <div>모달임</div>;
+interface ModalProps {
+  isOpen: boolean;
+}
+
+const Modal = ({ isOpen }: ModalProps) => {
+  return (
+    <>
+      {isOpen && (
+        <div id="modal">
+          <p>모달열림</p>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default Modal;
--- components/src/stories/Modal.stories.tsx
+++ components/src/stories/Modal.stories.tsx
@@ -10,4 +10,9 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {};
+export const Default: Story = {
+  render: Modal,
+  args: {
+    isOpen: false,
+  },
+};
