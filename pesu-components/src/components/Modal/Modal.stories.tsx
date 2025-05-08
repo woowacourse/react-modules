37c3e45b98373bf07373020f15246f1245a76358
guesung <gueit214@naver.com>
2025-05-08 11:03:28 +0900

    feat: 모달의 각 스토리북 구현

--- pesu-components/src/components/Modal/Modal.stories.tsx
+++ pesu-components/src/components/Modal/Modal.stories.tsx
@@ -13,21 +13,125 @@ const meta: Meta<typeof Modal> = {
 export default meta;
 type Story = StoryObj<typeof Modal>;
 
-export const Center: Story = {
+export const AlertModal: Story = {
   render: () => {
     return (
-      <Modal title="제목" isOpen position="center">
-        내용
+      <Modal isOpen position="center">
+        <Modal.Top>
+          <Modal.Title>아이디를 입력해 주세요.</Modal.Title>
+        </Modal.Top>
+        <Modal.Content>아이디는 필수로 입력해야 합니다.</Modal.Content>
+        <Modal.Bottom>
+          <Modal.ButtonContainer>
+            <Modal.ConfirmButton>확인</Modal.ConfirmButton>
+          </Modal.ButtonContainer>
+        </Modal.Bottom>
       </Modal>
     );
   },
 };
 
-export const Bottom: Story = {
+export const ConfirmModal: Story = {
   render: () => {
     return (
-      <Modal title="제목" isOpen position="bottom">
-        내용
+      <Modal isOpen position="center">
+        <Modal.Top>
+          <Modal.Title>카드를 삭제하시겠습니까?</Modal.Title>
+        </Modal.Top>
+        <Modal.Content>삭제하면 복구하실 수 없습니다.</Modal.Content>
+        <Modal.Bottom>
+          <Modal.ButtonContainer>
+            <Modal.CancelButton>취소</Modal.CancelButton>
+            <Modal.ConfirmButton>확인</Modal.ConfirmButton>
+          </Modal.ButtonContainer>
+        </Modal.Bottom>
+      </Modal>
+    );
+  },
+};
+
+export const PromptModal: Story = {
+  render: () => {
+    return (
+      <Modal isOpen position="center">
+        <Modal.Top>
+          <Modal.Title>쿠폰 번호를 입력해 주세요.</Modal.Title>
+        </Modal.Top>
+        <Modal.Content>
+          <Modal.PromptInput />
+        </Modal.Content>
+        <Modal.Bottom>
+          <Modal.ButtonContainer>
+            <Modal.CancelButton>취소</Modal.CancelButton>
+            <Modal.ConfirmButton>확인</Modal.ConfirmButton>
+          </Modal.ButtonContainer>
+        </Modal.Bottom>
+      </Modal>
+    );
+  },
+};
+
+export const SmallModal: Story = {
+  render: () => {
+    return (
+      <Modal isOpen position="center" size="small">
+        <Modal.Top>
+          <Modal.Title>약관에 동의해 주세요</Modal.Title>
+          <Modal.CloseButton />
+        </Modal.Top>
+        <Modal.Content>
+          <p>[필수] 개인정보 수집이용 동의</p>
+          <p>[필수] 고객정보 제 3자 제공동의</p>
+        </Modal.Content>
+        <Modal.Bottom>
+          <Modal.ButtonContainer>
+            <Modal.ConfirmButton isExpanded>동의하고 저장하기</Modal.ConfirmButton>
+          </Modal.ButtonContainer>
+        </Modal.Bottom>
+      </Modal>
+    );
+  },
+};
+
+export const MediumModal: Story = {
+  render: () => {
+    return (
+      <Modal isOpen position="center" size="medium">
+        <Modal.Top>
+          <Modal.Title>약관에 동의해 주세요</Modal.Title>
+          <Modal.CloseButton />
+        </Modal.Top>
+        <Modal.Content>
+          <p>[필수] 개인정보 수집이용 동의</p>
+          <p>[필수] 고객정보 제 3자 제공동의</p>
+        </Modal.Content>
+        <Modal.Bottom>
+          <Modal.ButtonContainer>
+            <Modal.ConfirmButton isExpanded>동의하고 저장하기</Modal.ConfirmButton>
+          </Modal.ButtonContainer>
+        </Modal.Bottom>
+      </Modal>
+    );
+  },
+};
+
+export const LargeModal: Story = {
+  render: () => {
+    return (
+      <Modal isOpen position="center" size="large">
+        <Modal.Top>
+          <Modal.Title>약관에 동의해 주세요</Modal.Title>
+          <Modal.CloseButton />
+        </Modal.Top>
+        <Modal.Content>
+          <p>[필수] 개인정보 수집이용 동의</p>
+          <p>[필수] 고객정보 제 3자 제공동의</p>
+        </Modal.Content>
+        <Modal.Bottom>
+          <Modal.ButtonContainer>
+            <Modal.ConfirmButton isExpanded>동의하고 저장하기</Modal.ConfirmButton>
+          </Modal.ButtonContainer>
+        </Modal.Bottom>
       </Modal>
     );
   },
