51cd5b0385bd3b93e798db6e56c81c09ee480eac
Beomtae <95863810+Beomtae@users.noreply.github.com>
2025-05-08 15:06:22 +0900

    🐛 fix: 커스텀 훅 변경으로 인한 테스트 깨지는 코드 수정

--- hooks/src/__test__/useCardInfo.test.tsx
+++ hooks/src/__test__/useCardInfo.test.tsx
@@ -1,56 +1,50 @@
 import { renderHook, act } from "@testing-library/react";
-import useCardInfo from "../lib/useCardInfo";
 import { CardInfo } from "../lib/types/Card";
+import {
+  useCardCVC,
+  useCardExpirationDate,
+  useCardNumber,
+  useCardPassword,
+} from "../lib";
 
 describe("useCardInfo 테스트", () => {
-  it("초기 상태가 올바르게 설정되어야 한다", () => {
-    const { result } = renderHook(() => useCardInfo());
-
-    expect(result.current.cardInfo).toEqual({
-      number: {
-        first: "",
-        second: "",
-        third: "",
-        fourth: "",
-      },
-      expiration: {
-        month: "",
-        year: "",
-      },
-      company: "",
-      cvc: "",
-      passwordFront: "",
-    });
-  });
-
-  it("handleCardInfo로 number의 특정 필드를 업데이트할 수 있어야 한다", () => {
-    const { result } = renderHook(() => useCardInfo());
+  it("handleCardNumber로 number의 특정 필드를 업데이트할 수 있어야 한다", () => {
+    const { result } = renderHook(() => useCardNumber());
 
     act(() => {
-      result.current.handleCardInfo("number", "1234", "first");
+      result.current.handleCardNumber("1234", "first");
     });
 
-    expect(result.current.cardInfo.number.first).toBe("1234");
+    expect(result.current.cardNumber.first).toBe("1234");
   });
 
-  it("handleCardInfo로 expiration의 특정 필드를 업데이트할 수 있어야 한다", () => {
-    const { result } = renderHook(() => useCardInfo());
+  it("handleExpirationDate로 expiration의 특정 필드를 업데이트할 수 있어야 한다", () => {
+    const { result } = renderHook(() => useCardExpirationDate());
 
     act(() => {
-      result.current.handleCardInfo("expiration", "12", "month");
+      result.current.handleExpirationDate("12", "month");
     });
 
-    expect(result.current.cardInfo.expiration.month).toBe("12");
+    expect(result.current.expirationDate.month).toBe("12");
   });
 
-  it.each([
-    ["company", "BC카드"],
-    ["cvc", "123"],
-    ["passwordFront", "12"],
-  ])("handleCardInfo로 %s를 업데이트할 수 있어야 한다", (field, value) => {
-    const { result } = renderHook(() => useCardInfo());
+  it("handleCvcNumber로 cvc 필드를 업데이트할 수 있어야 한다", () => {
+    const { result } = renderHook(() => useCardCVC());
 
-    act(() => result.current.handleCardInfo(field as keyof CardInfo, value));
-    expect(result.current.cardInfo[field as keyof CardInfo]).toBe(value);
+    act(() => {
+      result.current.handleCvcNumber("123");
+    });
+
+    expect(result.current.cvcNumber).toBe("123");
+  });
+
+  it("handlePassword로 password 필드를 업데이트할 수 있어야 한다", () => {
+    const { result } = renderHook(() => useCardPassword());
+
+    act(() => {
+      result.current.handlePassword("12");
+    });
+
+    expect(result.current.password).toBe("12");
   });
 });
