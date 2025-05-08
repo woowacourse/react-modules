09600556c71bb10151eaca054ccf170be1ab4953
kimyou1102 <yoosk5485@naver.com>
2025-05-08 21:28:30 +0900

    feat: ConfirmButton 컴포넌트 생성

---
+++ components/src/lib/ConfirmButton.tsx
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+interface ConfirmButtonProps {
+  onClick: () => void;
+  width?: number;
+}
+
+function ConfirmButton({ onClick, width }: ConfirmButtonProps) {
+  return (
+    <StyledButton onClick={onClick} width={width}>
+      확인
+    </StyledButton>
+  );
+}
+
+export default ConfirmButton;
+
+type StyledButtonProps = Pick<ConfirmButtonProps, 'width'>;
+
+const StyledButton = styled.button<StyledButtonProps>`
+  width: ${(props) => (props.width ? `${props.width}px` : '100%')};
+  height: 36px;
+  color: white;
+  border: 1px solid #333333;
+  border-radius: 5px;
+  font-weight: bold;
+  background-color: #333333;
+`;
