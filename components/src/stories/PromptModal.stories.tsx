6a95d893922848777acb2823c3aa9f5539147a3c
Beomtae <95863810+Beomtae@users.noreply.github.com>
2025-05-08 20:42:54 +0900

    ✅ test: 입력 모달 테스트 구현

---
+++ components/src/stories/PromptModal.stories.tsx
@@ -0,0 +1,21 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import PromptModal from "../lib/modals/PromptModal";
+
+const meta = {
+  title: "PromptModal",
+  component: PromptModal,
+} satisfies Meta<typeof PromptModal>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const OpenPromptModal: Story = {
+  args: {
+    isOpen: true,
+    onClose: () => {},
+    onConfirm: () => {},
+    title: "테스트",
+    content: <input></input>,
+  },
+};
