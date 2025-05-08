78cbfbebf2d076f4225a0b9a0e93335aebe763b0
youjeong Jeong <wjddbwjd1205@naver.com>
2025-05-08 16:47:05 +0900

    style: ConfirmButton 스타일을 button.styles.ts로 분리
    - 스타일 재사용성과 유지보수를 위해 Emotion 스타일 정의를 별도 파일로 분리
    - ConfirmButton 컴포넌트는 ConfirmButtonStyle을 import하여 사용

---
+++ components/src/lib/styles/Button.styles.ts
@@ -0,0 +1,11 @@
+import { css } from "@emotion/react";
+
+export const ConfirmButtonStyle = css`
+  border: 0;
+  background-color: #333333;
+  color: #ffffff;
+  width: 80px;
+  height: 36px;
+  border-radius: 5px;
+  cursor: pointer;
+`;
--- components/src/lib/styles/Modal.styles.ts
+++ components/src/lib/styles/Modal.styles.ts
@@ -101,13 +101,3 @@ export const getPositionStyle = (position: string) => {
       return css``;
   }
 };
-
-export const ConfirmButtonStyle = css`
-  border: 0;
-  background-color: #333333;
-  color: #ffffff;
-  width: 80px;
-  height: 36px;
-  border-radius: 5px;
-  cursor: pointer;
-`;
