326e41c0f126dc271facacc3fdc3b8f2e957d71f
Oliy <eunsoa0310@gmail.com>
2025-05-08 16:27:50 +0900

    refactor(hooks): 카드 입력 검증 로직에 입력값 길이 검증 추가 및 에러 메시지 업데이트

--- hooks/src/lib/validator/constants/errorMessage.ts
+++ hooks/src/lib/validator/constants/errorMessage.ts
@@ -4,19 +4,24 @@ export const ERROR_MESSAGE = {
       IS_NUMBER: '숫자를 입력해주세요',
       IS_NUMBER_RANGE: '1월부터 12월을 입력해주세요',
       IS_EXPIRATION: '유효기간을 다시 확인해주세요',
+      IS_VALID_LENGTH: '월은 2자리를 입력해주세요',
     },
     YEAR: {
       IS_NUMBER: '숫자를 입력해주세요',
       IS_EXPIRATION: '유효기간을 다시 확인해주세요',
+      IS_VALID_LENGTH: '연도는 2자리를 입력해주세요',
     },
   },
   CVC: {
     IS_NUMBER_STRING: '숫자를 입력해주세요',
+    IS_VALID_LENGTH: 'CVC는 3자리를 입력해주세요',
   },
   PASSWORD: {
-    IS_NUMBER_STRING: '비밀번호는 숫자 2자리입니다',
+    IS_NUMBER_STRING: '비밀번호는 숫자로 입력해주세요',
+    IS_VALID_LENGTH: '비밀번호는 2자리를 입력해주세요',
   },
   NUMBER: {
     IS_NUMBER_STRING: '숫자를 입력해주세요',
+    IS_VALID_LENGTH: '카드번호는 4자리를 입력해주세요',
   },
 };
--- hooks/src/lib/validator/validateCardInput.ts
+++ hooks/src/lib/validator/validateCardInput.ts
@@ -5,6 +5,7 @@ export function validateExpirationDateMonth(month: string, year: string) {
     IS_NUMBER: validatorUtils.isNumber(month),
     IS_NUMBER_RANGE: validatorUtils.isValidNumberRange(Number(month), 1, 12),
     IS_EXPIRATION: validatorUtils.isValidExpirationDate(month, year),
+    IS_VALID_LENGTH: validatorUtils.isValidLength(month, 2),
   };
   return errorResult;
 }
@@ -13,18 +14,28 @@ export function validateExpirationDateYear(month: string, year: string) {
   const errorResult = {
     IS_NUMBER: validatorUtils.isNumber(year),
     IS_EXPIRATION: validatorUtils.isValidExpirationDate(month, year),
+    IS_VALID_LENGTH: validatorUtils.isValidLength(year, 2),
   };
   return errorResult;
 }
 
 export function validateCVC(cvc: string) {
-  return { IS_NUMBER_STRING: validatorUtils.isNumber(cvc) };
+  return {
+    IS_NUMBER_STRING: validatorUtils.isNumber(cvc),
+    IS_VALID_LENGTH: validatorUtils.isValidLength(cvc, 3),
+  };
 }
 
 export function validatePassword(password: string) {
-  return { IS_NUMBER_STRING: validatorUtils.isNumber(password) };
+  return {
+    IS_NUMBER_STRING: validatorUtils.isNumber(password),
+    IS_VALID_LENGTH: validatorUtils.isValidLength(password, 2),
+  };
 }
 
 export function validateCardNumber(cardNumber: string) {
-  return { IS_NUMBER_STRING: validatorUtils.isNumber(cardNumber) };
+  return {
+    IS_NUMBER_STRING: validatorUtils.isNumber(cardNumber),
+    IS_VALID_LENGTH: validatorUtils.isValidLength(cardNumber, 4),
+  };
 }
