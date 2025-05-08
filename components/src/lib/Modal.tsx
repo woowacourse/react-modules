3326caf11807bfceaa331c68b234fd84e72eebcf
Minje Sung <minje813@dgu.ac.kr>
2025-05-08 21:02:06 +0900

    feat: Overlay 클릭으로 모달 닫는 기능 구현

--- components/src/lib/Modal.tsx
+++ components/src/lib/Modal.tsx
@@ -1,3 +1,5 @@
+import styled from '@emotion/styled';
+
 interface ModalProps {
   isOpen: boolean;
   onClose: () => void;
@@ -8,10 +10,13 @@ const Modal = ({ isOpen, onClose }: ModalProps) => {
     <>
       {isOpen && (
         <div id="modal">
-          <p>모달열림</p>
-          <div data-testid="modal-close" onClick={() => onClose()}>
-            모달닫기
-          </div>
+          <ModalOverlay data-testid="modal-overlay" onClick={onClose} />
+          <ModalContent>
+            <p>모달열림</p>
+            <div data-testid="modal-close" onClick={() => onClose()}>
+              모달닫기
+            </div>
+          </ModalContent>
         </div>
       )}
     </>
@@ -19,3 +24,28 @@ const Modal = ({ isOpen, onClose }: ModalProps) => {
 };
 
 export default Modal;
+
+const ModalContent = styled.div`
+  height: 216px;
+  width: 304px;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: white;
+  padding: 24px 32px;
+  border-radius: 8px;
+  color: #000;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+const ModalOverlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgba(0, 0, 0, 0.35);
+`;
