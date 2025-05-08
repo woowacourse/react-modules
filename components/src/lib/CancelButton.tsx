c31eea4cac3abd7f69716ba70d7b947021772652
kimyou1102 <yoosk5485@naver.com>
2025-05-08 21:28:13 +0900

    feat: CancelButton 컴포넌트 생성

---
+++ components/src/lib/CancelButton.tsx
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+interface CancelButtonProps {
+  onClick: () => void;
+  width?: number;
+}
+
+function CancelButton({ onClick, width }: CancelButtonProps) {
+  return (
+    <StyledButton onClick={onClick} width={width}>
+      취소
+    </StyledButton>
+  );
+}
+
+export default CancelButton;
+
+type StyledButtonProps = Pick<CancelButtonProps, 'width'>;
+
+const StyledButton = styled.button<StyledButtonProps>`
+  width: ${(props) => (props.width ? `${props.width}px` : '100%')};
+  height: 36px;
+  color: #333333bf;
+  border: 1px solid #33333340;
+  border-radius: 5px;
+  font-weight: bold;
+  background-color: white;
+`;
