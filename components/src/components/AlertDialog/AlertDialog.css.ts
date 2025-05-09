07507b0cc908b9f821d9cbb00e3ea04c4ff6cb2f
장정안 <wjddks96@naver.com>
2025-05-09 21:04:27 +0900

    feat: AlertDialog UI 구현 및 스토리북 작성

---
+++ components/src/components/AlertDialog/AlertDialog.css.ts
@@ -0,0 +1,17 @@
+import { css } from '@emotion/react';
+
+export const AlertDialogLayout = css`
+  display: flex;
+  width: 480px;
+  padding: 24px 32px;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 16px;
+`;
+
+export const AlertDialogContentContainer = css`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 16px;
+`;
