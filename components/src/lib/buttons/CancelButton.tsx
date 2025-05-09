02185836e0a49b22c41137c5d0209042a7de132b
youjeong Jeong <wjddbwjd1205@naver.com>
2025-05-09 14:29:56 +0900

    refactor: CancelButton 컴포넌트의 스타일을 분리하여 styles 파일로 이동
    - 컴포넌트 내 정의된 cancelStyle을 CancelButtonStyle로 분리

--- components/src/lib/buttons/CancelButton.tsx
+++ components/src/lib/buttons/CancelButton.tsx
@@ -1,19 +1,10 @@
-import { css } from "@emotion/react";
+import { CancelButtonStyle } from "../styles";
 import ModalButton from "./ModalButton";
-
-const cancelStyle = css`
-  background-color: #fff;
-  color: #333;
-  border: 1px solid #ccc;
-  width: 80px;
-  height: 36px;
-  border-radius: 5px;
-`;
 
 interface CancelButtonProps extends React.ComponentProps<typeof ModalButton> {}
 
 const CancelButton = ({ children = "취소", ...rest }: CancelButtonProps) => (
-  <ModalButton css={cancelStyle} {...rest}>
+  <ModalButton css={CancelButtonStyle} {...rest}>
     {children}
   </ModalButton>
 );
