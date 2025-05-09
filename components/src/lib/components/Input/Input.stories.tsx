49471c39d817ba2baac27e0154e9296786b7a875
kaori-killer <sksbthwjd@daum.net>
2025-05-10 01:17:41 +0900

    test: 입력 컴포넌트에 대한 스토리북 작성

---
+++ components/src/lib/components/Input/Input.stories.tsx
@@ -0,0 +1,30 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import Input from "./Input";
+
+const meta = {
+  title: "Input",
+  component: Input,
+  tags: ["autodocs"],
+  parameters: {
+    docs: {
+      description: {
+        component: "입력 컴포넌트입니다.",
+      },
+      story: {
+        inline: false,
+        iframeHeight: 100,
+      },
+    },
+  },
+} satisfies Meta<typeof Input>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    placeholder: "쿠폰 번호 입력",
+  },
+};
