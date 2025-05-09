7d842a3e073d2420704d849d00ecd1c08771e60b
kimyou1102 <yoosk5485@naver.com>
2025-05-09 15:00:40 +0900

    feat: ConfirmModal 컴포넌트 생성

---
+++ components/src/lib/ConfirmModal.tsx
@@ -0,0 +1,40 @@
+import styled from 'styled-components';
+
+import { Modal } from '.';
+
+interface ConfirmModalProps {
+  isOpen: boolean;
+  title: string;
+  content: string;
+  onConfirmClick: () => void;
+  size?: 'small' | 'medium' | 'large';
+}
+
+function ConfirmModal({
+  isOpen,
+  title,
+  content,
+  onConfirmClick,
+  size,
+}: ConfirmModalProps) {
+  return (
+    <>
+      <Modal isOpen={isOpen}>
+        <Modal.Overlay onClick={onConfirmClick} />
+        <Modal.Content position="center" size={size}>
+          <Modal.Title>{title}</Modal.Title>
+          <Modal.Body>{content}</Modal.Body>
+          <ButtonWrapper>
+            <Modal.ConfirmButton width={80} onClick={onConfirmClick} />
+          </ButtonWrapper>
+        </Modal.Content>
+      </Modal>
+    </>
+  );
+}
+
+export default ConfirmModal;
+
+const ButtonWrapper = styled.div`
+  margin-top: 22px;
+`;
