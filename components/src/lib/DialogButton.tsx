90f294bc778ee9bdcb7b18ca00c25b6815710f38
yeongipark <yeongi0111@naver.com>
2025-05-10 00:55:03 +0900

    css: DialogButton 스타일 분리

--- components/src/lib/DialogButton.tsx
+++ components/src/lib/DialogButton.tsx
@@ -1,21 +1,9 @@
 import { ComponentProps } from "react";
+import { StyledButton } from "./DialogButton.css";
 
 type DialogButtonProps = Omit<ComponentProps<"button">, "type"> & {
   type: "primary" | "basic";
   text: string;
-};
-
-const buttonStyle = {
-  primary: {
-    backgroundColor: "#333333",
-    color: "white",
-    border: "none",
-  },
-  basic: {
-    backgroundColor: "white",
-    color: "#333333",
-    border: "1px solid rgba(51,51,51,0.25)",
-  },
 };
 
 export default function DialogButton({
@@ -24,18 +12,8 @@ export default function DialogButton({
   ...rest
 }: DialogButtonProps) {
   return (
-    <button
-      {...rest}
-      style={{
-        ...buttonStyle[type],
-        cursor: "pointer",
-        fontSize: "15px",
-        fontWeight: 700,
-        padding: "6px 20px",
-        borderRadius: "5px",
-      }}
-    >
+    <StyledButton $type={type} {...rest}>
       {text}
-    </button>
+    </StyledButton>
   );
 }
