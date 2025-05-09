dcef787a7419b2debc1ad3f920f9068ecc213cba
kaori-killer <sksbthwjd@daum.net>
2025-05-10 00:43:10 +0900

    fix: input 컴포넌트가 부모 컴포넌트 width를 초과하지 않도록 수정

--- components/src/lib/components/Input/Input.styled.tsx
+++ components/src/lib/components/Input/Input.styled.tsx
@@ -3,6 +3,7 @@ import styled from "@emotion/styled";
 export const StyledInput = styled.input`
   width: 100%;
   height: 32px;
+  box-sizing: border-box;
   border: 1px solid #000000;
   border-radius: 3px;
   color: #000000;
