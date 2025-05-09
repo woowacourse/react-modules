d33a2e18527c6619ef40a21fc10d7993939542c2
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2025-05-09 16:34:08 +0900

    fix: ESC 키를 눌러야 모달이 꺼지도록 수정

---
+++ components/src/lib/hooks/utils/useKeyboardEffect.ts
@@ -0,0 +1,28 @@
+import { useEffect } from "react";
+
+interface useKeyboardEffectProps {
+  key: string;
+  handler: () => void;
+  enabled?: boolean;
+}
+
+const useKeyboardEffect = ({
+  key,
+  handler,
+  enabled = true,
+}: useKeyboardEffectProps) => {
+  useEffect(() => {
+    if (!enabled) return;
+
+    const handleKeyPress = (event) => {
+      if (event.key === key) {
+        handler();
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyPress);
+    return () => window.removeEventListener("keydown", handleKeyPress);
+  }, [key, handler, enabled]);
+};
+
+export default useKeyboardEffect;
