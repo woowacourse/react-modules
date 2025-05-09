07caad887ef7dd57b7b335afe4e813dbc187c9d7
spoyodevelop <kjy9505@gmail.com>
2025-05-09 16:27:26 +0900

    refactor: ValidationRule 및 ValidationRules 타입 정의 개선 및, errorMessage 조합 개선

--- hooks/src/lib/validator/index.ts
+++ hooks/src/lib/validator/index.ts
@@ -2,12 +2,23 @@ import { ValidateField, FieldValueType } from "./constants";
 import { validationRules } from "./validation-rules";
 import { FieldErrorCode } from "./constants/error-messages";
 
-type ValidationRule<T extends ValidateField> = {
+// ValidationRule 및 ValidationRules 타입 정의
+interface ValidationRule<T extends ValidateField> {
+  message: string | ((value: FieldValueType[T]) => string);
+  check: (value: FieldValueType[T]) => boolean;
+}
+
+type ValidationRules = {
+  [K in ValidateField]: Record<FieldErrorCode[K], ValidationRule<K>>;
+};
+
+type ValidationRuleParam<T extends ValidateField> = {
   check: (value: FieldValueType[T]) => boolean;
   errorMeta: {
     field: T;
     code: FieldErrorCode[T];
   };
+  applyWhen?: (value: FieldValueType[T]) => boolean;
 };
 
 type ValidationResult = {
@@ -20,7 +31,7 @@ type ValidationResult = {
 };
 
 function createValidator<T extends ValidateField>(
-  rules: ValidationRule<T>[]
+  rules: ValidationRuleParam<T>[]
 ): (value: FieldValueType[T]) => ValidationResult {
   return (value: FieldValueType[T]) => {
     if (value === "") {
@@ -28,20 +39,24 @@ function createValidator<T extends ValidateField>(
     }
 
     const errors: ValidationResult["errors"] = [];
-    rules.forEach(({ check, errorMeta }) => {
+    for (const { check, applyWhen, errorMeta } of rules) {
+      if (applyWhen && !applyWhen(value)) continue;
       if (!check(value)) {
         const { field, code } = errorMeta;
 
-        const rawMessage = (validationRules[field] as any)[code]?.message;
+        const ruleSet = (validationRules as ValidationRules)[field];
+        const rawMessage = ruleSet[code]?.message;
+
         const message =
           typeof rawMessage === "function" ? rawMessage(value) : rawMessage;
 
         errors.push({ field, code, message });
+        break;
       }
-    });
+    }
 
     return { valid: errors.length === 0, errors };
   };
 }
 
-export { createValidator, type ValidationRule, type ValidationResult };
+export { createValidator, type ValidationRuleParam, type ValidationResult };
