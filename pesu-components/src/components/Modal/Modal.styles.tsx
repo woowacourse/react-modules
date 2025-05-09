8a02091472e78f439e25b8886a2805e233746ae5
guesung <gueit214@naver.com>
2025-05-09 14:49:28 +0900

    feat: 자동 포커스 훅

--- pesu-components/src/components/Modal/Modal.styles.tsx
+++ pesu-components/src/components/Modal/Modal.styles.tsx
@@ -1,6 +1,6 @@
 import styled from '@emotion/styled';
 import { ModalInterface } from './Modal';
-import { Device } from '../../hooks/useDevice';
+import { Device } from '../../hooks/useDevice/type';
 
 const getModalWidth = (size: 'small' | 'medium' | 'large', position: 'center' | 'bottom') => {
   if (position === 'center') {
--- pesu-components/src/hooks/index.ts
+++ pesu-components/src/hooks/index.ts
@@ -1,1 +1,2 @@
 export { default as useDevice } from './useDevice/useDevice';
+export { default as useAutoFocus } from './useAutoFocus/useAutoFocus';
