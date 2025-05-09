8362ba107071d01f4244fc52633550c1620b2127
Minje Sung <minje813@dgu.ac.kr>
2025-05-09 19:43:27 +0900

    test: 하단 취소 버튼 스토리 작성

--- components/src/stories/Modal.stories.tsx
+++ components/src/stories/Modal.stories.tsx
@@ -1,6 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import Modal from '../lib/Modal';
 import { useState } from 'react';
+import TextButton from '../lib/components/TextButton';
 
 const meta: Meta<typeof Modal> = {
   title: 'Modal',
@@ -35,6 +36,30 @@ export const TitleModal: Story = {
         <Modal.Content>
           <Modal.Title title="모달 타이틀" />
           <p>모달열림</p>
+        </Modal.Content>
+      </Modal>
+    );
+  },
+};
+
+export const ModalWithBottomCancelButton: Story = {
+  render: () => {
+    const [isOpen, setIsOpen] = useState(true);
+    return (
+      <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
+        <Modal.Overlay />
+        <Modal.Content
+          style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
+          <Modal.Title title="모달 타이틀" />
+          <p>모달열림</p>
+          <TextButton
+            text="취소"
+            color="#8B95A1"
+            backgroundColor="transparent"
+            onClick={() => {
+              setIsOpen(false);
+            }}
+          />
         </Modal.Content>
       </Modal>
     );
