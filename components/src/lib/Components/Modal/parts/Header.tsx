b6201815a350ea4105cf9e1faabdfa377d176240
spoyodevelop <kjy9505@gmail.com>
2025-05-09 16:32:01 +0900

    feat: header 접근성 개선

--- components/src/lib/Components/Modal/parts/Header.tsx
+++ components/src/lib/Components/Modal/parts/Header.tsx
@@ -8,8 +8,14 @@ interface HeaderProps extends ComponentProps<"header"> {
 function Header({ children, ...props }: HeaderProps) {
   return (
     <div id="modal-header" className={styles.headerContainer}>
-      <header {...props} className={styles.modalHeader}>
-        <h2 className={styles.title}>{children}</h2>
+      <header
+        {...props}
+        className={styles.modalHeader}
+        aria-labelledby="modal-title"
+      >
+        <h2 id="modal-title" className={styles.title}>
+          {children}
+        </h2>
       </header>
     </div>
   );
