791e1a0fb3424a4b887830e190294d46042bc7e3
yeongipark <yeongi0111@naver.com>
2025-05-10 00:55:48 +0900

    feat: 웹 접근성을 위한 useFocus훅 작성

---
+++ components/src/lib/hooks/useFocus.ts
@@ -0,0 +1,37 @@
+import { useEffect, useRef } from "react";
+
+const useFocus = (isOpen: boolean) => {
+  const modalRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    if (!isOpen) return;
+    const modalEl = modalRef.current;
+    const focusableEls = modalEl?.querySelectorAll<HTMLElement>(
+      'button, a[href], input, select, textarea, [tabindex]:not([tabindex="-1"])'
+    );
+    const firstEl = focusableEls?.[0];
+    const lastEl = focusableEls?.[focusableEls.length - 1];
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if (e.key !== "Tab" || focusableEls?.length === 0) return;
+      if (e.shiftKey) {
+        if (document.activeElement === firstEl) {
+          e.preventDefault();
+          lastEl?.focus();
+        }
+      } else {
+        if (document.activeElement === lastEl) {
+          e.preventDefault();
+          firstEl?.focus();
+        }
+      }
+    };
+    document.addEventListener("keydown", handleKeyDown);
+    firstEl?.focus();
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+      //   modalEl?.focus();
+    };
+  }, [isOpen]);
+  return { modalRef };
+};
+export default useFocus;
