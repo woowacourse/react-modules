6805e6f2e1f0a0d5519ee0819581d40d57a66dd3
youjeong Jeong <wjddbwjd1205@naver.com>
2025-05-09 14:28:44 +0900

    style: 버튼 텍스트가 넘칠 경우를 대비해 width → padding 기반으로 스타일 수정
    - Confirm 버튼이 고정 width로 인해 텍스트가 길어질 경우 잘림 현상 발생
    - padding-left/right를 사용해 텍스트 길이에 유연하게 대응
    - Cancel 버튼 스타일도 동일한 방식으로 정의해 일관성 확보

--- components/src/lib/styles/Button.styles.ts
+++ components/src/lib/styles/Button.styles.ts
@@ -4,8 +4,21 @@ export const ConfirmButtonStyle = css`
   border: 0;
   background-color: #333333;
   color: #ffffff;
-  width: 80px;
+  /* width: 80px; */
+  padding-left: 18px;
+  padding-right: 18px;
   height: 36px;
   border-radius: 5px;
   cursor: pointer;
+`;
+
+export const CancelButtonStyle = css`
+  background-color: #fff;
+  color: #333;
+  border: 1px solid #ccc;
+  /* width: 80px; */
+  padding-left: 18px;
+  padding-right: 18px;
+  height: 36px;
+  border-radius: 5px;
 `;
