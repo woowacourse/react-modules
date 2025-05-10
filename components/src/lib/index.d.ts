eee5a560f1408b8407294da36da8d94799e41706
minji2219 <aabb0219@naver.com>
2025-05-11 02:20:06 +0900

    chore: 모달 타입 지정 파일 생성

---
+++ components/src/lib/index.d.ts
@@ -0,0 +1,32 @@
+import * as React from 'react';
+
+export interface TitleProps {
+  text?: string;
+  color?: string;
+  size?: number;
+}
+
+export interface MessageProps {
+  message: string;
+}
+
+export interface ModalProps {
+  position?: 'center' | 'bottom';
+  size?: 'small' | 'medium' | 'large';
+  title?: TitleProps;
+  showCloseButton?: boolean;
+  backgroundColor?: string;
+  children?: React.ReactNode;
+
+  alert?: MessageProps;
+  confirm?: MessageProps;
+  prompt?: boolean | MessageProps;
+
+  isOpen: boolean;
+  onClose: () => void;
+  onConfirm?: (value?: string) => void;
+}
+
+declare const Modal: React.FC<ModalProps>;
+
+export default Modal;
