9fa2eca113b91dab1ed029f4a9a006c850cd9b90
Beomtae <95863810+Beomtae@users.noreply.github.com>
2025-05-10 12:18:35 +0900

    🔨 chore: 모달 npm 배포 설정

--- components/src/lib/index.ts
+++ components/src/lib/index.ts
@@ -1,1 +1,4 @@
 export { default as Modal } from "./Modal/Modal";
+export { default as AlertModal } from "./ModalVariants/AlertModal";
+export { default as ConfirmModal } from "./ModalVariants/ConfirmModal";
+export { default as PromptModal } from "./ModalVariants/PromptModal";
