3ca40f48dd0d572b7ce769500678f22c1aed0554
dev-dino22 <wlsskdud02@naver.com>
2025-05-11 05:45:33 +0900

    feat: AlertModal 컴포넌트 작성

---
+++ modal-components/src/lib/components/AlertModal.tsx
@@ -0,0 +1,57 @@
+import ModalBox from "./ModalBox";
+import {
+  ModalContainer,
+  ModalBackdrop,
+  Container,
+  Wrapper,
+} from "../styles/ModalStyle";
+import { ConfirmModalProps } from "../types/modalTypes";
+import Button from "./common/Button";
+import styled from "@emotion/styled";
+import { useEscapeKeyClose } from "../hook/useEscapeKeyClose";
+
+const ButtonContainer = styled.div`
+  width: 100%;
+  height: 36px;
+  display: flex;
+  justify-content: flex-end;
+`;
+
+const AlertModal = ({
+  modalPosition = "center",
+  modalSize = "medium",
+  titleText = "",
+  descriptionText = "",
+  children,
+  closeType,
+  onClose,
+  onConfirm = onClose,
+}: ConfirmModalProps) => {
+  useEscapeKeyClose(onClose);
+
+  return (
+    <ModalContainer modalPosition={modalPosition}>
+      <ModalBackdrop onClick={onClose} />
+      <Container modalSize={modalSize}>
+        <Wrapper>
+          <ModalBox
+            modalPosition={modalPosition}
+            titleText={titleText}
+            closeType={closeType}
+            onClose={onClose}
+          >
+            {descriptionText && <p>{descriptionText}</p>}
+            {children}
+            <ButtonContainer>
+              <Button widthSize="80px" onClick={onConfirm} variant="primary">
+                확인
+              </Button>
+            </ButtonContainer>
+          </ModalBox>
+        </Wrapper>
+      </Container>
+    </ModalContainer>
+  );
+};
+
+export default AlertModal;
