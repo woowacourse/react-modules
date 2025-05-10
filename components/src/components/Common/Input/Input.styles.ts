129ca4cfafb74e9065d4fbaf88f18bdc10578984
sooyeoniya <tndus1306403@naver.com>
2025-05-10 18:02:32 +0900

    design: Input width 오류 수정
    - Modal 내부에 Input을 넣었을 때, 모달에서 설정된 Padding을 무시하고 그대로 튀어나오는 현상 있었음

--- components/src/components/Common/Input/Input.styles.ts
+++ components/src/components/Common/Input/Input.styles.ts
@@ -3,7 +3,6 @@ import styled from "@emotion/styled";
 export const InputWrapper = styled.input`
   border: 1px solid #000;
   border-radius: 5px;
-  width: 100%;
   height: 32px;
   padding: 8px;
   font-size: 16px;
