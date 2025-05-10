1041e04e1bd59f85962a16e309a8666e5352de75
thgml05 <aisohii@naver.com>
2025-05-10 17:44:12 +0900

    refactor: Modal import 파일 경로 수정

--- components/src/stories/Modal.stories.tsx
+++ components/src/stories/Modal.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import Modal from '../lib/Modal.tsx';
+import Modal from '../lib/module/Modal.tsx';
 import { useState } from 'react';
 
 const meta: Meta<typeof Modal> = {
