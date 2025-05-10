0b44e5135e80b80c3269622bb29477746f5c197d
sooyeoniya <tndus1306403@naver.com>
2025-05-10 16:14:58 +0900

    refactor: ButtonGroup 타입 수정
    - button 컴포넌트 타입 상속

--- components/src/components/Common/ButtonGroup/ButtonGroup.tsx
+++ components/src/components/Common/ButtonGroup/ButtonGroup.tsx
@@ -1,24 +1,33 @@
-import { ReactNode } from "react";
+import { ComponentProps } from "react";
 import { ButtonVarient } from "../../../constants/theme";
 import Button from "../Button/Button";
 import { ButtonGroupWrapper } from "./ButtonGroup.styles";
 
+interface ButtonProps extends ComponentProps<"button"> {
+  varient?: ButtonVarient;
+}
+
 export interface ButtonGroupProps {
-  left: {
-    varient: ButtonVarient;
-    children: ReactNode;
-  };
-  right: {
-    varient: ButtonVarient;
-    children: ReactNode;
-  };
+  left: ButtonProps;
+  right: ButtonProps;
 }
 
 const ButtonGroup = ({ left, right }: ButtonGroupProps) => {
+  const { varient: leftVarient, children: leftChildren, ...leftProps } = left;
+  const {
+    varient: rightVarient,
+    children: rightChildren,
+    ...rightProps
+  } = right;
+
   return (
     <ButtonGroupWrapper>
-      <Button varient={left.varient}>{left.children}</Button>
-      <Button varient={right.varient}>{right.children}</Button>
+      <Button varient={left.varient} {...leftProps}>
+        {left.children}
+      </Button>
+      <Button varient={right.varient} {...rightProps}>
+        {right.children}
+      </Button>
     </ButtonGroupWrapper>
   );
 };
