2764322d8b20d467b0866de267919bda004fd2b8
kimyou1102 <yoosk5485@naver.com>
2025-05-11 01:44:38 +0900

    test: 변경된 useCardNumber에 따라서 테스트 변경
    - 변경된 상태 타입, onChange 매개변수, 에러 메시지 변화에 맞춰 변경

--- hooks/src/__test__/useCardNumber.test.tsx
+++ hooks/src/__test__/useCardNumber.test.tsx
@@ -4,48 +4,49 @@ import useCardNumber from '../lib/hooks/useCardNumber';
 describe('useCardNumber 훅', () => {
   test('초기 상태', () => {
     const { result } = renderHook(() => useCardNumber());
-    expect(result.current.value).toEqual(['', '', '', '']);
-    expect(result.current.isError).toEqual([false, false, false, false]);
+    expect(result.current.formatted).toEqual(undefined);
+    expect(result.current.isError).toEqual(false);
     expect(result.current.errorMessage).toBe('');
   });
 
-  test('입력 수 초과시 입력 안됨', () => {
+  test('입력 수 초과시 입력 안됨', async () => {
     const { result } = renderHook(() => useCardNumber());
     act(() => {
-      result.current.onChange(
-        { target: { value: '12345' } } as React.ChangeEvent<HTMLInputElement>,
-        0
-      );
+      result.current.onChange({
+        target: { value: '12345678912345678' },
+      } as React.ChangeEvent<HTMLInputElement>);
     });
-    expect(result.current.value[0]).toBe('');
-    expect(result.current.isError[0]).toBe(false);
-    expect(result.current.errorMessage).toBe('');
+
+    expect(result.current.formatted).toEqual(['1234', '5678', '9123', '45678']);
+    expect(result.current.isError).toBe(true);
+    expect(result.current.errorMessage).toBe(
+      '4-4-4-4 형태의 16자리로 16자리로 입력해주세요'
+    );
   });
 
   test('유효한 입력 시 에러 없음', () => {
     const { result } = renderHook(() => useCardNumber());
     act(() => {
-      result.current.onChange(
-        { target: { value: '1234' } } as React.ChangeEvent<HTMLInputElement>,
-        0
-      );
+      result.current.onChange({
+        target: { value: '1234567891234567' },
+      } as React.ChangeEvent<HTMLInputElement>);
     });
-    expect(result.current.value[0]).toBe('1234');
-    expect(result.current.isError[0]).toBe(false);
+    expect(result.current.formatted).toEqual(['1234', '5678', '9123', '4567']);
+    expect(result.current.isError).toBe(false);
     expect(result.current.errorMessage).toBe('');
   });
 
   test('자리 수 부족 시 에러 메시지', () => {
     const { result } = renderHook(() => useCardNumber());
     act(() => {
-      result.current.onChange(
-        { target: { value: '123' } } as React.ChangeEvent<HTMLInputElement>,
-        1
-      );
+      result.current.onChange({
+        target: { value: '123456789123456' },
+      } as React.ChangeEvent<HTMLInputElement>);
     });
-    expect(result.current.isError[1]).toBe(true);
+    expect(result.current.formatted).toEqual(['1234', '5678', '9123', '456']);
+    expect(result.current.isError).toBe(true);
     expect(result.current.errorMessage).toBe(
-      '한 칸은 4자리 숫자를 입력해야합니다.'
+      '4-4-4-4 형태의 16자리로 16자리로 입력해주세요'
     );
   });
 });
