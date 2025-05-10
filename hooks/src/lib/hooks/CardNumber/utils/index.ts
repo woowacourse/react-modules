b3033c5b26aaf661e96086f9c41bf5c61acf7b35
wo-o29 <woogur29@gmail.com>
2025-05-10 14:56:19 +0900

    refactor: 카드 번호 유효성 검사 로직 수정

--- hooks/src/lib/hooks/CardNumber/utils/index.ts
+++ hooks/src/lib/hooks/CardNumber/utils/index.ts
@@ -3,32 +3,18 @@ import {
   getErrorByRules,
 } from "../../../../utils/validation";
 import { ErrorState, ValidationRule } from "../../../types";
-import { CardNumberState } from "../types";
-
-export const getCardNumbersError = (
-  cardNumbers: CardNumberState
-): ErrorState => {
-  for (const { value } of Object.values(cardNumbers)) {
-    const error = getCardNumberError(value);
-    if (!error.isValid) {
-      return error;
-    }
-  }
-
-  return { isValid: true, errorMessage: "" };
-};
 
 const cardNumberValidationRules: ValidationRule[] = [
   {
-    condition: commonConditions.isNumeric,
-    errorMessage: "숫자만 입력해주세요.",
+    condition: commonConditions.isFilled,
+    errorMessage: "카드 번호를 입력해주세요.",
   },
   {
-    condition: commonConditions.hasLength(4),
-    errorMessage: "카드 번호는 4자리여야 합니다.",
+    condition: commonConditions.hasLengthInRange(14, 16),
+    errorMessage: "카드 번호는 14 ~ 16자리여야 합니다.",
   },
 ];
 
-const getCardNumberError = (cardNumber: string): ErrorState => {
+export const getCardNumberError = (cardNumber: string): ErrorState => {
   return getErrorByRules(cardNumber, cardNumberValidationRules);
 };
