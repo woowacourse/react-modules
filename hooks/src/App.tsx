f1f0418fb9ce9bcee9324181d89b577b40c20be3
kaori-killer <sksbthwjd@daum.net>
2025-05-10 13:47:36 +0900

    chore: emotion 설치

--- hooks/src/App.tsx
+++ hooks/src/App.tsx
@@ -2,11 +2,7 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return (
-    <>
-      <h1>Hooks Modules</h1>
-    </>
-  );
+  return <h1>Hooks Modules</h1>;
 }
 
 export default App;
