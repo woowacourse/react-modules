fb5495165db189f59fe9a741dc5b18d73f9540a7
kaori-killer <sksbthwjd@daum.net>
2025-05-10 13:11:58 +0900

    test: Alert, Confirm, Prompt 모달에 대한 테스트 코드 작성

--- components/src/lib/components/Modal/Modal.stories.tsx
+++ components/src/lib/components/Modal/Modal.stories.tsx
@@ -1,14 +1,16 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
 import Modal from "./Modal";
-import ModalOverlay from "../ModalOverlay/ModalOverlay";
 import ModalContent from "../ModalContent/ModalContent";
 import ModalHeader from "../ModalHeader/ModalHeader";
 import ModalTitle from "../ModalTitle/ModalTitle";
 import ModalCloseButton from "../ModalCloseButton/ModalCloseButton";
 import ModalBody from "../ModalBody/ModalBody";
 import ModalFooter from "../ModalFooter/ModalFooter";
+import Button from "../Button/Button";
+
 import useModal from "../../hooks/useModal";
+import Input from "../Input/Input";
 
 const meta = {
   title: "Modal",
@@ -39,12 +41,11 @@ export const Default: Story = {
   },
 
   render: () => {
-    const { isOpen, handleOpen, handleClose } = useModal();
+    const { handleClose } = useModal();
 
     return (
       <>
-        <button onClick={handleOpen}>Open</button>
-        <Modal isOpen={isOpen} onClose={handleClose}>
+        <Modal isOpen={true} onClose={handleClose}>
           <ModalContent position="center">
             <ModalHeader direction="row" align="start" justify="start">
               <ModalTitle tag="h1" fontSize="25px" fontWeight="700">
@@ -57,6 +58,110 @@ export const Default: Story = {
               Footer이다!
             </ModalFooter>
           </ModalContent>
+        </Modal>
+      </>
+    );
+  },
+};
+
+export const Alert: Story = {
+  args: {
+    isOpen: true,
+    onClose: () => {},
+    children: "",
+  },
+
+  render: () => {
+    const { handleClose } = useModal();
+
+    return (
+      <>
+        <Modal isOpen={true} onClose={handleClose}>
+          <Modal.Content position="center" size="medium">
+            <Modal.Header direction="row" align="start" justify="start">
+              <Modal.Title tag="h1" fontSize="25px" fontWeight="700">
+                아이디를 입력해 주세요.
+              </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>아이디는 필수로 입력해야 합니다.</Modal.Body>
+            <Modal.Footer direction="row" align="end" justify="end">
+              <Button color="dark" size="small">
+                확인
+              </Button>
+            </Modal.Footer>
+          </Modal.Content>
+        </Modal>
+      </>
+    );
+  },
+};
+
+export const Confirm: Story = {
+  args: {
+    isOpen: true,
+    onClose: () => {},
+    children: "",
+  },
+
+  render: () => {
+    const { handleClose } = useModal();
+
+    return (
+      <>
+        <Modal isOpen={true} onClose={handleClose}>
+          <Modal.Content position="center" size="medium">
+            <Modal.Header direction="row" align="start" justify="start">
+              <Modal.Title tag="h1" fontSize="25px" fontWeight="700">
+                카드를 삭제하시겠습니까?
+              </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>삭제하면 복구하실 수 없습니다.</Modal.Body>
+            <Modal.Footer direction="row" align="end" justify="end">
+              <Button color="light" size="small">
+                취소
+              </Button>
+              <Button color="dark" size="small">
+                확인
+              </Button>
+            </Modal.Footer>
+          </Modal.Content>
+        </Modal>
+      </>
+    );
+  },
+};
+
+export const Prompt: Story = {
+  args: {
+    isOpen: true,
+    onClose: () => {},
+    children: "",
+  },
+
+  render: () => {
+    const { handleClose } = useModal();
+
+    return (
+      <>
+        <Modal isOpen={true} onClose={handleClose}>
+          <Modal.Content position="center" size="medium">
+            <Modal.Header direction="row" align="start" justify="start">
+              <Modal.Title tag="h1" fontSize="25px" fontWeight="700">
+                쿠폰 번호를 입력해 주세요.
+              </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <Input placeholder="쿠폰 번호 입력" />
+            </Modal.Body>
+            <Modal.Footer direction="row" align="end" justify="end">
+              <Button color="light" size="small">
+                취소
+              </Button>
+              <Button color="dark" size="small">
+                확인
+              </Button>
+            </Modal.Footer>
+          </Modal.Content>
         </Modal>
       </>
     );
