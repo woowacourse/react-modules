074d0ef3a6ebc6c94309f10d707954a94cc29c2a
sanghee01 <sang6299@naver.com>
2025-05-10 22:23:20 +0900

    style: Button 글자, 테두리 색깔 수정

--- components/src/lib/components/Button.styles.ts
+++ components/src/lib/components/Button.styles.ts
@@ -3,13 +3,18 @@ import { css } from '@emotion/css';
 export const ButtonStyle = (type: 'confirm' | 'cancel', width: string) => css`
   all: unset;
   background-color: ${type === 'confirm' ? '#333333' : 'white'};
-  color: ${type === 'confirm' ? 'white' : '#8b95a1'};
+  color: ${type === 'confirm' ? 'white' : '#333333BF'};
   width: ${width};
   height: 44px;
+  border: ${type === 'cancel' && '1px solid #8b95a1'};
   border-radius: 5px;
   cursor: pointer;
 
   &:focus {
     outline: none;
+  }
+
+  &:hover {
+    border: ${type === 'cancel' && '1px solid #333333'};
   }
 `;
