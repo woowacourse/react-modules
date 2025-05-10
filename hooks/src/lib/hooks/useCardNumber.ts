ca5e9da7a532173315ac81f6d0cfbb812c322d60
yeongipark <yeongi0111@naver.com>
2025-05-10 17:02:57 +0900

    feat: 포맷팅 완료

--- hooks/src/lib/hooks/useCardNumber.ts
+++ hooks/src/lib/hooks/useCardNumber.ts
@@ -1,18 +1,81 @@
-import useCardInfo from "./useCardInfo";
 import useError from "./useError";
 import { isValidLength, isValidNumber } from "../util";
+import { useState, useMemo } from "react";
+
+const cardBranRule = {
+  Visa: {
+    inputCount: 4,
+    length: 16,
+    ranges: [[4]],
+    format: (cardNumber: string) =>
+      cardNumber
+        .replace(/(\d{4})(\d{4})(\d{4})(\d{0,4})/, "$1 $2 $3 $4")
+        .split(" "),
+  },
+  MasterCard: {
+    inputCount: 4,
+    length: 16,
+    ranges: [[51, 55]],
+    format: (cardNumber: string) =>
+      cardNumber
+        .replace(/(\d{4})(\d{4})(\d{4})(\d{0,4})/, "$1 $2 $3 $4")
+        .split(" "),
+  },
+  Diners: {
+    inputCount: 3,
+    length: 14,
+    ranges: [[36]],
+    format: (cardNumber: string) =>
+      cardNumber.replace(/(\d{4})(\d{6})(\d{0,4})/, "$1 $2 $3").split(" "),
+  },
+  AMEX: {
+    inputCount: 3,
+    length: 15,
+    ranges: [[34], [37]],
+    format: (cardNumber: string) =>
+      cardNumber.replace(/(\d{4})(\d{6})(\d{0,5})/, "$1 $2 $3").split(" "),
+  },
+  UnionPay: {
+    inputCount: 4,
+    length: 16,
+    ranges: [
+      [622126, 622925],
+      [624, 626],
+      [6282, 6288],
+    ],
+    format: (cardNumber: string) =>
+      cardNumber
+        .replace(/(\d{4})(\d{4})(\d{4})(\d{0,4})/, "$1 $2 $3 $4")
+        .split(" "),
+  },
+  Unknown: {
+    inputCount: 1,
+    length: 16,
+    ranges: [[0]],
+    format: (cardNumber: string) => [cardNumber],
+  },
+};
+
+type CardType = keyof typeof cardBranRule;
 
 export default function useCardNumber({
-  initCardNumber,
   initCardNumberError,
 }: {
-  initCardNumber: Record<string, string>;
   initCardNumberError: Record<string, string>;
 }) {
-  const cardNumber = useCardInfo({
-    initValues: initCardNumber,
-    maxLength: 4,
-  });
+  const [cardNumber, setCardNumber] = useState("");
+
+  function changeCardNumber(value: string) {
+    setCardNumber(value);
+  }
+
+  const cardBrand: CardType = useMemo(() => {
+    const matched = Object.entries(cardBranRule).find(([_, rule]) => {
+      return rule.ranges.some((range) => isInRange(cardNumber, range));
+    });
+
+    return (matched?.[0] as CardType) ?? "Unknown";
+  }, [cardNumber]);
 
   const cardNumberError = useError({
     initError: initCardNumberError,
@@ -21,6 +84,10 @@ export default function useCardNumber({
 
   return {
     cardNumber,
+    cardBrand,
+    formattedCardNumber: cardBranRule[cardBrand].format(cardNumber),
+    inputCount: cardBranRule[cardBrand].inputCount,
+    changeCardNumber,
     cardNumberError,
   };
 }
@@ -42,3 +109,30 @@ function getCardNumberValidationFns(maxLength: number, cardNumber: string) {
     },
   ];
 }
+
+function isInRange(card: string, range: number[]): boolean {
+  const prefixLength = range[0].toString().length;
+  const prefix = parseInt(card.slice(0, prefixLength));
+
+  if (range.length === 1) {
+    return prefix === range[0];
+  }
+
+  const [start, end] = range;
+  return prefix >= start && prefix <= end;
+}
+
+/*
+Visa : 4로 시작하는 16자리 숫자
+- 예시: 4123 4567 8912 3456
+MasterCard : 51~55로 시작하는 16자리 숫자
+- 예시: 5123 4567 8912 3456
+Diners : 36으로 시작하는 14자리 숫자
+ - 예시: 3612 345678 9012
+AMEX : 34, 37로 시작하는 15자리 숫자 
+- 예시 (34로 시작): 3412 345678 90123
+유니온페이 : 카드의 앞 번호가 아래 3가지 조건을 만족하는 16자리 숫자
+- 622126~622925로 시작하는 경우: 6221 2612 3456 7890
+- 624~626로 시작하는 경우: 6240 1234 5678 9012
+- 6282~6288로 시작하는 경우: 6282 1234 5678 9012
+*/
--- hooks/src/lib/tests/useCardNumber.test.ts
+++ hooks/src/lib/tests/useCardNumber.test.ts
@@ -2,18 +2,8 @@ import { renderHook, act } from "@testing-library/react";
 import useCardNumber from "../hooks/useCardNumber";
 
 describe("useError", () => {
-  const cardNumber = {
-    first: "",
-    second: "",
-    third: "",
-    forth: "",
-  };
-
   const cardNumberError = {
-    first: "",
-    second: "",
-    third: "",
-    forth: "",
+    cardNumber: "",
   };
 
   let result: any;
@@ -21,7 +11,6 @@ describe("useError", () => {
   beforeEach(() => {
     const hook = renderHook(() =>
       useCardNumber({
-        initCardNumber: cardNumber,
         initCardNumberError: cardNumberError,
       })
     );
@@ -29,44 +18,82 @@ describe("useError", () => {
   });
 
   test("카드 번호 초기 값을 가져온다.", () => {
-    expect(result.current.cardNumber.values).toEqual(cardNumber);
+    expect(result.current.cardNumber).toEqual("");
   });
 
   test("카드 번호 값을 변경할 수 있다.", () => {
-    const type = "first";
-    const changeValue = "1001";
+    const changeValue = "1234";
 
     act(() => {
-      result.current.cardNumber.changeValues(type, changeValue);
+      result.current.changeCardNumber(changeValue);
     });
 
-    expect(result.current.cardNumber.values[type]).toEqual(changeValue);
+    expect(result.current.cardNumber).toEqual(changeValue);
   });
 
-  test("카드 번호가 올바르지 않은 경우 에러를 반환한다.", () => {
-    const type = "first";
-    const changeValue = "100a";
-    const maxLength = 4;
-
-    act(() => {
-      result.current.cardNumber.changeValues(type, changeValue);
-      result.current.cardNumberError.checkValidation({
-        length: maxLength,
-        type,
-        value: changeValue,
+  test.each([
+    ["412345", "Visa"],
+    ["512345", "MasterCard"],
+    ["361234", "Diners"],
+    ["341234", "AMEX"],
+    ["622126", "UnionPay"],
+    ["628299", "UnionPay"],
+    ["000000", "Unknown"],
+  ])(
+    "'%s' 입력 시 카드 브랜드는 '%s' 이어야 한다",
+    (changeValue, expectedBrand) => {
+      act(() => {
+        result.current.changeCardNumber(changeValue);
       });
-    });
 
-    expect(result.current.cardNumberError.isError()).toEqual(true);
-  });
+      expect(result.current.cardBrand).toEqual(expectedBrand);
+    }
+  );
+
+  test.each([
+    {
+      brand: "visa",
+      value: "4234567890123456",
+      expectedFormat: ["4234", "5678", "9012", "3456"],
+      expectedInputCount: 4,
+    },
+    {
+      brand: "amex",
+      value: "378282246310005",
+      expectedFormat: ["3782", "822463", "10005"],
+      expectedInputCount: 3,
+    },
+    {
+      brand: "mastercard",
+      value: "5555555555554444",
+      expectedFormat: ["5555", "5555", "5555", "4444"],
+      expectedInputCount: 4,
+    },
+    {
+      brand: "Diners",
+      value: "36555555555544",
+      expectedFormat: ["3655", "555555", "5544"],
+      expectedInputCount: 3,
+    },
+  ])(
+    "카드 브랜드 $brand에 따라 포맷팅을 진행한다.",
+    ({ value, expectedFormat, expectedInputCount }) => {
+      act(() => {
+        result.current.changeCardNumber(value);
+      });
+
+      expect(result.current.formattedCardNumber).toEqual(expectedFormat);
+      expect(result.current.inputCount).toEqual(expectedInputCount);
+    }
+  );
 
   test("카드 번호가 올바르지 않은 경우 에러 메시지를 반환한다.", () => {
-    const type = "first";
+    const type = "cardNumber";
     const changeValue = "100a";
     const maxLength = 4;
 
     act(() => {
-      result.current.cardNumber.changeValues(type, changeValue);
+      result.current.changeCardNumber(changeValue);
       result.current.cardNumberError.checkValidation({
         length: maxLength,
         type,
