9b39f4a17b0769f4b40380d36c96b1b761e53017
Jeongeun Lee <leeju1009@gmail.com>
2025-05-11 15:05:02 +0900

    refactor: 카드사별 앞자리 숫자를 데이터화한 상수의 타입을 일반 객체에서 Map으로 수정
    - 데이터 타입 변경에 따른 관련 로직 수정

--- hooks/src/lib/utils/cardNetwork.ts
+++ hooks/src/lib/utils/cardNetwork.ts
@@ -1,27 +1,35 @@
-import { NetworkType } from '../types';
+import { NetworkType } from '../constants';
 
-const NETWORK_LIST = {
-  visa: ['4'],
-  master: ['51', '52', '53', '54', '55'],
-  diners: ['36'],
-  amex: ['34', '37'],
-  union: [
-    '624',
-    '625',
-    '626',
-    '6282',
-    '6283',
-    '6284',
-    '6285',
-    '6286',
-    '6287',
-    '6288',
+const NETWORK_LIST = new Map<NetworkType, string[]>([
+  ['visa', ['4']],
+  ['master', ['51', '52', '53', '54', '55']],
+  ['diners', ['36']],
+  ['amex', ['34', '37']],
+  [
+    'union',
+    [
+      '624',
+      '625',
+      '626',
+      '6282',
+      '6283',
+      '6284',
+      '6285',
+      '6286',
+      '6287',
+      '6288',
+    ],
   ],
-};
+]);
 
-const NETWORK_RANGE = {
-  union: { start: 622126, end: 622925 },
-};
+interface RangeType {
+  start: number;
+  end: number;
+}
+
+const NETWORK_RANGE = new Map<Partial<NetworkType>, RangeType>([
+  ['union', { start: 622126, end: 622925 }],
+]);
 
 const NETWORK_FORMAT: Record<NetworkType, number[]> = {
   visa: [4, 4, 4, 4],
@@ -29,27 +37,33 @@ const NETWORK_FORMAT: Record<NetworkType, number[]> = {
   diners: [4, 6, 4],
   amex: [4, 6, 5],
   union: [4, 4, 4, 4],
-};
+} as const;
 
 const DEFAULT_FORMAT = [4, 4, 4, 4];
 
-export const identifyNetworkByList = (cardNumbers: string) => {
-  const identifiedNetwork = Object.entries(NETWORK_LIST).find(
-    ([_, identifyNumbers]) => {
-      return identifyNumbers.some((number) => cardNumbers.startsWith(number));
+export const identifyNetworkByList = (
+  cardNumbers: string
+): NetworkType | '' => {
+  for (const [network, identifyNumbers] of NETWORK_LIST) {
+    if (identifyNumbers.some((number) => cardNumbers.startsWith(number))) {
+      return network;
     }
-  );
+  }
 
-  return identifiedNetwork ? identifiedNetwork[0] : '';
+  return '';
 };
 
-export const identifyNetworkByRange = (cardNumbers: string) => {
-  const identifiedNetwork = Object.entries(NETWORK_RANGE).find(([_, range]) => {
+export const identifyNetworkByRange = (
+  cardNumbers: string
+): NetworkType | '' => {
+  for (const [network, range] of NETWORK_RANGE) {
     const parsedNumber = Number(cardNumbers);
-    return parsedNumber >= range.start && parsedNumber <= range.end;
-  });
+    if (parsedNumber >= range.start && parsedNumber <= range.end) {
+      return network;
+    }
+  }
 
-  return identifiedNetwork ? identifiedNetwork[0] : '';
+  return '';
 };
 
 export const formatNumbersByNetwork = (
