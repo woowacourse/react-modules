9ca8fe3b0c5e796151dec1c6e7b4c492cf71e431
Jeongeun Lee <leeju1009@gmail.com>
2025-05-11 17:14:01 +0900

    test: useCardNumbers 훅의 변경에 따른 테스트 코드 수정

--- hooks/src/lib/cardNumber/useCardNumbers.test.ts
+++ hooks/src/lib/cardNumber/useCardNumbers.test.ts
@@ -1,8 +1,9 @@
 import { renderHook, act } from '@testing-library/react';
 import useCardNumbers from './useCardNumbers';
-import { ChangeEvent } from 'react';
+import { ChangeEvent, FocusEvent } from 'react';
 import {
   CARD_NUMBER_ERROR_TYPES,
+  NetworkType,
   ValidateCardNumbersResult,
 } from '../constants';
 import { ValidationResult } from '../types';
@@ -14,11 +15,18 @@ interface RenderHookResult {
 interface RenderHookCurrent {
   cardNumbers: string;
   validationResults: ValidationResult;
-  validateCardNumbers: (value: string) => ValidateCardNumbersResult;
+  validateCardNumbersChange: (value: string) => ValidateCardNumbersResult;
+  validateCardNumbersBlur: (value: string) => ValidateCardNumbersResult;
   handleCardNumbersChange: (
     event: ChangeEvent<HTMLInputElement>,
     restrictChange?: boolean
   ) => void;
+  handleCardNumbersBlur: (event: FocusEvent<HTMLInputElement>) => void;
+  network: string;
+  formatNumbersByNetwork: (
+    cardNumbers: string,
+    network: NetworkType | ''
+  ) => string;
 }
 
 describe('useCardNumbers', () => {
@@ -31,24 +39,35 @@ describe('useCardNumbers', () => {
   it('이벤트 핸들러가 감지한 입력값이 hook 내부의 cardNumbers 상태(state)로 변경된다.', () => {
     act(() => {
       result.current.handleCardNumbersChange({
-        target: { value: '1234' },
+        target: { value: '1234567890123456' },
       } as ChangeEvent<HTMLInputElement>);
     });
 
-    expect(result.current.cardNumbers).toBe('1234');
+    expect(result.current.cardNumbers).toBe('1234567890123456');
   });
 
   it('입력값이 숫자가 아닐 때 isValid로 false를 반환하고 notNumber 에러 타입을 반환한다.', () => {
-    expect(result.current.validateCardNumbers('aaaa')).toEqual({
+    expect(result.current.validateCardNumbersChange('aaaa')).toEqual({
       isValid: false,
       errorType: CARD_NUMBER_ERROR_TYPES.notNumber,
     });
   });
 
-  it('입력값이 네 자리가 아닐 때 isValid로 false를 반환하고 invalidLength 에러 타입을 반환한다.', () => {
-    expect(result.current.validateCardNumbers('12345')).toEqual({
+  it('입력값이 유효한 길이가 아닐 때 isValid로 false를 반환하고 invalidLength 에러 타입을 반환한다.', () => {
+    expect(
+      result.current.validateCardNumbersChange('12345678901234567')
+    ).toEqual({
       isValid: false,
       errorType: CARD_NUMBER_ERROR_TYPES.invalidLength,
     });
   });
+
+  test.each(['1234567890123456', '341234567890123'])(
+    '입력값이 유효한 길이인 일 때 isValid로 true를 반환한다.',
+    (cardNumbers) => {
+      expect(result.current.validateCardNumbersChange(cardNumbers)).toEqual({
+        isValid: true,
+      });
+    }
+  );
 });
