69b80b749d480921dcd66a8cc58a49038102f3af
AHHYUNJU <wndkgus0225@naver.com>
2025-05-12 00:32:37 +0900

    test: cardTypeCases에 대한 테스트 코드 추가

--- hooks/src/hooks/useCardCompany/useCardCompany.test.ts
+++ hooks/src/hooks/useCardCompany/useCardCompany.test.ts
@@ -1,6 +1,23 @@
 import { renderHook, act } from "@testing-library/react";
 import useCardNumbers from "../useCardNumbers/useCardNumbers";
 
+const cardTypeCases: Array<[string, string]> = [
+  ["4000000000000000", "visa"],
+  ["4999999999999999", "visa"],
+  ["5100000000000000", "master"],
+  ["5599999999999999", "master"],
+  ["36111111111111", "diners"],
+  ["341111111111111", "amex"],
+  ["371111111111111", "amex"],
+  ["6221261111111111", "unionPay"],
+  ["6229259999999999", "unionPay"],
+  ["6241111111111111", "unionPay"],
+  ["6269999999999999", "unionPay"],
+  ["6282111111111111", "unionPay"],
+  ["6288999999999999", "unionPay"],
+  ["0000000000000000", "default"],
+];
+
 describe("useCardNumbers 훅", () => {
   test("초기 상태에는 빈 숫자 4개, 에러 없음", () => {
     const { result } = renderHook(() => useCardNumbers());
@@ -34,4 +51,15 @@ describe("useCardNumbers 훅", () => {
     expect(result.current.numbers[0]).toBe("1234");
     expect(result.current.error[0].isValid).toBe(false);
   });
+});
+
+describe("카드 타입 감지", () => {
+  test.each(cardTypeCases)(
+    "입력 %s → cardType이 %s로 설정",
+    (input, expectedType) => {
+      const { result } = renderHook(() => useCardNumbers());
+      act(() => result.current.handleCardNumberChange(input));
+      expect(result.current.cardType).toBe(expectedType);
+    }
+  );
 });
