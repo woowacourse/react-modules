de3f4ef0b2054bf11c86d0b70f00b7d799951778
keemsebeen <sebin0580@gmail.com>
2025-05-11 14:54:15 +0900

    refactor: useSingleCardInput JSDoc 수정, isValid 상태 제거

--- hooks/src/lib/hooks/useSingleCardInput/useSingleCardInput.ts
+++ hooks/src/lib/hooks/useSingleCardInput/useSingleCardInput.ts
@@ -1,17 +1,15 @@
 import { useState } from 'react';
 import { validateCardInput } from '../../validation/cardNumberLengthInfo';
 
-import { CardInputItem } from '../../types/cardInputItem.types';
-
 /**
  * @description
- * useSingleCardInput 훅은 카드 번호 입력을 관리하는 커스텀 훅입니다.
+ * useSingleCardInput는 CVC 훅은 카드 비밀번호 입력을 관리하는 커스텀 훅입니다.
  * 카드 번호의 유효성을 검사하고, 입력값을 상태로 관리합니다.
- * @param  validLength 카드 번호의 유효 길이
+ * @param {number} validLength 카드 번호의 유효 길이
  * @returns 카드 번호 입력 상태와 핸들러를 반환합니다.
- * @returns singleCardInput 카드 번호 입력 상태
- * @returns errorMessage 카드 번호 입력 오류 메시지
- * @returns handleSingleCardInputChange 카드 번호 입력 핸들러
+ * @returns {string} value 카드 번호 입력 상태
+ * @returns {string} errorMessage 카드 번호 입력 오류 메시지
+ * @returns {function} handleSingleCardInputChange 카드 번호 입력 핸들러
  * @example
  * const { singleCardInput, errorMessage, handleSingleCardInputChange } = useSingleCardInput(16);
  * <input
@@ -23,25 +21,19 @@ import { CardInputItem } from '../../types/cardInputItem.types';
  */
 
 export const useSingleCardInput = (validLength: number) => {
-  const [singleCardInput, setSingleCardInput] = useState<CardInputItem>({
-    value: '',
-    isValid: true,
-  });
-
+  const [value, setValue] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
 
   const handleSingleCardInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const newValue = event.target.value;
-
     const { isValid, errorMessage } = validateCardInput(newValue, validLength);
 
-    setSingleCardInput({
-      value: newValue,
-      isValid,
-    });
-
+    setValue(newValue);
     setErrorMessage(isValid ? '' : errorMessage);
   };
+
+  const isValid = Boolean(errorMessage);
+  const singleCardInput = { value, isValid };
 
   return {
     singleCardInput,
