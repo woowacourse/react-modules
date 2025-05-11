a14d4e17ffe96a6d96137775cbf443932cab557f
Minje Sung <minje813@dgu.ac.kr>
2025-05-12 00:07:27 +0900

    feat: useCardExpirationInput 월 추가 유효성 검사 구현

--- hooks/src/lib/useCardExpirationInput.ts
+++ hooks/src/lib/useCardExpirationInput.ts
@@ -8,6 +8,8 @@ const useCardExpirationInput = () => {
   const monthErrorText = (() => {
     if (!isNumeric(monthInputValue)) return '입력값이 숫자가 아닙니다.';
     if (!isNotOverMaxLength(monthInputValue, 2)) return '입력값이 최대 길이를 초과했습니다.';
+    if (Number(monthInputValue) < 1 || Number(monthInputValue) > 12)
+      return '월은 01 ~ 12 사이의 숫자여야 합니다.';
     return '';
   })();
 
