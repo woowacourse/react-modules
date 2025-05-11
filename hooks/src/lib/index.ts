5d4f903666b0d27f496bc619011a09a6e294f844
hoyyChoi <hoyeon8165@gmail.com>
2025-05-11 17:17:40 +0900

    chore: useErrors 훅 및 useCheckErrorComplete 훅 제거

--- hooks/src/lib/index.ts
+++ hooks/src/lib/index.ts
@@ -3,4 +3,3 @@ export { default as useExpirationDate } from "./Hooks/useExpirationDate";
 export { default as useCvcNumber } from "./Hooks/useCvcNumber";
 export { default as usePassword } from "./Hooks/usePassword";
 export { default as useInputValue } from "./Hooks/common/useInputValue";
-export { default as useErrors } from "./Hooks/common/useErrors";
