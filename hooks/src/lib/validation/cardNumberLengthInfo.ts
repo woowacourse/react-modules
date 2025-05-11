bd17325dc90401eada05cacd89efff3d0eede4c4
keemsebeen <sebin0580@gmail.com>
2025-05-11 14:53:16 +0900

    refactor: validateCardInput 함수 내 trimmedValue 변수 추가

--- hooks/src/lib/validation/cardNumberLengthInfo.ts
+++ hooks/src/lib/validation/cardNumberLengthInfo.ts
@@ -1,12 +1,14 @@
 export const validateCardInput = (value: string, validLength: number) => {
-  if (/^[0-9]+$/.test(value) === false) {
+  const trimmedValue = value?.replace(/\s/g, '');
+
+  if (/^[0-9]+$/.test(trimmedValue) === false) {
     return {
       isValid: false,
       errorMessage: `숫자(0~9)만 입력 가능합니다.`,
     };
   }
 
-  if (value.length !== validLength) {
+  if (trimmedValue.length !== validLength) {
     return {
       isValid: false,
       errorMessage: `숫자 ${validLength}자리를 정확히 입력해주세요.`,
