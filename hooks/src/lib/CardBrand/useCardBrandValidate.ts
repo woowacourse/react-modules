349cfca30b80cf600d26bd9c4f15b4b6f152ab38
ohgus <anakin1108@naver.com>
2025-05-11 18:51:12 +0900

    refactor: isValid 삭제
    errorMessage 상태만으로 isValid를 판별할 수 있다.

--- hooks/src/lib/CardBrand/useCardBrandValidate.ts
+++ hooks/src/lib/CardBrand/useCardBrandValidate.ts
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 
-export type CardBrandValidateResult = {
-  isValid: boolean;
+type CardBrandValidateResult = {
   errorMessage: string | null;
   validateCardBrand: (cardBrand: string) => void;
 };
@@ -9,21 +8,18 @@ export type CardBrandValidateResult = {
 const useCardBrandValidate = (
   cardBrands: string[]
 ): CardBrandValidateResult => {
-  const [isValid, setIsValid] = useState<boolean>(false);
   const [errorMessage, setErrorMessage] = useState<string | null>(null);
 
   const validateCardBrand = (cardBrand: string) => {
     if (!cardBrands.includes(cardBrand)) {
-      setIsValid(false);
       setErrorMessage('카드 브랜드가 올바르지 않습니다.');
       return;
     }
 
-    setIsValid(true);
     setErrorMessage(null);
   };
 
-  return { isValid, errorMessage, validateCardBrand };
+  return { errorMessage, validateCardBrand };
 };
 
 export default useCardBrandValidate;
