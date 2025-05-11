668c92d473e74d61d9e985cbf5a49b977358fb2d
hoyyChoi <hoyeon8165@gmail.com>
2025-05-11 23:56:03 +0900

    feat: 훅 적용해보기 -> App.tsx 임시 페이지 생성

--- hooks/src/App.tsx
+++ hooks/src/App.tsx
@@ -1,11 +1,34 @@
-import React from "react";
 import "./App.css";
+import { useCardNumber, useCvcNumber, useExpirationDate, usePassword } from "./lib";
 
 function App() {
+  const cardNumber = useCardNumber(" - ");
+  const expirationDate = useExpirationDate(" / ");
+  const cvcNumber = useCvcNumber();
+  const password = usePassword();
+
+  const cardInformation = {
+    카드번호: cardNumber,
+    유효기간: expirationDate,
+    cvc: cvcNumber,
+    비밀번호: password,
+  };
+
   return (
-    <>
-      <h1>Hooks Modules</h1>
-    </>
+    <main>
+      <h2 className="cardType">카드 타입: {cardNumber.cardType}</h2>
+      {Object.entries(cardInformation).map(([label, field]) => (
+        <div key={label} className="form-group">
+          <label className="form-label">{label}</label>
+          <input
+            className={`form-input ${field.error && "form-input-error"}`}
+            value={field.value}
+            onChange={(e) => field.onChange(e.target.value)}
+          />
+          <p className="form-error-message">{field.errorMessage}</p>
+        </div>
+      ))}
+    </main>
   );
 }
 
