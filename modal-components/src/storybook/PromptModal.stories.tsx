8c630c62aae27cfa1231f4b248cbbbc38167f535
dev-dino22 <wlsskdud02@naver.com>
2025-05-11 19:34:18 +0900

    feat: PromptModal 컴포넌트 스토리북 작성

---
+++ modal-components/src/storybook/PromptModal.stories.tsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import PromptModal from "../lib/components/modals/PromptModal";
+
+export default {
+  title: "Modals/PromptModal",
+  component: PromptModal,
+};
+
+export const Default = () => {
+  const [isOpened, setIsOpened] = useState(false);
+  const onCloseHandler = () => {
+    setIsOpened(false);
+  };
+  return (
+    <>
+      <button onClick={() => setIsOpened(true)}>프롬프트 모달 열기 버튼</button>
+      {isOpened && (
+        <PromptModal
+          modalPosition="center"
+          modalSize="large"
+          closeType="top"
+          titleText="쿠폰 번호를 입력해주세요"
+          onClose={onCloseHandler}
+          onCancel={() => {
+            alert("취소버튼을 눌렀습니다");
+            onCloseHandler();
+          }}
+          onConfirm={() => {
+            alert("확인버튼을 눌렀습니다");
+            onCloseHandler();
+          }}
+        />
+      )}
+    </>
+  );
+};
