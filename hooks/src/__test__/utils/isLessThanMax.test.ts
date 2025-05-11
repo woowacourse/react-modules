0e6b4e807809a89b9ebd2a22ddd956bf56aba138
Minje Sung <minje813@dgu.ac.kr>
2025-05-11 17:33:57 +0900

    test: 특정 길이보다 작은지 확인하는 유틸함수 테스트 작성

---
+++ hooks/src/__test__/utils/isLessThanMax.test.ts
@@ -0,0 +1,12 @@
+describe('isLessThanMax Util 테스트', () => {
+  it.each(['mingo', '12345'])(
+    '최대 값보다 더 긴 값(%s)이 들어오면 false를 반환한다.',
+    (value) => {
+      expect(isLessThanMax(value, 4)).toBeFalsy();
+    }
+  );
+
+  it.each(['813', 'hi!'])('최대 값보다 더 작은 값(%s)이 들어오면 true를 반환한다.', (value) => {
+    expect(isLessThanMax(value, 4).toBeTruthy();
+  });
+});
