18fca26a065b3abffde367ce8a6af325eb77b260
sooyeoniya <tndus1306403@naver.com>
2025-05-11 23:58:54 +0900

    design: Button heigth 줄이기

--- components/src/components/Common/Button/Button.styles.ts
+++ components/src/components/Common/Button/Button.styles.ts
@@ -3,7 +3,7 @@ import { BUTTON_COLOR_MAP, ButtonVarient } from "../../../constants/theme";
 
 export const ButtonWrapper = styled.button<{ $varient: ButtonVarient }>`
   width: 100%;
-  height: 55px;
+  height: 48px;
   background-color: ${({ $varient }) => BUTTON_COLOR_MAP[$varient].background};
   color: ${({ $varient }) => BUTTON_COLOR_MAP[$varient].text};
   border: 1.5px solid ${({ $varient }) => BUTTON_COLOR_MAP[$varient].border};
