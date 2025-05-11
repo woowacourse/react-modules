d295051c68c2520b398f4fe7db2b5476e056dad7
dev-dino22 <wlsskdud02@naver.com>
2025-05-11 19:33:42 +0900

    feat: AgreementModal 컴포넌트 스토리북 작성

---
+++ modal-components/src/storybook/AgreementModal.stories.tsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import AgreementModal from "../lib/components/modals/AgreementModal";
+import { agreementContents } from "./AgreementModalData";
+
+export default {
+  title: "Modals/AgreementModal",
+  component: AgreementModal,
+};
+
+export const Default = () => {
+  const [isOpened, setIsOpened] = useState(false);
+  const onCloseHandler = () => setIsOpened(false);
+
+  return (
+    <>
+      <button onClick={() => setIsOpened(true)}>
+        약관 동의 모달 열기 버튼
+      </button>
+      {isOpened && (
+        <AgreementModal
+          modalPosition="bottom"
+          modalSize="large"
+          closeType="top"
+          titleText="약관에 동의해 주세요"
+          onClose={onCloseHandler}
+          agreementContents={agreementContents}
+          descriptionText="필수 약관에 동의하셔야 서비스 이용이 가능합니다."
+        />
+      )}
+    </>
+  );
+};
