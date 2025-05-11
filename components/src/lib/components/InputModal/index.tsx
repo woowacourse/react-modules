1fc94fb2469815524c2f9bfb984fe7b449bebf94
keemsebeen <sebin0580@gmail.com>
2025-05-11 14:50:18 +0900

    feat: InputModal 컴포넌트 구현

---
+++ components/src/lib/components/InputModal/index.tsx
@@ -0,0 +1,61 @@
+import { Button } from '../Button';
+import { Flex } from '../Flex';
+import { Input, InputProps } from '../Input';
+import { Modal, type ModalProps } from '../Modal';
+
+export type InputModalProps = {
+  /**
+   * submit function to be called when the submit button is clicked
+   */
+  onSubmit: VoidFunction;
+} & ModalProps &
+  InputProps;
+
+export const InputModal = ({
+  isOpen,
+  title,
+  value,
+  placeholder,
+  position,
+  onChange,
+  onClose,
+  onSubmit,
+  ...props
+}: InputModalProps) => {
+  return (
+    <Modal
+      isOpen={isOpen}
+      title={title}
+      position={position}
+      onClose={onClose}
+      showCloseButton={false}
+      {...props}
+    >
+      <Flex
+        as="form"
+        height="100%"
+        direction="column"
+        justify="flex-start"
+        gap={1}
+        padding="1rem 0 0 0"
+      >
+        <Input autoFocus size={2.25} value={value} onChange={onChange} placeholder={placeholder} />
+        <Flex gap="0.5rem" justify="flex-end">
+          <Button
+            size="md"
+            width={position === 'center' ? '20%' : '100%'}
+            variant="outlined"
+            color="#808080"
+            fontColor="gray"
+            onClick={onClose}
+          >
+            취소
+          </Button>
+          <Button size="md" width={position === 'center' ? '20%' : '100%'} onClick={onSubmit}>
+            확인
+          </Button>
+        </Flex>
+      </Flex>
+    </Modal>
+  );
+};
