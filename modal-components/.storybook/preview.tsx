8980bf2ff07853be10894b360fb2c82d717ca40b
dev-dino22 <wlsskdud02@naver.com>
2025-05-11 20:33:28 +0900

    fix: storybook viewport 가 조금만 보이는 문제 수정

---
+++ modal-components/.storybook/preview.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+import type { Preview, Decorator } from "@storybook/react";
+import { Global } from "@emotion/react";
+import { resetCss } from "./resetCss";
+import {
+  MINIMAL_VIEWPORTS,
+  INITIAL_VIEWPORTS,
+} from "@storybook/addon-viewport";
+
+export const decorators: Decorator[] = [
+  (Story, context) => (
+    <>
+      <Global styles={resetCss} />
+      {Story(context)}
+    </>
+  ),
+];
+
+const customViewports = {
+  largePC: {
+    name: "Large PC",
+    styles: {
+      width: "1920px",
+      height: "1080px",
+    },
+    type: "desktop",
+  },
+  mediumPC: {
+    name: "Medium PC",
+    styles: {
+      width: "1366px",
+      height: "768px",
+    },
+    type: "desktop",
+  },
+  tabletLandscape: {
+    name: "Tablet Landscape",
+    styles: {
+      width: "1024px",
+      height: "768px",
+    },
+    type: "tablet",
+  },
+};
+
+const preview: Preview = {
+  parameters: {
+    docs: {
+      story: {
+        height: "700px",
+      },
+    },
+    viewport: {
+      viewports: {
+        ...customViewports,
+        ...MINIMAL_VIEWPORTS,
+        ...INITIAL_VIEWPORTS,
+      },
+    },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+};
+
+export default preview;
