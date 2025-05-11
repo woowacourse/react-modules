8c5d5003368dbda3208e7c839689b2e21bcd771f
guesung <gueit214@naver.com>
2025-05-11 11:36:27 +0900

    refactor: 중복 코드 제거

--- pesu-hooks/src/hooks/useForm/utils.ts
+++ pesu-hooks/src/hooks/useForm/utils.ts
@@ -2,7 +2,6 @@ import { Validation } from './types';
 
 export function validate(validation: Validation, value: string) {
   if (validation.required && !value) return validation.errorMessage;
-  if (validation.required && !value) return validation.errorMessage;
   if (validation.length && value.length !== validation.length) return validation.errorMessage;
   if (validation.validateRegex && !validation.validateRegex.test(value)) return validation.errorMessage;
 
