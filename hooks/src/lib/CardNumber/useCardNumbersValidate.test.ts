7a59e714eb38b50ab5de7045f95eb3eddf226036
ohgus <anakin1108@naver.com>
2025-05-12 02:12:56 +0900

    test: 테스트 로직 변경
    카드사 타입 별 포매팅, 카드 타입 설정이 잘 이루어지는지 체크

--- hooks/src/lib/CardNumber/useCardNumbersValidate.test.ts
+++ hooks/src/lib/CardNumber/useCardNumbersValidate.test.ts
@@ -1,41 +1,157 @@
 import { renderHook, act } from '@testing-library/react';
 
-import useCardNumbersValidate, {
-  CardNumberValidateResult
-} from './useCardNumbersValidate';
+import useCardNumbers, { CardNumbersResult } from './useCardNumbers';
 
 describe('useCardNumbersValidate', () => {
-  let result: { current: CardNumberValidateResult };
+  let result: { current: CardNumbersResult };
 
   beforeEach(() => {
-    const rendered = renderHook(() => useCardNumbersValidate());
+    const rendered = renderHook(() => useCardNumbers());
     result = rendered.result;
   });
 
-  it('숫자로 이루어진 4자리 값이 들어오면 isValid가 true이고 에러 메시지가 null이다.', () => {
+  it('4로 시작하는 16자리 카드 번호가 들어오면 카드 타입이 Visa이다.', () => {
     act(() => {
-      result.current.validateCardNumbers('1234', 'first');
+      result.current.handleCardNumberChange({
+        target: { value: '4123456789012345' }
+      } as React.ChangeEvent<HTMLInputElement>);
     });
 
-    expect(result.current.isValid.first).toBe(true);
+    expect(result.current.cardNumbers).toBe('4123456789012345');
+    expect(result.current.formattedCardNumbers).toBe('4123-4567-8901-2345');
+    expect(result.current.cardType).toBe('Visa');
+    expect(result.current.cardNumberMaxLength).toBe(16);
     expect(result.current.errorMessage).toBeNull();
   });
 
-  it('숫자로 이루어지지 않은 값이 들어오면 isValid가 false이고 에러 메시지가 나온다.', () => {
-    act(() => {
-      result.current.validateCardNumbers('-1', 'first');
+  describe('master 카드는 51 ~ 55로 시작하는 16자리 카드 번호이다.', () => {
+    it('51로 시작하는 16자리 카드 번호가 들어오면 카드 타입이 Master이다.', () => {
+      act(() => {
+        result.current.handleCardNumberChange({
+          target: { value: '5123456789012345' }
+        } as React.ChangeEvent<HTMLInputElement>);
+      });
+
+      expect(result.current.cardNumbers).toBe('5123456789012345');
+      expect(result.current.formattedCardNumbers).toBe('5123-4567-8901-2345');
+      expect(result.current.cardType).toBe('Master');
+      expect(result.current.cardNumberMaxLength).toBe(16);
+      expect(result.current.errorMessage).toBeNull();
     });
 
-    expect(result.current.isValid.first).toBe(false);
-    expect(result.current.errorMessage).toBe('숫자만 입력해주세요.');
+    it('55로 시작하는 16자리 카드 번호가 들어오면 카드 타입이 Master이다.', () => {
+      act(() => {
+        result.current.handleCardNumberChange({
+          target: { value: '5523456789012345' }
+        } as React.ChangeEvent<HTMLInputElement>);
+      });
+
+      expect(result.current.cardNumbers).toBe('5523456789012345');
+      expect(result.current.formattedCardNumbers).toBe('5523-4567-8901-2345');
+      expect(result.current.cardType).toBe('Master');
+      expect(result.current.cardNumberMaxLength).toBe(16);
+      expect(result.current.errorMessage).toBeNull();
+    });
   });
 
-  it('4자리 이상의 값이 들어오면 isValid가 false이고 에러 메시지가 나온다.', () => {
-    act(() => {
-      result.current.validateCardNumbers('12345', 'third');
+  describe('amex 카드는 34 또는 37로 시작하는 15자리 카드 번호이다.', () => {
+    it('34로 시작하는 15자리 카드 번호가 들어오면 카드 타입이 Amex이다.', () => {
+      act(() => {
+        result.current.handleCardNumberChange({
+          target: { value: '342345678901234' }
+        } as React.ChangeEvent<HTMLInputElement>);
+      });
+
+      expect(result.current.cardNumbers).toBe('342345678901234');
+      expect(result.current.formattedCardNumbers).toBe('3423-456789-01234');
+      expect(result.current.cardType).toBe('Amex');
+      expect(result.current.cardNumberMaxLength).toBe(15);
+      expect(result.current.errorMessage).toBeNull();
     });
 
-    expect(result.current.isValid.third).toBe(false);
-    expect(result.current.errorMessage).toBe('4자리만 입력해주세요.');
+    it('37로 시작하는 15자리 카드 번호가 들어오면 카드 타입이 Amex이다.', () => {
+      act(() => {
+        result.current.handleCardNumberChange({
+          target: { value: '372345678901234' }
+        } as React.ChangeEvent<HTMLInputElement>);
+      });
+
+      expect(result.current.cardNumbers).toBe('372345678901234');
+      expect(result.current.formattedCardNumbers).toBe('3723-456789-01234');
+      expect(result.current.cardType).toBe('Amex');
+      expect(result.current.cardNumberMaxLength).toBe(15);
+      expect(result.current.errorMessage).toBeNull();
+    });
+  });
+  describe('diners 카드는 36 ~ 38로 시작하는 14자리 카드 번호이다.', () => {
+    it('36로 시작하는 14자리 카드 번호가 들어오면 카드 타입이 Diners이다.', () => {
+      act(() => {
+        result.current.handleCardNumberChange({
+          target: { value: '36234567890123' }
+        } as React.ChangeEvent<HTMLInputElement>);
+      });
+
+      expect(result.current.cardNumbers).toBe('36234567890123');
+      expect(result.current.formattedCardNumbers).toBe('3623-456789-0123');
+      expect(result.current.cardType).toBe('Diners');
+      expect(result.current.cardNumberMaxLength).toBe(14);
+      expect(result.current.errorMessage).toBeNull();
+    });
+  });
+
+  describe('unionpay 카드는 62로 시작하는 16자리 카드 번호이다.', () => {
+    it('622126 ~ 622925로 시작하고 16자리 카드 번호이다.', () => {
+      act(() => {
+        result.current.handleCardNumberChange({
+          target: { value: '6221262345678901' }
+        } as React.ChangeEvent<HTMLInputElement>);
+      });
+
+      expect(result.current.cardNumbers).toBe('6221262345678901');
+      expect(result.current.formattedCardNumbers).toBe('6221-2623-4567-8901');
+      expect(result.current.cardType).toBe('UnionPay');
+      expect(result.current.cardNumberMaxLength).toBe(16);
+      expect(result.current.errorMessage).toBeNull();
+    });
+
+    it('6282 ~ 6288로 시작하고 16자리 카드 번호이다.', () => {
+      act(() => {
+        result.current.handleCardNumberChange({
+          target: { value: '6282234567890123' }
+        } as React.ChangeEvent<HTMLInputElement>);
+      });
+
+      expect(result.current.cardNumbers).toBe('6282234567890123');
+      expect(result.current.formattedCardNumbers).toBe('6282-2345-6789-0123');
+      expect(result.current.cardType).toBe('UnionPay');
+      expect(result.current.cardNumberMaxLength).toBe(16);
+      expect(result.current.errorMessage).toBeNull();
+    });
+
+    it('6282 ~ 6288로 시작하고 16자리 카드 번호이다.', () => {
+      act(() => {
+        result.current.handleCardNumberChange({
+          target: { value: '6282234567890123' }
+        } as React.ChangeEvent<HTMLInputElement>);
+      });
+
+      expect(result.current.cardNumbers).toBe('6282234567890123');
+      expect(result.current.formattedCardNumbers).toBe('6282-2345-6789-0123');
+      expect(result.current.cardType).toBe('UnionPay');
+      expect(result.current.cardNumberMaxLength).toBe(16);
+      expect(result.current.errorMessage).toBeNull();
+    });
+  });
+
+  describe('숫자가 아닌 문자가 들어오면 에러 메시지가 나온다.', () => {
+    it('숫자가 아닌 문자가 들어오면 에러 메시지가 나온다.', () => {
+      act(() => {
+        result.current.handleCardNumberChange({
+          target: { value: '12345a' }
+        } as React.ChangeEvent<HTMLInputElement>);
+      });
+
+      expect(result.current.errorMessage).toBe('숫자만 입력해주세요.');
+    });
   });
 });
