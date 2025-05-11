4960d39d5bf8d2eec22e997964f91a4eeb47a988
Minje Sung <minje813@dgu.ac.kr>
2025-05-11 17:59:51 +0900

    feat: useSingleNumberInput 유효성 검사 결과 반환기능 구현

--- hooks/src/lib/useSingleNumberInput.ts
+++ hooks/src/lib/useSingleNumberInput.ts
@@ -1,9 +1,19 @@
 import { useState } from 'react';
+import isNumeric from './utils/isNumeric';
+import isNotOverThanMax from './utils/isNotOverThanMax';
 
-const useSingleNumberInput = () => {
+const useSingleNumberInput = (maxLength: number) => {
   const [inputValue, setInputValue] = useState('');
 
-  return { inputValue, setInputValue };
+  const errorText = (() => {
+    if (!isNumeric(inputValue)) return '입력값이 숫자가 아닙니다.';
+    if (!isNotOverThanMax(inputValue, maxLength)) return '입력값이 최대 길이를 초과했습니다.';
+    return '';
+  })();
+
+  const isError = errorText !== '';
+
+  return { inputValue, setInputValue, errorInfo: { isError, errorText } };
 };
 
 export default useSingleNumberInput;
