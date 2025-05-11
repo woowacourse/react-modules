34a6f43f0e87acaee6afb9660d3cd4c7d0aa14bc
dev-dino22 <wlsskdud02@naver.com>
2025-05-11 19:34:11 +0900

    feat: BasicModal 컴포넌트 스토리북 작성

---
+++ modal-components/src/storybook/BasicModal.stories.tsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import BasicModal from "../lib/components/modals/BasicModal";
+
+export default {
+  title: "Modals/BasicModal",
+  component: BasicModal,
+};
+
+export const Default = () => {
+  const [isOpened, setIsOpened] = useState(false);
+  const onCloseHandler = () => setIsOpened(false);
+
+  return (
+    <>
+      <button onClick={() => setIsOpened(true)}>기본 모달 열기 버튼</button>
+      {isOpened && (
+        <BasicModal
+          modalPosition="center"
+          modalSize="medium"
+          closeType="top"
+          titleText="기본 모달"
+          onClose={onCloseHandler}
+        >
+          <div>여기에 원하는 내용을 넣으세요.</div>
+        </BasicModal>
+      )}
+    </>
+  );
+};
