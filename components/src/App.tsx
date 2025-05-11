7fb4db834e66ffb1514efc982a628fc1d60713ea
ShinjungOh <tlswjd9260@gmail.com>
2025-05-11 18:00:36 +0900

    feat: ConfirmModal UI 구현

--- components/src/App.tsx
+++ components/src/App.tsx
@@ -1,10 +1,12 @@
 // import Modal from "./lib/Modal.tsx";
-import AlertModal from "./lib/Modal/AlertModal.tsx";
+// import AlertModal from "./lib/Modal/AlertModal.tsx";
+import ConfirmModal from "./lib/Modal/ConfirmModal.tsx";
 
 function App() {
   return <div>
     {/*<Modal size='sm' position="center" onClose={() => console.log("close")}/>*/}
-    <AlertModal size='sm' message='비밀번호' description='필수입니다' onConfirm={() => console.log("close")}/>
+    {/*<AlertModal size='sm' message='비밀번호' description='필수입니다' onConfirm={() => console.log("close")}/>*/}
+    <ConfirmModal size='sm' message='비밀번호' description='필수입니다' onConfirm={() => console.log("close")}/>
   </div>;
 }
 
--- components/src/lib/Modal/ConfirmModal.tsx
+++ components/src/lib/Modal/ConfirmModal.tsx
@@ -1,26 +1,28 @@
 import Modal from '../Modal';
 import styled from '@emotion/styled';
 
-type AlertModalProps = {
+type ConfirmModalProps = {
   message: string;
   description?: string;
+  onCancel: () => void;
   onConfirm: () => void;
   size?: 'sm' | 'md' | 'lg';
 };
 
-function AlertModal({message, description, onConfirm, size = 'md'}: AlertModalProps) {
+function ConfirmModal({message, description, onCancel, onConfirm, size = 'md'}: ConfirmModalProps) {
   return (
     <Modal position="center" size={size} onClose={onConfirm}>
       <Message>{message}</Message>
       <Description>{description}</Description>
       <ButtonContainer>
+        <CancelButton onClick={onCancel}>취소</CancelButton>
         <ConfirmButton onClick={onConfirm}>확인</ConfirmButton>
       </ButtonContainer>
     </Modal>
   );
 }
 
-export default AlertModal;
+export default ConfirmModal;
 
 const Message = styled.p`
     margin: 0;
@@ -43,6 +45,22 @@ const ButtonContainer = styled.div`
     justify-content: flex-end;
     flex-direction: row;
     align-items: flex-end;
+    gap: 12px;
+`;
+
+const CancelButton = styled.button`
+    width: 80px;
+    height: 36px;
+    border-radius: 5px;
+    font-weight: 700;
+    font-size: 15px;
+    background-color: #ffffff;
+    color: #333333BF;
+    border: 1px solid #33333340;
+
+    &:hover {
+        background-color: rgba(206, 206, 206, 0.6);
+    }
 `;
 
 const ConfirmButton = styled.button`
