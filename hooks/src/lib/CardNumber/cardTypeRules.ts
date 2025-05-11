817bd8263c7bb807cad3dc04b941d15b08178ef6
ohgus <anakin1108@naver.com>
2025-05-12 02:09:39 +0900

    feat: 카드 타입 별 길이, 포맷 추가

---
+++ hooks/src/lib/CardNumber/cardTypeRules.ts
@@ -0,0 +1,22 @@
+export const cardTypeRules = {
+  Visa: {
+    length: 16,
+    format: ['4', '4', '4', '4']
+  },
+  Master: {
+    length: 16,
+    format: ['4', '4', '4', '4']
+  },
+  Amex: {
+    length: 15,
+    format: ['4', '6', '5']
+  },
+  Diners: {
+    length: 14,
+    format: ['4', '6', '4']
+  },
+  UnionPay: {
+    length: 16,
+    format: ['4', '4', '4', '4']
+  }
+};
