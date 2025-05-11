10863f71e5d3a6513a876faac62956de0045d4c5
eunoia-jaxson <eunoia@handong.ac.kr>
2025-05-12 02:34:20 +0900

    feat: 카드 브랜드 length 상수 정의

--- hooks/src/lib/card.contant.ts
+++ hooks/src/lib/card.contant.ts
@@ -1,10 +1,19 @@
-import { CardExpiration } from './card.type';
+import { CardBrand, CardExpiration } from './card.type';
 
 const INITIAL_CARD_EXPIRATION: CardExpiration = {
   month: '',
   year: '',
 };
 
+const BRAND_LENGTHS: Record<CardBrand, number> = {
+  Visa: 16,
+  Mastercard: 16,
+  AMEX: 15,
+  Diners: 14,
+  UnionPay: 16,
+  Unknown: 0,
+};
+
 const CARD_EXPIRATION_KEYS = Object.keys(INITIAL_CARD_EXPIRATION) as Array<keyof CardExpiration>;
 
-export { INITIAL_CARD_EXPIRATION, CARD_EXPIRATION_KEYS };
+export { INITIAL_CARD_EXPIRATION, CARD_EXPIRATION_KEYS, BRAND_LENGTHS };
