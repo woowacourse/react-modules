2bd4d245a20d4dd63d21cb0541081b98e9688f09
minji2219 <aabb0219@naver.com>
2025-05-11 14:44:11 +0900

    feat: hooks 타입 생성

---
+++ hooks/src/lib/index.d.ts
@@ -0,0 +1,35 @@
+export interface ValidationType {
+  isError: boolean;
+  errorMessage: string | null;
+}
+
+export interface UseCardNumberResult {
+  onChange: (value: string) => void;
+  cardNumber: string;
+  cardNumberValidationResult: ValidationType;
+  cardBrand?: string;
+  formattingCardNumber: string;
+}
+
+export interface UseExpirationDateResult {
+  onChange: (label: string, value: string) => void;
+  expirationDate: {month: string; year: string};
+  expirationDateValidationResult: {month: ValidationType; year: ValidationType};
+}
+
+export interface UseCvcResult {
+  onChange: (value: string) => void;
+  cvc: string;
+  cvcValidationResult: ValidationType;
+}
+
+export interface UsePasswordResult {
+  onChange: (value: string) => void;
+  password: string;
+  passwordValidationResult: ValidationType;
+}
+
+export declare function useCardNumber(): UseCardNumberResult;
+export declare function useExpirationDate(): UseExpirationDateResult;
+export declare function useCvc(): UseCvcResult;
+export declare function usePassword(): UsePasswordResult;
