55eea4ff2a41dc7f765842ca00d157d0cb9f7db4
Minje Sung <minje813@dgu.ac.kr>
2025-05-11 21:45:03 +0900

    feat: getRangeStringList 유틸 함수 구현

--- hooks/src/lib/utils/index.ts
+++ hooks/src/lib/utils/index.ts
@@ -1,2 +1,3 @@
 export { default as isNumeric } from './isNumeric';
 export { default as isNotOverMaxLength } from './isNotOverMaxLength';
+export { default as getRangeStringList } from './getRangeStringList';
