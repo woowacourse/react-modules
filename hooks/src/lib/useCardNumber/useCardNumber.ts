c14a1e5bc7e59416ca3e27ec10f59254d9e40754
ShinjungOh <tlswjd9260@gmail.com>
2025-05-12 15:33:42 +0900

    feat: useCardNumber에서 카드 브랜드 유효성 검사 훅 적용

--- hooks/src/lib/useCardNumber/useCardNumber.ts
+++ hooks/src/lib/useCardNumber/useCardNumber.ts
@@ -4,12 +4,14 @@ import { CARD_NUMBER_ERROR } from '../constants/errorMessages';
 import { isOnlyDigits } from '../utils/validateNumber';
 import { CARD_NUMBER } from '../constants/cardConfig';
 import { useCardFormatter } from "../useCardFormatter";
+import { useCardBrandValidation } from "../useCardBrandValidation";
 
 export const useCardNumber = (initialCardNumber: CardNumber, initialError: CardNumberError) => {
   const [cardNumber, setCardNumber] = useState<CardNumber>(initialCardNumber);
   const [cardNumberError, setCardNumberError] = useState<CardNumberError>(initialError);
   const rawNumber = `${cardNumber.first}${cardNumber.second}${cardNumber.third}${cardNumber.fourth}`;
   const { formattedNumber, handleCardNumberChange: handleFormatChange } = useCardFormatter(rawNumber);
+  const { cardBrand, isValid: isBrandValid, error: brandError } = useCardBrandValidation(formattedNumber);
 
   const handleCardNumberChange = (e: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
@@ -34,24 +36,41 @@ export const useCardNumber = (initialCardNumber: CardNumber, initialError: CardN
 
       return;
     }
+
+    if (brandError) {
+      setCardNumberError(prevErrors => ({
+        ...prevErrors,
+        [name]: brandError,
+      }));
+    } else {
+      setCardNumberError(prevErrors => ({
+        ...prevErrors,
+        [name]: null,
+      }));
+    }
   };
 
   const isCardNumberValid = () => {
     const { first, second, third, fourth } = cardNumber;
     const length = CARD_NUMBER.maxLength;
 
-    return (
+    const isBasicFormatValid = (
       first.length === length &&
       second.length === length &&
       third.length === length &&
       fourth.length === length
     );
+
+    return isBasicFormatValid && isBrandValid;
   };
 
   return {
     cardNumber,
     cardNumberError,
     formattedNumber,
+    cardBrand,
+    isBrandValid,
+    brandError,
     handleCardNumberChange,
     isCardNumberValid,
   };
