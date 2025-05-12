e3b8c7610e6969f6e0004ede21fed6cfad835451
deokbin <deokbin@naver.com>
2025-05-12 09:49:08 +0900

    test: 초기 상태 테스트 추가 및 테스트 명세 변경

--- hooks/src/lib/useCardCompany/useCardCompany.test.ts
+++ hooks/src/lib/useCardCompany/useCardCompany.test.ts
@@ -2,7 +2,14 @@ import { renderHook, act } from "@testing-library/react";
 import useCardCompany from "./useCardCompany";
 
 describe("useCardCompany 테스트", () => {
-  it("CardCompany가 선택되었을 때 isValid는 true, errorMessage는 빈 문자열인지 확인한다.", () => {
+  it("초기 상태에서는 errorState는 false, message는 빈 문자열이어야 한다.", () => {
+    const { result } = renderHook(() => useCardCompany());
+
+    expect(result.current.validationResult.errorState).toBe(false);
+    expect(result.current.validationResult.message).toEqual("");
+  });
+
+  it("CardCompany가 선택되었을 때 errorState는 false, message는 빈 문자열이어야 한다.", () => {
     const { result } = renderHook(() => useCardCompany());
 
     act(() => {
@@ -15,7 +22,7 @@ describe("useCardCompany 테스트", () => {
     expect(result.current.validationResult.message).toEqual("");
   });
 
-  it("CardCompany가 선택되지 않았을 때 isValid는 false, errorMessage 값이 반환되는지 확인한다.", () => {
+  it("CardCompany가 선택되지 않았을 때 errorState는 true, message는 '카드사를 선택해주세요' 여야 한다. ", () => {
     const { result } = renderHook(() => useCardCompany());
     act(() => {
       result.current.handleChange({
