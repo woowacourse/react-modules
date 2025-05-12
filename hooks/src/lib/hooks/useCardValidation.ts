bb77dace036b0f4dcb09d23145205b3b906e6c11
spoyodevelop <kjy9505@gmail.com>
2025-05-12 15:42:24 +0900

    feat: add useCardNetwork and useCardFormat hooks, update useCardValidation to remove unused dependencies

--- hooks/src/lib/hooks/useCardValidation.ts
+++ hooks/src/lib/hooks/useCardValidation.ts
@@ -3,32 +3,13 @@ import useStrictCardNumber from "./useStrictCardNumber";
 import useCVCNumber from "./useCVCNumber";
 import useExpiryDateNumber from "./useExpiryDateNumber";
 import usePasswordNumber from "./usePasswordNumber";
-import useCardNetwork from "./useCardNetwork";
-import useCardFormat from "./useCardFormat";
-import { UseCardFormatOptions } from "./useCardFormat";
 
-interface UseCardValidationOptions {
-  formatOptions?: UseCardFormatOptions;
-}
-
-export default function useCardValidation(
-  option: UseCardValidationOptions = {
-    formatOptions: { placeholderChar: "X", splitter: " " },
-  }
-) {
-  const { formatOptions } = option;
-  const { splitter, placeholderChar } = formatOptions || {
-    splitter: " ",
-    placeholderChar: "X",
-  };
-
+export default function useCardValidation() {
   const card = useCardNumber();
   const strictCard = useStrictCardNumber();
   const cvc = useCVCNumber();
   const expiry = useExpiryDateNumber();
   const password = usePasswordNumber();
-  const network = useCardNetwork();
-  const format = useCardFormat({ splitter, placeholderChar });
 
-  return { card, cvc, expiry, password, network, strictCard, format };
+  return { card, cvc, expiry, password, strictCard };
 }
