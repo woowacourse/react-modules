ee3e9808ab90d612740e93403d5ca808b8ca533c
dlsxjzld <sinjuk1@naver.com>
2025-05-12 15:14:48 +0900

    fix: validateCardInput 함수에서 유효성 검사 결과에 isValid 속성 추가

--- hooks/src/lib/validation/cardNumberLengthInfo.ts
+++ hooks/src/lib/validation/cardNumberLengthInfo.ts
@@ -2,16 +2,19 @@ export const validateCardInput = (value: string, validLength: number) => {
   if (/^[0-9]+$/.test(value) === false) {
     return {
       errorMessage: `숫자(0~9)만 입력 가능합니다.`,
+      isValid: false,
     };
   }
 
   if (value.length < validLength || value.length > validLength) {
     return {
       errorMessage: `숫자 ${validLength}자리를 입력해주세요.`,
+      isValid: false,
     };
   }
 
   return {
     errorMessage: '',
+    isValid: true,
   };
 };
