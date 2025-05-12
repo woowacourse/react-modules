fa622da6205b859b6c93a32da57fc2783adc2ffb
spoyodevelop <kjy9505@gmail.com>
2025-05-12 17:21:51 +0900

    refactor: enhance useCardValidation to return a structured interface for card validation results

--- hooks/src/lib/hooks/useCardValidation.ts
+++ hooks/src/lib/hooks/useCardValidation.ts
@@ -3,13 +3,20 @@ import useStrictCardNumber from "./useStrictCardNumber";
 import useCVCNumber from "./useCVCNumber";
 import useExpiryDateNumber from "./useExpiryDateNumber";
 import usePasswordNumber from "./usePasswordNumber";
+export interface UseCardValidationReturn {
+  card: ReturnType<typeof useCardNumber>;
+  strictCard: ReturnType<typeof useStrictCardNumber>;
+  cvc: ReturnType<typeof useCVCNumber>;
+  expiry: ReturnType<typeof useExpiryDateNumber>;
+  password: ReturnType<typeof usePasswordNumber>;
+}
 
-export default function useCardValidation() {
+export default function useCardValidation(): UseCardValidationReturn {
   const card = useCardNumber();
   const strictCard = useStrictCardNumber();
   const cvc = useCVCNumber();
   const expiry = useExpiryDateNumber();
   const password = usePasswordNumber();
 
-  return { card, cvc, expiry, password, strictCard };
+  return { card, strictCard, cvc, expiry, password };
 }
