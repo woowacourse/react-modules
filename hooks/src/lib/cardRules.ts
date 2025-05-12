09860dba8f49430b373572159b16fe47a83a299f
Beomtae <95863810+Beomtae@users.noreply.github.com>
2025-05-12 15:56:33 +0900

    ♻️ refactor: 카드사 관련 공통 관심사 통합

--- hooks/src/lib/cardRules.ts
+++ hooks/src/lib/cardRules.ts
@@ -1,22 +1,53 @@
-export const CARD_TYPE_RULES = {
-  VISA: (prefix: string) => prefix.startsWith("4"),
-  MasterCard: (prefix: string) => {
-    const p = Number(prefix.slice(0, 2));
-    return p >= 51 && p <= 55;
+import { CardNumber, CardType } from "./types/Card";
+
+export interface CardRule {
+  type: CardType;
+  match: (cardNumber: string) => boolean;
+  numberLengths: Record<keyof CardNumber, number>;
+}
+
+export const cardRules: CardRule[] = [
+  {
+    type: "VISA",
+    match: (prefix: string) => {
+      const p = Number(prefix.slice(0, 2));
+      return p >= 51 && p <= 55;
+    },
+    numberLengths: { first: 4, second: 4, third: 4, fourth: 4 },
   },
-  AMEX: (prefix: string) => {
-    const p = prefix.slice(0, 2);
-    return p === "34" || p === "37";
+  {
+    type: "MasterCard",
+    match: (prefix: string) => {
+      const p = Number(prefix.slice(0, 2));
+      return p >= 51 && p <= 55;
+    },
+    numberLengths: { first: 4, second: 4, third: 4, fourth: 4 },
   },
-  Diners: (prefix: string) => prefix.startsWith("36"),
-  UnionPay: (prefix: string) => {
-    const p3 = Number(prefix.slice(0, 3));
-    const p4 = Number(prefix.slice(0, 4));
-    const p6 = Number(prefix.slice(0, 6));
-    return (
-      (p6 >= 622126 && p6 <= 622925) ||
-      (p3 >= 624 && p3 <= 626) ||
-      (p4 >= 6282 && p4 <= 6288)
-    );
+  {
+    type: "AMEX",
+    match: (prefix: string) => {
+      const p = prefix.slice(0, 2);
+      return p === "34" || p === "37";
+    },
+    numberLengths: { first: 4, second: 6, third: 5, fourth: 0 },
   },
-};
+  {
+    type: "Diners",
+    match: (prefix: string) => prefix.startsWith("36"),
+    numberLengths: { first: 4, second: 6, third: 4, fourth: 0 },
+  },
+  {
+    type: "UnionPay",
+    match: (prefix: string) => {
+      const p3 = Number(prefix.slice(0, 3));
+      const p4 = Number(prefix.slice(0, 4));
+      const p6 = Number(prefix.slice(0, 6));
+      return (
+        (p6 >= 622126 && p6 <= 622925) ||
+        (p3 >= 624 && p3 <= 626) ||
+        (p4 >= 6282 && p4 <= 6288)
+      );
+    },
+    numberLengths: { first: 4, second: 4, third: 4, fourth: 4 },
+  },
+];
--- hooks/src/lib/getCardType.ts
+++ hooks/src/lib/getCardType.ts
@@ -1,14 +1,13 @@
-import { CARD_TYPE_RULES } from "./cardRules";
+import { cardRules } from "./cardRules";
 import { CardNumber, CardType } from "./types/Card";
 
 export function getCardType(cardNumber: CardNumber): CardType {
   const cardNumberPrefix = `${cardNumber.first}${cardNumber.second}${cardNumber.third}${cardNumber.fourth}`;
 
-  for (const [cardType, matcher] of Object.entries(CARD_TYPE_RULES)) {
-    if (matcher(cardNumberPrefix)) {
-      return cardType as CardType;
+  cardRules.map((cardRule) => {
+    if (cardRule.match(cardNumberPrefix)) {
+      return cardRule.type;
     }
-  }
-
+  });
   return "None";
 }
