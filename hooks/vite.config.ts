5a735a1b8ecb18a368ba265eb3409a3c759ebff6
kimyou1102 <yoosk5485@naver.com>
2025-05-12 16:58:59 +0900

    chore: base url 설정

--- hooks/vite.config.ts
+++ hooks/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc';
 import dts from 'vite-plugin-dts';
 
 export default defineConfig({
+  base: '/react-modules/',
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/lib/index.ts'),
