c3b01758cc0f85df439043107029131becdbcb7c
dlsxjzld <sinjuk1@naver.com>
2025-05-12 09:20:00 +0900

    refactor: 카드 번호 배열 객체에서 하나의 문자값으로 변경 및 유효성 검사 로직을 16자리로 수정하고 핸들러 인자를 간소화

--- hooks/src/__test__/useCardNumber.test.tsx
+++ hooks/src/__test__/useCardNumber.test.tsx
@@ -2,15 +2,15 @@ import { act, renderHook } from '@testing-library/react';
 import { useCardNumber } from '../lib/hooks/useCardNumber/useCardNumber';
 
 describe('카드 번호 검증 테스트입니다.', () => {
-  test('사용자가 입력한 value값의 길이와 validLength(4)가 같다면, errorMessage를 빈 값으로 반환한다.', () => {
+  test('사용자가 입력한 value값의 길이와 validLength(16)가 같다면, errorMessage를 빈 값으로 반환한다.', () => {
     const { result } = renderHook(() => useCardNumber());
 
     const mockEvent = {
-      target: { value: '1234' },
+      target: { value: '1234123412341234' },
     } as React.ChangeEvent<HTMLInputElement>;
 
     act(() => {
-      result.current.handleCardNumberChange(mockEvent, 0);
+      result.current.handleCardNumberChange(mockEvent);
     });
 
     expect(result.current.errorMessage).toBe('');
@@ -23,13 +23,13 @@ describe('카드 번호 검증 테스트입니다.', () => {
     } as React.ChangeEvent<HTMLInputElement>;
 
     act(() => {
-      result.current.handleCardNumberChange(mockEvent, 0);
+      result.current.handleCardNumberChange(mockEvent);
     });
 
     expect(result.current.errorMessage).toBe('숫자(0~9)만 입력 가능합니다.');
   });
 
-  test('사용자가 입력한 value값의 길이가 validLength(4)보다 작다면 errorMessage를 반환한다.', () => {
+  test('사용자가 입력한 value값의 길이가 validLength(16)보다 작다면 errorMessage를 반환한다.', () => {
     const { result } = renderHook(() => useCardNumber());
 
     const mockEvent = {
@@ -37,13 +37,13 @@ describe('카드 번호 검증 테스트입니다.', () => {
     } as React.ChangeEvent<HTMLInputElement>;
 
     act(() => {
-      result.current.handleCardNumberChange(mockEvent, 0);
+      result.current.handleCardNumberChange(mockEvent);
     });
 
-    expect(result.current.errorMessage).toBe('숫자 4자리를 입력해주세요.');
+    expect(result.current.errorMessage).toBe('숫자 16자리를 입력해주세요.');
   });
 
-  test('사용자가 입력한 value값의 길이가 validLength(4)보다 크다면 errorMessage를 반환한다.', () => {
+  test('사용자가 입력한 value값의 길이가 validLength(16)보다 크다면 errorMessage를 반환한다.', () => {
     const { result } = renderHook(() => useCardNumber());
 
     const mockEvent = {
@@ -51,9 +51,9 @@ describe('카드 번호 검증 테스트입니다.', () => {
     } as React.ChangeEvent<HTMLInputElement>;
 
     act(() => {
-      result.current.handleCardNumberChange(mockEvent, 0);
+      result.current.handleCardNumberChange(mockEvent);
     });
 
-    expect(result.current.errorMessage).toBe('숫자 4자리를 입력해주세요.');
+    expect(result.current.errorMessage).toBe('숫자 16자리를 입력해주세요.');
   });
 });
--- hooks/src/lib/hooks/useCardNumber/useCardNumber.ts
+++ hooks/src/lib/hooks/useCardNumber/useCardNumber.ts
@@ -1,9 +1,7 @@
 import { useState } from 'react';
 import { validateCardInput } from '../../validation/cardNumberLengthInfo';
 
-import { CardInputItem } from '../../types/cardInputItem.types';
-
-const CARD_NUMBER_INPUTS_LENGTH = 4;
+const CARD_NUMBER_INPUTS_LENGTH = 16; // 카드 번호의 길이 (16자리)
 
 /**
  *
@@ -13,34 +11,28 @@ const CARD_NUMBER_INPUTS_LENGTH = 4;
  * @returns 카드 번호 입력 상태와 핸들러를 반환합니다.
  * @returns cardNumbers - 카드 번호 입력 상태 배열 (4개 항목)
  * @returns errorMessage 카드 번호 입력 오류 메시지
+ * @returns isValid 카드 번호 입력 유효성 검사 결과
  * @returns handleCardNumberChange 카드 번호 입력 핸들러
  * @example
- * const { cardNumbers, errorMessage, handleCardNumberChange } = useCardNumber();
+ * const { cardNumbers, errorMessage, isValid, handleCardNumberChange } = useCardNumber();
  * <input
  *   type="text"
- *   value={cardNumbers[0].value}
- *   onChange={(e) => handleCardNumberChange(e, 0)}
+ *   value={cardNumbers}
+ *   onChange={(e) => handleCardNumberChange(e)}
  *   placeholder="카드 번호를 입력하세요"
  * />
  */
 export const useCardNumber = () => {
-  const [cardNumbers, setCardNumbers] = useState<CardInputItem[]>(
-    Array.from({ length: CARD_NUMBER_INPUTS_LENGTH }, () => ({ value: '', isValid: true }))
-  );
+  const [cardNumbers, setCardNumbers] = useState('');
 
   const [errorMessage, setErrorMessage] = useState('');
 
-  const handleCardNumberChange = (event: React.ChangeEvent<HTMLInputElement>, index: number) => {
+  const handleCardNumberChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const newValue = event.target.value;
 
     const { isValid, errorMessage } = validateCardInput(newValue, CARD_NUMBER_INPUTS_LENGTH);
 
-    setCardNumbers((prev) => {
-      const newCardNumbers = [...prev];
-      newCardNumbers[index].value = newValue;
-      newCardNumbers[index].isValid = isValid;
-      return newCardNumbers;
-    });
+    setCardNumbers(newValue);
 
     if (!isValid) {
       setErrorMessage(errorMessage);
@@ -52,6 +44,7 @@ export const useCardNumber = () => {
   return {
     cardNumbers,
     errorMessage,
+    isValid: errorMessage === '',
     handleCardNumberChange,
   };
 };
