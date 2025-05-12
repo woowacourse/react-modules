a5df7be82768483c9593e4ddf17b511585c5c880
dlsxjzld <sinjuk1@naver.com>
2025-05-12 10:20:56 +0900

    feat: 카드 번호 유효성 검사 로직을 개선하고 카드 타입을 동적으로 설정하도록 수정

--- hooks/src/lib/hooks/useCardNumber/useCardNumber.ts
+++ hooks/src/lib/hooks/useCardNumber/useCardNumber.ts
@@ -1,8 +1,6 @@
 import { useState } from 'react';
 import { validateCardInput } from '../../validation/cardNumberLengthInfo';
 
-const CARD_NUMBER_INPUTS_LENGTH = 16; // 카드 번호의 길이 (16자리)
-
 /**
  *
  * @description
@@ -24,15 +22,19 @@ const CARD_NUMBER_INPUTS_LENGTH = 16; // 카드 번호의 길이 (16자리)
  */
 export const useCardNumber = () => {
   const [cardNumbers, setCardNumbers] = useState('');
-
   const [errorMessage, setErrorMessage] = useState('');
+  const [cardType, setCardType] = useState('Unknown');
 
   const handleCardNumberChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const newValue = event.target.value;
+    // 카드 번호가 들어오면 카드의 타입을 검사하고 공통 유효성 검사(14~16자리, 숫자만 입력 가능)을 한다.
+    // 카드 타입과 카드 길이를 받으면 카드 길이가 유효한지를 검사한다.
+    const { type, numberLengths } = getCardType({ cardNumber: newValue, cardRules: cardRules });
 
-    const { isValid, errorMessage } = validateCardInput(newValue, CARD_NUMBER_INPUTS_LENGTH);
+    const { isValid, errorMessage } = validateCardInput(newValue, numberLengths);
 
     setCardNumbers(newValue);
+    setCardType(type);
 
     if (!isValid) {
       setErrorMessage(errorMessage);
@@ -45,6 +47,64 @@ export const useCardNumber = () => {
     cardNumbers,
     errorMessage,
     isValid: errorMessage === '',
+    cardType,
     handleCardNumberChange,
+  };
+};
+
+const CARD_BRANDS = {
+  Visa: 'Visa',
+  MasterCard: 'MasterCard',
+  Unknown: 'Unknown',
+} as const;
+
+type CardType = keyof typeof CARD_BRANDS;
+
+const isCardType = (cardType: string): cardType is CardType => {
+  return cardType in CARD_BRANDS;
+};
+
+interface CardRule {
+  type: CardType;
+  match: (cardNumber: string) => boolean;
+  numberLengths: number;
+}
+
+const cardRules: CardRule[] = [
+  {
+    type: 'Visa',
+    // Visa 카드 번호는 4로 시작합니다.
+    match: (cardNumber: string) => /^4[0-9]/.test(cardNumber),
+    numberLengths: 16,
+  },
+  {
+    type: 'MasterCard',
+    // MasterCard 카드 번호는 51~55로 시작합니다.
+    match: (cardNumber: string) => /^5[1-5][0-9]/.test(cardNumber),
+    numberLengths: 16,
+  },
+];
+
+// cardRules를 순회하면서 카드 번호가 일치하는 규칙이 있으면 type과 numberLengths를 반환합니다.
+export const getCardType = ({
+  cardNumber,
+  cardRules,
+}: {
+  cardNumber: string;
+  cardRules: CardRule[];
+}): Omit<CardRule, 'match'> => {
+  for (const cardRule of cardRules) {
+    const { type, match, numberLengths } = cardRule;
+    if (match(cardNumber) && isCardType(type)) {
+      return {
+        type,
+        numberLengths,
+      };
+    }
+  }
+
+  return {
+    type: 'Unknown',
+    numberLengths: 16,
   };
 };
