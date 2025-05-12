68d7d7fd88e9d593f08fb5ea53be35c5822ac15a
Minje Sung <minje813@dgu.ac.kr>
2025-05-12 16:38:28 +0900

    refactor: 카드 브랜드 관련 정보 상수 분리

--- hooks/src/lib/utils/checkCardBrand.ts
+++ hooks/src/lib/utils/checkCardBrand.ts
@@ -1,19 +1,11 @@
-import getRangeStringList from './getRangeStringList';
+import CARD_BRAND_INFO from '../constants/CARD_BRAND_INFO';
 
-const checkCardBrand = (cardNumber: string) => {
-  if (cardNumber.startsWith('4') && cardNumber.length === 16) return 'Visa';
-  for (let prefix of getRangeStringList(51, 55))
-    if (cardNumber.startsWith(prefix) && cardNumber.length === 16) return 'MasterCard';
-  if (cardNumber.startsWith('36') && cardNumber.length === 14) return 'Diners';
-  for (let prefix of ['34', '37'])
-    if (cardNumber.startsWith(prefix) && cardNumber.length === 15) return 'AMEX';
-  for (let prefix of getRangeStringList(622126, 622925))
-    if (cardNumber.startsWith(prefix) && cardNumber.length === 16) return '유니온페이';
-  for (let prefix of getRangeStringList(624, 626))
-    if (cardNumber.startsWith(prefix) && cardNumber.length === 16) return '유니온페이';
-  for (let prefix of getRangeStringList(6282, 6288))
-    if (cardNumber.startsWith(prefix) && cardNumber.length === 16) return '유니온페이';
-
+const checkCardBrand = (cardNumber: string): string => {
+  for (const [brand, { PREFIX, LENGTH }] of Object.entries(CARD_BRAND_INFO)) {
+    if (cardNumber.length === LENGTH && PREFIX.some((prefix) => cardNumber.startsWith(prefix))) {
+      return brand;
+    }
+  }
   return '';
 };
 
