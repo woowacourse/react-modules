f1eeb70609f23a55318bbec35fd6255ff91e131b
Jeongeun Lee <leeju1009@gmail.com>
2025-05-13 18:13:09 +0900

    refactor: 카드 유효기간 입력에 대한 inputField의 설명 수정

--- hooks/src/App.tsx
+++ hooks/src/App.tsx
@@ -40,7 +40,7 @@ function App() {
             expiryDateResult.validationResults.month.errorMessage
           }
           title="카드 유효기간을 입력해 주세요."
-          description="월/년도(MMYY)를 순서대로 입력해 주세요."
+          description="월/년도(MM/YY)를 순서대로 입력해 주세요."
           label="유효기간"
         >
           <Input
