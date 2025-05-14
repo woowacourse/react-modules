914ae2af4c2550c2f0d50a6526d2dcc712a16967
jaeyoung-kwon <61729032+jaeyoung-kwon@users.noreply.github.com>
2025-05-15 01:31:44 +0900

    refactor: 사용하지 않는 상수 및 타입 삭제

--- hooks/src/lib/config/formKeys.ts
+++ hooks/src/lib/config/formKeys.ts
@@ -1,11 +1,3 @@
-export const CARD_NUMBERS_KEY = {
-  part1: 'part1',
-  part2: 'part2',
-  part3: 'part3',
-  part4: 'part4',
-} as const;
-export type CardNumbersKey = keyof typeof CARD_NUMBERS_KEY;
-
 export const EXPIRY_DATE_KEY = {
   month: 'month',
   year: 'year',
