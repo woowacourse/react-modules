82932eb467d64c829bddfc6b288311ab9ced3617
jaeyoung-kwon <61729032+jaeyoung-kwon@users.noreply.github.com>
2025-05-15 09:38:13 +0900

    chore: tsconfig 설정 변경 및 배포 버전 업데이트

--- components/src/App.tsx
+++ components/src/App.tsx
@@ -1,8 +1,10 @@
 import Modal from './modal/Modal';
+import { useModalContext } from './modal/ModalProvider';
 
 function App() {
+  const { onClose } = useModalContext();
   return (
-    <Modal role="alert-modal">
+    <Modal>
       <Modal.OpenTrigger>
         <button>열기</button>
       </Modal.OpenTrigger>
@@ -11,11 +13,15 @@ function App() {
         <Modal.PromptInput />
 
         <Modal.ButtonGroup direction="row" align="end">
-          <Modal.CloseTrigger>
-            <Modal.Button variant="primary" onClick={() => alert('클릭됨')}>
-              동의하고 저장하기
-            </Modal.Button>
-          </Modal.CloseTrigger>
+          <Modal.Button
+            variant="primary"
+            onClick={() => {
+              onClose();
+              alert('클릭됨');
+            }}
+          >
+            동의하고 저장하기
+          </Modal.Button>
           <Modal.CloseTrigger>
             <Modal.Button>확인</Modal.Button>
           </Modal.CloseTrigger>
