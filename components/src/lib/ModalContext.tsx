0c774cd7b252b260b50cdf41f7e986364a5b0652
ha-kuku <hakukudh@gmail.com>
2025-05-15 18:38:40 +0900

    refactor: 에러메시지 구체화

--- components/src/lib/ModalContext.tsx
+++ components/src/lib/ModalContext.tsx
@@ -12,7 +12,7 @@ const ModalContext = createContext<ModalContextProps | undefined>(undefined);
 export const useModalContext = () => {
   const context = useContext(ModalContext);
   if (!context) {
-    throw new Error('');
+    throw new Error('useModalContext는 반드시 ModalProvider 내에서 사용되어야 합니다.');
   }
   return context;
 };
