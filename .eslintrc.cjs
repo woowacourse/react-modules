ee2c691e75f411867cff050af8c2995b9f1283a0
eunoia-jaxson <eunoia@handong.ac.kr>
2025-05-17 07:10:14 +0900

    build: eslint-plugin-jsx-a11y 설치 및 설정 세팅

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,11 +1,19 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+  ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'jsx-a11y'],
   rules: {
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'jsx-a11y/aria-role': 'error',
+    'jsx-a11y/aria-props': 'error',
+    'jsx-a11y/role-has-required-aria-props': 'error',
   },
 };
