69941007f8bcdfef465b9fbad2b49d40d8c0de61
ha-kuku <hakukudh@gmail.com>
2025-05-16 22:33:06 +0900

    refactor: useEffect 내부 이벤트 클린업 함수 추가

--- components/src/lib/useFocus.ts
+++ components/src/lib/useFocus.ts
@@ -1,16 +1,21 @@
 import { useEffect, useRef } from 'react';
+
 const useFocus = (isOpen: boolean) => {
   const modalRef = useRef<HTMLDivElement>(null);
+
   useEffect(() => {
     if (!isOpen) return;
+
     const modalEl = modalRef.current;
     const focusableEls = modalEl?.querySelectorAll<HTMLElement>(
       'button, a[href], input, select, textarea, [tabindex]:not([tabindex="-1"])'
     );
     const firstEl = focusableEls?.[0];
     const lastEl = focusableEls?.[focusableEls.length - 1];
+
     const handleKeyDown = (e: KeyboardEvent) => {
-      if (e.key !== 'Tab' || focusableEls?.length === 0) return;
+      if (e.key !== 'Tab' || !focusableEls?.length) return;
+
       if (e.shiftKey) {
         if (document.activeElement === firstEl) {
           e.preventDefault();
@@ -23,9 +28,16 @@ const useFocus = (isOpen: boolean) => {
         }
       }
     };
-    document.addEventListener('keydown', handleKeyDown);
+
     firstEl?.focus();
+    document.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
   }, [isOpen]);
+
   return { modalRef };
 };
+
 export default useFocus;
