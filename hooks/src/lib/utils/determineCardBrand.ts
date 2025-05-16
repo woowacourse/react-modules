67ba9eec5f5762ec245d724a81fa194ecd91948a
sanghee01 <sang6299@naver.com>
2025-05-16 18:01:35 +0900

    refactor: 카드 시작 번호 매직넘버로 수정

--- hooks/src/lib/utils/determineCardBrand.ts
+++ hooks/src/lib/utils/determineCardBrand.ts
@@ -1,24 +1,19 @@
-export function determineCardBrand(cardNumber: string) {
-  if (cardNumber.startsWith('4')) {
-    return 'visa';
-  }
+import { CardBrand } from '../types/card';
 
-  if (51 <= Number(cardNumber.slice(0, 2)) && Number(cardNumber.slice(0, 2)) <= 55) {
-    return 'master';
-  }
+export function determineCardBrand(cardNumber: string): CardBrand {
+  const first2 = Number(cardNumber.slice(0, 2));
+  const first3 = Number(cardNumber.slice(0, 3));
+  const first4 = Number(cardNumber.slice(0, 4));
+  const first6 = Number(cardNumber.slice(0, 6));
 
-  if (cardNumber.startsWith('36')) {
-    return 'diners';
-  }
-
-  if (cardNumber.startsWith('34') || cardNumber.startsWith('37')) {
-    return 'amex';
-  }
-
+  if (cardNumber.startsWith('4')) return 'visa';
+  if (first2 >= 51 && first2 <= 55) return 'master';
+  if (cardNumber.startsWith('36')) return 'diners';
+  if (cardNumber.startsWith('34') || cardNumber.startsWith('37')) return 'amex';
   if (
-    (622126 <= Number(cardNumber.slice(0, 6)) && Number(cardNumber.slice(0, 6)) <= 622925) ||
-    (624 <= Number(cardNumber.slice(0, 3)) && Number(cardNumber.slice(0, 3)) <= 626) ||
-    (6282 <= Number(cardNumber.slice(0, 4)) && Number(cardNumber.slice(0, 4)) <= 6288)
+    (first6 >= 622126 && first6 <= 622925) ||
+    (first3 >= 624 && first3 <= 626) ||
+    (first4 >= 6282 && first4 <= 6288)
   ) {
     return 'unionpay';
   }
