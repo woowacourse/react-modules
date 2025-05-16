838d581ce3a4c22aaff9f8ac3036b3c2436c35cf
yeongipark <yeongi0111@naver.com>
2025-05-16 15:40:24 +0900

    refactor: App에서 테스트 위한 코드 개선

--- components/src/App.tsx
+++ components/src/App.tsx
@@ -1,9 +1,27 @@
 import { useState } from "react";
 import useModalState from "./lib/hooks/useModalState";
 import Prompt from "./lib/Prompt/Prompt";
+import { Alert, Confirm } from "./lib";
 
 function App() {
-  const { isOpen, modalClose, modalOpen } = useModalState(true);
+  const {
+    isOpen: isAlertOpen,
+    modalClose: alertClose,
+    modalOpen: alertOpen,
+  } = useModalState(false);
+
+  const {
+    isOpen: isPromptOpen,
+    modalClose: promptClose,
+    modalOpen: promptOpen,
+  } = useModalState(false);
+
+  const {
+    isOpen: isConfirmOpen,
+    modalClose: confirmClose,
+    modalOpen: confirmOpen,
+  } = useModalState(false);
+
   const [value, setValue] = useState("");
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setValue(e.target.value);
@@ -11,19 +29,21 @@ function App() {
 
   return (
     <>
-      <button onClick={() => modalOpen()}>모달 열기</button>
-      {/* <Alert
-        open={isOpen}
-        modalClose={modalClose}
+      <button onClick={() => alertOpen()}>Alert 모달 열기</button>
+      <button onClick={() => confirmOpen()}>Confirm 모달 열기</button>
+      <button onClick={() => promptOpen()}>Prompt 모달 열기</button>
+      <Alert
+        open={isAlertOpen}
+        modalClose={alertClose}
         title="아이디를 입력해 주세요."
         content="아이디는 필수로 입력해야 합니다."
         buttonText="확인"
         size="medium"
         position="center"
-      /> */}
-      {/* <Confirm
-        open={isOpen}
-        modalClose={modalClose}
+      />
+      <Confirm
+        open={isConfirmOpen}
+        modalClose={confirmClose}
         title="아이디를 입력해 주세요."
         content="아이디는 필수로 입력해야 합니다."
         closeButtonText="취소"
@@ -31,10 +51,10 @@ function App() {
         size="medium"
         position="center"
         onCheckButtonClick={() => console.log("확인 버튼 클릭")}
-      /> */}
+      />
       <Prompt
-        open={isOpen}
-        modalClose={modalClose}
+        open={isPromptOpen}
+        modalClose={promptClose}
         title="아이디를 입력해 주세요."
         value={value}
         onChange={handleChange}
