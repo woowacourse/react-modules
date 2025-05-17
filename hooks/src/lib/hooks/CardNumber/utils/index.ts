6f3b5ff6a4e09d09bd17b81ac04843018e663b2e
bunju20 <ghkd2271@naver.com>
2025-05-17 22:03:28 +0900

    refactor: carNumber 유효성 검사 로직에서 카드타입 객체로 관리

--- hooks/src/lib/hooks/CardNumber/utils/index.ts
+++ hooks/src/lib/hooks/CardNumber/utils/index.ts
@@ -1,5 +1,32 @@
 import { ValidationResult } from "../../../types";
 
+const CARD_TYPES = {
+  diners: {
+    displayName: "Diners",
+    numberLength: 14,
+  },
+  amex: {
+    displayName: "AMEX",
+    numberLength: 15,
+  },
+  visa: {
+    displayName: "Visa",
+    numberLength: 16,
+  },
+  mastercard: {
+    displayName: "MasterCard",
+    numberLength: 16,
+  },
+  unionpay: {
+    displayName: "UnionPay",
+    numberLength: 16,
+  },
+  unknown: {
+    displayName: "알 수 없는 카드",
+    numberLength: 16,
+  },
+};
+
 export const validateCardNumber = (
   cardNumber: string,
   cardType: string
@@ -11,47 +38,13 @@ export const validateCardNumber = (
     };
   }
 
-  if (cardType === "diners") {
-    if (cardNumber.length !== 14) {
+  if (cardType in CARD_TYPES) {
+    const { displayName, numberLength } =
+      CARD_TYPES[cardType as keyof typeof CARD_TYPES];
+    if (cardNumber.length !== numberLength) {
       return {
         isValid: false,
-        errorMessage: "Diners 카드 번호는 14자리여야 합니다.",
-      };
-    }
-  }
-
-  if (cardType === "amex") {
-    if (cardNumber.length !== 15) {
-      return {
-        isValid: false,
-        errorMessage: "AMEX 카드 번호는 15자리여야 합니다.",
-      };
-    }
-  }
-
-  if (cardType === "visa") {
-    if (cardNumber.length !== 16) {
-      return {
-        isValid: false,
-        errorMessage: "Visa 카드 번호는 16자리여야 합니다.",
-      };
-    }
-  }
-
-  if (cardType === "mastercard") {
-    if (cardNumber.length !== 16) {
-      return {
-        isValid: false,
-        errorMessage: "MasterCard 카드 번호는 16자리여야 합니다.",
-      };
-    }
-  }
-
-  if (cardType === "unionpay") {
-    if (cardNumber.length !== 16) {
-      return {
-        isValid: false,
-        errorMessage: "UnionPay 카드 번호는 16자리여야 합니다.",
+        errorMessage: `${displayName} 카드 번호는 ${numberLength}자리여야 합니다.`,
       };
     }
   }
