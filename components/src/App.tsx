46bc248069bb164fa770d06f0019d303f8585f30
thgml05 <aisohii@naver.com>
2025-05-17 15:25:12 +0900

    chore: 모듈 재배포 및 설치

--- components/src/App.tsx
+++ components/src/App.tsx
@@ -4,7 +4,7 @@ import { useState } from 'react';
 import './App.css';
 // import { PromptModal } from 'jurunghappy-modal';
 import AlertModal from './lib/components/AlertModal/AlertModal';
-import { ConfirmModal, PromptModal } from './lib';
+// import { ConfirmModal, PromptModal } from './lib';
 // import ConfirmModal from './lib/components/ConfirmModal/ConfirmModal';
 // import PromptModal from './lib/components/PromptModal/PromptModal';
 
@@ -15,15 +15,14 @@ function App() {
   return (
     <>
       <button onClick={() => setIsOpen(true)}>Modal Open</button>
-      {/* <AlertModal
+      <AlertModal
         isOpen={isOpen}
         position="center"
         size="large"
         title="타이틀"
         message="메시지"
         onClose={() => setIsOpen(false)}
-        onBackdropClick={handleBackdropClick}
-      /> */}
+      />
       {/* <ConfirmModal
         isOpen={isOpen}
         position="center"
@@ -34,7 +33,7 @@ function App() {
         onConfirm={() => {}}
         onBackdropClick={handleBackdropClick}
       /> */}
-      <PromptModal
+      {/* <PromptModal
         isOpen={isOpen}
         position="center"
         size="large"
@@ -47,7 +46,7 @@ function App() {
         onSubmit={(e) => {
           e.preventDefault();
         }}
-      />
+      /> */}
     </>
   );
 }
