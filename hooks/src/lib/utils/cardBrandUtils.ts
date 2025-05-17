7b81044217d37a2f3c61c74d084ec2454a9d9c40
thgml05 <aisohii@naver.com>
2025-05-17 09:42:00 +0900

    refactor: util 함수의 numbers 인자명 value로 수정

--- hooks/src/lib/utils/cardBrandUtils.ts
+++ hooks/src/lib/utils/cardBrandUtils.ts
@@ -8,15 +8,16 @@ function isCardBrandType(key: string): key is CardBrandType {
   return key in CARD_BRAND_INFO;
 }
 
-export const identifyCardBrand = (numbers: string): CardBrandType | null => {
+export const identifyCardBrand = (value: string): CardBrandType | null => {
   for (const brand in CARD_BRAND_INFO) {
     if (!isCardBrandType(brand)) continue;
 
     const { pattern } = CARD_BRAND_INFO[brand];
-    if (pattern.test(numbers)) return brand;
+    if (pattern.test(value)) return brand;
   }
   return null;
 };
+
 export const getCardNumberLength = (brand: CardBrandType | null): number => {
   return brand ? CARD_BRAND_INFO[brand].length : FALLBACK_CARD_INFO.length;
 };
@@ -25,20 +26,20 @@ export const getFormat = (brand: CardBrandType | null): number[] => {
   return brand ? CARD_BRAND_INFO[brand].format : FALLBACK_CARD_INFO.format;
 };
 
-export const getCardBrandInfo = (numbers: string) => {
-  const cardBrand = identifyCardBrand(numbers);
+export const getCardBrandInfo = (value: string) => {
+  const cardBrand = identifyCardBrand(value);
   const cardNumberLength = getCardNumberLength(cardBrand);
   const format = getFormat(cardBrand);
 
   return { cardBrand, cardNumberLength, format };
 };
 
-export const getFormattedNumber = (number: string, format: number[]) => {
+export const getFormattedNumber = (value: string, format: number[]) => {
   const result: string[] = [];
   let index = 0;
   if (!format) return;
   for (const length of format) {
-    const numberPart = number.slice(index, index + length);
+    const numberPart = value.slice(index, index + length);
     if (!numberPart) break;
     result.push(numberPart);
     index += length;
