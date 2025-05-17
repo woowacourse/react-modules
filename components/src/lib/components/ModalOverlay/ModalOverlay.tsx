9ae12ea0aec05e5e63e190aeada467755a1456c9
kaori-killer <sksbthwjd@daum.net>
2025-05-17 23:51:25 +0900

    fix: 모달이 열렸을 때 오버레이가 hidden 되도록 수정

--- components/src/lib/components/ModalOverlay/ModalOverlay.tsx
+++ components/src/lib/components/ModalOverlay/ModalOverlay.tsx
@@ -3,7 +3,7 @@ import { StyledModalOverlay } from "./ModalOverlay.styled";
 import { ModalOverlayProps } from "./ModalOverlay.types";
 
 const ModalOverlay = ({ isOpen, onClose }: ModalOverlayProps) => {
-  return <StyledModalOverlay aria-hidden={isOpen} onClick={onClose} />;
+  return <StyledModalOverlay aria-hidden={!isOpen} onClick={onClose} />;
 };
 
 export default ModalOverlay;
