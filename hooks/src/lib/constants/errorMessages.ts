3bac41f568d58f17ee9882cc12616daa9ca720ad
ShinjungOh <tlswjd9260@gmail.com>
2025-05-18 20:42:32 +0900

    refactor: 에러 메시지에 as const 처리

--- hooks/src/lib/constants/errorMessages.ts
+++ hooks/src/lib/constants/errorMessages.ts
@@ -4,22 +4,22 @@ export const CARD_NUMBER_ERROR = {
   onlyNumbers: '숫자만 입력 가능합니다.',
   invalidFormat: '카드 번호 형식이 올바르지 않습니다.',
   required: '카드 번호를 입력해 주세요.',
-  invalidBrand: '지원하지 않는 카드사입니다.'
-};
+  invalidBrand: '지원하지 않는 카드사입니다.',
+} as const;
 
 export const CARD_EXPIRATION_ERROR = {
   onlyNumbers: '숫자만 입력 가능합니다.',
   invalidLength: '2자리 숫자를 입력해 주세요.',
   invalidMonth: `${CARD_EXPIRATION.minMonth}~${CARD_EXPIRATION.maxMonth} 사이의 값을 입력해 주세요.`,
   invalidYear: `${CARD_EXPIRATION.minYear}년 이상의 값을 입력해 주세요.`,
-};
+} as const;
 
 export const CARD_CVC_ERROR = {
   onlyNumbers: '숫자만 입력 가능합니다.',
   invalidLength: '3자리 숫자를 입력해 주세요.',
-};
+} as const;
 
 export const CARD_PASSWORD_ERROR = {
   onlyNumbers: '숫자만 입력 가능합니다.',
   invalidLength: '비밀번호는 2자리 숫자여야 합니다.',
-};
+} as const;
