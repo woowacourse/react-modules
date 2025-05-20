edc47abd49947a1a50e2da1d1ac32dffd72f70fc
Ko Daeun <drechsu@gmail.com>
2025-05-20 21:21:22 +0900

    refactor : cardNumberLengthByPotition 타입 적용

--- payments-hook/src/constants/cardNumberlengthByPosition.ts
+++ payments-hook/src/constants/cardNumberlengthByPosition.ts
@@ -1,31 +1,39 @@
 //각 카드 네크워크 별 각 자리숫자자리 상수화
-export const VISA_CARD_NUMBER_LENGTH_BY_POSITION = {
+
+import { CardNumbersKeys } from '../useCardNumbers/useCardNumbers';
+
+type CardNumberLengthByPosition = Record<CardNumbersKeys, number>;
+
+export const VISA_CARD_NUMBER_LENGTH_BY_POSITION: CardNumberLengthByPosition = {
   FIRST: 4,
   SECOND: 4,
   THIRD: 4,
   FOURTH: 4,
 };
-export const MASTER_CARD_NUMBER_LENGTH_BY_POSITION = {
-  FIRST: 4,
-  SECOND: 4,
-  THIRD: 4,
-  FOURTH: 4,
-};
-export const AMEX_CARD_NUMBER_LENGTH_BY_POSITION = {
+export const MASTER_CARD_NUMBER_LENGTH_BY_POSITION: CardNumberLengthByPosition =
+  {
+    FIRST: 4,
+    SECOND: 4,
+    THIRD: 4,
+    FOURTH: 4,
+  };
+export const AMEX_CARD_NUMBER_LENGTH_BY_POSITION: CardNumberLengthByPosition = {
   FIRST: 4,
   SECOND: 6,
   THIRD: 5,
   FOURTH: 0,
 };
-export const DINERS_CARD_NUMBER_LENGTH_BY_POSITION = {
-  FIRST: 4,
-  SECOND: 6,
-  THIRD: 4,
-  FOURTH: 0,
-};
-export const UNIONPAY_CARD_NUMBER_LENGTH_BY_POSITION = {
-  FIRST: 4,
-  SECOND: 4,
-  THIRD: 4,
-  FOURTH: 4,
-};
+export const DINERS_CARD_NUMBER_LENGTH_BY_POSITION: CardNumberLengthByPosition =
+  {
+    FIRST: 4,
+    SECOND: 6,
+    THIRD: 4,
+    FOURTH: 0,
+  };
+export const UNIONPAY_CARD_NUMBER_LENGTH_BY_POSITION: CardNumberLengthByPosition =
+  {
+    FIRST: 4,
+    SECOND: 4,
+    THIRD: 4,
+    FOURTH: 4,
+  };
